b26f99f9d273f7ab282b010418d7f357bc442ec7
unknown <Tristan_Lewis@yahoo.com>
2016-10-15 11:40:34 -0400

    Initial check in

---
+++ src/main/java/Bespoke/Heart/HeartApplication.java
@@ -0,0 +1,13 @@
+
+package Bespoke.Heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartApplication.class, args);
+	}
+}
