cbaa30afd5dc7dfb9b26abe5d4cc0ad82cdf7a76
Justin Richer <jricher@mit.edu>
2017-09-20 14:41:24 -0400

    update spring version and fix dependencies

--- src/main/java/io/bspk/testframework/strawman/HeartApplication.java
+++ src/main/java/io/bspk/testframework/strawman/HeartApplication.java
@@ -2,10 +2,7 @@
 package io.bspk.testframework.strawman;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 
 @SpringBootApplication
 public class HeartApplication {
