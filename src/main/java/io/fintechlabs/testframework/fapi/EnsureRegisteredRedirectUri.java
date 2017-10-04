a6e68e6e5902750f6c2b108ba47fb3aa4724b8d5
Justin Richer <justin@bspk.io>
2017-10-04 13:57:00 -0400

    create test for testing bad redirect URIs

---
+++ src/main/java/io/fintechlabs/testframework/fapi/EnsureRegisteredRedirectUri.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.fapi;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonObject;
+
+import io.fintechlabs.testframework.condition.BuildPlainRedirectToAuthorizationEndpoint;
+import io.fintechlabs.testframework.condition.CheckServerConfiguration;
+import io.fintechlabs.testframework.condition.CreateBadRedirectUri;
+import io.fintechlabs.testframework.condition.CreateRandomStateValue;
+import io.fintechlabs.testframework.condition.GetClientConfiguration;
+import io.fintechlabs.testframework.condition.GetServerConfiguration;
+import io.fintechlabs.testframework.frontChannel.BrowserControl;
+import io.fintechlabs.testframework.logging.EventLog;
+import io.fintechlabs.testframework.testmodule.AbstractTestModule;
+import io.fintechlabs.testframework.testmodule.TestFailureException;
+
+/**
+ * Tests that the AS will reject a non-registered redirect URI by 
+ * 
+ * @author jricher
+ *
+ */
+public class EnsureRegisteredRedirectUri extends AbstractTestModule {
+
+	/**
+	 * @param name
+	 */
+	public EnsureRegisteredRedirectUri(String name) {
+		super("ensure-redirect-uri-is-registered");
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.testmodule.TestModule#configure(com.google.gson.JsonObject, io.fintechlabs.testframework.logging.EventLog, java.lang.String, io.fintechlabs.testframework.frontChannel.BrowserControl, java.lang.String)
+	 */
+	@Override
+	public void configure(JsonObject config, EventLog eventLog, String id, BrowserControl browser, String baseUrl) {
+		this.id = id;
+		this.eventLog = eventLog;
+		this.browser = browser;
+		
+		env.putString("base_url", baseUrl);
+		env.put("config", config);
+		
+		// create a random redirect URI 
+		require(CreateBadRedirectUri.class);
+
+		// this is inserted by the create call above, expose it to the test environment for publication
+		exposeEnvString("redirect_uri");
+
+		// Make sure we're calling the right server configuration
+		require(GetServerConfiguration.class);
+		
+		// make sure the server configuration passes some basic sanity checks
+		require(CheckServerConfiguration.class);
+		
+		// Set up the client configuration
+		require(GetClientConfiguration.class);
+		
+		exposeEnvString("client_id");
+
+		this.status = Status.CONFIGURED;
+		fireSetupDone();
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.testmodule.TestModule#start()
+	 */
+	@Override
+	public void start() {
+		if (this.status != Status.CONFIGURED) {
+			throw new RuntimeException("Invalid State: " + this.status);
+		}
+		
+		this.status = Status.RUNNING;
+		
+		require(CreateRandomStateValue.class);
+		exposeEnvString("state");
+		
+		require(BuildPlainRedirectToAuthorizationEndpoint.class);
+		
+		String redirectTo = env.getString("redirect_to_authorization_endpoint");
+		
+		eventLog.log(getId(), getName(), "Redirecting to url" + redirectTo);
+
+		browser.goToUrl(redirectTo);
+
+		/**
+		 * We never expect the browser to come back from here
+		 */
+		
+		this.status = Status.WAITING;
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.testmodule.TestModule#stop()
+	 */
+	@Override
+	public void stop() {
+		eventLog.log(getId(), getName(), "Finsihed");
+		
+		this.status = Status.FINISHED;
+		
+		if (getResult().equals(Result.UNKNOWN)) {
+			fireInterrupted();
+		}
+
+		logFinalEnv();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.testmodule.TestModule#handleHttp(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.http.HttpSession, org.springframework.util.MultiValueMap, org.springframework.ui.Model)
+	 */
+	@Override
+	public ModelAndView handleHttp(String path, HttpServletRequest req, HttpServletResponse res, HttpSession session, MultiValueMap<String, String> params, Model m) {
+
+		// If we get any kind of callback to this, it's an error: the authorization server should not ever respond the authorization request
+		
+		eventLog.log(getId(), getName(), ImmutableMap.of(
+			"msg", "Receved unexpected incoming request",
+			"path", path,
+			"method", req.getMethod(),
+			"requirements", ImmutableSet.of("FAPI-1-5.2.2-15"),
+			"params", params.toSingleValueMap()
+		));
+		
+		throw new TestFailureException(getId(), "Got an HTTP response on a call we weren't expecting");
+		
+	}
+
+}
