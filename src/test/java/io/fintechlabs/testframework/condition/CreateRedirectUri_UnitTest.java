3a243f59b1062dcdd7e570529c4bab70290449fb
Leo Uino <leo.uino@fintechlabs.io>
2017-10-16 16:17:32 +0900

    Unit test for CreateRedirectUri [#31]

---
+++ src/test/java/io/fintechlabs/testframework/condition/CreateRedirectUri_UnitTest.java
@@ -0,0 +1,78 @@
+package io.fintechlabs.testframework.condition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+import io.fintechlabs.testframework.logging.EventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateRedirectUri_UnitTest {
+	
+	@Spy
+	private Environment env = new Environment();
+	
+	@Mock
+	private EventLog eventLog;
+	
+	private CreateRedirectUri cond;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+		cond = new CreateRedirectUri("UNIT-TEST", eventLog, false);
+		
+	}
+	
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.CreateRedirectUri#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test
+	public void testEvaluate_valuePresent() {
+		
+		env.putString("base_url", "https://example.com");
+		
+		cond.evaluate(env);
+		
+		verify(env, atLeastOnce()).getString("base_url");
+		
+		assertThat(env.getString("redirect_uri")).isEqualTo("https://example.com/callback");
+		
+	}
+	
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.CreateRedirectUri#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test(expected = ConditionError.class)
+	public void testEvaluate_valueEmpty() {
+		
+		env.putString("base_url", "");
+		
+		cond.evaluate(env);
+		
+	}
+	
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.CreateRedirectUri#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test(expected = ConditionError.class)
+	public void testEvaluate_valueNull() {
+		
+		env.putString("base_url", null);
+		
+		cond.evaluate(env);
+		
+	}
+}
