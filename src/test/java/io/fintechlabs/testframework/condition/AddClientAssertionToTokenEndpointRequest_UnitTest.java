8555b55a77babce86426d757c99bf519daaeed7d
Justin Richer <justin@bspk.io>
2017-11-06 13:39:23 -0500

    unit test for assertion copy

---
+++ src/test/java/io/fintechlabs/testframework/condition/AddClientAssertionToTokenEndpointRequest_UnitTest.java
@@ -0,0 +1,74 @@
+package io.fintechlabs.testframework.condition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.gson.JsonObject;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+import io.fintechlabs.testframework.logging.EventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddClientAssertionToTokenEndpointRequest_UnitTest {
+	
+	@Spy
+	private Environment env = new Environment();
+	
+	@Mock
+	private EventLog eventLog;
+	
+	private AddClientAssertionToTokenEndpointRequest cond;
+	
+	private String clientAssertion;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+		cond = new AddClientAssertionToTokenEndpointRequest("UNIT-TEST", eventLog, false);
+		
+		clientAssertion = "client.assertion.string"; // note that this is normally a JWT calculated by another module, this module just copies the value
+	}
+	
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.CreateRedirectUri#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test
+	public void testEvaluate() {
+		
+		env.put("token_endpoint_request_form_parameters", new JsonObject());
+		env.putString("client_assertion", clientAssertion);
+
+		cond.evaluate(env);
+		
+		verify(env, atLeastOnce()).getString("client_assertion");
+		
+		assertThat(env.getString("token_endpoint_request_form_parameters", "client_assertion")).isEqualTo(clientAssertion);
+		assertThat(env.getString("token_endpoint_request_form_parameters", "client_assertion_type")).isEqualTo("urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+		
+	}
+	
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.CreateRedirectUri#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test(expected = ConditionError.class)
+	public void testEvaluate_missingForm() {
+		
+		env.putString("client_assertion", clientAssertion);
+
+		cond.evaluate(env);
+		
+	}
+	
+}
