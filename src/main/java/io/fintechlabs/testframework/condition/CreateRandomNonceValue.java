cafb5048d68c1f7559b34eca4e9affb728c0b562
Justin Richer <justin@bspk.io>
2017-11-08 21:22:14 -0500

    added nonce creation conditions

---
+++ src/main/java/io/fintechlabs/testframework/condition/CreateRandomNonceValue.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.condition;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import io.fintechlabs.testframework.logging.EventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+/**
+ * @author jricher
+ *
+ */
+public class CreateRandomNonceValue extends AbstractCondition {
+
+	/**
+	 * @param testId
+	 * @param log
+	 * @param optional
+	 */
+	public CreateRandomNonceValue(String testId, EventLog log, boolean optional) {
+		super(testId, log, optional);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.condition.Condition#evaluate(io.fintechlabs.testframework.testmodule.Environment)
+	 */
+	@Override
+	public Environment evaluate(Environment env) {
+
+		String nonce = RandomStringUtils.randomAlphanumeric(10);
+		env.putString("nonce", nonce);
+		
+		log("Created nonce value", args("nonce", nonce));
+		
+		return env;
+		
+	}
+
+}
