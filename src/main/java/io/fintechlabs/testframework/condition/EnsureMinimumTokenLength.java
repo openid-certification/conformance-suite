a58b358b41f2f245e2f99c28b08a903f83ea0e78
Justin Richer <justin@bspk.io>
2017-11-20 11:28:24 -0500

    Make sure tokens are of sufficient length, closes #133

---
+++ src/main/java/io/fintechlabs/testframework/condition/EnsureMinimumTokenLength.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.condition;
+
+import com.google.common.base.Strings;
+
+import io.fintechlabs.testframework.logging.EventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+/**
+ * @author jricher
+ *
+ */
+public class EnsureMinimumTokenLength extends AbstractCondition {
+
+	private final double requiredLength = 128;
+	
+	/**
+	 * @param testId
+	 * @param log
+	 * @param optional
+	 */
+	public EnsureMinimumTokenLength(String testId, EventLog log, boolean optional) {
+		super(testId, log, optional, "FAPI-1-5.2.2-16");
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.condition.Condition#evaluate(io.fintechlabs.testframework.testmodule.Environment)
+	 */
+	@Override
+	public Environment evaluate(Environment env) {
+
+		String accessToken = env.getString("token_endpoint_response", "access_token");
+
+		if (Strings.isNullOrEmpty(accessToken)) {
+			return error("Can't find access token");
+		}
+
+		byte[] bytes = accessToken.getBytes();
+		
+		int bitLength = bytes.length * 8;
+		
+		if (bitLength >= requiredLength) {
+			logSuccess("Access token is of sufficient length", args("required", requiredLength, "actual", bitLength));
+			return env;
+		} else {
+			return error("Access token is not of sufficient length", args("required", requiredLength, "actual", bitLength));
+		}
+		
+	}
+
+}
