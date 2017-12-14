4f929e323dddb5f1556af3c81aa463babee1894c
Leo Uino <leo.uino@fintechlabs.io>
2017-12-14 16:35:35 +0900

    Alternative package build script (#143)

---
+++ make-package.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$(git describe --tags HEAD)
+
+echo Building ${VERSION}...
+
+mvn clean package
+
+TMPDIR=$(mktemp --directory)
+
+mkdir -p $TMPDIR/server
+cp target/fapi-test-suite.jar $TMPDIR/server
+cp package/Dockerfile.server $TMPDIR/server/Dockerfile
+
+cp -R httpd $TMPDIR
+cp package/docker-compose.yml $TMPDIR
+
+echo Archiving...
+
+TARGET=${PWD}/fapi-conformance-suite-${VERSION}.zip
+
+(cd $TMPDIR && zip -r $TARGET .)
+
+rm -r $TMPDIR
+
+echo Created archive
+
+ls -l $TARGET
+
