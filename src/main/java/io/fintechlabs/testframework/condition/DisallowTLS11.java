c5647f807158d131878634c4b710932b0973c259
Leo Uino <leo.uino@fintechlabs.io>
2017-12-15 14:35:53 +0900

    Abort TLS connection after server hello for TLS version tests (fix #174)

--- src/main/java/io/fintechlabs/testframework/condition/DisallowTLS11.java
+++ src/main/java/io/fintechlabs/testframework/condition/DisallowTLS11.java
@@ -15,17 +15,22 @@
 package io.fintechlabs.testframework.condition;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.SecureRandom;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import org.bouncycastle.crypto.tls.AlertDescription;
+import org.bouncycastle.crypto.tls.Certificate;
+import org.bouncycastle.crypto.tls.CertificateRequest;
+import org.bouncycastle.crypto.tls.DefaultTlsClient;
+import org.bouncycastle.crypto.tls.ProtocolVersion;
+import org.bouncycastle.crypto.tls.TlsAuthentication;
+import org.bouncycastle.crypto.tls.TlsClient;
+import org.bouncycastle.crypto.tls.TlsClientProtocol;
+import org.bouncycastle.crypto.tls.TlsCredentials;
+import org.bouncycastle.crypto.tls.TlsFatalAlertReceived;
 
 import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableMap;
 
 import io.fintechlabs.testframework.logging.EventLog;
 import io.fintechlabs.testframework.testmodule.Environment;
@@ -35,6 +40,22 @@ import io.fintechlabs.testframework.testmodule.Environment;
  *
  */
 public class DisallowTLS11 extends AbstractCondition {
+
+	// Signals that the connection was aborted after discovering the server version
+	@SuppressWarnings("serial")
+	private static class ServerHelloReceived extends IOException {
+
+		private ProtocolVersion serverVersion;
+
+		public ServerHelloReceived(ProtocolVersion serverVersion) {
+			this.serverVersion = serverVersion;
+		}
+
+		public ProtocolVersion getServerVersion() {
+			return serverVersion;
+		}
+
+	}
 	
 	/**
 	 * @param testId
@@ -62,54 +83,83 @@ public class DisallowTLS11 extends AbstractCondition {
 		if (tlsTestPort == null) {
 			return error("Couldn't find port to connect for TLS");
 		}
-		
-		// even though we make a TLS connection we ignore the server cert validation here
-		TrustManager[] trustAllCerts = new TrustManager[] {
-			new X509TrustManager() {
-				
-				@Override
-				public X509Certificate[] getAcceptedIssuers() {
-					return new X509Certificate[0];
-				}
-				
-				@Override
-				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-				}
-				
-				@Override
-				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+		try {
+			Socket socket = new Socket(InetAddress.getByName(tlsTestHost), tlsTestPort);
+
+			try {
+
+				TlsClientProtocol protocol = new TlsClientProtocol(socket.getInputStream(), socket.getOutputStream(), new SecureRandom());
+
+				TlsClient client = new DefaultTlsClient() {
+
+					@Override
+					public TlsAuthentication getAuthentication() {
+						return new TlsAuthentication() {
+
+							@Override
+							public TlsCredentials getClientCredentials(CertificateRequest certificateRequest) throws IOException {
+								return null;
+							}
+
+							@Override
+							public void notifyServerCertificate(Certificate serverCertificate) throws IOException {
+								// even though we make a TLS connection we ignore the server cert validation here
+							}
+						};
+					}
+
+					@Override
+					public ProtocolVersion getMinimumVersion() {
+						// Disallow anything earlier than TLS 1.1
+						return ProtocolVersion.TLSv11;
+					}
+
+					@Override
+					public ProtocolVersion getClientVersion() {
+						// Try to connect with TLS 1.1
+						return ProtocolVersion.TLSv11;
+					}
+
+					@Override
+					public void notifyServerVersion(ProtocolVersion serverVersion) throws IOException {
+						// don't need to proceed further
+						throw new ServerHelloReceived(serverVersion);
+					}
+				};
+
+				protocol.connect(client);
+
+				// By the time handshake completes an exception should have been thrown, but just in case:
+				return error("Connection completed unexpectedly");
+
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					// Don't care
 				}
 			}
-		};
-		
-		try {
-			
-			SSLContext sc = SSLContext.getInstance("TLS"); 
-		    sc.init(null, trustAllCerts, new java.security.SecureRandom());
-
-		    SSLSocket socket = (SSLSocket) sc.getSocketFactory().createSocket(tlsTestHost, tlsTestPort);		    
-		    // set the connection to use only TLS 1.1
-		    socket.setEnabledProtocols(new String[] {"TLSv1.1"});
-
-		    // this makes the actual connection
-		    socket.startHandshake();
-		    
-		    String cipherSuite = socket.getSession().getCipherSuite();
-		    
-		    socket.close();
-
-		    return error("Successfully connected with disallowed TLS 1.1", args("cipher_suite", cipherSuite));
-		    
-		} catch (GeneralSecurityException e) {
-		    logSuccess("Couldn't connect to socket with disallowed TLS 1.1");
-		    return env;
+		} catch (ServerHelloReceived e) {
+			ProtocolVersion serverVersion = e.getServerVersion();
+			if (serverVersion == ProtocolVersion.TLSv11) {
+				return error("Server agreed to disallowed TLS 1.1", args("host", tlsTestHost, "port", tlsTestPort));
+			} else {
+				return error("Server used incorrect TLS version",
+						args("server_version", serverVersion.toString(),
+								"host", tlsTestHost,
+								"port", tlsTestPort));
+			}
 		} catch (IOException e) {
-		    logSuccess("Couldn't connect to socket with disallowed TLS 1.1");
-		    return env;
-		} finally {
-			
+			if ((e instanceof TlsFatalAlertReceived)
+					&& ((TlsFatalAlertReceived) e).getAlertDescription() == AlertDescription.handshake_failure) {
+				// If we get here then we haven't received a server hello agreeing on a version
+				logSuccess("Server refused TLS 1.1 handshake", args("host", tlsTestHost, "port", tlsTestPort));
+			    return env;
+			} else {
+				return error("Failed to make TLS connection", e, args("host", tlsTestHost, "port", tlsTestPort));
+			}
 		}
-		
 		
 	}
 
