33c26223679691b752a6c2ba1fd0094325fe2433
Leo Uino <leo.uino@fintechlabs.io>
2018-01-19 13:24:57 +0900

    Extra tests for variations on the Accept header (fix #220)

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/SetPlainJsonAcceptHeaderForResourceEndpointRequest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.condition.client;
+
+import org.springframework.http.HttpHeaders;
+
+import com.google.gson.JsonObject;
+
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class SetPlainJsonAcceptHeaderForResourceEndpointRequest extends AbstractCondition {
+
+	public SetPlainJsonAcceptHeaderForResourceEndpointRequest(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	public Environment evaluate(Environment env) {
+
+		JsonObject requestHeaders = env.get("resource_endpoint_request_headers");
+
+		if (requestHeaders == null) {
+			requestHeaders = new JsonObject();
+			env.put("resource_endpoint_request_headers", requestHeaders);
+		}
+
+		requestHeaders.addProperty(HttpHeaders.ACCEPT, "application/json");
+
+		logSuccess("Set Accept header", args("Accept", requestHeaders.get("Accept")));
+
+		return env;
+	}
+
+}
---
+++ src/test/java/io/fintechlabs/testframework/condition/client/SetPlainJsonAcceptHeaderForResourceEndpointRequest_UnitTest.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.condition.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.gson.JsonObject;
+
+import io.fintechlabs.testframework.condition.Condition.ConditionResult;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SetPlainJsonAcceptHeaderForResourceEndpointRequest_UnitTest {
+
+	@Spy
+	private Environment env = new Environment();
+
+	@Mock
+	private TestInstanceEventLog eventLog;
+
+	private SetPlainJsonAcceptHeaderForResourceEndpointRequest cond;
+
+	private final String expectedHeader = "application/json";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+
+		cond = new SetPlainJsonAcceptHeaderForResourceEndpointRequest("UNIT-TEST", eventLog, ConditionResult.INFO);
+	}
+
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.SetPlainJsonAcceptHeaderForResourceEndpointRequest#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test
+	public void testEvaluate_noHeaders() {
+
+		cond.evaluate(env);
+
+		assertThat(env.getString("resource_endpoint_request_headers", "Accept")).isEqualTo(expectedHeader);
+	}
+
+	/**
+	 * Test method for {@link io.fintechlabs.testframework.condition.SetPlainJsonAcceptHeaderForResourceEndpointRequest#evaluate(io.fintechlabs.testframework.testmodule.Environment).
+	 */
+	@Test
+	public void testEvaluate_replace() {
+
+		JsonObject headers = new JsonObject();
+		headers.addProperty("Accept", "something else");
+		env.put("resource_endpoint_request_headers", headers);
+
+		cond.evaluate(env);
+
+		assertThat(env.getString("resource_endpoint_request_headers", "Accept")).isEqualTo(expectedHeader);
+	}
+
+}
