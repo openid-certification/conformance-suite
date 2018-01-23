b5768035fddffdccc49e353336619c91061978eb
Steve Moore <steve@visexcell.com>
2018-01-23 15:02:06 -0500

    Condition loads in the cert and the key, and validates that they go together. CA is also loaded in to make sure it is a cert.

--- src/main/java/io/fintechlabs/testframework/condition/client/ValidateMTLSCertificatesAsX509.java
+++ src/main/java/io/fintechlabs/testframework/condition/client/ValidateMTLSCertificatesAsX509.java
@@ -6,6 +6,9 @@ import io.fintechlabs.testframework.condition.PreEnvironment;
 import io.fintechlabs.testframework.logging.TestInstanceEventLog;
 import io.fintechlabs.testframework.testmodule.Environment;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.provider.PEMUtil;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemObjectParser;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -13,6 +16,11 @@ import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 public class ValidateMTLSCertificatesAsX509 extends AbstractCondition {
@@ -41,26 +49,29 @@ public class ValidateMTLSCertificatesAsX509 extends AbstractCondition {
 
 			certFactory = CertificateFactory.getInstance("X.509","BC");
 			X509Certificate certificate = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(Base64.getDecoder().decode(certString)));
-			String modifiedKeyString = "-----BEGIN RSA PRIVATE KEY-----\n" +
-					new String(Base64.getDecoder().decode(keyString)) +
-					"\n-----END RSA PRIVATE KEY-----";
-			KeyStore keystore = KeyStore.getInstance("PKCS12", "BC");
 
-			// This might work for the keyPassword?
-			char[] keyPassword = {};
-			// keystore.load(new ByteArrayInputStream(modifiedKeyString.getBytes()), keyPassword);
-			keystore.load(new ByteArrayInputStream(Base64.getDecoder().decode(keyString)), keyPassword);
+			KeyFactory kf = KeyFactory.getInstance("RSA","BC");
+			KeySpec kspec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(keyString));
+			RSAPrivateKey privateKey = (RSAPrivateKey) kf.generatePrivate(kspec);
 
+
+			// Check that the private key and the certificate match
+			RSAPublicKey publicKey = (RSAPublicKey)certificate.getPublicKey();
+			if (!(privateKey.getModulus().equals(publicKey.getModulus()))){
+				return error("MTLS Private Key and Cert do not match",args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
+			}
+
+			if (!Strings.isNullOrEmpty(caString)) {
+				X509Certificate caCertificate = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(Base64.getDecoder().decode(caString)));
+			}
 
 		} catch (CertificateException e) {
 			return error("Couldn't validate certificate, key, or CA chain from Base64", e, args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
 		} catch (NoSuchProviderException e) {
 			return error("Couldn't validate certificate, key, or CA chain from Base64", e, args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
-		} catch (KeyStoreException e) {
-			return error("Couldn't validate certificate, key, or CA chain from Base64", e, args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
 		} catch (NoSuchAlgorithmException e) {
 			return error("Couldn't validate certificate, key, or CA chain from Base64", e, args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
-		} catch (IOException e) {
+		} catch (InvalidKeySpecException e) {
 			return error("Couldn't validate certificate, key, or CA chain from Base64", e, args("cert", certString, "key", keyString, "ca", Strings.emptyToNull(caString)));
 		}
 
