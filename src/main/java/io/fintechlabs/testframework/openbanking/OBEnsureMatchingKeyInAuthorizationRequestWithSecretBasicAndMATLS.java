4668a54203bb6544be68ab648f0e6c239da35920
Leo Uino <leo.uino@fintechlabs.io>
2018-01-24 16:17:05 +0900

    Ensure request object signature is checked (fix #70)

---
+++ src/main/java/io/fintechlabs/testframework/openbanking/OBEnsureMatchingKeyInAuthorizationRequestWithSecretBasicAndMATLS.java
@@ -0,0 +1,71 @@
+package io.fintechlabs.testframework.openbanking;
+
+import java.util.Map;
+
+import com.google.gson.JsonObject;
+
+import io.fintechlabs.testframework.condition.client.AddBasicAuthClientSecretAuthenticationParameters;
+import io.fintechlabs.testframework.condition.client.AddClientIdToTokenEndpointRequest;
+import io.fintechlabs.testframework.condition.client.CreateTokenEndpointRequestForAuthorizationCodeGrant;
+import io.fintechlabs.testframework.condition.client.CreateTokenEndpointRequestForClientCredentialsGrant;
+import io.fintechlabs.testframework.condition.common.EnsureMinimumClientSecretEntropy;
+import io.fintechlabs.testframework.frontChannel.BrowserControl;
+import io.fintechlabs.testframework.info.TestInfoService;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "ob-ensure-matching-key-in-authorization-request-with-secret-basic-and-matls",
+	displayName = "OB: ensure matching key in authorization request (client_secret_basic authentication with MATLS)",
+	profile = "OB",
+	configurationFields = {
+		"server.discoveryUrl",
+		"client.client_id",
+		"client.scope",
+		"client.jwks",
+		"client.client_secret",
+		"mtls.key",
+		"mtls.cert",
+		"mtls.ca",
+		"client2.client_id",
+		"client2.jwks",
+		"resource.resourceUrl",
+		"resource.institution_id"
+	}
+)
+public class OBEnsureMatchingKeyInAuthorizationRequestWithSecretBasicAndMATLS extends AbstractOBEnsureMatchingKeyInAuthorizationRequest {
+
+	public OBEnsureMatchingKeyInAuthorizationRequestWithSecretBasicAndMATLS(String id, Map<String, String> owner, TestInstanceEventLog eventLog, BrowserControl browser, TestInfoService testInfo) {
+		super(id, owner, eventLog, browser, testInfo);
+		logClientSecretWarning();
+	}
+
+	@Override
+	protected void onConfigure(JsonObject config, String baseUrl) {
+
+		super.onConfigure(config, baseUrl);
+
+		callAndStopOnFailure(EnsureMinimumClientSecretEntropy.class, "RFC6819-5.1.4.2-2");
+	}
+
+	@Override
+	protected void createClientCredentialsRequest() {
+
+		callAndStopOnFailure(CreateTokenEndpointRequestForClientCredentialsGrant.class);
+
+		callAndStopOnFailure(AddBasicAuthClientSecretAuthenticationParameters.class);
+
+		callAndStopOnFailure(AddClientIdToTokenEndpointRequest.class);
+	}
+
+	@Override
+	protected void createAuthorizationCodeRequest() {
+
+		callAndStopOnFailure(CreateTokenEndpointRequestForAuthorizationCodeGrant.class);
+
+		callAndStopOnFailure(AddBasicAuthClientSecretAuthenticationParameters.class);
+
+		callAndStopOnFailure(AddClientIdToTokenEndpointRequest.class);
+	}
+
+}
