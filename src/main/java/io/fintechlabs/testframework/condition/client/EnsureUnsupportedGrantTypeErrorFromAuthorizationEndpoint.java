495a79e7026e48b700eaa834d34c5e9a21663027
Steve Moore <steve@visexcell.com>
2018-01-30 15:05:10 -0500

    Test to make sure the Authorization Server rejects the code flow

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/EnsureUnsupportedGrantTypeErrorFromAuthorizationEndpoint.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package io.fintechlabs.testframework.condition.client;
+
+import com.google.common.base.Strings;
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+/**
+ * Check to make sure a "unsupported_response_type" error was received from the server
+ * 
+ * @author srmoore
+ *
+ */
+public class EnsureUnsupportedGrantTypeErrorFromAuthorizationEndpoint extends AbstractCondition {
+
+	/**
+	 * @param testId
+	 * @param log
+	 */
+	public EnsureUnsupportedGrantTypeErrorFromAuthorizationEndpoint(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	/* (non-Javadoc)
+	 * @see io.fintechlabs.testframework.testmodule.Condition#evaluate(io.fintechlabs.testframework.testmodule.Environment)
+	 */
+	@Override
+	@PreEnvironment(required = "callback_params")
+	public Environment evaluate(Environment in) {
+		if (!in.containsObj("callback_params")) {
+			return error("Couldn't find callback parameters");
+		}
+
+		if (!Strings.isNullOrEmpty(in.getString("callback_params", "error"))) {
+			if (in.getString("callback_params","error").equals("unsupported_response_type")){
+				logSuccess("unsupported_response_type error from the authorization endpoint");
+				return in;
+			} else {
+				return error("Incorrect error from the authorization endpoint", in.get("callback_params"));
+			}
+		} else {
+			return error("No unsupported_response_type error found from the authorization endpoint", in.get("callback_params"));
+		}
+
+	}
+
+}
