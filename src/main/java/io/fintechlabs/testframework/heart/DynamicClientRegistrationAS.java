75f4596953356199ae6db955cc6d0b308cc0d396
Steve Moore <steve@visexcell.com>
2018-04-26 15:08:50 -0400

    Initial pass of Dynamic Client Registration test.

---
+++ src/main/java/io/fintechlabs/testframework/heart/DynamicClientRegistrationAS.java
@@ -0,0 +1,123 @@
+package io.fintechlabs.testframework.heart;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import io.fintechlabs.testframework.condition.client.*;
+import io.fintechlabs.testframework.condition.common.CheckForKeyIdInJWKs;
+import io.fintechlabs.testframework.condition.common.CheckHeartServerConfiguration;
+import io.fintechlabs.testframework.condition.common.DisallowTLS10;
+import io.fintechlabs.testframework.condition.common.DisallowTLS11;
+import io.fintechlabs.testframework.condition.common.EnsureTLS12;
+import io.fintechlabs.testframework.condition.common.SetTLSTestHostFromConfig;
+import io.fintechlabs.testframework.frontChannel.BrowserControl;
+import io.fintechlabs.testframework.info.TestInfoService;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.AbstractTestModule;
+import io.fintechlabs.testframework.testmodule.TestFailureException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class DynamicClientRegistrationAS extends AbstractTestModule {
+
+	public static Logger logger = LoggerFactory.getLogger(DynamicClientRegistrationAS.class);
+
+	public DynamicClientRegistrationAS(String id, Map<String, String> owner, TestInstanceEventLog eventLog, BrowserControl browser, TestInfoService testInfo) {
+		super(id, owner, eventLog, browser, testInfo);
+	}
+
+	@Override
+	public void configure(JsonObject config, String baseUrl) {
+		env.putString("base_url", baseUrl);
+		env.put("config", config);
+
+		callAndStopOnFailure(SetTLSTestHostFromConfig.class);
+		callAndStopOnFailure(EnsureTLS12.class, "HEART-OAuth2-6");
+		call(DisallowTLS10.class, "HEART-OAuth2-6");
+		call(DisallowTLS11.class, "HEART-OAuth2-6");
+
+		callAndStopOnFailure(CreateRedirectUri.class);
+
+		// this is inserted by the create call above, expose it to the test environment for publication
+		exposeEnvString("redirect_uri");
+
+		// Get the server's configuration
+		call(GetDynamicServerConfiguration.class, "HEART-OAuth2-3.1.5");
+
+		// make sure the server configuration passes some basic sanity checks
+		callAndStopOnFailure(CheckHeartServerConfiguration.class, "HEART-OAuth2-3.1.5");
+
+		// fetch or load the server's keys as needed
+		callAndStopOnFailure(FetchServerKeys.class, "HEART-OAuth2-3.1.5");
+		callAndStopOnFailure(CheckHeartServerJwksFields.class, "HEART-OAuth2-3.1.5");
+		callAndStopOnFailure(CheckForKeyIdInJWKs.class, "OIDCC-10.1");
+
+		// get the client configuration that we'll use to dynamically register
+		callAndStopOnFailure(GetStaticClientConfiguration.class);
+		callAndStopOnFailure(ExtractJWKsFromClientConfiguration.class, "HEART-OAuth2-2.1.5");
+
+		callAndStopOnFailure(CreateJwksUri.class);
+		exposeEnvString("jwks_uri");
+
+		callAndStopOnFailure(CheckRedirectUri.class);
+
+		exposeEnvString("client_id");
+
+		setStatus(Status.CONFIGURED);
+		fireSetupDone();
+	}
+
+	@Override
+	public void start() {
+
+		setStatus(Status.RUNNING);
+
+		// create dynamic registration request
+		// submit request
+		// check response back - is good, no?
+		
+		// since we're not waiting for any browser interaction, we can just finish?
+		fireTestFinished();
+		stop();
+	}
+
+	@Override
+	public Object handleHttp(String path, HttpServletRequest req, HttpServletResponse res, HttpSession session, JsonObject requestParts) {
+		logIncomingHttpRequest(path, requestParts);
+
+		// dispatch based on the path
+		// since we're only doing the registration, nothing should come back on the callback
+		// if (path.equals("callback")) {
+		// 	return handleCallback(requestParts);
+		//}
+		if (path.equals("jwks")) {
+			return handleJwks(requestParts);
+		} else {
+			return new ModelAndView("testError");
+		}
+	}
+
+	private Object handleJwks(JsonObject requestParts) {
+		setStatus(Status.RUNNING);
+		JsonObject jwks = env.get("public_jwks");
+
+		setStatus(Status.WAITING);
+
+		return new ResponseEntity<Object>(jwks, HttpStatus.OK);
+	}
+
+	@Override
+	public Object handleHttpMtls(String path, HttpServletRequest req, HttpServletResponse res, HttpSession session, JsonObject requestParts) {
+		throw new TestFailureException(getId(), "Got an HTTP response on a call we weren't expecting");
+	}
+}
