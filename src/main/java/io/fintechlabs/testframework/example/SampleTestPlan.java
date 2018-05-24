5ebe5096e69b15ee6162ebc7714609b9d514cb46
Steve Moore <steve@visexcell.com>
2018-05-24 14:22:32 -0400

    Removed unused functions from TestPlan interface

--- src/main/java/io/fintechlabs/testframework/example/SampleTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/example/SampleTestPlan.java
@@ -36,39 +36,4 @@ import io.fintechlabs.testframework.plan.TestPlan;
 )
 public class SampleTestPlan implements TestPlan {
 
-	/**
-	 * 
-	 */
-	public SampleTestPlan() {
-	}
-
-	@Override
-	public List<String> getTestModules() {
-		// TODO: this is currently fully reflective, could be ways to make this better?
-		PublishTestPlan pub = getClass().getAnnotation(PublishTestPlan.class);
-		if (pub != null) {
-			return Arrays.asList(pub.testModuleNames());
-		} else {
-			return Collections.emptyList();
-		}
-	}
-
-	@Override
-	public String getNextTestModule(String currentTestModuleName) {
-		List<String> modules = getTestModules();
-		
-		if (modules.contains(currentTestModuleName)) {
-			int prev = modules.indexOf(currentTestModuleName);
-			if (prev + 1 <= modules.size()) {
-				// we're still in bounds
-				return modules.get(prev + 1);
-			} else {
-				// we're at the last element
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
-	
 }
