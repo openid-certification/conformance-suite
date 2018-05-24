bcaff922c55b1ddae30d4e52c59eefe949ed001b
Steve Moore <steve@visexcell.com>
2018-05-24 14:22:32 -0400

    Removed unused functions from TestPlan interface

--- src/main/java/io/fintechlabs/testframework/plan/TestPlan.java
+++ src/main/java/io/fintechlabs/testframework/plan/TestPlan.java
@@ -17,23 +17,12 @@ package io.fintechlabs.testframework.plan;
 import java.util.List;
 
 /**
- * A collection of test modules intended to be run with a single 
+ * A collection of test modules intended to be run with a single
  * test configuration.
- * 
+ *
  * @author jricher
  *
  */
 public interface TestPlan {
-
-	/**
-	 * @param currentTestModuleName
-	 * @return
-	 */
-	String getNextTestModule(String currentTestModuleName);
-
-	/**
-	 * @return
-	 */
-	List<String> getTestModules();
 
 }
