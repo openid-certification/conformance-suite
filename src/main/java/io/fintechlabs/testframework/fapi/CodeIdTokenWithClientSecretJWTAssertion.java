ac601c86a5a543ee8ac2e3e0028b45725d34911e
Justin Richer <justin@bspk.io>
2018-08-01 17:41:59 -0400

    background implicit submission for one test module

--- src/main/java/io/fintechlabs/testframework/fapi/CodeIdTokenWithClientSecretJWTAssertion.java
+++ src/main/java/io/fintechlabs/testframework/fapi/CodeIdTokenWithClientSecretJWTAssertion.java
@@ -303,148 +303,153 @@ public class CodeIdTokenWithClientSecretJWTAssertion extends AbstractTestModule 
 	}
 
 	private Object handleImplicitSubmission(JsonObject requestParts) {
+		
+		getTestExecutionManager().runInBackground(() -> {
+			
+			// process the callback
+			setStatus(Status.RUNNING);
 
-		// process the callback
-		setStatus(Status.RUNNING);
+			JsonElement body = requestParts.get("body");
 
-		JsonElement body = requestParts.get("body");
+			if (body != null) {
+				String hash = body.getAsString();
 
-		if (body != null) {
-			String hash = body.getAsString();
+				logger.info("Hash: " + hash);
 
-			logger.info("Hash: " + hash);
+				env.putString("implicit_hash", hash);
+			} else {
+				logger.warn("No hash submitted");
 
-			env.putString("implicit_hash", hash);
-		} else {
-			logger.warn("No hash submitted");
+				env.putString("implicit_hash", ""); // Clear any old value
+			}
 
-			env.putString("implicit_hash", ""); // Clear any old value
-		}
+			callAndStopOnFailure(ExtractImplicitHashToCallbackResponse.class);
 
-		callAndStopOnFailure(ExtractImplicitHashToCallbackResponse.class);
+			callAndStopOnFailure(CheckIfAuthorizationEndpointError.class);
 
-		callAndStopOnFailure(CheckIfAuthorizationEndpointError.class);
+			callAndStopOnFailure(CheckMatchingStateParameter.class);
 
-		callAndStopOnFailure(CheckMatchingStateParameter.class);
+			// check the ID token from the hybrid response
 
-		// check the ID token from the hybrid response
+			callAndStopOnFailure(ExtractIdTokenFromAuthorizationResponse.class, "FAPI-2-5.2.2-3");
 
-		callAndStopOnFailure(ExtractIdTokenFromAuthorizationResponse.class, "FAPI-2-5.2.2-3");
+			callAndStopOnFailure(ValidateIdToken.class, "FAPI-2-5.2.2-3");
 
-		callAndStopOnFailure(ValidateIdToken.class, "FAPI-2-5.2.2-3");
+			callAndStopOnFailure(ValidateIdTokenSignature.class, "FAPI-2-5.2.2-3");
 
-		callAndStopOnFailure(ValidateIdTokenSignature.class, "FAPI-2-5.2.2-3");
+			callAndStopOnFailure(CheckForSubscriberInIdToken.class, "FAPI-1-5.2.2-24");
 
-		callAndStopOnFailure(CheckForSubscriberInIdToken.class, "FAPI-1-5.2.2-24");
+			call(ExtractSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
 
-		call(ExtractSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
+			skipIfMissing(new String[] { "state_hash" }, new String[] {}, ConditionResult.INFO,
+				ValidateSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
 
-		skipIfMissing(new String[] { "state_hash" }, new String[] {}, ConditionResult.INFO,
-			ValidateSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
+			call(ExtractCHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
 
-		call(ExtractCHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+			skipIfMissing(new String[] { "c_hash" }, new String[] {}, ConditionResult.INFO,
+				ValidateCHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
 
-		skipIfMissing(new String[] { "c_hash" }, new String[] {}, ConditionResult.INFO,
-			ValidateCHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+			call(ExtractAtHash.class, ConditionResult.INFO, "OIDCC-3.3.2.11");
 
-		call(ExtractAtHash.class, ConditionResult.INFO, "OIDCC-3.3.2.11");
-
-		skipIfMissing(new String[] { "at_hash" }, new String[] {}, ConditionResult.INFO,
-			ValidateAtHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+			skipIfMissing(new String[] { "at_hash" }, new String[] {}, ConditionResult.INFO,
+				ValidateAtHash.class, ConditionResult.FAILURE, "OIDCC-3.3.2.11");
 
 
-		// call the token endpoint and complete the flow
+			// call the token endpoint and complete the flow
 
-		callAndStopOnFailure(ExtractAuthorizationCodeFromAuthorizationResponse.class);
+			callAndStopOnFailure(ExtractAuthorizationCodeFromAuthorizationResponse.class);
 
-		callAndStopOnFailure(CreateTokenEndpointRequestForAuthorizationCodeGrant.class);
+			callAndStopOnFailure(CreateTokenEndpointRequestForAuthorizationCodeGrant.class);
 
-		callAndStopOnFailure(CreateClientAuthenticationAssertionClaims.class);
+			callAndStopOnFailure(CreateClientAuthenticationAssertionClaims.class);
 
-		callAndStopOnFailure(SignClientAuthenticationAssertion.class);
+			callAndStopOnFailure(SignClientAuthenticationAssertion.class);
 
-		callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
+			callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
 
-		callAndStopOnFailure(CallTokenEndpoint.class);
+			callAndStopOnFailure(CallTokenEndpoint.class);
 
-		callAndStopOnFailure(CheckIfTokenEndpointResponseError.class);
+			callAndStopOnFailure(CheckIfTokenEndpointResponseError.class);
 
-		callAndStopOnFailure(CheckForAccessTokenValue.class, "FAPI-1-5.2.2-14");
+			callAndStopOnFailure(CheckForAccessTokenValue.class, "FAPI-1-5.2.2-14");
 
-		callAndStopOnFailure(ExtractAccessTokenFromTokenResponse.class);
+			callAndStopOnFailure(ExtractAccessTokenFromTokenResponse.class);
 
-		callAndStopOnFailure(CheckForScopesInTokenResponse.class, "FAPI-1-5.2.2-15");
+			callAndStopOnFailure(CheckForScopesInTokenResponse.class, "FAPI-1-5.2.2-15");
 
-		callAndStopOnFailure(ExtractIdTokenFromTokenResponse.class, "FAPI-1-5.2.2-24");
+			callAndStopOnFailure(ExtractIdTokenFromTokenResponse.class, "FAPI-1-5.2.2-24");
 
-		callAndStopOnFailure(ValidateIdToken.class, "FAPI-1-5.2.2-24");
+			callAndStopOnFailure(ValidateIdToken.class, "FAPI-1-5.2.2-24");
 
-		callAndStopOnFailure(ValidateIdTokenSignature.class, "FAPI-1-5.2.2-24");
+			callAndStopOnFailure(ValidateIdTokenSignature.class, "FAPI-1-5.2.2-24");
 
-		callAndStopOnFailure(CheckForSubscriberInIdToken.class, "FAPI-1-5.2.2-24");
+			callAndStopOnFailure(CheckForSubscriberInIdToken.class, "FAPI-1-5.2.2-24");
 
-		call(ExtractSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
+			call(ExtractSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
 
-		skipIfMissing(new String[] { "state_hash" }, new String[] {}, ConditionResult.INFO,
-			ValidateSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
+			skipIfMissing(new String[] { "state_hash" }, new String[] {}, ConditionResult.INFO,
+				ValidateSHash.class, ConditionResult.FAILURE, "FAPI-2-5.2.2-4");
 
-		call(CheckForRefreshTokenValue.class);
+			call(CheckForRefreshTokenValue.class);
 
-		call(EnsureMinimumTokenLength.class, ConditionResult.FAILURE, "FAPI-1-5.2.2-16");
+			call(EnsureMinimumTokenLength.class, ConditionResult.FAILURE, "FAPI-1-5.2.2-16");
 
-		call(EnsureMinimumTokenEntropy.class, ConditionResult.FAILURE, "FAPI-1-5.2.2-16");
+			call(EnsureMinimumTokenEntropy.class, ConditionResult.FAILURE, "FAPI-1-5.2.2-16");
 
-		// verify the access token against a protected resource
+			// verify the access token against a protected resource
 
-		callAndStopOnFailure(CreateRandomFAPIInteractionId.class);
-		exposeEnvString("fapi_interaction_id");
+			callAndStopOnFailure(CreateRandomFAPIInteractionId.class);
+			exposeEnvString("fapi_interaction_id");
 
-		callAndStopOnFailure(GenerateResourceEndpointRequestHeaders.class);
+			callAndStopOnFailure(GenerateResourceEndpointRequestHeaders.class);
 
-		callAndStopOnFailure(AddFAPIInteractionIdToResourceEndpointRequest.class, "FAPI-1-6.2.2-6");
+			callAndStopOnFailure(AddFAPIInteractionIdToResourceEndpointRequest.class, "FAPI-1-6.2.2-6");
 
-		callAndStopOnFailure(CallAccountsEndpointWithBearerToken.class, "FAPI-1-6.2.1-1", "FAPI-1-6.2.1-3");
+			callAndStopOnFailure(CallAccountsEndpointWithBearerToken.class, "FAPI-1-6.2.1-1", "FAPI-1-6.2.1-3");
 
-		callAndStopOnFailure(CheckForDateHeaderInResourceResponse.class, "FAPI-1-6.2.1-11");
+			callAndStopOnFailure(CheckForDateHeaderInResourceResponse.class, "FAPI-1-6.2.1-11");
 
-		callAndStopOnFailure(CheckForFAPIInteractionIdInResourceResponse.class, "FAPI-1-6.2.1-12");
+			callAndStopOnFailure(CheckForFAPIInteractionIdInResourceResponse.class, "FAPI-1-6.2.1-12");
 
-		call(EnsureMatchingFAPIInteractionId.class, ConditionResult.FAILURE, "FAPI-1-6.2.1-12");
+			call(EnsureMatchingFAPIInteractionId.class, ConditionResult.FAILURE, "FAPI-1-6.2.1-12");
 
-		callAndStopOnFailure(EnsureResourceResponseContentTypeIsJsonUTF8.class, "FAPI-1-6.2.1-9", "FAPI-1-6.2.1-10");
+			callAndStopOnFailure(EnsureResourceResponseContentTypeIsJsonUTF8.class, "FAPI-1-6.2.1-9", "FAPI-1-6.2.1-10");
 
-		callAndStopOnFailure(DisallowAccessTokenInQuery.class, "FAPI-1-6.2.1-4");
+			callAndStopOnFailure(DisallowAccessTokenInQuery.class, "FAPI-1-6.2.1-4");
 
-		// get token for second client
-		eventLog.startBlock("Second client");
-		env.mapKey("client", "client2");
-		env.mapKey("client_jwks", "client_jwks2");
+			// get token for second client
+			eventLog.startBlock("Second client");
+			env.mapKey("client", "client2");
+			env.mapKey("client_jwks", "client_jwks2");
 
-		callAndStopOnFailure(CreateAuthorizationEndpointRequestFromClientInformation.class);
+			callAndStopOnFailure(CreateAuthorizationEndpointRequestFromClientInformation.class);
 
-		callAndStopOnFailure(CreateRandomStateValue.class);
-		exposeEnvString("state");
-		callAndStopOnFailure(AddStateToAuthorizationEndpointRequest.class);
+			callAndStopOnFailure(CreateRandomStateValue.class);
+			exposeEnvString("state");
+			callAndStopOnFailure(AddStateToAuthorizationEndpointRequest.class);
 
-		callAndStopOnFailure(CreateRandomNonceValue.class);
-		exposeEnvString("nonce");
-		callAndStopOnFailure(AddNonceToAuthorizationEndpointRequest.class);
+			callAndStopOnFailure(CreateRandomNonceValue.class);
+			exposeEnvString("nonce");
+			callAndStopOnFailure(AddNonceToAuthorizationEndpointRequest.class);
 
-		callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken.class);
+			callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken.class);
 
-		callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
+			callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
 
-		callAndStopOnFailure(SignRequestObject.class);
+			callAndStopOnFailure(SignRequestObject.class);
 
-		callAndStopOnFailure(BuildRequestObjectRedirectToAuthorizationEndpoint.class);
+			callAndStopOnFailure(BuildRequestObjectRedirectToAuthorizationEndpoint.class);
 
-		String redirectTo = env.getString("redirect_to_authorization_endpoint");
+			String redirectTo = env.getString("redirect_to_authorization_endpoint");
 
-		eventLog.log(getName(), args("msg", "Redirecting to url", "redirect_to", redirectTo));
+			eventLog.log(getName(), args("msg", "Redirecting to url", "redirect_to", redirectTo));
 
-		browser.goToUrl(redirectTo);
+			browser.goToUrl(redirectTo);
 
-		setStatus(Status.WAITING);
+			setStatus(Status.WAITING);
+
+			return "done";
+		});
 
 		return redirectToLogDetailPage();
 
