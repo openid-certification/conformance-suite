8a4a0f726103281c6fa03ba856d51a3c9d76af42
Joseph Heenan <joseph.heenan@fintechlabs.io>
2018-08-06 12:11:24 +0100

    Compile fix for FAPI tests

--- src/main/java/io/fintechlabs/testframework/fapi/FAPIRWCodeIdTokenWithMTLS.java
+++ src/main/java/io/fintechlabs/testframework/fapi/FAPIRWCodeIdTokenWithMTLS.java
@@ -24,6 +24,7 @@ import io.fintechlabs.testframework.info.TestInfoService;
 import io.fintechlabs.testframework.logging.TestInstanceEventLog;
 import io.fintechlabs.testframework.openbanking.AbstractOBServerTestModuleCodeIdToken;
 import io.fintechlabs.testframework.openbanking.OBCodeIdTokenWithMTLS;
+import io.fintechlabs.testframework.runner.TestExecutionManager;
 import io.fintechlabs.testframework.testmodule.PublishTestModule;
 
 import java.util.Map;
@@ -52,8 +53,8 @@ import java.util.Map;
 )
 public class FAPIRWCodeIdTokenWithMTLS extends OBCodeIdTokenWithMTLS {
 
-	public FAPIRWCodeIdTokenWithMTLS(String id, Map<String, String> owner, TestInstanceEventLog eventLog, BrowserControl browser, TestInfoService testInfo) {
-		super(id, owner, eventLog, browser, testInfo);
+	public FAPIRWCodeIdTokenWithMTLS(String id, Map<String, String> owner, TestInstanceEventLog eventLog, BrowserControl browser, TestInfoService testInfo, TestExecutionManager executionManager) {
+		super(id, owner, eventLog, browser, testInfo, executionManager);
 	}
 
 	@Override
