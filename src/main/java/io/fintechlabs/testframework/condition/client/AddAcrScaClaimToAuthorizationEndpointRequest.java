de1e5b0aa500e6ad23175a1d307757eb44e7ae82
Joseph Heenan <joseph.heenan@fintechlabs.io>
2018-08-17 09:03:21 +0100

    Closes #284. Adding code to test the SCA acr claim mechanism. Tests are for Client2 only.

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/AddAcrScaClaimToAuthorizationEndpointRequest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+/**
+ * @author ddrysdale
+ *
+ */
+
+package io.fintechlabs.testframework.condition.client;
+
+import io.fintechlabs.testframework.condition.PostEnvironment;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class AddAcrScaClaimToAuthorizationEndpointRequest extends AbstractAddClaimToAuthorizationEndpointRequest {
+
+	public AddAcrScaClaimToAuthorizationEndpointRequest(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	@PreEnvironment(required = "authorization_endpoint_request")
+	@PostEnvironment(required = "authorization_endpoint_request")
+	public Environment evaluate(Environment env) {
+		return addClaim(env, "acr", "urn:openbanking:psd2:sca", true);
+	}
+
+}
---
+++ src/main/java/io/fintechlabs/testframework/condition/client/ValidateIdTokenACRClaims.java
@@ -0,0 +1,80 @@
+package io.fintechlabs.testframework.condition.client;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+/**
+ * @author ddrysdale
+ *
+ */
+public class ValidateIdTokenACRClaims extends AbstractCondition {
+
+	/**
+	 * @param testId
+	 * @param log
+	 */
+	public ValidateIdTokenACRClaims(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure,
+			String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * io.fintechlabs.testframework.condition.Condition#evaluate(io.fintechlabs.
+	 * testframework.testmodule.Environment)
+	 */
+	@Override
+	@PreEnvironment(required = { "id_token",  "authorization_endpoint_request" })
+	public Environment evaluate(Environment env) {
+
+		JsonObject authorizationEndpointRequest = env.getObject("authorization_endpoint_request");
+		JsonElement acrValue = env.getElementFromObject("authorization_endpoint_request", "claims.id_token.acr.value");
+		if (acrValue != null) {
+
+			if (acrValue.isJsonPrimitive()) {
+
+				// Split our requirements as per the spec
+				String[] valuesNeeded = acrValue.getAsString().split(" ");
+
+				// Read what the server has sent us
+				JsonElement acrServerClaims = env.getElementFromObject("id_token", "claims.acr");
+				if (acrServerClaims == null || !acrServerClaims.isJsonPrimitive()) {
+					throw error("Missing or invalid acr value in id_token", args("id_token", env.getObject("id_token")));
+				}
+				List<String> valuesReceived = Arrays.asList(acrServerClaims.getAsString().split(" "));
+
+				// Test the sets
+				Boolean foundEnough = false;
+
+				for (String singleAcrValue : valuesNeeded) {
+					if (valuesReceived.contains(singleAcrValue)) {
+						foundEnough = true;
+						break;
+					}
+				}
+
+				if (!foundEnough) {
+					throw error("acr value in id_token does not match requested value", args("required", valuesNeeded, "actual", valuesReceived));
+				} else {
+					logSuccess("acr value in id_token is as expected", args("required", valuesNeeded, "actual", valuesReceived));
+				}
+
+			} else {
+				throw error("Invalid acr claim in request object", args("actual", acrValue));
+			}
+		} else {
+			logSuccess("Nothing to check; no acr claim in request object");
+		}
+		return env;
+	}
+}
