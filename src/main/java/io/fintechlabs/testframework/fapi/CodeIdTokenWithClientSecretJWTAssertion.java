c081a3559b7928cc276712d1be2837d5a2ff0d95
Justin Richer <justin@bspk.io>
2018-08-21 10:51:26 -0400

    clean up extraneous comments

--- src/main/java/io/fintechlabs/testframework/fapi/CodeIdTokenWithClientSecretJWTAssertion.java
+++ src/main/java/io/fintechlabs/testframework/fapi/CodeIdTokenWithClientSecretJWTAssertion.java
@@ -1,17 +1,3 @@
-/*******************************************************************************
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
-
 package io.fintechlabs.testframework.fapi;
 
 import java.util.Map;
@@ -246,12 +232,6 @@ public class CodeIdTokenWithClientSecretJWTAssertion extends AbstractTestModule 
 
 		callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken.class);
 
-//		callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
-//
-//		callAndStopOnFailure(SignRequestObject.class);
-//
-//		callAndStopOnFailure(BuildRequestObjectRedirectToAuthorizationEndpoint.class);
-
 		call(condition(CreateRandomCodeVerifier.class));
 		call(exec().exposeEnvironmentString("code_verifier"));
 		call(condition(CreateS256CodeChallenge.class));
@@ -448,12 +428,6 @@ public class CodeIdTokenWithClientSecretJWTAssertion extends AbstractTestModule 
 			callAndStopOnFailure(AddNonceToAuthorizationEndpointRequest.class);
 
 			callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken.class);
-
-//			callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
-//
-//			callAndStopOnFailure(SignRequestObject.class);
-//
-//			callAndStopOnFailure(BuildRequestObjectRedirectToAuthorizationEndpoint.class);
 
 			call(condition(CreateRandomCodeVerifier.class));
 			call(exec().exposeEnvironmentString("code_verifier"));
