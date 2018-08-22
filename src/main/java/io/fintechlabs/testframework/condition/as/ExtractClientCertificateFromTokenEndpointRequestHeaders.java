ad984501a510b52976ca588e7fdfbcc2948cfb67
Joseph Heenan <joseph.heenan@fintechlabs.io>
2018-08-22 13:39:47 +0100

    Cleanup some comments

--- src/main/java/io/fintechlabs/testframework/condition/as/ExtractClientCertificateFromTokenEndpointRequestHeaders.java
+++ src/main/java/io/fintechlabs/testframework/condition/as/ExtractClientCertificateFromTokenEndpointRequestHeaders.java
@@ -1,17 +1,3 @@
-/*******************************************************************************
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
-
 package io.fintechlabs.testframework.condition.as;
 
 import java.io.ByteArrayInputStream;
@@ -31,11 +17,6 @@ import io.fintechlabs.testframework.testmodule.Environment;
 
 public class ExtractClientCertificateFromTokenEndpointRequestHeaders extends AbstractCondition {
 
-	/**
-	 * @param testId
-	 * @param log
-	 * @param optional
-	 */
 	public ExtractClientCertificateFromTokenEndpointRequestHeaders(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
 		super(testId, log, conditionResultOnFailure, requirements);
 	}
