965cff49c4c127e9f148a13dcbd1b19d23c0f376
Joseph Heenan <joseph.heenan@fintechlabs.io>
2018-08-25 11:01:50 +0100

    Cleanup comments / dead code

--- src/main/java/io/fintechlabs/testframework/condition/as/CheckForClientCertificate.java
+++ src/main/java/io/fintechlabs/testframework/condition/as/CheckForClientCertificate.java
@@ -1,17 +1,3 @@
-/*******************************************************************************
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
-
 package io.fintechlabs.testframework.condition.as;
 
 import io.fintechlabs.testframework.condition.AbstractCondition;
@@ -20,18 +6,10 @@ import io.fintechlabs.testframework.testmodule.Environment;
 
 public class CheckForClientCertificate extends AbstractCondition {
 
-	/**
-	 * @param testId
-	 * @param log
-	 * @param optional
-	 */
 	public CheckForClientCertificate(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
 		super(testId, log, conditionResultOnFailure, requirements);
 	}
 
-	/* (non-Javadoc)
-	 * @see io.fintechlabs.testframework.condition.Condition#evaluate(io.fintechlabs.testframework.testmodule.Environment)
-	 */
 	@Override
 	// note, we don't use the @PreEnvironment check here so we can do a more direct check below
 	public Environment evaluate(Environment env) {
