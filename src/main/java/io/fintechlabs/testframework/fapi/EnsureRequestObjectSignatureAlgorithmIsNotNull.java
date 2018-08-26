12a961e8ac7e416b42fc801d5224908f402b2b80
Joseph Heenan <joseph.heenan@fintechlabs.io>
2018-08-26 20:00:35 +0100

    Cleanup two FAPI-RW tests
    They appear to be erroneously requested PKCE, but they're confidential
    client tests so don't need to request PKCE in FAPI-RW.
    
    Also clean up some useless comments / unused imports.

--- src/main/java/io/fintechlabs/testframework/fapi/EnsureRequestObjectSignatureAlgorithmIsNotNull.java
+++ src/main/java/io/fintechlabs/testframework/fapi/EnsureRequestObjectSignatureAlgorithmIsNotNull.java
@@ -1,20 +1,15 @@
 package io.fintechlabs.testframework.fapi;
 
-import java.util.Map;
-
 import com.google.gson.JsonObject;
 
-import io.fintechlabs.testframework.condition.client.AddCodeChallengeToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.AddNonceToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.AddStateToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.BuildRequestObjectRedirectToAuthorizationEndpoint;
 import io.fintechlabs.testframework.condition.client.ConvertAuthorizationEndpointRequestToRequestObject;
 import io.fintechlabs.testframework.condition.client.CreateAuthorizationEndpointRequestFromClientInformation;
-import io.fintechlabs.testframework.condition.client.CreateRandomCodeVerifier;
 import io.fintechlabs.testframework.condition.client.CreateRandomNonceValue;
 import io.fintechlabs.testframework.condition.client.CreateRandomStateValue;
 import io.fintechlabs.testframework.condition.client.CreateRedirectUri;
-import io.fintechlabs.testframework.condition.client.CreateS256CodeChallenge;
 import io.fintechlabs.testframework.condition.client.ExpectRequestObjectUnverifiableErrorPage;
 import io.fintechlabs.testframework.condition.client.ExtractJWKsFromClientConfiguration;
 import io.fintechlabs.testframework.condition.client.FetchServerKeys;
@@ -25,10 +20,6 @@ import io.fintechlabs.testframework.condition.client.SetAuthorizationEndpointReq
 import io.fintechlabs.testframework.condition.common.CheckForKeyIdInClientJWKs;
 import io.fintechlabs.testframework.condition.common.CheckForKeyIdInServerJWKs;
 import io.fintechlabs.testframework.condition.common.CheckServerConfiguration;
-import io.fintechlabs.testframework.frontChannel.BrowserControl;
-import io.fintechlabs.testframework.info.TestInfoService;
-import io.fintechlabs.testframework.logging.TestInstanceEventLog;
-import io.fintechlabs.testframework.runner.TestExecutionManager;
 import io.fintechlabs.testframework.testmodule.AbstractTestModule;
 import io.fintechlabs.testframework.testmodule.PublishTestModule;
 
@@ -98,15 +89,6 @@ public class EnsureRequestObjectSignatureAlgorithmIsNotNull extends AbstractTest
 		callAndStopOnFailure(AddNonceToAuthorizationEndpointRequest.class);
 
 		callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken.class);
-
-		call(condition(CreateRandomCodeVerifier.class));
-		call(exec().exposeEnvironmentString("code_verifier"));
-		call(condition(CreateS256CodeChallenge.class));
-		call(exec()
-			.exposeEnvironmentString("code_challenge")
-			.exposeEnvironmentString("code_challenge_method"));
-		call(condition(AddCodeChallengeToAuthorizationEndpointRequest.class)
-			.requirement("FAPI-1-5.2.2-7"));
 
 		callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
 
--- src/main/java/io/fintechlabs/testframework/fapi/RejectCodeFlow.java
+++ src/main/java/io/fintechlabs/testframework/fapi/RejectCodeFlow.java
@@ -1,40 +1,18 @@
-/*******************************************************************************
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
-
 package io.fintechlabs.testframework.fapi;
-
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.gson.JsonObject;
 
-import io.fintechlabs.testframework.condition.client.AddCodeChallengeToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.AddNonceToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.AddStateToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.BuildPlainRedirectToAuthorizationEndpoint;
 import io.fintechlabs.testframework.condition.client.CreateAuthorizationEndpointRequestFromClientInformation;
-import io.fintechlabs.testframework.condition.client.CreateRandomCodeVerifier;
 import io.fintechlabs.testframework.condition.client.CreateRandomNonceValue;
 import io.fintechlabs.testframework.condition.client.CreateRandomStateValue;
 import io.fintechlabs.testframework.condition.client.CreateRedirectUri;
-import io.fintechlabs.testframework.condition.client.CreateS256CodeChallenge;
 import io.fintechlabs.testframework.condition.client.EnsureUnsupportedGrantTypeErrorFromAuthorizationEndpoint;
 import io.fintechlabs.testframework.condition.client.FetchServerKeys;
 import io.fintechlabs.testframework.condition.client.GetDynamicServerConfiguration;
@@ -42,10 +20,6 @@ import io.fintechlabs.testframework.condition.client.GetStaticClientConfiguratio
 import io.fintechlabs.testframework.condition.client.SetAuthorizationEndpointRequestResponseTypeToCode;
 import io.fintechlabs.testframework.condition.common.CheckServerConfiguration;
 import io.fintechlabs.testframework.condition.common.ExpectGrantTypeErrorPage;
-import io.fintechlabs.testframework.frontChannel.BrowserControl;
-import io.fintechlabs.testframework.info.TestInfoService;
-import io.fintechlabs.testframework.logging.TestInstanceEventLog;
-import io.fintechlabs.testframework.runner.TestExecutionManager;
 import io.fintechlabs.testframework.testmodule.AbstractTestModule;
 import io.fintechlabs.testframework.testmodule.PublishTestModule;
 import io.fintechlabs.testframework.testmodule.TestFailureException;
@@ -68,11 +42,6 @@ import io.fintechlabs.testframework.testmodule.UserFacing;
 )
 public class RejectCodeFlow extends AbstractTestModule {
 
-	public static Logger logger = LoggerFactory.getLogger(RejectCodeFlow.class);
-
-	/* (non-Javadoc)
-	 * @see io.bspk.selenium.TestModule#configure(com.google.gson.JsonObject)
-	 */
 	@Override
 	public void configure(JsonObject config, String baseUrl) {
 		env.putString("base_url", baseUrl);
@@ -101,9 +70,6 @@ public class RejectCodeFlow extends AbstractTestModule {
 		fireSetupDone();
 	}
 
-	/* (non-Javadoc)
-	 * @see io.bspk.selenium.TestModule#start()
-	 */
 	@Override
 	public void start() {
 		setStatus(Status.RUNNING);
@@ -120,15 +86,6 @@ public class RejectCodeFlow extends AbstractTestModule {
 
 		callAndStopOnFailure(SetAuthorizationEndpointRequestResponseTypeToCode.class);
 
-		call(condition(CreateRandomCodeVerifier.class));
-		call(exec().exposeEnvironmentString("code_verifier"));
-		call(condition(CreateS256CodeChallenge.class));
-		call(exec()
-			.exposeEnvironmentString("code_challenge")
-			.exposeEnvironmentString("code_challenge_method"));
-		call(condition(AddCodeChallengeToAuthorizationEndpointRequest.class)
-			.requirement("FAPI-1-5.2.2-7"));
-
 		callAndStopOnFailure(BuildPlainRedirectToAuthorizationEndpoint.class);
 
 		String redirectTo = env.getString("redirect_to_authorization_endpoint");
@@ -144,9 +101,6 @@ public class RejectCodeFlow extends AbstractTestModule {
 		browser.goToUrl(redirectTo);
 	}
 
-	/* (non-Javadoc)
-	 * @see io.fintechlabs.testframework.TestModule#handleHttp(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.http.HttpSession, org.springframework.util.MultiValueMap, org.springframework.ui.Model)
-	 */
 	@Override
 	public Object handleHttp(String path, HttpServletRequest req, HttpServletResponse res, HttpSession session, JsonObject requestParts) {
 		// dispatch based on the path
@@ -158,15 +112,6 @@ public class RejectCodeFlow extends AbstractTestModule {
 
 	}
 
-	/**
-	 * @param path
-	 * @param req
-	 * @param res
-	 * @param session
-	 * @param params
-	 * @param m
-	 * @return
-	 */
 	@UserFacing
 	private Object handleCallback(JsonObject requestParts) {
 
