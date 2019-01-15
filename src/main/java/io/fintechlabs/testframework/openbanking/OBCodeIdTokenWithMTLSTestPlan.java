ec64fefeae532f7ee5dd3f1b9a443b8e5ee8703d
VinhLe <quoc.vinhle@fintechlabs.io>
2019-01-15 15:00:28 +0700

    Create new tests ensure request object without scope fails
    We add the new test ensure request object without "scope" fails both private_key_jwt and mtls client authentication. We will not send "scope" in request object for the authorization flow. The expected result is failed for all of these tests.
    We will add these tests to test plans (ob-code-id-token-with-mtls and ob-code-id-token-with-private-key-and-matls).
    
    Close #375

--- src/main/java/io/fintechlabs/testframework/openbanking/OBCodeIdTokenWithMTLSTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/openbanking/OBCodeIdTokenWithMTLSTestPlan.java
@@ -23,6 +23,7 @@ import io.fintechlabs.testframework.plan.TestPlan;
 		"ob-user-rejects-authentication-code-id-token-with-mtls",
 		"ob-ensure-server-handles-non-matching-intent-id-code-id-token-with-mtls",
 		"ob-ensure-request-object-without-exp-fails-with-mtls",
+		"ob-ensure-request-object-without-scope-fails-with-mtls",
 	}
 )
 public class OBCodeIdTokenWithMTLSTestPlan implements TestPlan {
