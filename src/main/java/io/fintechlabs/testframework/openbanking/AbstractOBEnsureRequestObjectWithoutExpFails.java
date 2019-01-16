d4921ca958645308651b62b554b5aed8e098480e
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-01-16 13:37:51 +0000

    Create new tests ensure request object without exp fails
    We add the new test ensure request object without exp fails both private_key_jwt and mtls client authentication. We will not send "exp" in request object for the authorization flow. The expected result is failed for all of these tests.
    We will add these tests to test plans (ob-code-id-token-with-mtls and ob-code-id-token-with-private-key-and-matls).
    
    Close #373

---
+++ src/main/java/io/fintechlabs/testframework/openbanking/AbstractOBEnsureRequestObjectWithoutExpFails.java
@@ -0,0 +1,62 @@
+package io.fintechlabs.testframework.openbanking;
+
+import io.fintechlabs.testframework.condition.Condition;
+import io.fintechlabs.testframework.condition.client.BuildRequestObjectRedirectToAuthorizationEndpoint;
+import io.fintechlabs.testframework.condition.client.ConvertAuthorizationEndpointRequestToRequestObject;
+import io.fintechlabs.testframework.condition.client.EnsureInvalidRequestObjectError;
+import io.fintechlabs.testframework.condition.client.ExpectRequestObjectMissingExpClaimErrorPage;
+import io.fintechlabs.testframework.condition.client.SignRequestObject;
+import io.fintechlabs.testframework.condition.client.ValidateErrorResponseFromAuthorizationEndpoint;
+
+public abstract class AbstractOBEnsureRequestObjectWithoutExpFails extends AbstractOBServerTestModule {
+
+	@Override
+	protected void performAuthorizationFlow() {
+		performPreAuthorizationSteps();
+
+		createAuthorizationRequest();
+
+		createAuthorizationRedirect();
+
+		String redirectTo = env.getString("redirect_to_authorization_endpoint");
+
+		eventLog.log(getName(), args("msg", "Redirecting to authorization endpoint",
+			"redirect_to", redirectTo,
+			"http", "redirect"));
+
+		eventLog.log(getName(), "Redirecting to url " + redirectTo);
+
+		callAndStopOnFailure(ExpectRequestObjectMissingExpClaimErrorPage.class, "FAPI-RW-5.2.2-13");
+
+		setStatus(Status.WAITING);
+
+		waitForPlaceholders();
+
+		browser.goToUrl(redirectTo, env.getString("request_object_unverifiable_error"));
+	}
+
+	@Override
+	protected void createAuthorizationRedirect() {
+		callAndStopOnFailure(ConvertAuthorizationEndpointRequestToRequestObject.class);
+
+		callAndStopOnFailure(SignRequestObject.class);
+
+		callAndStopOnFailure(BuildRequestObjectRedirectToAuthorizationEndpoint.class);
+	}
+
+	@Override
+	protected void onAuthorizationCallbackResponse() {
+
+		// We now have callback_query_params and callback_params (containing the hash) available, as well as authorization_endpoint_response (which test conditions should use if they're looking for the response)
+
+		/* If we get an error back from the authorisation server:
+		 * - It must be a 'invalid_request_object' error
+		 * - It must have the correct state we supplied
+		 */
+
+		callAndContinueOnFailure(ValidateErrorResponseFromAuthorizationEndpoint.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
+		callAndContinueOnFailure(EnsureInvalidRequestObjectError.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
+		fireTestFinished();
+
+	}
+}
