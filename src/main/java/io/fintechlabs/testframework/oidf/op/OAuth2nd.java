1b05679444d8e92a0ab6ad2dfa59f873cfd52e4b
Stephen Moore <srmoore@gmail.com>
2019-01-24 12:21:29 -0500

    More first pass

--- src/main/java/io/fintechlabs/testframework/oidf/op/OAuth2nd.java
+++ src/main/java/io/fintechlabs/testframework/oidf/op/OAuth2nd.java
@@ -1,13 +1,19 @@
 package io.fintechlabs.testframework.oidf.op;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import io.fintechlabs.testframework.condition.common.CreateRandomImplicitSubmitUrl;
 import io.fintechlabs.testframework.sequence.ConditionSequence;
 import io.fintechlabs.testframework.sequence.client.CreateAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.sequence.client.LoadServerAndClientConfiguration;
+import io.fintechlabs.testframework.sequence.client.ProcessAuthorizationEndpointResponse;
 import io.fintechlabs.testframework.testmodule.AbstractTestModule;
 import io.fintechlabs.testframework.testmodule.ConditionCallBuilder;
 import io.fintechlabs.testframework.testmodule.HandleHttp;
+import io.fintechlabs.testframework.testmodule.UserFacing;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -57,10 +63,45 @@ public class OAuth2nd extends AbstractTestModule {
 	}
 
 	@HandleHttp("callback")
+	@UserFacing
 	public Object handleCallback(HttpServletRequest request, HttpServletResponse response, HttpSession session, JsonObject requestParts){
+		setStatus(Status.RUNNING);
 
+		env.putObject("callback_query_params", requestParts.get("params").getAsJsonObject());
 
-		return null;
+		call(condition(CreateRandomImplicitSubmitUrl.class));
+		setStatus(Status.WAITING);
+
+		return new ModelAndView("implicitCallback",
+			args("implicitSumbitUrl", env.getString("implicit_submit", "fullUrl"),
+				"returnUrl", "/log-detail.html?log=" + getId()));
+
+	}
+
+	@HandleHttp("implicit/**")
+	public Object handleImplicitCallback(HttpServletRequest request, HttpServletResponse response, HttpSession session, JsonObject requestParts){
+		getTestExecutionManager().runInBackground(() -> {
+			setStatus(Status.RUNNING);
+
+			JsonElement body = requestParts.get("body");
+
+			if (body != null) {
+				String hash = body.getAsString();
+
+				//logger.info("Hash: " + hash);
+
+				env.putString("implicit_hash", hash);
+			} else {
+				//logger.warn("No hash submitted");
+
+				env.putString("implicit_hash", ""); // Clear any old value
+			}
+
+			call(sequence(ProcessAuthorizationEndpointResponse.class));
+
+			return "done";
+		});
+		return ResponseEntity.noContent().build();
 	}
 
 }
---
+++ src/main/java/io/fintechlabs/testframework/sequence/client/ProcessAuthorizationEndpointResponse.java
@@ -0,0 +1,67 @@
+package io.fintechlabs.testframework.sequence.client;
+
+import io.fintechlabs.testframework.condition.Condition;
+import io.fintechlabs.testframework.condition.client.*;
+import io.fintechlabs.testframework.sequence.AbstractConditionSequence;
+
+public class ProcessAuthorizationEndpointResponse extends AbstractConditionSequence {
+	@Override
+	public void evaluate() {
+
+		call(exec().mapKey("callback_params", "implicit_callback_params"));
+
+		call(condition(ExtractImplicitHashToCallbackResponse.class));
+
+		// TODO: key for processing implicit parameters here; for now just unmap
+		call(exec().unmapKey("callback_params"));
+
+		call(condition(CheckIfAuthorizationEndpointError.class));
+
+		call(condition(CheckMatchingStateParameter.class));
+
+		call(condition(ExtractAuthorizationCodeFromAuthorizationResponse.class));
+
+		call(condition(CreateTokenEndpointRequestForAuthorizationCodeGrant.class));
+
+		if (hasAccessory("client_token_endpoint_authentication")) {
+			call(getAccessories("client_token_endpoint_authentication"));
+		} else {
+			call(condition(AddBasicAuthClientSecretAuthenticationParameters.class));
+		}
+
+		//runAccessory("client_token_endpoint_authenticaiton", condition(AddBasicAuthClientSecretAuthenticationParameters.class))
+
+		if (hasAccessory("token_endpoint_request")) {
+			call(getAccessories("token_endpoint_request"));
+		}
+
+		//runAccessory("token_endpoint_request");
+
+		call(condition(CallTokenEndpoint.class));
+
+		call(condition(CheckIfTokenEndpointResponseError.class));
+
+		call(condition(CheckForAccessTokenValue.class));
+
+		call(condition(ExtractAccessTokenFromTokenResponse.class));
+
+		call(condition(CheckForScopesInTokenResponse.class).dontStopOnFailure());
+
+		call(condition(ExtractIdTokenFromTokenResponse.class));
+
+		call(condition(ValidateIdToken.class));
+
+		call(condition(ValidateIdTokenSignature.class));
+
+		call(condition(CheckForSubjectInIdToken.class));
+
+		call(condition(CheckForRefreshTokenValue.class).dontStopOnFailure());
+
+		call(condition(EnsureMinimumTokenLength.class).dontStopOnFailure());
+
+		call(condition(EnsureMinimumTokenEntropy.class).dontStopOnFailure());
+
+
+
+	}
+}
