7c9731d526dd9b4a5098219978d459d1184f582d
VinhLe <quoc.vinhle@fintechlabs.io>
2019-02-18 18:08:11 +0700

    Try sending the different nonce inside and outside request object
    We added two new tests that send the different "nonce" to authorization server for private_key_jwt and mtls_client_auth authentication. The server have to show error (and conformance suite request image) or use the value inside the request object.
    
    Added these tests to test plans (fapi-ob-code-id-token-with-mtls-test-plan and fapi-ob-code-id-token-with-private-key-and-matls).
    
    closes #379

--- src/main/java/io/fintechlabs/testframework/openbanking/FAPIOBCodeIdTokenWithPrivateKeyAndMATLSTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/openbanking/FAPIOBCodeIdTokenWithPrivateKeyAndMATLSTestPlan.java
@@ -30,6 +30,7 @@ import io.fintechlabs.testframework.plan.TestPlan;
 		"fapi-ob-ensure-request-object-with-multiple-aud-succeeds-with-private-key-and-matls",
 		"fapi-ob-ensure-wrong-client-id-in-token-endpoint-fails-with-private-key-and-matls",
 		"fapi-ob-ensure-expired-request-object-fails-with-private-key-and-matls",
+		"fapi-ob-ensure-different-nonce-inside-and-outside-request-object-with-private-key-and-matls",
 	}
 )
 public class FAPIOBCodeIdTokenWithPrivateKeyAndMATLSTestPlan implements TestPlan {
