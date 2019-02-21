dcf43b5c029b33294e5a7b5eb4a0f7ce12b9b7f1
VinhLe <quoc.vinhle@fintechlabs.io>
2019-02-21 18:33:33 +0700

    Try sending response_type with only code fails
    Added the new test ensure send "response_type" with only code fails for private_key_jwt and mtls_client_auth authentication. We put only code into response type which is a parameter in the authorization request. The expected result is failed for the test.
    Added these tests to test plans (fapi-ob-code-id-token-with-mtls-test-plan and fapi-ob-code-id-token-with-private-key-and-matls).
    
    closes #384

---
+++ src/main/java/io/fintechlabs/testframework/openbanking/FAPIOBEnsureResponseTypeCodeFailsWithPrivateKeyAndMATLS.java
@@ -0,0 +1,63 @@
+package io.fintechlabs.testframework.openbanking;
+
+import io.fintechlabs.testframework.condition.client.AddClientAssertionToTokenEndpointRequest;
+import io.fintechlabs.testframework.condition.client.CreateClientAuthenticationAssertionClaims;
+import io.fintechlabs.testframework.condition.client.CreateTokenEndpointRequestForAuthorizationCodeGrant;
+import io.fintechlabs.testframework.condition.client.CreateTokenEndpointRequestForClientCredentialsGrant;
+import io.fintechlabs.testframework.condition.client.SetAccountScopeOnTokenEndpointRequest;
+import io.fintechlabs.testframework.condition.client.SignClientAuthenticationAssertion;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "fapi-ob-ensure-response-type-code-fails-with-private-key-and-matls",
+	displayName = "FAPI-OB: ensure response_type code fails (with private key authentication and MATLS)",
+	summary = "This test puts only code into response type which is a parameter in the authorization request. The authorization server should show an error message that the response type is unsupported (a screenshot of which should be uploaded) or with the user being redirected back to the conformance suite with a correct error response.",
+	profile = "FAPI-OB",
+	configurationFields = {
+		"server.discoveryUrl",
+		"client.client_id",
+		"client.scope",
+		"client.jwks",
+		"mtls.key",
+		"mtls.cert",
+		"mtls.ca",
+		"client2.client_id",
+		"client2.scope",
+		"client2.jwks",
+		"mtls2.key",
+		"mtls2.cert",
+		"mtls2.ca",
+		"resource.resourceUrl",
+		"resource.resourceUrlAccountRequests",
+		"resource.resourceUrlAccountsResource",
+		"resource.institution_id"
+	}
+)
+public class FAPIOBEnsureResponseTypeCodeFailsWithPrivateKeyAndMATLS extends AbstractFAPIOBEnsureResponseTypeCodeFails {
+
+	@Override
+	protected void createClientCredentialsRequest() {
+
+		callAndStopOnFailure(CreateTokenEndpointRequestForClientCredentialsGrant.class);
+		callAndStopOnFailure(SetAccountScopeOnTokenEndpointRequest.class);
+
+		callAndStopOnFailure(CreateClientAuthenticationAssertionClaims.class);
+
+		callAndStopOnFailure(SignClientAuthenticationAssertion.class);
+
+		callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
+	}
+
+	@Override
+	protected void createAuthorizationCodeRequest() {
+
+		callAndStopOnFailure(CreateTokenEndpointRequestForAuthorizationCodeGrant.class);
+
+		callAndStopOnFailure(CreateClientAuthenticationAssertionClaims.class);
+
+		callAndStopOnFailure(SignClientAuthenticationAssertion.class);
+
+		callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
+	}
+
+}
