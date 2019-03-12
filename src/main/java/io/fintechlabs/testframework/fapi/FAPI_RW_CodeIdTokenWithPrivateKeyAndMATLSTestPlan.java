4ee21088f8af0afd808ccfb13a3f62608ed1b6fa
VinhLe <quoc.vinhle@fintechlabs.io>
2019-03-12 17:26:20 +0700

    Add test that certificate access_token and authorization code
    Added FAPI-RW test that certificates access_token are implemented correctly and tries reusing an authorization code for private_key_jwt authentication.
    Added the test to test plans (fapi-rw-code-id-token-with-private-key-and-matls).
    
    part of #386

--- src/main/java/io/fintechlabs/testframework/fapi/FAPI_RW_CodeIdTokenWithPrivateKeyAndMATLSTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/fapi/FAPI_RW_CodeIdTokenWithPrivateKeyAndMATLSTestPlan.java
@@ -8,6 +8,7 @@ import io.fintechlabs.testframework.plan.TestPlan;
 	displayName = "FAPI-RW: code id_token with private key and matls Test Plan",
 	profile = "FAPI-RW",
 	testModuleNames = {
+		"fapi-rw-code-id-token-with-private-key-and-matls",
 		"fapi-rw-ensure-client-id-in-token-endpoint-with-private-key-and-matls",
 		"fapi-rw-ensure-request-object-without-exp-fails-with-private-key-and-matls",
 		"fapi-rw-ensure-request-object-without-scope-fails-with-private-key-and-matls",
