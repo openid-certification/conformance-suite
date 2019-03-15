1c12ca21ac8821a81a7cf21a671ce4a0b0544677
Stewart Lait <stewart@256kelvin.com>
2019-03-15 09:37:22 +0000

    Negative Test : Add multiple aud values to ID token Thus create a fail case to check that the client calls the token endpoint with multiple aud values. The client will error "missing required JWT property azp"
    Implement a runinbackground() call so that the test suite passes.
    
    Adheres to #419

---
+++ src/main/java/io/fintechlabs/testframework/condition/as/AddSecondAudValueToIdToken.java
@@ -0,0 +1,39 @@
+package io.fintechlabs.testframework.condition.as;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PostEnvironment;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class AddSecondAudValueToIdToken extends AbstractCondition {
+
+	public AddSecondAudValueToIdToken(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	@PreEnvironment(required = "id_token_claims")
+	@PostEnvironment(required = "id_token_claims")
+	public Environment evaluate(Environment env) {
+
+
+		String aud = env.getString("id_token_claims", "aud");
+
+		JsonArray audArray = new JsonArray();
+		audArray.add(aud);
+		audArray.add(aud + 1);
+
+		JsonObject claims = env.getObject("id_token_claims");
+		claims.add("aud", audArray);
+
+		logSuccess("Added a second, invalid aud value in ID token claims", args("id_token_claims", claims, "aud", audArray));
+
+		return env;
+
+	}
+
+}
---
+++ src/main/java/io/fintechlabs/testframework/openbanking/FAPIOBClientTestCodeIdTokenWithPrivateKeyJWTAndMATLSInvalidSecondaryAud.java
@@ -0,0 +1,36 @@
+package io.fintechlabs.testframework.openbanking;
+
+import io.fintechlabs.testframework.condition.ConditionError;
+import io.fintechlabs.testframework.condition.as.AddSecondAudValueToIdToken;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "fapi-ob-client-test-code-id-token-with-private-key-jwt-and-matls-invalid-secondary-aud",
+	displayName = "FAPI-OB: client test - multiple aud values in id_token from authorization_endpoint should be rejected (code id_token with private_key_jwt and MATLS)",
+	profile = "FAPI-OB",
+	configurationFields = {
+		"server.jwks",
+		"client.client_id",
+		"client.scope",
+		"client.redirect_uri",
+		"client.certificate",
+		"client.jwks",
+	}
+)
+
+public class FAPIOBClientTestCodeIdTokenWithPrivateKeyJWTAndMATLSInvalidSecondaryAud extends AbstractFAPIOBClientPrivateKeyExpectNothingAfterAuthorisationEndpoint {
+
+	@Override
+	protected void addCustomValuesToIdToken() {
+
+		callAndStopOnFailure(AddSecondAudValueToIdToken.class, "OIDCC-3.1.3.7.8");
+	}
+
+	@Override
+	protected Object authorizationCodeGrantType(String requestId) {
+
+		throw new ConditionError(getId(), "Client has incorrectly called token_endpoint after receiving an id_token with multiple aud values from the authorization_endpoint.");
+
+	}
+
+}
