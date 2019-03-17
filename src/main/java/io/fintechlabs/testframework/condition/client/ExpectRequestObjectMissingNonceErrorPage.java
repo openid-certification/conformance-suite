9fade0f389fd004c25282f727e2b447c95fd2aa1
Xuan Ext <xuan.nguyen@fintechlabs.io>
2019-03-18 02:43:17 +0700

    Update condition failure for the test without nonce fails
    Added new condition class EnsureInvalidRequestOrInvalidRequestObjectError to support two types of errors invalid_request and invalid_request_object.
    Replaced the call condition failure from EnsureInvalidRequestObjectError to EnsureInvalidRequestOrInvalidRequestObjectError in class AbstractFAPIRWEnsureRequestObjectWithoutNonceFails.
    Updated summary of publish test modules of test name "fapi-ob-ensure-request-object-without-nonce-fails-with-private-key-and-matls" and "fapi-ob-ensure-request-object-without-nonce-fails-with-mtls".
    Applied for fapi-rw and fapi-ob.
    
    closes #423

--- src/main/java/io/fintechlabs/testframework/condition/client/ExpectRequestObjectMissingNonceErrorPage.java
+++ src/main/java/io/fintechlabs/testframework/condition/client/ExpectRequestObjectMissingNonceErrorPage.java
@@ -15,7 +15,7 @@ public class ExpectRequestObjectMissingNonceErrorPage extends AbstractCondition 
 	@PostEnvironment(strings = "request_object_unverifiable_error")
 	public Environment evaluate(Environment env) {
 
-		String placeholder = createBrowserInteractionPlaceholder("If the server does not return an invalid_request_object error back to the client, it must show an error page saying the request object is invalid as it is missing the 'nonce' claim - upload a screenshot of the error page.");
+		String placeholder = createBrowserInteractionPlaceholder("If the server does not return an invalid_request or invalid_request_object error back to the client, it must show an error page saying the request object is invalid as it is missing the 'nonce' claim - upload a screenshot of the error page.");
 		env.putString("request_object_unverifiable_error", placeholder);
 
 		return env;
