ed865a64658aff3f7167914d52bcfb5823de510a
Stewart Lait <stewart@256kelvin.com>
2019-03-21 15:51:48 +0000

    Negative Test : Sign to ID token with null alg & alternate alg
    1. Sign the idToken from auth endpoint with null alg (create PlainJWT).
    2. If the signing alg of the idToken that originates from the first token endpoint is PS256, then modify to sign with RS256 in the auth endpoint.
    
    Thus create two  fail cases to check that the client receives an invalid id_token from the authorization endpoint, and hence shouldn't call the token endpoint. The client will error "unexpected algorithm received"
    
    Implement a runinbackground() call so that the test suite passes.
    
    Adheres to #419

---
+++ src/main/java/io/fintechlabs/testframework/openbanking/FAPIOBClientTestCodeIdTokenWithMATLSInvalidAlternateAlg.java
@@ -0,0 +1,41 @@
+package io.fintechlabs.testframework.openbanking;
+
+import io.fintechlabs.testframework.condition.ConditionError;
+import io.fintechlabs.testframework.condition.as.ForceIdTokenToBeSignedWithRS256;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "fapi-ob-client-test-code-id-token-with-matls-invalid-alternate-alg",
+	displayName = "FAPI-OB: client test - if the alg of id_token is PS256, then sign with RS256 in the authorization endpoint, should be rejected (code id_token with MATLS)",
+	profile = "FAPI-OB",
+	configurationFields = {
+		"server.jwks",
+		"client.client_id",
+		"client.scope",
+		"client.redirect_uri",
+		"client.certificate",
+		"client.jwks",
+	}
+)
+
+public class FAPIOBClientTestCodeIdTokenWithMATLSInvalidAlternateAlg extends AbstractFAPIOBClientMATLSExpectNothingAfterAuthorisationEndpoint {
+
+	@Override
+	protected void addCustomValuesToIdToken() {
+
+		//Do Nothing
+	}
+
+	@Override
+	protected void addCustomSignatureOfIdToken(){
+
+		callAndStopOnFailure(ForceIdTokenToBeSignedWithRS256.class,"OIDCC-3.1.3.7.7");
+	}
+
+	@Override
+	protected Object authorizationCodeGrantType(String requestId) {
+
+		throw new ConditionError(getId(), "Client has incorrectly called token_endpoint after receiving an id_token with an alg value of RS256 (it was originally PS256) from the authorization_endpoint.");
+	}
+
+}
