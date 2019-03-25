e1855c1d4d82ae53c7a8374a3a356d0f40e0c885
Xuan Ext <xuan.nguyen@fintechlabs.io>
2019-03-25 19:48:52 +0700

    Add an additional outcome to allow the authorization succeeds
    Modified onAuthorizationCallbackResponse to allow the authorization endpoint response without any errors in abstract class AbstractFAPIRWEnsureRequestObjectWithoutStateFails.
    Added the override method of performPostAuthorizationFlow() to allow s_hash not be present in the id_token in AbstractFAPIRWEnsureRequestObjectWithoutStateFails class.
    Removed the required "state" to allow state value can be missing from the authorization endpoint response in the CheckMatchingStateParameter condition.
    For these 4 tests:
    ・fapi-ob-ensure-request-object-without-state-fails-with-private-key-and-matls
    ・fapi-ob-ensure-request-object-without-state-fails-with-mtls
    ・fapi-rw-ensure-request-object-without-state-fails-with-private-key-and-matls
    ・fapi-rw-ensure-request-object-without-state-fails-with-mtls
    
    closes #422

--- src/main/java/io/fintechlabs/testframework/fapi/AbstractFAPIRWEnsureRequestObjectWithoutStateFails.java
+++ src/main/java/io/fintechlabs/testframework/fapi/AbstractFAPIRWEnsureRequestObjectWithoutStateFails.java
@@ -1,19 +1,39 @@
 package io.fintechlabs.testframework.fapi;
 
+import com.google.gson.JsonObject;
 import io.fintechlabs.testframework.condition.Condition;
 import io.fintechlabs.testframework.condition.client.AddExpToRequestObject;
 import io.fintechlabs.testframework.condition.client.AddNonceToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.AddStateToAuthorizationEndpointRequest;
 import io.fintechlabs.testframework.condition.client.BuildRequestObjectRedirectToAuthorizationEndpoint;
+import io.fintechlabs.testframework.condition.client.CallAccountsEndpointWithBearerToken;
+import io.fintechlabs.testframework.condition.client.CheckForSubjectInIdToken;
 import io.fintechlabs.testframework.condition.client.ConvertAuthorizationEndpointRequestToRequestObject;
 import io.fintechlabs.testframework.condition.client.CreateAuthorizationEndpointRequestFromClientInformation;
 import io.fintechlabs.testframework.condition.client.CreateRandomNonceValue;
 import io.fintechlabs.testframework.condition.client.CreateRandomStateValue;
+import io.fintechlabs.testframework.condition.client.DisallowAccessTokenInQuery;
 import io.fintechlabs.testframework.condition.client.EnsureInvalidRequestObjectError;
 import io.fintechlabs.testframework.condition.client.ExpectRequestObjectMissingStateErrorPage;
+import io.fintechlabs.testframework.condition.client.ExtractAtHash;
+import io.fintechlabs.testframework.condition.client.ExtractCHash;
+import io.fintechlabs.testframework.condition.client.ExtractIdTokenFromAuthorizationResponse;
+import io.fintechlabs.testframework.condition.client.ExtractSHash;
+import io.fintechlabs.testframework.condition.client.FAPIValidateIdTokenSigningAlg;
 import io.fintechlabs.testframework.condition.client.SetAuthorizationEndpointRequestResponseTypeToCodeIdtoken;
+import io.fintechlabs.testframework.condition.client.SetPermissiveAcceptHeaderForResourceEndpointRequest;
+import io.fintechlabs.testframework.condition.client.SetPlainJsonAcceptHeaderForResourceEndpointRequest;
 import io.fintechlabs.testframework.condition.client.SignRequestObject;
+import io.fintechlabs.testframework.condition.client.ValidateAtHash;
+import io.fintechlabs.testframework.condition.client.ValidateCHash;
 import io.fintechlabs.testframework.condition.client.ValidateErrorResponseFromAuthorizationEndpoint;
+import io.fintechlabs.testframework.condition.client.ValidateIdToken;
+import io.fintechlabs.testframework.condition.client.ValidateIdTokenNonce;
+import io.fintechlabs.testframework.condition.client.ValidateIdTokenSignature;
+import io.fintechlabs.testframework.condition.common.DisallowInsecureCipher;
+import io.fintechlabs.testframework.condition.common.DisallowTLS10;
+import io.fintechlabs.testframework.condition.common.DisallowTLS11;
+import io.fintechlabs.testframework.condition.common.EnsureTLS12;
 
 public abstract class AbstractFAPIRWEnsureRequestObjectWithoutStateFails extends AbstractFAPIRWServerTestModule {
 
@@ -72,14 +92,91 @@ public abstract class AbstractFAPIRWEnsureRequestObjectWithoutStateFails extends
 	@Override
 	protected void onAuthorizationCallbackResponse() {
 		// We now have callback_query_params and callback_params (containing the hash) available, as well as authorization_endpoint_response (which test conditions should use if they're looking for the response)
+		JsonObject callbackParams = env.getObject("authorization_endpoint_response");
 
-		/* If we get an error back from the authorisation server:
-		 * - It must be a 'invalid_request_object' error
-		 * - It must have the correct state we supplied
-		 */
+		if (!callbackParams.has("error")) {
 
-		callAndContinueOnFailure(ValidateErrorResponseFromAuthorizationEndpoint.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
-		callAndContinueOnFailure(EnsureInvalidRequestObjectError.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
+			super.onAuthorizationCallbackResponse();
+
+		} else {
+			/* If we get an error back from the authorisation server:
+			 * - It must be a 'invalid_request_object' error
+			 * - It must have the correct state we supplied
+			 */
+
+			callAndContinueOnFailure(ValidateErrorResponseFromAuthorizationEndpoint.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
+			callAndContinueOnFailure(EnsureInvalidRequestObjectError.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.2.6");
+
+			fireTestFinished();
+		}
+	}
+
+	@Override
+	protected void performPostAuthorizationFlow() {
+		callAndStopOnFailure(ExtractIdTokenFromAuthorizationResponse.class, "FAPI-RW-5.2.2-3");
+
+		callAndStopOnFailure(ValidateIdToken.class, "FAPI-RW-5.2.2-3");
+
+		callAndStopOnFailure(ValidateIdTokenNonce.class, "OIDCC-2");
+
+		performProfileIdTokenValidation();
+
+		callAndStopOnFailure(ValidateIdTokenSignature.class, "FAPI-RW-5.2.2-3");
+
+		callAndStopOnFailure(CheckForSubjectInIdToken.class, "FAPI-R-5.2.2-24", "OB-5.2.2-8");
+		callAndContinueOnFailure(FAPIValidateIdTokenSigningAlg.class, Condition.ConditionResult.WARNING, "FAPI-RW-8.6");
+
+		// s_hash are optional.
+		callAndContinueOnFailure(ExtractSHash.class, Condition.ConditionResult.INFO, "FAPI-RW-5.2.2-4");
+
+		callAndContinueOnFailure(ExtractCHash.class, Condition.ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+
+		skipIfMissing(new String[]{"c_hash"}, null, Condition.ConditionResult.INFO,
+			ValidateCHash.class, Condition.ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+
+		callAndContinueOnFailure(ExtractAtHash.class, Condition.ConditionResult.INFO, "OIDCC-3.3.2.11");
+
+		skipIfMissing(new String[]{"at_hash"}, null, Condition.ConditionResult.INFO,
+			ValidateAtHash.class, Condition.ConditionResult.FAILURE, "OIDCC-3.3.2.11");
+
+		// call the token endpoint and complete the flow
+
+		createAuthorizationCodeRequest();
+
+		requestAuthorizationCode();
+
+		eventLog.startBlock("Accounts request endpoint TLS test");
+		env.mapKey("tls", "accounts_request_endpoint_tls");
+		callAndContinueOnFailure(EnsureTLS12.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+		callAndContinueOnFailure(DisallowTLS10.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+		callAndContinueOnFailure(DisallowTLS11.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+
+		callAndContinueOnFailure(DisallowInsecureCipher.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-1");
+		eventLog.endBlock();
+
+
+		eventLog.startBlock("Accounts resource endpoint TLS test");
+		env.mapKey("tls", "accounts_resource_endpoint_tls");
+		callAndContinueOnFailure(EnsureTLS12.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+		callAndContinueOnFailure(DisallowTLS10.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+		callAndContinueOnFailure(DisallowTLS11.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-2");
+
+		callAndContinueOnFailure(DisallowInsecureCipher.class, Condition.ConditionResult.FAILURE, "FAPI-RW-8.5-1");
+		env.unmapKey("tls");
+		eventLog.endBlock();
+
+		requestProtectedResource();
+
+		callAndContinueOnFailure(DisallowAccessTokenInQuery.class, Condition.ConditionResult.FAILURE, "FAPI-R-6.2.1-4");
+
+		callAndStopOnFailure(SetPlainJsonAcceptHeaderForResourceEndpointRequest.class);
+
+		callAndStopOnFailure(CallAccountsEndpointWithBearerToken.class, "RFC7231-5.3.2");
+
+		callAndStopOnFailure(SetPermissiveAcceptHeaderForResourceEndpointRequest.class);
+
+		callAndContinueOnFailure(CallAccountsEndpointWithBearerToken.class, Condition.ConditionResult.FAILURE, "RFC7231-5.3.2");
+
 		fireTestFinished();
 	}
 
