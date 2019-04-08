12dd66bd8a4dd0f10a6dfeaaed73b84f0a411aea
Stewart Lait <stewart@256kelvin.com>
2019-04-08 10:46:22 +0100

    Negative Test : Add expired iat value ID token Add expired iat value to id_token. Thus create a fail case to check that the client receives an invalid id_token from the authorization endpoint, and hence shouldn't call the token endpoint.
    The client will error;
    
    For expired iat: "iat is too far in the past".
    
    Requires sample-openbanking-client-nodejs!12
    
    The conformance-suite should also be able to handle a client that does not verify the status of the iat value, hence callAndStopOnFailure(AddInvalidExpiredIatValueToIdToken.class, ConditionResult.WARNING). Alerting the user that an invalid iat value was created in the id_token and the client allows for it to continue with the authorization flow.
    
    https://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation - Section 3.1.3.7-10
    
    Relates to #419
    
    Additionally, create missing FAPI-RW exp test modules:
    
    *  "fapi-rw-id2-client-test-with-mtls-holder-of-key-invalid-missing-exp"
    *  "fapi-rw-id2-client-test-with-mtls-holder-of-key-invalid-expired-exp"

---
+++ src/main/java/io/fintechlabs/testframework/condition/as/ClientContinuedAfterReceivingIdTokenIssuedInPast.java
@@ -0,0 +1,21 @@
+package io.fintechlabs.testframework.condition.as;
+
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class ClientContinuedAfterReceivingIdTokenIssuedInPast extends AbstractCondition {
+
+	public ClientContinuedAfterReceivingIdTokenIssuedInPast(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	public Environment evaluate(Environment env) {
+
+		logFailure("Client has incorrectly called token_endpoint after receiving an id_token with an expired iat value from the authorization_endpoint.");
+
+		return env;
+	}
+
+}
---
+++ src/main/java/io/fintechlabs/testframework/fapi/FAPIRWID2ClientTestWithMTLSHolderOfKeyInvalidExpiredIat.java
@@ -0,0 +1,42 @@
+package io.fintechlabs.testframework.fapi;
+
+import io.fintechlabs.testframework.condition.Condition.ConditionResult;
+import io.fintechlabs.testframework.condition.as.AddInvalidExpiredIatValueToIdToken;
+import io.fintechlabs.testframework.condition.as.ClientContinuedAfterReceivingIdTokenIssuedInPast;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@PublishTestModule(
+	testName = "fapi-rw-id2-client-test-with-mtls-holder-of-key-invalid-expired-iat",
+	displayName = "FAPI-RW-ID2: client test - expired iat value in id_token from authorization_endpoint, should be rejected (code id_token with MTLS)",
+	summary = "This test should end with the client displaying an error message that the iat value in the id_token (from the authorization_endpoint) has expired (in the request object)",
+	profile = "FAPI-RW-ID2",
+	configurationFields = {
+		"server.jwks",
+		"client.client_id",
+		"client.scope",
+		"client.redirect_uri",
+		"client.certificate",
+		"client.jwks",
+	}
+)
+
+public class FAPIRWID2ClientTestWithMTLSHolderOfKeyInvalidExpiredIat extends AbstractFAPIRWID2ClientExpectNothingAfterAuthorisationEndpoint {
+
+	@Override
+	protected void addCustomValuesToIdToken() {
+
+		callAndStopOnFailure(AddInvalidExpiredIatValueToIdToken.class, "OIDCC-3.1.3.7-10");
+	}
+
+	@Override
+	protected Object authorizationCodeGrantType(String requestId) {
+
+		callAndContinueOnFailure(ClientContinuedAfterReceivingIdTokenIssuedInPast.class, ConditionResult.WARNING);
+		setStatus(Status.WAITING);
+		fireTestFinished();
+		return new ResponseEntity<Object>("Client has incorrectly called token_endpoint after receiving an id_token with an expired iat value from the authorization_endpoint.", HttpStatus.BAD_REQUEST);
+	}
+
+}
