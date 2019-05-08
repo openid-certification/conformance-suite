8e06a3216009fffd01e4ba17e0f95e71f7a38f84
Nhat Le <nhat.le@fintechlabs.io>
2019-05-08 14:01:09 +0700

    Create new FAPI-CIBA test module to test 'iat' value is a week in the past
    Created new FAPI-CIBA test module to test 'iat' value is a week in the past with Ping mode.
    Created new FAPI-CIBA test module to test 'iat' value is a week in the past with Poll mode.
    
    part of #447

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/AddIatValueIsWeekInPastToRequestObject.java
@@ -0,0 +1,37 @@
+package io.fintechlabs.testframework.condition.client;
+
+import com.google.gson.JsonObject;
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PostEnvironment;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+import java.time.Instant;
+
+public class AddIatValueIsWeekInPastToRequestObject extends AbstractCondition {
+
+	public AddIatValueIsWeekInPastToRequestObject(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	@PreEnvironment(required = "request_object_claims")
+	@PostEnvironment(required = "request_object_claims")
+	public Environment evaluate(Environment env) {
+
+		JsonObject requestObjectClaims = env.getObject("request_object_claims");
+
+		Instant iat = Instant.now().minusSeconds(60 * 60 * 24 * 7);
+
+		requestObjectClaims.addProperty("iat", iat.getEpochSecond());
+
+		env.putObject("request_object_claims", requestObjectClaims);
+
+		logSuccess("Added expired iat value to request object which is a week in the past", args("request_object_claims", requestObjectClaims, "iat_is_week_in_the_past", iat));
+
+		return env;
+
+	}
+
+}
---
+++ src/test/java/io/fintechlabs/testframework/condition/client/AddIatValueIsWeekInPastToRequestObject_UnitTest.java
@@ -0,0 +1,62 @@
+package io.fintechlabs.testframework.condition.client;
+
+import com.google.gson.JsonObject;
+import io.fintechlabs.testframework.condition.Condition;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddIatValueIsWeekInPastToRequestObject_UnitTest {
+
+	@Spy
+	private Environment env = new Environment();
+
+	@Mock
+	private TestInstanceEventLog eventLog;
+
+	private AddIatValueIsWeekInPastToRequestObject cond;
+
+	@Before
+	public void setUp() throws Exception {
+		cond = new AddIatValueIsWeekInPastToRequestObject("UNIT-TEST", eventLog, Condition.ConditionResult.INFO);
+	}
+
+	@Test
+	public void testEvaluate_presentIatValue() {
+
+		JsonObject requestObjectClaims = new JsonObject();
+
+		env.putObject("request_object_claims", requestObjectClaims);
+
+		cond.evaluate(env);
+
+		assertThat(env.getObject("request_object_claims").has("iat")).isTrue();
+
+	}
+
+	@Test
+	public void testEvaluate_iatValueIsOneWeekInThePast() {
+
+		long iatExpect = 60 * 60 * 24 * 7;
+
+		JsonObject requestObjectClaims = new JsonObject();
+
+		env.putObject("request_object_claims", requestObjectClaims);
+
+		cond.evaluate(env);
+
+		assertThat(Instant.now().getEpochSecond() - env.getLong("request_object_claims", "iat")).isEqualTo(iatExpect);
+
+	}
+
+}
