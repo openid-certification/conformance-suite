b1738d5f9cbd884b06c70a334a98da6dd33da44d
Nhat Le <nhat.le@fintechlabs.io>
2019-05-08 16:56:51 +0700

    Create new FAPI-CIBA test module to test 'exp' value is a year in the future
    Created new FAPI-CIBA test module to test 'exp' value is a year in the future with Ping mode.
    Created new FAPI-CIBA test module to test 'exp' value is a year in the future with Poll mode.
    
    closes #447

--- src/main/java/io/fintechlabs/testframework/fapiciba/FAPICIBAPingWithMTLSTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/fapiciba/FAPICIBAPingWithMTLSTestPlan.java
@@ -16,6 +16,7 @@ import io.fintechlabs.testframework.plan.TestPlan;
 		"fapi-ciba-ping-with-mtls-ensure-request-object-bad-iss-fails",
 		"fapi-ciba-ping-with-mtls-ensure-request-object-missing-exp-fails",
 		"fapi-ciba-ping-with-mtls-ensure-request-object-expired-exp-fails",
+		"fapi-ciba-ping-with-mtls-ensure-request-object-exp-is-year-in-future-fails",
 		"fapi-ciba-ping-with-mtls-ensure-request-object-missing-iat-fails",
 		"fapi-ciba-ping-with-mtls-ensure-request-object-iat-is-week-in-past-fails",
 		"fapi-ciba-ping-with-mtls-ensure-request-object-iat-is-hour-in-future-fails",
---
+++ src/main/java/io/fintechlabs/testframework/fapiciba/FAPICIBAPollWithMTLSEnsureRequestObjectExpIsYearInFutureFails.java
@@ -0,0 +1,59 @@
+package io.fintechlabs.testframework.fapiciba;
+
+import io.fintechlabs.testframework.condition.Condition;
+import io.fintechlabs.testframework.condition.client.AddExpValueIsYearInFutureToRequestObject;
+import io.fintechlabs.testframework.condition.client.EnsureErrorFromBackchannelAuthenticationEndpointResponse;
+import io.fintechlabs.testframework.condition.client.EnsureInvalidRequestErrorBackchannelAuthenticationEndpointResponse;
+import io.fintechlabs.testframework.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "fapi-ciba-poll-with-mtls-ensure-request-object-exp-is-year-in-future-fails",
+	displayName = "FAPI-CIBA: Poll mode - 'exp' value in request object is a year in the future, should return an error (MTLS client authentication)",
+	summary = "This test should return an error that the 'exp' value in request object from back channel authentication endpoint request is a year in the future",
+	profile = "FAPI-CIBA",
+	configurationFields = {
+		"server.discoveryUrl",
+		"client.client_id",
+		"client.scope",
+		"client.jwks",
+		"client.hint_type",
+		"client.hint_value",
+		"mtls.key",
+		"mtls.cert",
+		"mtls.ca",
+		"client2.client_id",
+		"client2.scope",
+		"client2.jwks",
+		"mtls2.key",
+		"mtls2.cert",
+		"mtls2.ca",
+		"resource.resourceUrl"
+	}
+)
+public class FAPICIBAPollWithMTLSEnsureRequestObjectExpIsYearInFutureFails extends AbstractFAPICIBAWithMTLSEnsureRequestObjectFails {
+
+	@Override
+	protected void buildRequestObject() {
+
+		super.buildRequestObject();
+
+		callAndStopOnFailure(AddExpValueIsYearInFutureToRequestObject.class, "CIBA-7.1.1");
+
+	}
+
+	@Override
+	protected void onCallBackChannelAuthenticationEndpointResponse() {
+
+		callAndContinueOnFailure(EnsureErrorFromBackchannelAuthenticationEndpointResponse.class, Condition.ConditionResult.FAILURE);
+
+		callAndContinueOnFailure(EnsureInvalidRequestErrorBackchannelAuthenticationEndpointResponse.class, Condition.ConditionResult.FAILURE);
+
+		fireTestFinished();
+
+	}
+
+	@Override
+	protected void modeSpecificAuthorizationEndpointRequest() {
+		/* Nothing to do */
+	}
+}
