c03f114e9bf28b2d41b5cd18788df237860f185e
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-05-11 17:43:37 +0100

    Fix for generated PKCE code verifiers being invalid
    The + and = characters are being used and aren't permitted,
    and several permitted characters aren't being used.
    
    This has the side effect of logging the actual verifier string
    instead of the binary sequence of bytes.
    
    closes #468

--- src/main/java/io/fintechlabs/testframework/condition/client/CreateRandomCodeVerifier.java
+++ src/main/java/io/fintechlabs/testframework/condition/client/CreateRandomCodeVerifier.java
@@ -4,17 +4,12 @@ import io.fintechlabs.testframework.condition.AbstractCondition;
 import io.fintechlabs.testframework.condition.PostEnvironment;
 import io.fintechlabs.testframework.logging.TestInstanceEventLog;
 import io.fintechlabs.testframework.testmodule.Environment;
-
+import org.apache.commons.text.RandomStringGenerator;
 
 import java.security.SecureRandom;
-import java.util.Base64;
 
 public class CreateRandomCodeVerifier extends AbstractCondition {
 
-	/**
-	 * @param testId
-	 * @param log
-	 */
 	public CreateRandomCodeVerifier(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
 		super(testId, log, conditionResultOnFailure, requirements);
 	}
@@ -22,15 +17,36 @@ public class CreateRandomCodeVerifier extends AbstractCondition {
 	@Override
 	@PostEnvironment(strings = "code_verifier")
 	public Environment evaluate(Environment env) {
-		SecureRandom sr = new SecureRandom();
-		byte[] code = new byte[32];
 
-		sr.nextBytes(code);
-		String verifier = Base64.getEncoder().encodeToString(code);
+		// https://tools.ietf.org/html/rfc7636#section-4.1
+		//
+		// code_verifier = high-entropy cryptographic random STRING using the
+		// unreserved characters [A-Z] / [a-z] / [0-9] / "-" / "." / "_" / "~"
+		// from Section 2.3 of [RFC3986], with a minimum length of 43 characters
+		// and a maximum length of 128 characters.
+
+		char [][] pairs = {
+			{'A','Z'},
+			{'a','z'},
+			{'0','9'},
+			{'-','-'},
+			{'.','.'},
+			{'_','_'},
+			{'~','~'},
+		};
+		// In a real client base64url of SecureRandom would be fine/simpler; we don't
+		// use it here as it would not include the . and ~ characters and we want to
+		// test the full range of permitted characters
+		final SecureRandom rand = new SecureRandom();
+		RandomStringGenerator generator = new RandomStringGenerator.Builder()
+			.usingRandom(rand::nextInt)
+			.withinRange(pairs).build();
+		// test maximum permitted length
+		String verifier = generator.generate(128);
 
 		env.putString("code_verifier", verifier);
 
-		log("Created code_verifier value", args("code_verifier", code));
+		log("Created code_verifier value", args("code_verifier", verifier));
 
 		return env;
 	}
