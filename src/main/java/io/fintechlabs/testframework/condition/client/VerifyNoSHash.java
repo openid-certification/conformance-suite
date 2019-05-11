ee253adabcb3e1d448418d352b8cb7fcf55e62ab
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-05-11 19:28:31 +0100

    'Request Object Without State' tests must not have s_hash
    These tests pass a state outside of the request object, so
    the authorization server must ignore it and not return an
    s_hash.
    
    Noticed whilst fixing #462

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/VerifyNoSHash.java
@@ -0,0 +1,28 @@
+package io.fintechlabs.testframework.condition.client;
+
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PreEnvironment;
+import io.fintechlabs.testframework.logging.TestInstanceEventLog;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class VerifyNoSHash extends AbstractCondition {
+
+	public VerifyNoSHash(String testId, TestInstanceEventLog log, ConditionResult conditionResultOnFailure, String... requirements) {
+		super(testId, log, conditionResultOnFailure, requirements);
+	}
+
+	@Override
+	@PreEnvironment(required = "id_token")
+	public Environment evaluate(Environment env) {
+
+		String hash = env.getString("id_token", "claims.s_hash");
+		if (hash != null) {
+			throw error("s_hash has been returned in ID token, when it shouldn't have been");
+		}
+
+		logSuccess("ID Token is correctly missing s_hash");
+
+		return env;
+	}
+
+}
