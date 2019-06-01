0dba1276b4f208d6969e9f618e3e2b857ba59892
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-06-01 12:44:56 +0100

    Add the callAnd* helpers for sequences
    This makes it easy to transplant code to/from test modules
    without having to rewrite it.

--- src/main/java/io/fintechlabs/testframework/sequence/AbstractConditionSequence.java
+++ src/main/java/io/fintechlabs/testframework/sequence/AbstractConditionSequence.java
@@ -151,4 +151,49 @@ public abstract class AbstractConditionSequence implements ConditionSequence, Da
 		}
 	}
 
+	/**
+	 * Create and evaluate a Condition in the current environment. Throw a @TestFailureException if the Condition fails.
+	 *
+	 * onFail is set to FAILURE
+	 *
+	 */
+	protected void callAndStopOnFailure(Class<? extends Condition> conditionClass, String... requirements) {
+		call(condition(conditionClass)
+			.onFail(Condition.ConditionResult.FAILURE)
+			.requirements(requirements));
+	}
+
+	/**
+	 * Create and evaluate a Condition in the current environment. Throw a @TestFailureException if the Condition fails.
+	 */
+	protected void callAndStopOnFailure(Class<? extends Condition> conditionClass, Condition.ConditionResult onFail, String... requirements) {
+		call(condition(conditionClass)
+			.requirements(requirements)
+			.onFail(onFail));
+	}
+
+	/**
+	 * Create and evaluate a Condition in the current environment. Log but ignore if the Condition fails.
+	 *
+	 * onFail is set to INFO if requirements is null or empty, WARNING if requirements are specified
+	 *
+	 */
+	protected void callAndContinueOnFailure(Class<? extends Condition> conditionClass, String... requirements) {
+		call(condition(conditionClass)
+			.onFail((requirements == null || requirements.length == 0) ? Condition.ConditionResult.INFO : Condition.ConditionResult.WARNING)
+			.requirements(requirements)
+			.dontStopOnFailure());
+	}
+
+	/**
+	 * Create and evaluate a Condition in the current environment. Log but ignore if the Condition fails.
+	 *
+	 */
+	protected void callAndContinueOnFailure(Class<? extends Condition> conditionClass, Condition.ConditionResult onFail, String... requirements) {
+		call(condition(conditionClass)
+			.requirements(requirements)
+			.onFail(onFail)
+			.dontStopOnFailure());
+	}
+
 }
