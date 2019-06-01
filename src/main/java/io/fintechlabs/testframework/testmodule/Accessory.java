f5bcd51f6cc17720f305897d00789a5de94f7b14
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-06-01 10:49:26 +0100

    Add comments to Accessory

--- src/main/java/io/fintechlabs/testframework/testmodule/Accessory.java
+++ src/main/java/io/fintechlabs/testframework/testmodule/Accessory.java
@@ -6,8 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import io.fintechlabs.testframework.sequence.ConditionSequence;
 
 /**
- * @author jricher
+ * Provide a replacement for an existing sequence
  *
+ * An accessory annotation is used inside a @Variant and causes a runAccessory() call within that test module to
+ * run an alternative sequence.
+ *
+ * @see Variant
+ *
+ * @see io.fintechlabs.testframework.sequence.AbstractConditionSequence#runAccessory
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Accessory {
--- src/main/java/io/fintechlabs/testframework/testmodule/Variant.java
+++ src/main/java/io/fintechlabs/testframework/testmodule/Variant.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * @author jricher
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
