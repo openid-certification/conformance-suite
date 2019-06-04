35a0eb1c6af3cade9de018ac0eaf0130e86d2600
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-06-04 18:28:59 +0100

    CIBA: Refactor binding message test
    It can just use super then add the binding message.

--- src/main/java/io/fintechlabs/testframework/fapiciba/AbstractFAPICIBAEnsureAuthorizationRequestWithBindingMessageSucceedsWithMTLS.java
+++ src/main/java/io/fintechlabs/testframework/fapiciba/AbstractFAPICIBAEnsureAuthorizationRequestWithBindingMessageSucceedsWithMTLS.java
@@ -9,19 +9,9 @@ public abstract class AbstractFAPICIBAEnsureAuthorizationRequestWithBindingMessa
 
 	@Override
 	protected void createAuthorizationRequest() {
-		callAndStopOnFailure(CreateEmptyAuthorizationEndpointRequest.class);
-		callAndStopOnFailure(AddScopeToAuthorizationEndpointRequest.class, "CIBA-7.1");
-		callAndStopOnFailure(AddHintToAuthorizationEndpointRequest.class, "CIBA-7.1");
+		super.createAuthorizationRequest();
 
-		// The spec also defines these parameters that we don't currently set:
-		// acr_values
-		// user_code
-		// requested_expiry
 		callAndStopOnFailure(AddBindingMessageToAuthorizationEndpointRequest.class, "CIBA-7.1");
-
-		modeSpecificAuthorizationEndpointRequest();
-
-		performProfileAuthorizationEndpointSetup();
 	}
 
 }
