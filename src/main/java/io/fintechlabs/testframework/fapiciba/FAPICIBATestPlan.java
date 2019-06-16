da5796ee5d8e1ca3e916f53611de2243f809385f
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-06-16 09:55:40 +0900

    Add all tests into the variant-based ciba test plan
    Many of these tests are still not expanded to cope with all
    variants, but the current code just skips them.

--- src/main/java/io/fintechlabs/testframework/fapiciba/FAPICIBATestPlan.java
+++ src/main/java/io/fintechlabs/testframework/fapiciba/FAPICIBATestPlan.java
@@ -8,11 +8,53 @@ import io.fintechlabs.testframework.plan.TestPlan;
 	displayName = "FAPI-CIBA: test plan",
 	profile = "FAPI-CIBA",
 	testModules = {
+		// Normal well behaved client cases
 		FAPICIBADiscoveryEndpointVerification.class,
 		FAPICIBA.class,
-		FAPICIBAAuthReqIdExpired.class,
 		FAPICIBAUserRejectsAuthentication.class,
-//		FAPICIBAPollEnsureAuthorizationRequestWithBindingMessageSucceeds.class,
+		FAPICIBAPollMultipleCallToTokenEndpointWithMTLS.class,
+		FAPICIBAAuthReqIdExpired.class,
+		FAPICIBAPollEnsureAuthorizationRequestWithBindingMessageSucceeds.class,
+
+		// Possible failure case
+		FAPICIBAPollEnsureAuthorizationRequestWithPotentiallyBadBindingMessageWithMTLS.class,
+
+		// Negative tests for request objects
+		FAPICIBAPollWithMTLSEnsureRequestObjectMissingAudFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectBadAudFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectMissingIssFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectBadIssFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectMissingExpFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectExpiredExpFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectExpIsYearInFutureFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectMissingIatFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectIatIsWeekInPastFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectIatIsHourInFutureFails.class,
+		FAPICIBAPollWithMTLSEnsureRequestObjectMissingJtiFails.class,
+		FAPICIBAPollEnsureRequestObjectSignatureAlgorithmIsNoneFailsWithMTLS.class,
+		FAPICIBAPollEnsureRequestObjectSignatureAlgorithmIsBadFailsWithMTLS.class,
+		FAPICIBAPollEnsureRequestObjectSignatureAlgorithmIsRS256FailsWithMTLS.class,
+		FAPICIBAPollEnsureRequestObjectSignedByOtherClientFailsWithMTLS.class,
+
+		// Negative tests for hints
+		FAPICIBAPollEnsureAuthorizationRequestWithMultipleHintsFailsWithMTLS.class,
+
+		// Negative tests for token endpoint
+		FAPICIBAPollEnsureWrongAuthenticationRequestIdInTokenEndpointRequestWithMTLS.class,
+
+		// MTLS specific tests - not possible to test with private_key_jwt
+		FAPICIBAPollEnsureDifferentClientIdAndIssuerInBackchannelAuthorizationRequestWithMTLS.class,
+		FAPICIBAPollEnsureWrongClientIdInTokenEndpointRequestWithMTLS.class,
+		FAPICIBAPollEnsureWrongClientIdInBackchannelAuthorizationRequestWithMTLS.class,
+
+		// Ping specific tests, not applicable to poll
+		FAPICIBAPingNotificationEndpointReturnsABody.class,
+		FAPICIBAPingNotificationEndpointReturns401.class,
+		FAPICIBAPingNotificationEndpointReturns403.class,
+
+		// FAPI specific
+		FAPICIBAPollEnsureBackchannelAuthorizationRequestWithoutRequestFailsWithMTLS.class,
+
 	},
 	variants = {
 		FAPICIBA.variant_ping_mtls,
