70b107ba9eafe0e69c8dfb46c54b5322948e6718
Nhat Le <nhat.le@fintechlabs.io>
2019-06-17 17:53:35 +0700

    Group all the tests of FAPI-RW-ID2 and FAPI-RW-ID2-OB test plans
    Group and order the tests of all 4 fapi test plan is consistent (fapi-rw-id2 mtls, fapi-rw-id2 private key, fapi-rw-id2-ob mtls & fapi-rw-id2-ob private key).
    
    Closes #488

--- src/main/java/io/fintechlabs/testframework/fapi/FAPI_RW_ID2_WithPrivateKeyAndMTLSHolderOfKeyTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/fapi/FAPI_RW_ID2_WithPrivateKeyAndMTLSHolderOfKeyTestPlan.java
@@ -8,32 +8,43 @@ import io.fintechlabs.testframework.plan.TestPlan;
 	displayName = "FAPI-RW-ID2: Authorization server test using private_key_jwt client authentication",
 	profile = "FAPI-RW-ID2-OpenID-Provider-Authorization-Server-Test",
 	testModuleNames = {
+		// Normal well behaved client cases
 		"fapi-rw-id2-discovery-end-point-verification",
 		"fapi-rw-id2-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-client-id-in-token-endpoint-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-user-rejects-authentication-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-request-object-with-multiple-aud-succeeds-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-authorization-request-without-state-success-with-private-key-and-mtls-holder-of-key",
+
+		// Possible failure case
+		"fapi-rw-id2-ensure-response-mode-query-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-different-nonce-inside-and-outside-request-object-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-registered-redirect-uri-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for request objects
 		"fapi-rw-id2-ensure-request-object-without-exp-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-request-object-without-scope-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-request-object-without-state-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-request-object-without-nonce-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-request-object-without-redirect-uri-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-request-object-with-multiple-aud-succeeds-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-expired-request-object-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-different-nonce-inside-and-outside-request-object-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-response-type-code-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-request-object-with-bad-aud-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-mtls-holder-of-key-required-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-matching-key-in-authorization-request-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-redirect-uri-in-authorization-request-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-authorization-code-is-bound-to-client-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-registered-redirect-uri-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-request-object-signature-algorithm-is-not-none-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-user-rejects-authentication-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-signed-client-assertion-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-signed-request-object-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-request-object-signature-algorithm-is-not-none-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-matching-key-in-authorization-request-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for authorization request
 		"fapi-rw-id2-ensure-authorization-request-without-request-object-fails-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-redirect-uri-in-authorization-request-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-response-type-code-fails-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for token endpoint
+		"fapi-rw-id2-ensure-client-id-in-token-endpoint-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-mtls-holder-of-key-required-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ensure-authorization-code-is-bound-to-client-with-private-key-and-mtls-holder-of-key",
+
+		// Private key specific tests
+		"fapi-rw-id2-ensure-signed-client-assertion-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ensure-client-assertion-in-token-endpoint-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-response-mode-query-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ensure-authorization-request-without-state-success-with-private-key-and-mtls-holder-of-key",
 	}
 )
 public class FAPI_RW_ID2_WithPrivateKeyAndMTLSHolderOfKeyTestPlan implements TestPlan {
--- src/main/java/io/fintechlabs/testframework/openbanking/FAPIRWID2OBWithPrivateKeyAndMTLSHolderOfKeyTestPlan.java
+++ src/main/java/io/fintechlabs/testframework/openbanking/FAPIRWID2OBWithPrivateKeyAndMTLSHolderOfKeyTestPlan.java
@@ -8,33 +8,46 @@ import io.fintechlabs.testframework.plan.TestPlan;
 	displayName = "FAPI-RW-ID2-OpenBankingUK: Authorization server test using private_key_jwt client authentication",
 	profile = "FAPI-RW-ID2-OpenBankingUK-OpenID-Provider-Authorization-Server-Test",
 	testModuleNames = {
+		// Normal well behaved client cases
 		"fapi-rw-id2-ob-discovery-end-point-verification",
 		"fapi-rw-id2-ob-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-mtls-holder-of-key-required-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-matching-key-in-authorization-request-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-redirect-uri-in-authorization-request-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-authorization-code-is-bound-to-client-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-registered-redirect-uri-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-request-object-signature-algorithm-is-not-none-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-user-rejects-authentication-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-server-handles-non-matching-intent-id-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-request-object-with-multiple-aud-succeeds-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-authorization-request-without-state-success-with-private-key-and-mtls-holder-of-key",
+
+		// Possible failure case
+		"fapi-rw-id2-ob-ensure-response-mode-query-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-different-nonce-inside-and-outside-request-object-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-registered-redirect-uri-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for request objects
 		"fapi-rw-id2-ob-ensure-request-object-without-exp-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-request-object-without-scope-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-request-object-without-state-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-request-object-without-nonce-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-request-object-without-redirect-uri-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-request-object-with-multiple-aud-succeeds-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-wrong-client-id-in-token-endpoint-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-expired-request-object-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-different-nonce-inside-and-outside-request-object-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-response-type-code-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-request-object-with-bad-aud-fails-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-signed-client-assertion-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-signed-request-object-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-request-object-signature-algorithm-is-not-none-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-matching-key-in-authorization-request-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for authorization request
 		"fapi-rw-id2-ob-ensure-authorization-request-without-request-object-fails-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-redirect-uri-in-authorization-request-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-response-type-code-fails-with-private-key-and-mtls-holder-of-key",
+
+		// Negative tests for token endpoint
+		"fapi-rw-id2-ob-ensure-wrong-client-id-in-token-endpoint-fails-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-mtls-holder-of-key-required-with-private-key-and-mtls-holder-of-key",
+		"fapi-rw-id2-ob-ensure-authorization-code-is-bound-to-client-with-private-key-and-mtls-holder-of-key",
+
+		// Private key specific tests
+		"fapi-rw-id2-ob-ensure-signed-client-assertion-with-RS256-fails-with-private-key-and-mtls-holder-of-key",
 		"fapi-rw-id2-ob-ensure-client-assertion-in-token-endpoint-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-response-mode-query-with-private-key-and-mtls-holder-of-key",
-		"fapi-rw-id2-ob-ensure-authorization-request-without-state-success-with-private-key-and-mtls-holder-of-key",
+
+		// OB systems specific tests
+		"fapi-rw-id2-ob-ensure-server-handles-non-matching-intent-id-with-private-key-and-mtls-holder-of-key",
 	}
 )
 public class FAPIRWID2OBWithPrivateKeyAndMTLSHolderOfKeyTestPlan implements TestPlan {
