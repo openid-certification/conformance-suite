9dfa33acdf0a98325096e36ad32f7c5d43b5027b
Serkan Özkan <serkan@sozkan.com>
2019-07-01 22:57:27 +0300

    Allow one of invalid_request, invalid_grant or invalid_client
    Fixes #519

--- src/main/java/io/fintechlabs/testframework/fapi/FAPIRWID2EnsureMTLSHolderOfKeyRequiredWithPrivateKeyAndMTLSHolderOfKey.java
+++ src/main/java/io/fintechlabs/testframework/fapi/FAPIRWID2EnsureMTLSHolderOfKeyRequiredWithPrivateKeyAndMTLSHolderOfKey.java
@@ -1,9 +1,19 @@
 package io.fintechlabs.testframework.fapi;
 
+import io.fintechlabs.testframework.condition.Condition;
 import io.fintechlabs.testframework.condition.client.AddClientAssertionToTokenEndpointRequest;
+import io.fintechlabs.testframework.condition.client.CallTokenEndpointAllowingTLSFailure;
+import io.fintechlabs.testframework.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidClient;
+import io.fintechlabs.testframework.condition.client.CheckTokenEndpointHttpStatus401;
+import io.fintechlabs.testframework.condition.client.CheckTokenEndpointReturnedInvalidClientGrantOrRequestError;
+import io.fintechlabs.testframework.condition.client.CheckTokenEndpointReturnedJsonContentType;
 import io.fintechlabs.testframework.condition.client.CreateClientAuthenticationAssertionClaims;
 import io.fintechlabs.testframework.condition.client.CreateTokenEndpointRequestForAuthorizationCodeGrant;
+import io.fintechlabs.testframework.condition.client.RemoveMTLSCertificates;
 import io.fintechlabs.testframework.condition.client.SignClientAuthenticationAssertion;
+import io.fintechlabs.testframework.condition.client.ValidateErrorDescriptionFromTokenEndpointResponseError;
+import io.fintechlabs.testframework.condition.client.ValidateErrorFromTokenEndpointResponseError;
+import io.fintechlabs.testframework.condition.client.ValidateErrorUriFromTokenEndpointResponseError;
 import io.fintechlabs.testframework.testmodule.PublishTestModule;
 
 @PublishTestModule(
@@ -40,6 +50,30 @@ public class FAPIRWID2EnsureMTLSHolderOfKeyRequiredWithPrivateKeyAndMTLSHolderOf
 		callAndStopOnFailure(SignClientAuthenticationAssertion.class);
 
 		callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
+	}
+
+	@Override
+	protected void performPostAuthorizationFlow() {
+		createAuthorizationCodeRequest();
+
+		callAndStopOnFailure(RemoveMTLSCertificates.class);
+
+		callAndContinueOnFailure(CallTokenEndpointAllowingTLSFailure.class, Condition.ConditionResult.FAILURE,  "FAPI-RW-5.2.2-6");
+		boolean sslError = env.getBoolean("token_endpoint_response_ssl_error");
+		if (sslError) {
+			// the ssl connection was dropped; that's an acceptable way for a server to indicate that a TLS client cert
+			// is required, so there's no further checks to do
+		} else {
+			// otherwise we expect one of invalid_request, invalid_grant or invalid_client
+			callAndContinueOnFailure(CheckTokenEndpointReturnedInvalidClientGrantOrRequestError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+
+			callAndContinueOnFailure(CheckTokenEndpointReturnedJsonContentType.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.3.4");
+			callAndContinueOnFailure(ValidateErrorFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+			callAndContinueOnFailure(ValidateErrorDescriptionFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+			callAndContinueOnFailure(ValidateErrorUriFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+		}
+
+		fireTestFinished();
 	}
 
 }
