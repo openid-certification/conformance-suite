c24d7360544cc9ba02c985be57377fa49d33d73c
Lien Hong Vu <fapi.lienhv@gmail.com>
2019-07-08 15:08:20 +0000

    Permit user for uploading a screenshot
    - Permit user for uploading a screenshot in case AS only shows error instead of returning error back to redirect url
    - Apply for test fapi-rw-id2-ob-ensure-server-handles-non-matching-intent-id-with-mtls and fapi-rw-id2-ob-ensure-server-handles-non-matching-intent-id-with-private-key-and-mtls-holder-of-key
    
    Closes #547

---
+++ src/main/java/io/fintechlabs/testframework/condition/client/ExpectInvalidRequestInvalidRequestObjectOrAccessDeniedErrorPage.java
@@ -0,0 +1,18 @@
+package io.fintechlabs.testframework.condition.client;
+
+import io.fintechlabs.testframework.condition.AbstractCondition;
+import io.fintechlabs.testframework.condition.PostEnvironment;
+import io.fintechlabs.testframework.testmodule.Environment;
+
+public class ExpectInvalidRequestInvalidRequestObjectOrAccessDeniedErrorPage extends AbstractCondition {
+
+	@Override
+	@PostEnvironment(strings = "request_unverifiable_error")
+	public Environment evaluate(Environment env) {
+
+		String placeholder = createBrowserInteractionPlaceholder("If the server does not return an invalid_request or invalid_request_object or access_denied error back to the client, it must show an error page saying the request is invalid as it is missing the request_object or invalid_request_object or access_denied - upload a screenshot of the error page.");
+		env.putString("request_unverifiable_error", placeholder);
+
+		return env;
+	}
+}
