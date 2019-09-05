6df1dd8667ac5b6d4ce2b307fd53ed85f89403b9
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-09-05 11:30:29 +0100

    CheckForAccessTokenValue: make errors better
    There's no point telling the user "x or y might be wrong" when we can
    easily tell them which is wrong.

--- src/main/java/io/fintechlabs/testframework/condition/client/CheckForAccessTokenValue.java
+++ src/main/java/io/fintechlabs/testframework/condition/client/CheckForAccessTokenValue.java
@@ -11,17 +11,16 @@ public class CheckForAccessTokenValue extends AbstractCondition {
 	@Override
 	@PreEnvironment(required = "token_endpoint_response")
 	public Environment evaluate(Environment env) {
-		if (!Strings.isNullOrEmpty(env.getString("token_endpoint_response", "access_token"))
-			&& !Strings.isNullOrEmpty(env.getString("token_endpoint_response", "token_type"))) {
-
-			logSuccess("Found an access token",
-				args("access_token", env.getString("token_endpoint_response", "access_token")));
-			return env;
-
-		} else {
-			throw error("Couldn't find required access token or token_type");
+		if (Strings.isNullOrEmpty(env.getString("token_endpoint_response", "access_token"))) {
+			throw error("access_token is missing or empty in token endpoint response");
+		}
+		if (Strings.isNullOrEmpty(env.getString("token_endpoint_response", "token_type"))) {
+			throw error("token_type is missing or empty in token endpoint response");
 		}
 
+		logSuccess("Found an access token",
+				args("access_token", env.getString("token_endpoint_response", "access_token")));
+		return env;
 	}
 
 }
