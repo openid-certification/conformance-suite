362058f70f345f57e01137b25d8e4e4ad2b44a45
Leo Uino <leo.uino@fintechlabs.io>
2019-12-10 15:03:13 +0900

    OIDCC discovery endpoint tests

---
+++ src/main/java/net/openid/conformance/condition/client/OIDCCCheckDiscEndpointClaimsSupported.java
@@ -0,0 +1,23 @@
+package net.openid.conformance.condition.client;
+
+import java.util.Arrays;
+
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class OIDCCCheckDiscEndpointClaimsSupported extends ValidateJsonArray {
+
+	private static final String environmentVariable = "claims_supported";
+
+	private static final String[] SET_VALUES = new String[] { };
+	private static final int minimumMatchesRequired = SET_VALUES.length;
+
+	private static final String errorMessageNotEnough = "The server does not support enough of the required claims.";
+
+	@Override
+	@PreEnvironment(required = "server")
+	public Environment evaluate(Environment env) {
+
+		return validate(env, environmentVariable, Arrays.asList(SET_VALUES), minimumMatchesRequired, errorMessageNotEnough);
+	}
+}
---
+++ src/main/java/net/openid/conformance/openid/OIDCCDiscoveryEndpointVerification.java
@@ -0,0 +1,126 @@
+package net.openid.conformance.openid;
+
+import com.google.gson.JsonObject;
+
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.CheckDiscEndpointAuthorizationEndpoint;
+import net.openid.conformance.condition.client.CheckDiscEndpointClaimsParameterSupported;
+import net.openid.conformance.condition.client.CheckDiscEndpointDiscoveryUrl;
+import net.openid.conformance.condition.client.CheckDiscEndpointIssuer;
+import net.openid.conformance.condition.client.CheckDiscEndpointRegistrationEndpoint;
+import net.openid.conformance.condition.client.CheckDiscEndpointRequestParameterSupported;
+import net.openid.conformance.condition.client.CheckDiscEndpointRequestUriParameterSupported;
+import net.openid.conformance.condition.client.CheckDiscEndpointTokenEndpoint;
+import net.openid.conformance.condition.client.CheckDiscEndpointUserinfoEndpoint;
+import net.openid.conformance.condition.client.CheckDiscEndpointUserinfoSigningAlgValuesSupported;
+import net.openid.conformance.condition.client.CheckJwksUri;
+import net.openid.conformance.condition.client.GetDynamicServerConfiguration;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointClaimsSupported;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointGrantTypesSupported;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointIdTokenSigningAlgValuesSupported;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointRequestObjectSigningAlgValuesSupported;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointResponseTypesSupported;
+import net.openid.conformance.condition.client.OIDCCCheckDiscEndpointScopesSupported;
+import net.openid.conformance.testmodule.AbstractTestModule;
+import net.openid.conformance.testmodule.PublishTestModule;
+
+// Corresponds to OP-Discovery-* tests
+@PublishTestModule(
+	testName = "oidcc-discovery-endpoint-verification",
+	displayName = "OIDCC: Discovery Endpoint Verification",
+	summary = "This test ensures that the server's configurations (including scopes, response_types, grant_types etc) is containing the required value in the specification",
+	profile = "OIDCC",
+	configurationFields = {
+		"server.discoveryUrl",
+	}
+)
+public class OIDCCDiscoveryEndpointVerification extends AbstractTestModule {
+
+	@Override
+	public void configure(JsonObject config, String baseUrl, String externalUrlOverride) {
+
+		env.putString("base_url", baseUrl);
+		env.putObject("config", config);
+
+		// Includes check-http-response assertion (OIDC test)
+		callAndStopOnFailure(GetDynamicServerConfiguration.class);
+
+		setStatus(Status.CONFIGURED);
+		fireSetupDone();
+	}
+
+	@Override
+	public void start() {
+
+		setStatus(Status.RUNNING);
+
+		performEndpointVerification();
+
+		fireTestFinished();
+	}
+
+	protected void performEndpointVerification() {
+
+		callAndContinueOnFailure(OIDCCCheckDiscEndpointResponseTypesSupported.class, Condition.ConditionResult.FAILURE, "OIDCD-3");
+
+		callAndContinueOnFailure(CheckDiscEndpointDiscoveryUrl.class,Condition.ConditionResult.FAILURE);
+		callAndContinueOnFailure(CheckDiscEndpointIssuer.class, Condition.ConditionResult.FAILURE, "OIDCD-4.3", "OIDCD-7.2");
+
+		// Includes verify-id_token_signing-algorithm-is-supported assertion (OIDC test)
+		callAndContinueOnFailure(OIDCCCheckDiscEndpointIdTokenSigningAlgValuesSupported.class, Condition.ConditionResult.FAILURE, "OIDCD-3");
+
+		call(condition(CheckDiscEndpointUserinfoSigningAlgValuesSupported.class)
+			.skipIfElementMissing("server", "userinfo_signing_alg_values_supported")
+			.onFail(Condition.ConditionResult.FAILURE)
+			.onSkip(Condition.ConditionResult.INFO)
+			.requirement("FAPI-RW-8.6")
+			.dontStopOnFailure()
+		);
+
+		// Includes verify-op-endpoints-use-https assertion (OIDC test) for each endpoint tested
+		callAndContinueOnFailure(CheckDiscEndpointAuthorizationEndpoint.class, Condition.ConditionResult.FAILURE, "OIDCD-3");
+		callAndContinueOnFailure(CheckDiscEndpointTokenEndpoint.class, Condition.ConditionResult.FAILURE, "OIDCD-3");
+		call(condition(CheckDiscEndpointUserinfoEndpoint.class)
+			.skipIfElementMissing("server", "userinfo_endpoint")
+			.onFail(Condition.ConditionResult.FAILURE)
+			.onSkip(Condition.ConditionResult.INFO)
+			.requirement("OIDCD-3")
+			.dontStopOnFailure());
+		call(condition(CheckDiscEndpointRegistrationEndpoint.class)
+			.skipIfElementMissing("server", "registration_endpoint")
+			.onFail(Condition.ConditionResult.FAILURE)
+			.onSkip(Condition.ConditionResult.INFO)
+			.requirement("OIDCD-3")
+			.dontStopOnFailure());
+
+		// Includes profiderinfo-has-jwks_uri
+		callAndContinueOnFailure(CheckJwksUri.class, Condition.ConditionResult.FAILURE, "OIDCD-3");
+
+		callAndContinueOnFailure(CheckDiscEndpointRequestParameterSupported.class, Condition.ConditionResult.INFO);
+		callAndContinueOnFailure(CheckDiscEndpointRequestUriParameterSupported.class, Condition.ConditionResult.INFO);
+		call(condition(OIDCCCheckDiscEndpointRequestObjectSigningAlgValuesSupported.class)
+				.skipIfElementMissing("server", "request_object_signing_alg_values_supported")
+				.onFail(Condition.ConditionResult.WARNING)
+				.onSkip(Condition.ConditionResult.INFO)
+				.requirement("OIDCD-3")
+				.dontStopOnFailure());
+
+		callAndContinueOnFailure(CheckDiscEndpointClaimsParameterSupported.class, Condition.ConditionResult.INFO, "OIDCD-3");
+
+		// Includes providerinfo-has-claims_supported assertion (OIDC test)
+		callAndContinueOnFailure(OIDCCCheckDiscEndpointClaimsSupported.class, Condition.ConditionResult.WARNING, "OIDCD-3");
+		call(condition(OIDCCCheckDiscEndpointGrantTypesSupported.class)
+				.skipIfElementMissing("server", "grant_types_supported")
+				.onFail(Condition.ConditionResult.FAILURE)
+				.onSkip(Condition.ConditionResult.INFO)
+				.requirement("OIDCD-3")
+				.dontStopOnFailure());
+		call(condition(OIDCCCheckDiscEndpointScopesSupported.class)
+				.skipIfElementMissing("server", "scopes_supported")
+				.onFail(Condition.ConditionResult.FAILURE)
+				.onSkip(Condition.ConditionResult.WARNING)
+				.requirement("OIDCD-3")
+				.dontStopOnFailure());
+	}
+
+}
