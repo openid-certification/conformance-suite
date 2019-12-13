402152774561628454649c626fb7bc42665ed03e
Leo Uino <leo.uino@fintechlabs.io>
2019-12-13 11:07:51 +0900

    Comment for OIDCCEnsureRequestObjectWithRedirectUri

--- src/main/java/net/openid/conformance/openid/OIDCCEnsureRequestObjectWithRedirectUri.java
+++ src/main/java/net/openid/conformance/openid/OIDCCEnsureRequestObjectWithRedirectUri.java
@@ -4,6 +4,7 @@ import net.openid.conformance.condition.client.AddInvalidRedirectUriToAuthorizat
 import net.openid.conformance.condition.client.ConvertAuthorizationEndpointRequestToRequestObject;
 import net.openid.conformance.testmodule.PublishTestModule;
 
+// This does not correspond to a particular OIDC python test
 @PublishTestModule(
 	testName = "oidcc-ensure-request-object-with-redirect-uri",
 	displayName = "OIDCC: ensure request object redirect_uri takes precedence",
