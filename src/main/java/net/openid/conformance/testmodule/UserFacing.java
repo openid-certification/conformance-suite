4a4615bce9ca42c87030d806095c5b87a7915526
Joseph Heenan <joseph.heenan@fintechlabs.io>
2019-12-28 11:29:45 +0000

    Add documentation for @UserFacing annotation
    It's not entirely obvious so should have documentation.

--- src/main/java/net/openid/conformance/testmodule/UserFacing.java
+++ src/main/java/net/openid/conformance/testmodule/UserFacing.java
@@ -5,6 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate a method as 'user facing'.
+ *
+ * This only makes a difference when handling exceptions or test errors thrown from incoming http to test modules; if
+ * any method (from the test module) in the call stack is marked as user facing the suite will redirect the user to
+ * log-detail.html where they should be able to see the failure.
+ *
+ * This is relatively rare and is only the case where the test module is returning html that might be displayed in the
+ * user's web browser.
+ *
+ * If the method is not user facing, a HTTP 500 error containing a payload detailing the exception will be returned.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserFacing {
