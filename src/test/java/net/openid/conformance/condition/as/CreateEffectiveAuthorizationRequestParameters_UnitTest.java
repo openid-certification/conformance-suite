2b8dc602846742115451f13d9e12a7dbbefa3de0
Serkan Özkan <serkan@sozkan.com>
2020-01-08 13:28:22 +0100

    Add testEvaluate_maxAgeNullInRequestObjectNotNullInHttpRequest
    Tests CreateEffectiveAuthorizationRequestParameters max_age handling
    when max_age has a value in http request parameters but is json null in
    request object
    
    RelatedTo #701

--- src/test/java/net/openid/conformance/condition/as/CreateEffectiveAuthorizationRequestParameters_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/as/CreateEffectiveAuthorizationRequestParameters_UnitTest.java
@@ -69,6 +69,29 @@ public class CreateEffectiveAuthorizationRequestParameters_UnitTest {
 	}
 
 	@Test
+	public void testEvaluate_maxAgeNullInRequestObjectNotNullInHttpRequest() {
+		JsonObject httpReqParams = new JsonObject();
+		httpReqParams.addProperty("max_age", 123);
+		httpReqParams.addProperty("p2", "234");
+		httpReqParams.addProperty("p1", "345");
+		env.putObject("authorization_endpoint_http_request_params", httpReqParams);
+
+		JsonObject requestObject = new JsonObject();
+		JsonObject requestObjectClaims = new JsonObject();
+		requestObjectClaims.add("max_age", JsonNull.INSTANCE);
+
+		requestObject.add("claims", requestObjectClaims);
+		env.putObject("authorization_request_object", requestObject);
+		cond.execute(env);
+
+		JsonObject result = env.getObject(CreateEffectiveAuthorizationRequestParameters.ENV_KEY);
+		//when max_age is json null in request object, even when it's non-null in http request
+		//CreateEffectiveAuthorizationRequestParameters will ignore/remove it
+		httpReqParams.remove("max_age");
+		assertEquals(httpReqParams, result);
+	}
+
+	@Test
 	public void testEvaluate_withRequestObject() {
 		JsonObject httpReqParams = new JsonObject();
 		httpReqParams.addProperty("p1", "123");
