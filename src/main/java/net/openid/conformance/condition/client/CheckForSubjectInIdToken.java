d014d163c14a11092cc58dcf5236623e2945cadc
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-01-08 22:26:28 +0900

    Add length/content check to CheckForSubjectInIdToken

--- src/main/java/net/openid/conformance/condition/client/CheckForSubjectInIdToken.java
+++ src/main/java/net/openid/conformance/condition/client/CheckForSubjectInIdToken.java
@@ -1,7 +1,6 @@
 package net.openid.conformance.condition.client;
 
 import com.google.common.base.Strings;
-
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 
@@ -11,12 +10,28 @@ public class CheckForSubjectInIdToken extends AbstractCondition {
 
 		String sub = env.getString("id_token", "claims.sub");
 
-		if (!Strings.isNullOrEmpty(sub)) {
-			logSuccess("Found 'sub' in id_token", args("sub", sub));
-			return env;
-		} else {
+		if (Strings.isNullOrEmpty(sub)) {
 			throw error("id_token does not contain 'sub'");
 		}
+
+		// As per https://openid.net/specs/openid-connect-core-1_0.html#IDToken :
+		// "It MUST NOT exceed 255 ASCII characters in length."
+		if (sub.length() > 255) {
+			throw error("id_token 'sub' exceeds 255 ASCII characters", args("sub", sub));
+		}
+
+		for (int i = 0; i < sub.length(); i++) {
+			int c = sub.charAt(i);
+			if (c < 0x20) {
+				throw error(String.format("id_token 'sub' contains non-printable character 0x%02x at offset %d", c, i), args("sub", sub));
+			}
+			if (c >= 0x7F) {
+				throw error(String.format("id_token 'sub' contains non-ASCII character 0x%02x at offset %d", c, i), args("sub", sub));
+			}
+		}
+
+		logSuccess("Found 'sub' in id_token", args("sub", sub));
+		return env;
 	}
 
 }
