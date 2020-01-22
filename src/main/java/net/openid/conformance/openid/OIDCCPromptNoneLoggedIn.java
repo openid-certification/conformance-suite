04a6247423151a398020608e3471b42c1b341c7d
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-01-22 10:53:17 +0900

    Refactor OIDCCPromptNoneLoggedIn to create an abstract superclass
    It has behaviour the same as the OP-Req-id_token_hint so create a class
    we can reuse for that.

--- src/main/java/net/openid/conformance/openid/OIDCCPromptNoneLoggedIn.java
+++ src/main/java/net/openid/conformance/openid/OIDCCPromptNoneLoggedIn.java
@@ -1,9 +1,6 @@
 package net.openid.conformance.openid;
 
-import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.client.AddPromptNoneToAuthorizationEndpointRequest;
-import net.openid.conformance.condition.client.CheckIdTokenAuthTimeClaimsSameIfPresent;
-import net.openid.conformance.condition.client.CheckIdTokenSubConsistentForSecondAuthorization;
 import net.openid.conformance.testmodule.PublishTestModule;
 
 // Corresponds to https://github.com/rohe/oidctest/blob/master/test_tool/cp/test_op/flows/OP-prompt-none-LoggedIn.json
@@ -16,49 +13,13 @@ import net.openid.conformance.testmodule.PublishTestModule;
 			"server.discoveryUrl"
 	}
 )
-public class OIDCCPromptNoneLoggedIn extends AbstractOIDCCServerTest {
-	private boolean firstTime = true;
+public class OIDCCPromptNoneLoggedIn extends AbstractOIDCCSameAuthTwiceServerTest {
 
 	@Override
-	protected String currentClientString() {
-		return firstTime ? "" : "Second authorization: ";
+	protected void createSecondAuthorizationRequest() {
+		// with prompt=none this time
+		call(new CreateAuthorizationRequestSteps()
+			.then(condition(AddPromptNoneToAuthorizationEndpointRequest.class).requirements("OIDCC-3.1.2.1", "OIDCC-15.1")));
 	}
 
-	@Override
-	protected void createAuthorizationRequest() {
-		if (firstTime) {
-			// capture id_token from first authentication for later comparison (we don't care if it's from
-			// the authorization endpoint or the token endpoint)
-			env.mapKey("id_token", "first_id_token");
-			super.createAuthorizationRequest();
-		} else {
-			env.unmapKey("id_token");
-			call(new CreateAuthorizationRequestSteps()
-				.then(condition(AddPromptNoneToAuthorizationEndpointRequest.class).requirements("OIDCC-3.1.2.1", "OIDCC-15.1")));
-		}
-	}
-
-	protected void onPostAuthorizationFlowComplete() {
-		if (firstTime) {
-			firstTime = false;
-			// do the process again, but with prompt=none this time
-			performAuthorizationFlow();
-		} else {
-			// these two checks are equivalent to same-authn, https://github.com/rohe/oidctest/blob/a306ff8ccd02da456192b595cf48ab5dcfd3d15a/src/oidctest/op/check.py#L1117
-
-			// this check only works if the server actually returns auth_time; it might be better to explicitly request auth_time
-			// we could also time how long the second authorization endpoint call takes; it should really only take seconds as it
-			// should just redirect straight back instantly.
-			callAndContinueOnFailure(CheckIdTokenAuthTimeClaimsSameIfPresent.class, Condition.ConditionResult.FAILURE, "OIDCC-2");
-			callAndContinueOnFailure(CheckIdTokenSubConsistentForSecondAuthorization.class, Condition.ConditionResult.FAILURE, "OIDCC-2");
-
-			fireTestFinished();
-		}
-	}
-
-	@Override
-	public void cleanup() {
-		firstTime = true; // to avoid any blocks created in cleanup being prefixed in currentClientString()
-		super.cleanup();
-	}
 }
