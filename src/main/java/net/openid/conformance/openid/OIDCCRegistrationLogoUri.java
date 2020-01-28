60bda50e0f6d785510979c31aa14ae436caa3b5e
Leo Uino <leo.uino@fintechlabs.io>
2020-01-28 17:00:15 +0900

    Extend comment for login page tests to explain the need to clear cookies and take a screenshot

--- src/main/java/net/openid/conformance/openid/OIDCCRegistrationLogoUri.java
+++ src/main/java/net/openid/conformance/openid/OIDCCRegistrationLogoUri.java
@@ -11,7 +11,7 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "oidcc-registration-logo-uri",
 	displayName = "OIDCC: dynamic registration with Logo URI",
-	summary = "This test calls the dynamic registration endpoint with a logo URI. This should result in the browser being redirected to a login page with the RP logo displayed.",
+	summary = "This test calls the dynamic registration endpoint with a logo URI. This should result in the browser being redirected to a login page with the RP logo displayed. To make sure you get a fresh login page, you need to remove any cookies you may have received from the OP before proceeding. A screenshot of the login page should be uploaded.",
 	profile = "OIDCC",
 	configurationFields = {
 		"server.discoveryUrl"
