df6d9ebe29846dbd468ecc9f0f250c931756c521
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-02-03 19:02:32 +0000

    Refactor OIDCCEnsureRequestWithoutNonceFails
    It now overrides a slightly later place so it doesn't need to make the
    assumption that the response is returned in the query, an assumption
    that breaks once form_post support is added

--- src/main/java/net/openid/conformance/openid/OIDCCEnsureRequestWithoutNonceFails.java
+++ src/main/java/net/openid/conformance/openid/OIDCCEnsureRequestWithoutNonceFails.java
@@ -35,17 +35,12 @@ public class OIDCCEnsureRequestWithoutNonceFails extends AbstractOIDCCServerTest
 	}
 
 	@Override
-	protected void processCallback() {
-		eventLog.startBlock(currentClientString() + "Verify authorization endpoint response");
-
-		env.mapKey("authorization_endpoint_response", "callback_params");
-
+	protected void onAuthorizationCallbackResponse() {
 		performGenericAuthorizationEndpointErrorResponseValidation();
 		callAndContinueOnFailure(CheckErrorFromAuthorizationEndpointErrorInvalidRequest.class,
-				Condition.ConditionResult.FAILURE,
-				"OIDCC-3.2.2.1");
-
-		eventLog.endBlock();
+			Condition.ConditionResult.FAILURE,
+			"OIDCC-3.2.2.1");
 		fireTestFinished();
 	}
+
 }
