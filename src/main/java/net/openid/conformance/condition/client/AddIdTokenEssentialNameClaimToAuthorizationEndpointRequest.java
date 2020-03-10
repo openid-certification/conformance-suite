cbb2faa06fa30503c0bf58120db36fa6f8b93584
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-03-10 08:36:51 +0000

    Remove incorrect comment
    As per https://gitlab.com/openid/conformance-suite/-/merge_requests/873#note_301938757

--- src/main/java/net/openid/conformance/condition/client/AddIdTokenEssentialNameClaimToAuthorizationEndpointRequest.java
+++ src/main/java/net/openid/conformance/condition/client/AddIdTokenEssentialNameClaimToAuthorizationEndpointRequest.java
@@ -10,20 +10,6 @@ public class AddIdTokenEssentialNameClaimToAuthorizationEndpointRequest extends 
 	@PreEnvironment(required = "authorization_endpoint_request")
 	@PostEnvironment(required = "authorization_endpoint_request")
 	public Environment evaluate(Environment env) {
-		/*
-		The python suite sends:
-
-		https://fapidev-www.authlete.net/api/authorization?state=9d4Uoc5cCHR3nVLC&nonce=ilTAjEj9bdysSoSX&response_type=code&scope=openid&redirect_uri=https%3A%2F%2Fop.certification.openid.net%3A61757%2Fauthz_cb&claims=%7B%22userinfo%22%3A+%7B%22name%22%3A+%7B%22essential%22%3A+true%7D%7D%7D&client_id=138292314413510
-
-		or the decoded claims value:
-
-        "userinfo": {
-            "name": {
-                "essential": true
-            }
-        }
-
-		 */
 
 		return addClaim(env, LocationToRequestClaim.ID_TOKEN, "name", null, true);
 
