12baf3dfa03f6cd513982ec64386cc979a838634
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-03-17 17:50:52 +0000

    Prevent 'prompt' parameter being duplicated unnecessarily
    This was added here:
    
    https://gitlab.com/openid/conformance-suite/-/merge_requests/578/diffs#008d49298b1bfd775fd1f5e6ccd397548cf03fb8_26_26
    
    for reasons that aren't clear. I think it was probably a mistake and
    hence have removed it, and more clearly documented why these values
    are here.

--- src/main/java/net/openid/conformance/condition/client/BuildRequestObjectRedirectToAuthorizationEndpoint.java
+++ src/main/java/net/openid/conformance/condition/client/BuildRequestObjectRedirectToAuthorizationEndpoint.java
@@ -1,29 +1,48 @@
 package net.openid.conformance.condition.client;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.google.gson.JsonElement;
-import net.openid.conformance.testmodule.OIDFJSON;
-import net.openid.conformance.testmodule.Environment;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import com.google.common.base.Strings;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
-
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.testmodule.OIDFJSON;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BuildRequestObjectRedirectToAuthorizationEndpoint extends AbstractCondition {
 
+	/**
+	 * A list of parameters that must also be included in the url query, even when they are already in the request
+	 * object.
+	 *
+	 * response_type, client_id, scope are required by these clauses from
+	 * https://openid.net/specs/openid-connect-core-1_0.html#RequestObject :
+	 *
+	 *    So that the request is a valid OAuth 2.0 Authorization Request, values for the response_type and client_id
+	 *    parameters MUST be included using the OAuth 2.0 request syntax, since they are REQUIRED by OAuth 2.0. The
+	 *    values for these parameters MUST match those in the Request Object, if present.
+	 *
+	 *    Even if a scope parameter is present in the Request Object value, a scope parameter MUST always be passed
+	 *    using the OAuth 2.0 request syntax containing the openid scope value to indicate to the underlying OAuth
+	 *    2.0 logic that this is an OpenID Connect request.
+	 *
+	 * redirect_uri is required because of this clause from https://tools.ietf.org/html/rfc6749#section-3.1.2.3 :
+	 *
+	 *    If multiple redirection URIs have been registered, if only part of
+	 *    the redirection URI has been registered, or if no redirection URI has
+	 *    been registered, the client MUST include a redirection URI with the
+	 *    authorization request using the "redirect_uri" request parameter.
+	 */
 	private static final List<String> REQUIRED_PARAMETERS = Arrays.asList(new String[] {
 		"response_type",
 		"client_id",
 		"scope",
-		"redirect_uri",
-		"prompt"
+		"redirect_uri"
 	});
 
 	@Override
