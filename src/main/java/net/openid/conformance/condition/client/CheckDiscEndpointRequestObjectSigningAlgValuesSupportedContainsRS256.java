70037b0772886f27014e5c43f07f73183db3f398
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-03-23 16:29:19 +0000

    Uppercase final static constants
    As per https://gitlab.com/openid/conformance-suite/-/merge_requests/881#note_309450480

--- src/main/java/net/openid/conformance/condition/client/CheckDiscEndpointRequestObjectSigningAlgValuesSupportedContainsRS256.java
+++ src/main/java/net/openid/conformance/condition/client/CheckDiscEndpointRequestObjectSigningAlgValuesSupportedContainsRS256.java
@@ -7,15 +7,15 @@ import java.util.Arrays;
 
 public class CheckDiscEndpointRequestObjectSigningAlgValuesSupportedContainsRS256 extends ValidateJsonArray {
 
-	private static final String environmentVariable = "request_object_signing_alg_values_supported";
+	private static final String ENVIRONMENT_VARIABLE = "request_object_signing_alg_values_supported";
 
 	private static final String[] SET_VALUES = new String[] { "RS256" };
 
-	private static final String errorMessageNotEnough = "Discovery endpoint request_object_signing_alg_values_supported does not include RS256.";
+	private static final String ERROR_MESSAGE_NOT_ENOUGH = "Discovery endpoint request_object_signing_alg_values_supported does not include RS256.";
 
 	@Override
 	@PreEnvironment(required = "server")
 	public Environment evaluate(Environment env) {
-		return validate(env, environmentVariable, Arrays.asList(SET_VALUES), 1, errorMessageNotEnough);
+		return validate(env, ENVIRONMENT_VARIABLE, Arrays.asList(SET_VALUES), 1, ERROR_MESSAGE_NOT_ENOUGH);
 	}
 }
--- src/main/java/net/openid/conformance/condition/client/EnsureServerConfigurationSupportsClientSecretPost.java
+++ src/main/java/net/openid/conformance/condition/client/EnsureServerConfigurationSupportsClientSecretPost.java
@@ -7,18 +7,18 @@ import java.util.Arrays;
 
 public class EnsureServerConfigurationSupportsClientSecretPost extends ValidateJsonArray {
 
-	private static final String environmentVariable = "token_endpoint_auth_methods_supported";
+	private static final String ENVIRONMENT_VARIABLE = "token_endpoint_auth_methods_supported";
 
 	private static final String[] SET_VALUES = new String[] { "client_secret_post" };
 
-	private static final String errorMessageNotEnough = "server discovery document does not contain client_secret_post in token_endpoint_auth_methods_supported";
+	private static final String ERROR_MESSAGE_NOT_ENOUGH = "server discovery document does not contain client_secret_post in token_endpoint_auth_methods_supported";
 
 	@Override
 	@PreEnvironment(required = "server")
 	public Environment evaluate(Environment env) {
 
-		return validate(env, environmentVariable, Arrays.asList(SET_VALUES), 1,
-			errorMessageNotEnough);
+		return validate(env, ENVIRONMENT_VARIABLE, Arrays.asList(SET_VALUES), 1,
+			ERROR_MESSAGE_NOT_ENOUGH);
 	}
 
 }
