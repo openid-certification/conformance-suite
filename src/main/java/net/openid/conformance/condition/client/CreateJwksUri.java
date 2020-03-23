18048706159c6f74517582ccfb70a6fe7db18ded
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-03-23 16:03:48 +0000

    CreateJwksUri: Enable local testing
    Apply the 'external url override' pattern used elsewhere to allow an
    OP on the internet to reach java running on the developer's local
    machine (via an ssh port forward). This makes it easy to run the tests
    that use a jwks_uri in the debugger.

--- src/main/java/net/openid/conformance/condition/client/CreateJwksUri.java
+++ src/main/java/net/openid/conformance/condition/client/CreateJwksUri.java
@@ -1,7 +1,6 @@
 package net.openid.conformance.condition.client;
 
 import com.google.common.base.Strings;
-
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
@@ -17,6 +16,12 @@ public class CreateJwksUri extends AbstractCondition {
 
 		if (baseUrl.isEmpty()) {
 			throw error("Base URL is empty");
+		}
+
+		// see https://gitlab.com/openid/conformance-suite/wikis/Developers/Build-&-Run#ciba-notification-endpoint
+		String externalUrlOverride = env.getString("external_url_override");
+		if (!Strings.isNullOrEmpty(externalUrlOverride)) {
+			baseUrl = externalUrlOverride;
 		}
 
 		// calculate the redirect URI based on our given base URL
