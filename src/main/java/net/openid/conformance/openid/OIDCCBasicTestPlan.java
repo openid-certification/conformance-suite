7c05ee536e1216603287d99a9fa279cd2c9df489
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-04-24 16:42:18 +0100

    Add 'basic' & 'implicit' certification profiles

---
+++ src/main/java/net/openid/conformance/openid/OIDCCBasicTestPlan.java
@@ -0,0 +1,96 @@
+package net.openid.conformance.openid;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.variant.ClientAuthType;
+import net.openid.conformance.variant.ResponseMode;
+import net.openid.conformance.variant.ResponseType;
+
+import java.util.List;
+
+@PublishTestPlan(
+	testPlanName = "oidcc-basic-certifcation-test-plan",
+	displayName = "OpenID Connect Core: Basic Certification Profile Authorization server test ",
+	profile = TestPlan.ProfileNames.optest
+)
+public class OIDCCBasicTestPlan implements TestPlan {
+
+	public static List<ModuleListEntry> testModulesWithVariants() {
+		// ClientRegistration.class is not specified so will be offered in the menu
+		// This plan attempts to match 'basic'' as defined here:
+		// https://openid.net/wordpress-content/uploads/2018/06/OpenID-Connect-Conformance-Profiles.pdf
+		// the tests are in the same order as the table and the comments list the 3rd column in the table.
+
+		final List<Variant> variant_code_basic = List.of(
+			new Variant(ResponseType.class, "code"),
+			new Variant(ClientAuthType.class, "client_secret_basic"),
+			new Variant(ResponseMode.class, "default")
+		);
+		return List.of(
+			new ModuleListEntry(
+				List.of(
+					OIDCCServerTest.class, // OP-Response-code
+					OIDCCResponseTypeMissing.class, // OP-Response-Missing
+					// 4 x IdToken.verify() are covered by OIDCCServerTest
+					OIDCCIdTokenSignature.class, // OP-IDToken-Signature & OP-IDToken-kid
+					OIDCCIdTokenUnsigned.class, // OP-IDToken-none
+					OIDCCUserInfoGet.class, // OP-UserInfo-Endpoint
+					OIDCCUserInfoPostHeader.class, // OP-UserInfo-Header
+					OIDCCUserInfoPostBody.class, // OP-UserInfo-Body
+					// OpenIDSchema.verify() covered by OP-UserInfo-Endpoint
+					OIDCCEnsureRequestWithoutNonceSucceedsForCodeFlow.class, // OP-nonce-NoReq-code
+					// OP-nonce-code covered by OIDCCServerTest
+					// OP-IDToken-Signature here is a duplicate covered above
+					OIDCCScopeProfile.class, // OP-scope-profile
+					OIDCCScopeEmail.class, // OP-scope-email
+					OIDCCScopeAddress.class, // OP-scope-address
+					OIDCCScopePhone.class, // OP-scope-phone
+					OIDCCScopeAll.class, // OP-scope-All
+					OIDCCDisplayPage.class, // OP-display-page
+					OIDCCDisplayPopup.class, // OP-display-popup
+					OIDCCPromptLogin.class, // OP-prompt-login
+					OIDCCPromptNoneNotLoggedIn.class, // OP-prompt-none-NotLoggedIn
+					OIDCCPromptNoneLoggedIn.class, // OP-prompt-none-LoggedIn
+					OIDCCMaxAge1.class, // 3 x OP-Req-max_age=1
+					OIDCCMaxAge10000.class, // OP-Req-max_age=10000
+
+
+
+					OIDCCEnsureRequestWithUnknownParameterSucceeds.class, // OP-Req-NotUnderstood
+					OIDCCIdTokenHint.class, // OP-Req-id_token_hint
+					OIDCCLoginHint.class, // OP-Req-login_hint
+					OIDCCUiLocales.class, // OP-Req-ui_locales
+					OIDCCClaimsLocales.class, // OP-Req-claims_locales
+					OIDCCEnsureRequestWithAcrValuesSucceeds.class, // OP-Req-acr_values
+					// VerifyState() covered by OIDCCServerTest
+					OIDCCAuthCodeReuse.class, // OP-OAuth-2nd
+					OIDCCAuthCodeReuseAfter30Seconds.class, // OP-OAuth-2nd-30s
+					// OP-OAuth-2nd-Revokes covered by oidcc-codereuse-30seconds / CallProtectedResourceWithBearerTokenExpectingError
+					// OP-OAuth-2nd-30s is already covered
+					OIDCCEnsureRegisteredRedirectUri.class // OP-redirect_uri-NotReg
+					// OP-ClientAuth-Basic-Dynamic covered by OIDCServerTest
+					// OP-ClientAuth-Basic-Static covered by OIDCServerTest
+				),
+				variant_code_basic
+			),
+			new ModuleListEntry(
+				List.of(OIDCCServerTest.class), // OP-ClientAuth-SecretPost-Dynamic
+				List.of(
+					new Variant(ResponseType.class, "code"),
+					new Variant(ClientAuthType.class, "client_secret_post"),
+					new Variant(ResponseMode.class, "default")
+				)
+			),
+			// OP-ClientAuth-SecretPost-Static same as OP-ClientAuth-SecretPost-Dynamic
+			new ModuleListEntry(
+				List.of(
+					OIDCCRequestUriUnsigned.class, // OP-request_uri-Unsigned
+					OIDCCUnsignedRequestObject.class, // OP-request-Unsigned
+					OIDCCClaimsEssential.class, // OP-claims-essential
+					OIDCCRefreshToken.class // new test; skipped if refresh tokens not supported
+					),
+				variant_code_basic
+			)
+		);
+	}
+}
---
+++ src/main/java/net/openid/conformance/openid/OIDCCImplicitTestPlan.java
@@ -0,0 +1,91 @@
+package net.openid.conformance.openid;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.testmodule.TestModule;
+import net.openid.conformance.variant.ClientAuthType;
+import net.openid.conformance.variant.ResponseMode;
+import net.openid.conformance.variant.ResponseType;
+
+import java.util.List;
+
+@PublishTestPlan(
+	testPlanName = "oidcc-implicit-certifcation-test-plan",
+	displayName = "OpenID Connect Core: Implicit Certification Profile Authorization server test ",
+	profile = TestPlan.ProfileNames.optest
+)
+public class OIDCCImplicitTestPlan implements TestPlan {
+
+	public static List<ModuleListEntry> testModulesWithVariants() {
+		// ClientRegistration.class is not specified so will be offered in the menu
+		// This plan attempts to match 'basic'' as defined here:
+		// https://openid.net/wordpress-content/uploads/2018/06/OpenID-Connect-Conformance-Profiles.pdf
+		// the tests are in the same order as the table and the comments list the 3rd column in the table.
+
+		// OP-Response-id_token+token
+
+		List<Class<? extends TestModule>> moduleList = List.of(
+			OIDCCServerTest.class, // OP-Response-id_token
+			OIDCCResponseTypeMissing.class, // OP-Response-Missing
+			// 4 x IdToken.verify() are covered by OIDCCServerTest
+			OIDCCIdTokenSignature.class, // OP-IDToken-Signature & OP-IDToken-kid
+			// OP-IDToken-at_hash covered by OIDCCServerTest
+			// 3 x userinfo tests aren't applicable for response_type=id_token (listed separately below for response_type=id_token token)
+			// OpenIDSchema.verify() covered by OP-UserInfo-Endpoint
+			OIDCCEnsureRequestWithoutNonceFails.class, // OP-nonce-NoReq-noncode
+			// OP-nonce-nonecode covered by OIDCCServerTest
+			// OP-IDToken-Signature here is a duplicate covered above
+			OIDCCScopeProfile.class, // OP-scope-profile
+			OIDCCScopeEmail.class, // OP-scope-email
+			OIDCCScopeAddress.class, // OP-scope-address
+			OIDCCScopePhone.class, // OP-scope-phone
+			OIDCCScopeAll.class, // OP-scope-All
+			OIDCCDisplayPage.class, // OP-display-page
+			OIDCCDisplayPopup.class, // OP-display-popup
+			OIDCCPromptLogin.class, // OP-prompt-login
+			OIDCCPromptNoneNotLoggedIn.class, // OP-prompt-none-NotLoggedIn
+			OIDCCPromptNoneLoggedIn.class, // OP-prompt-none-LoggedIn
+			OIDCCMaxAge1.class, // 3 x OP-Req-max_age=1
+			OIDCCMaxAge10000.class, // OP-Req-max_age=10000
+			OIDCCEnsureRequestWithUnknownParameterSucceeds.class, // OP-Req-NotUnderstood
+			OIDCCIdTokenHint.class, // OP-Req-id_token_hint
+			OIDCCLoginHint.class, // OP-Req-login_hint
+			OIDCCUiLocales.class, // OP-Req-ui_locales
+			OIDCCClaimsLocales.class, // OP-Req-claims_locales
+			OIDCCEnsureRequestWithAcrValuesSucceeds.class, // OP-Req-acr_values
+			// VerifyState() covered by OIDCCServerTest
+			OIDCCEnsureRegisteredRedirectUri.class, // OP-redirect_uri-NotReg
+			OIDCCRequestUriUnsigned.class, // OP-request_uri-Unsigned
+			OIDCCUnsignedRequestObject.class, // OP-request-Unsigned
+			OIDCCClaimsEssential.class // OP-claims-essential
+		);
+
+		final List<Variant> variantIdTokenTokenBasic = List.of(
+			new Variant(ResponseType.class, "id_token token"),
+			new Variant(ClientAuthType.class, "client_secret_basic"), // not actually used but we still have to specify it (there's no way to mark a variant not applicable depending on the value of another variant)
+			new Variant(ResponseMode.class, "default")
+		);
+		return List.of(
+			new ModuleListEntry(
+				moduleList,
+				List.of(
+					new Variant(ResponseType.class, "id_token"),
+					new Variant(ClientAuthType.class, "client_secret_basic"), // not actually used but we still have to specify it (there's no way to mark a variant not applicable depending on the value of another variant)
+					new Variant(ResponseMode.class, "default")
+				)
+			),
+			new ModuleListEntry(
+				moduleList,
+				variantIdTokenTokenBasic
+			),
+			new ModuleListEntry(
+				List.of(
+					OIDCCUserInfoGet.class, // OP-UserInfo-Endpoint
+					OIDCCUserInfoPostHeader.class, // OP-UserInfo-Header
+					OIDCCUserInfoPostBody.class // OP-UserInfo-Body
+				),
+				variantIdTokenTokenBasic
+			)
+		);
+	}
+}
