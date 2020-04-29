d1dbf5d0291db0353503e6834c7dc5f54292b579
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-04-29 20:13:54 +0100

    Add config certification profile testplan
    A slightly odd plan as it only contains a single test module, but that's
    all that is necessary.
    
    On review of the PDF a few tests were missing from the discovery test
    module (some because they were tested in other modules/profiles, some
    because the python test was, on review, more comprehensive than the
    existing java one); these have been added/tweaked as necessary.
    
    One test mentioned in the certification pdf is irrelevant and also not
    included in the python.

---
+++ src/main/java/net/openid/conformance/openid/OIDCCConfigTestPlan.java
@@ -0,0 +1,37 @@
+package net.openid.conformance.openid;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+
+@PublishTestPlan(
+	testPlanName = "oidcc-config-certification-test-plan",
+	displayName = "OpenID Connect Core: Config Certification Profile Authorization server test ",
+	profile = TestPlan.ProfileNames.optest,
+	testModules = {
+		// This plan attempts to match 'config' as defined here:
+		// https://openid.net/wordpress-content/uploads/2018/06/OpenID-Connect-Conformance-Profiles.pdf
+		// the tests are in the same order as the table and the comments list the 3rd column in the table.
+
+		// OP-IDToken-none - this appears to be a mistake in the above PDF; this test has no relevance to the config
+		// profile and it not included in the python config profile.
+		OIDCCDiscoveryEndpointVerification.class // OP-Discovery-Config
+		// ProviderConfigurationResponse.verify() included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointIssuer
+		// ProviderConfigurationResponse.verify() included in OIDCCDiscoveryEndpointVerification, also in CheckDiscEndpointIssuer
+		// IdToken.verify() - described as "Discovered issuer matches ID Token iss value" - isn't in the python config
+		//                    tests, can't be done in a pure discovery test as we have no id token (checked by
+		//                    OIDCCServerTest in the other certification profiles)
+		// CheckEndpoint() included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointAuthorizationEndpoint
+		// CheckEndpoint() included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointTokenEndpoint
+		// CheckEndpoint() included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointUserinfoEndpoint
+		// OP-Discovery-jwks_uri included in OIDCCDiscoveryEndpointVerification, CheckJwksUri
+		// OP-Discovery-JWKs - included in OIDCCDiscoveryEndpointVerification, ValidateServerJWKs
+		// CheckScopeSupport() - included in OIDCCDiscoveryEndpointVerification, OIDCCCheckDiscEndpointScopesSupported
+		// ProviderConfigurationResponse.verify() - included in OIDCCDiscoveryEndpointVerification, OIDCCCheckDiscEndpointResponseTypesSupported
+		// ProviderConfigurationResponse.verify() - included in OIDCCDiscoveryEndpointVerification, OIDCCCheckDiscEndpointSubjectTypesSupported
+		// ProviderConfigurationResponse.verify() - included in OIDCCDiscoveryEndpointVerification, OIDCCCheckDiscEndpointIdTokenSigningAlgValuesSupported
+		// OP-Discovery-claims_supported - included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointClaimsParameterSupported
+		// VerifyOPEndpointsUseHTTPS() - included in OIDCCDiscoveryEndpointVerification, CheckDiscEndpointAllEndpointsAreHttps
+	}
+)
+public class OIDCCConfigTestPlan implements TestPlan {
+}
