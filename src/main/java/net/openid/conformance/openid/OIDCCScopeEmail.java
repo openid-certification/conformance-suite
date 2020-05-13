6acd582f0c0caeb8f3802e94c3b7bdc10bfe5db1
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-05-13 20:30:55 +0100

    Add additional warnings about recieving unexpected PII
    I noticed that gitlab's iDP returns the name claim when you request
    scope=email. We didn't have any checks for that. They agree that whilst
    deliberate it's probably not desireable:
    
    https://gitlab.com/gitlab-com/gl-infra/infrastructure/-/issues/2804#note_340838307
    
    I've added two extra checks, and used one existing check in a new place.
    When requesting scope=email we check:
    
    id_token does not contain name
    
    id_token does not contain email if response_type contains access token
    
    userinfo does not contain name
    
    For now at least I've deliberately not checked for any unknown data in
    any situation, as that risks raising a lot of warnings.

--- src/main/java/net/openid/conformance/openid/OIDCCScopeEmail.java
+++ src/main/java/net/openid/conformance/openid/OIDCCScopeEmail.java
@@ -1,5 +1,9 @@
 package net.openid.conformance.openid;
 
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.EnsureIdTokenDoesNotContainEmail;
+import net.openid.conformance.condition.client.EnsureIdTokenDoesNotContainName;
+import net.openid.conformance.condition.client.EnsureUserInfoDoesNotContainName;
 import net.openid.conformance.condition.client.SetScopeInClientConfigurationToOpenIdEmail;
 import net.openid.conformance.testmodule.PublishTestModule;
 
@@ -7,7 +11,7 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "oidcc-scope-email",
 	displayName = "OIDCC: check email scope",
-	summary = "This test requests authorization with email scope.",
+	summary = "This test requests authorization with email scope and issues a warning if an email address isn't returned in the expected place.",
 	profile = "OIDCC"
 )
 public class OIDCCScopeEmail extends AbstractOIDCCReturnedClaimsServerTest {
@@ -18,4 +22,22 @@ public class OIDCCScopeEmail extends AbstractOIDCCReturnedClaimsServerTest {
 		super.skipTestIfScopesNotSupported();
 	}
 
+	@Override
+	protected void performIdTokenValidation() {
+		super.performIdTokenValidation();
+
+		// the python test did not check this as far as I know
+		callAndContinueOnFailure(EnsureIdTokenDoesNotContainName.class, Condition.ConditionResult.WARNING,  "OIDCC-5.5", "OIDC-5.5.1");
+
+		if (responseType.includesCode() || responseType.includesToken()) {
+			// we have an access token so response should not be in id_token
+			callAndContinueOnFailure(EnsureIdTokenDoesNotContainEmail.class, Condition.ConditionResult.WARNING, "OIDCC-5.4");
+		}
+	}
+
+	@Override
+	protected void validateUserInfoResponse() {
+		super.validateUserInfoResponse();
+		callAndContinueOnFailure(EnsureUserInfoDoesNotContainName.class, Condition.ConditionResult.WARNING,  "OIDCC-5.5", "OIDC-5.5.1");
+	}
 }
