dfa89458d69659b57b6f9b1509dc9c8413894cc0
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-05-18 16:57:22 +0100

    Fix an incorrect log message that referenced name instead of email.

--- src/main/java/net/openid/conformance/condition/client/EnsureIdTokenDoesNotContainEmail.java
+++ src/main/java/net/openid/conformance/condition/client/EnsureIdTokenDoesNotContainEmail.java
@@ -9,11 +9,11 @@ public class EnsureIdTokenDoesNotContainEmail extends AbstractCondition {
 	@Override
 	@PreEnvironment(required = "id_token" )
 	public Environment evaluate(Environment env) {
-		String name = env.getString("id_token", "claims.email");
+		String email = env.getString("id_token", "claims.email");
 
-		if (name != null) {
+		if (email != null) {
 			// see discussion on certification email list, 10th March 2020
-			throw error("Unexpectedly found email in id_token. The conformance suite did not request the 'email' claim is returned in the id_token and hence did not expect the server to include it. Technically this does not violate the specifications but it is likely a bug in the server and may result in user data being exposed in unintended ways.", args("name", name));
+			throw error("Unexpectedly found email in id_token. The conformance suite did not request the 'email' claim is returned in the id_token and hence did not expect the server to include it. Technically this does not violate the specifications but it is likely a bug in the server and may result in user data being exposed in unintended ways.", args("email", email));
 		}
 
 		logSuccess("email claim not found in id_token, which is expected as it was not requested to be returned there");
