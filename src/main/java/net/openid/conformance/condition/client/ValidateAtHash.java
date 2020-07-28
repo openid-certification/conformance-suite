ae9de6621384a85b90b412ebf1517d14ce109f1e
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-07-28 14:11:44 +0100

    Rename 'ValidateHash' to 'AbstractValidateHash'
    Just to match the pattern used in the rest of the code.

--- src/main/java/net/openid/conformance/condition/client/ValidateAtHash.java
+++ src/main/java/net/openid/conformance/condition/client/ValidateAtHash.java
@@ -3,7 +3,7 @@ package net.openid.conformance.condition.client;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
-public class ValidateAtHash extends ValidateHash {
+public class ValidateAtHash extends AbstractValidateHash {
 
 	@Override
 	@PreEnvironment(required = { "access_token", "at_hash" } )
