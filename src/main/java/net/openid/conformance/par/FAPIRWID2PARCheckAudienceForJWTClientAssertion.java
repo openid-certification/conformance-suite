2bee9041c04e640a2299142db8968a04782c5e52
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-09-28 19:15:06 +0100

    FAPI-RW: Stop institution id configuration field appearing
    Previous changes had relegated this to only appearing for OBUK tests,
    but the PAR tests unfortunately added it back - back them into line
    with the non-PAR tests.

--- src/main/java/net/openid/conformance/par/FAPIRWID2PARCheckAudienceForJWTClientAssertion.java
+++ src/main/java/net/openid/conformance/par/FAPIRWID2PARCheckAudienceForJWTClientAssertion.java
@@ -28,8 +28,7 @@ import net.openid.conformance.variant.VariantNotApplicable;
 		"mtls2.key",
 		"mtls2.cert",
 		"mtls2.ca",
-		"resource.resourceUrl",
-		"resource.institution_id"
+		"resource.resourceUrl"
 	}
 )
 @VariantNotApplicable(parameter = FAPIAuthRequestMethod.class, values = {
