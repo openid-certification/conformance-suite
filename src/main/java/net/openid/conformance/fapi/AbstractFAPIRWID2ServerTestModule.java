313b21028fd814fb282e9205fb806a3930c21238
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-10-12 18:03:13 +0100

    FAPI-RW: Include nbf in second client request object
    nbf is valid (but not required) here in FAPI-RW ID2 so we should
    probably try sending it, as no server should rejects request objects
    that include nbf.
    
    nbf is mandatory here in FAPI1-Advanced, so this change helps ensure there is
    a smooth migration path from FAPIRW-ID2 to FAPI1-Advanced-Final.
    
    FAPI working group was notified of change here:
    
    https://bitbucket.org/openid/fapi/issues/363/proposed-change-to-fapi-rw-id2
    
    and no objections were received.

--- src/main/java/net/openid/conformance/fapi/AbstractFAPIRWID2ServerTestModule.java
+++ src/main/java/net/openid/conformance/fapi/AbstractFAPIRWID2ServerTestModule.java
@@ -17,6 +17,7 @@ import net.openid.conformance.condition.client.AddFAPIInteractionIdToResourceEnd
 import net.openid.conformance.condition.client.AddIatToRequestObject;
 import net.openid.conformance.condition.client.AddIpV4FapiCustomerIpAddressToResourceEndpointRequest;
 import net.openid.conformance.condition.client.AddIssToRequestObject;
+import net.openid.conformance.condition.client.AddNbfToRequestObject;
 import net.openid.conformance.condition.client.AddNonceToAuthorizationEndpointRequest;
 import net.openid.conformance.condition.client.AddPlainErrorResponseAsAuthorizationEndpointResponseForJARM;
 import net.openid.conformance.condition.client.AddStateToAuthorizationEndpointRequest;
@@ -402,6 +403,8 @@ public abstract class AbstractFAPIRWID2ServerTestModule extends AbstractRedirect
 
 			if (isSecondClient) {
 				callAndStopOnFailure(AddIatToRequestObject.class);
+				// nbf is optional in current FAPI-RW-ID2; it will become mandatory in ID3
+				callAndStopOnFailure(AddNbfToRequestObject.class);
 			}
 			callAndStopOnFailure(AddExpToRequestObject.class, "FAPI-RW-5.2.2-12");
 
