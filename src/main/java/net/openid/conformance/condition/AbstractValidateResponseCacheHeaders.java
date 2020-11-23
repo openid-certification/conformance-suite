7c2c6ea5d083fb1eafb65140ea577e9f67ecc755
Joseph Heenan <joseph.heenan@fintechlabs.io>
2020-11-23 13:56:55 +0000

    Refactor cache header verification out of BC logout condition
    We have two different sets of code attempting to do this validation,
    both of which suffer from bug #858 - i.e. not coping if the server
    passes the cache-control header multiple times, as is allowed.
    
    The two requirements are also slightly different, further commits
    will resolve this. This commit just moves the code without changing
    any behaviour.

---
+++ src/main/java/net/openid/conformance/condition/AbstractValidateResponseCacheHeaders.java
@@ -0,0 +1,56 @@
+package net.openid.conformance.condition;
+
+import com.google.common.base.Strings;
+import com.google.gson.JsonObject;
+import net.openid.conformance.testmodule.OIDFJSON;
+
+public abstract class AbstractValidateResponseCacheHeaders extends AbstractCondition {
+
+	protected void validateCacheHeaders(JsonObject headers, String humanReadableResponseName) {
+		String noStore = "no-store";
+		String noCache = "no-cache";
+
+		if (!headers.has("cache-control")) {
+			throw error(humanReadableResponseName + " does not contain 'cache-control' header", args("response_headers", headers));
+		}
+
+		if (!headers.has("pragma")) {
+			throw error(humanReadableResponseName + " does not contain 'pragma' header", args("response_headers", headers));
+		}
+
+		String cacheControl = OIDFJSON.getString(headers.get("cache-control"));
+
+
+		if (Strings.isNullOrEmpty(cacheControl) || !doesHeaderContainExpectedValue(cacheControl, noStore)) {
+			throw error("'cache-control' header in " + humanReadableResponseName + " does not contain expected value.",
+				args("expected", noStore, "actual", cacheControl));
+		}
+		if (Strings.isNullOrEmpty(cacheControl) || !doesHeaderContainExpectedValue(cacheControl, noCache)) {
+			throw error("'cache-control' header in "+humanReadableResponseName+" does not contain expected value.",
+				args("expected", noCache, "actual", cacheControl));
+		}
+
+		String pragma = OIDFJSON.getString(headers.get("pragma"));
+		if (Strings.isNullOrEmpty(pragma) || !doesHeaderContainExpectedValue(pragma, noCache)) {
+			throw error("'pragma' header in "+humanReadableResponseName+" does not contain expected value.",
+						args("expected", noCache, "actual", pragma));
+		}
+
+		logSuccess("'pragma' and 'cache-control' headers in " + humanReadableResponseName + " contain expected values.",
+					args("cache_control_header", cacheControl, "pragma_header", pragma));
+	}
+
+	private boolean doesHeaderContainExpectedValue(String header, String expected) {
+		if (header == null || header.isEmpty()) {
+			return false;
+		}
+
+		for (String piece : header.split(",")) {
+			if (piece.trim().equals(expected)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
