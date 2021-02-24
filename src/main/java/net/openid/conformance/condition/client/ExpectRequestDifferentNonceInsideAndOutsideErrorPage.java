7ad44bc1bd91cd1871c4c64230637944da5639f1
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-02-24 21:23:07 +0000

    FAPI: Clarify description in 'different nonces outside request object' tests

--- src/main/java/net/openid/conformance/condition/client/ExpectRequestDifferentNonceInsideAndOutsideErrorPage.java
+++ src/main/java/net/openid/conformance/condition/client/ExpectRequestDifferentNonceInsideAndOutsideErrorPage.java
@@ -10,7 +10,7 @@ public class ExpectRequestDifferentNonceInsideAndOutsideErrorPage extends Abstra
 	@PostEnvironment(strings = "request_unverifiable_error")
 	public Environment evaluate(Environment env) {
 
-		String placeholder = createBrowserInteractionPlaceholder("If the server does not return an invalid_request error back to the client, it must either show an error page (saying the request object is invalid as the 'nonce' value in the request object and outside it are different - upload a screenshot of the error page) or must successfully authenticate and but return the nonce from inside the request object.");
+		String placeholder = createBrowserInteractionPlaceholder("If the server does not return an invalid_request error back to the client, it must either show an error page (saying the request is invalid as the 'nonce' value in the request object and outside it are different - upload a screenshot of the error page) or must successfully authenticate and but return the nonce from inside the request object.");
 		env.putString("request_unverifiable_error", placeholder);
 
 		return env;
