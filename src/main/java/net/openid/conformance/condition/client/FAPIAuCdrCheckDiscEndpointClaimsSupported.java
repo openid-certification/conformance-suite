845754a593f8bf666f090a590f543d5d6b6213ce
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-04-14 12:42:13 +0100

    FAPI-Final: Adjust discovery tests for removal of acr requirement
    Although the FAPI spec no longer explicitly mentions acr, both the
    Australian and UK specs do.
    
    We also never checked that Australia servers include the required
    sharing_duration claim in their discovery endpoint, that is now checked.
    
    part of #885

---
+++ src/main/java/net/openid/conformance/condition/client/FAPIAuCdrCheckDiscEndpointClaimsSupported.java
@@ -0,0 +1,25 @@
+package net.openid.conformance.condition.client;
+
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+import java.util.Arrays;
+
+public class FAPIAuCdrCheckDiscEndpointClaimsSupported extends AbstractValidateJsonArray {
+
+	private static final String environmentVariable = "claims_supported";
+
+	private static final String[] SET_VALUES = new String[] { "acr", "sharing_duration" };
+	private static final int minimumMatchesRequired = SET_VALUES.length;
+
+	private static final String errorMessageNotEnough = "The server does not support enough of the required claims.";
+
+	@Override
+	@PreEnvironment(required = "server")
+	public Environment evaluate(Environment env) {
+
+		return validate(env, environmentVariable, Arrays.asList(SET_VALUES), minimumMatchesRequired,
+			errorMessageNotEnough);
+	}
+
+}
