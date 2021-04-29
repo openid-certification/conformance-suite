10f7b7b3a770bd80d51cb86fda5ab123cbec4e96
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-04-29 15:24:41 +0100

    Further improve error message in EnsureIncomingTlsSecureCipher
    As per Serkan's comment on
    https://gitlab.com/openid/conformance-suite/-/merge_requests/988#note_562008696
    and also be a bit more explicit about what people should do in an
    attempt to avoid questions really asked as a support request.

--- src/main/java/net/openid/conformance/condition/common/EnsureIncomingTlsSecureCipher.java
+++ src/main/java/net/openid/conformance/condition/common/EnsureIncomingTlsSecureCipher.java
@@ -30,7 +30,7 @@ public class EnsureIncomingTlsSecureCipher extends AbstractCondition {
 			logSuccess("TLS cipher is allowed", args("expected", ALLOWED_CIPHERS, "actual", cipher));
 			return env;
 		} else {
-			throw error("The incoming connection used a TLS cipher is not permitted by FAPI.", args("expected", ALLOWED_CIPHERS, "actual", cipher));
+			throw error("The incoming connection from the client used a TLS cipher that is not permitted by FAPI. The client should be reconfigured so it will only use permitted TLS ciphers.", args("expected", ALLOWED_CIPHERS, "actual", cipher));
 		}
 
 	}
