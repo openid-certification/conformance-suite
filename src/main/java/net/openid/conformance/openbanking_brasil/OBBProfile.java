c6c3336efabb3929d5f5756cb31d054fd36c048f
George McIntosh <george@georgemcintosh.com>
2021-06-01 09:35:38 +0100

    Only surface Raidiam tests now

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/OBBProfile.java
@@ -0,0 +1,7 @@
+package net.openid.conformance.openbanking_brasil;
+
+public interface OBBProfile {
+
+	String OBB_PROFILE = "Open Banking Brasil Functional Tests";
+
+}
---
+++ src/main/java/net/openid/conformance/raidiam/RaidiamProfile.java
@@ -0,0 +1,7 @@
+package net.openid.conformance.raidiam;
+
+public interface RaidiamProfile {
+
+	String RAIDIAM_PROFILE = "Some sample tests built by Raidiam";
+
+}
