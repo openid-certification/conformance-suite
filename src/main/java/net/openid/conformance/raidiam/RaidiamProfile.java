d3a68084ae52882287c324a841e71ee3dc1fa8e9
George McIntosh <george@georgemcintosh.com>
2021-06-02 10:46:18 +0100

    Fixed up unit tests

--- src/main/java/net/openid/conformance/raidiam/RaidiamProfile.java
+++ src/main/java/net/openid/conformance/raidiam/RaidiamProfile.java
@@ -2,6 +2,6 @@ package net.openid.conformance.raidiam;
 
 public interface RaidiamProfile {
 
-	String RAIDIAM_PROFILE = "Some sample tests built by Raidiam";
+	String RAIDIAM_PROFILE = "Raidiam Directory Tests";
 
 }
