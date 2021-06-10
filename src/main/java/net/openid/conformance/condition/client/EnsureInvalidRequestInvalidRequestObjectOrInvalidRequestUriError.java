4f5cfb02dc24c5ae2bde4970926a5cd6521b00ec
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-06-10 21:29:45 +0100

    Refactor to create a helper for checking errors
    Just reduces code duplication.

--- src/main/java/net/openid/conformance/condition/client/EnsureInvalidRequestInvalidRequestObjectOrInvalidRequestUriError.java
+++ src/main/java/net/openid/conformance/condition/client/EnsureInvalidRequestInvalidRequestObjectOrInvalidRequestUriError.java
@@ -8,24 +8,12 @@ import net.openid.conformance.testmodule.Environment;
 
 import java.util.List;
 
-public class EnsureInvalidRequestInvalidRequestObjectOrInvalidRequestUriError extends AbstractCondition {
-
-	private static final List<String> PERMITTED_ERRORS = ImmutableList.of(
-		"invalid_request",
-		"invalid_request_object",
-		"invalid_request_uri");
-
+public class EnsureInvalidRequestInvalidRequestObjectOrInvalidRequestUriError extends AbstractEnsureError {
 	@Override
-	@PreEnvironment(required = "authorization_endpoint_response")
-	public Environment evaluate(Environment env) {
-
-		String error = env.getString("authorization_endpoint_response", "error");
-		if (Strings.isNullOrEmpty(error)) {
-			throw error("The server was expected to return an error, but no error was returned");
-		} else if (!PERMITTED_ERRORS.contains(error)) {
-			throw error("'error' field has unexpected value", args("permitted", PERMITTED_ERRORS, "actual", error));
-		}
-		logSuccess("Authorization endpoint returned 'error'", args("permitted", PERMITTED_ERRORS, "error", error));
-		return env;
+	protected ImmutableList<String> getPermittedErrors() {
+		return ImmutableList.of(
+			"invalid_request",
+			"invalid_request_object",
+			"invalid_request_uri");
 	}
 }
