fe901be739d09e5f48883cac8039bf2ca108d31e
Serkan Özkan <serkan@sozkan.com>
2021-06-19 00:53:25 +0300

    Add note about Brazil encryption requirements

--- src/main/java/net/openid/conformance/fapi1advancedfinal/FAPI1AdvancedFinalClientEncryptedRequestObjectTest.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/FAPI1AdvancedFinalClientEncryptedRequestObjectTest.java
@@ -7,6 +7,7 @@ import net.openid.conformance.condition.as.LogAccessTokenAlwaysRejectedToForceAR
 import net.openid.conformance.condition.as.RemoveIssuedAccessTokenFromEnvironment;
 import net.openid.conformance.testmodule.PublishTestModule;
 import net.openid.conformance.variant.FAPI1FinalOPProfile;
+import net.openid.conformance.variant.FAPIAuthRequestMethod;
 import net.openid.conformance.variant.VariantNotApplicable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,11 @@ import org.springframework.http.ResponseEntity;
 	}
 )
 @VariantNotApplicable(parameter = FAPI1FinalOPProfile.class, values = {"plain_fapi", "consumerdataright_au", "openbanking_uk"})
+//TODO re-evaluate if this test is necessary or how this could be implemented better.
+// when not using PAR these checks will be always applied at the authorization endpoint so a separate test is not
+// needed for this. But when using PAR, we need a separate test to test Brazil "5.2.3-1 shall support encrypted request objects"
+// but as per "5.2.3-2 shall support Pushed Authorisation Requests PAR" a client must always support PAR and also encrypted
+// request objects.
 public class FAPI1AdvancedFinalClientEncryptedRequestObjectTest extends AbstractFAPI1AdvancedFinalClientTest {
 
 	@Override
