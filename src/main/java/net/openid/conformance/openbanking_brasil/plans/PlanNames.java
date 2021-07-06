241cba6994c5ad3284e117ce93daccf0ec2eaf82
GeorgeMc <george@georgemcintosh.com>
2021-07-06 08:07:15 +0000

    Update PlanNames.java

--- src/main/java/net/openid/conformance/openbanking_brasil/plans/PlanNames.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/plans/PlanNames.java
@@ -1,12 +1,10 @@
 package net.openid.conformance.openbanking_brasil.plans;
 
 public class PlanNames {
-
-	public static final String ACCOUNT_API_NAME = "Functional tests for accounts API - based on Swagger version: 1.0.0-rc6.7";
-	public static final String CONSENTS_API_NAME = "Functional tests for consents API - based on Swagger version: 1.0.0-rc6.7 (WIP)";
-	public static final String CREDIT_CARDS_API_PLAN_NAME = "Functional tests for credit card API - based on Swagger version: 1.0.0-rc6.7 (WIP)";
-	public static final String CREDIT_OPERATIONS_API_PLAN_NAME = "Functional tests for credit operations API - based on Swagger version: 1.0.0-rc6.7 (WIP)";
-	public static final String REGISTRATION_DATA_API_PLAN_NAME = "Functional tests for registration data API - based on Swagger version: 1.0.0-rc6.7 (WIP)";
-	public static final String RESOURCES_API_PLAN_NAME = "Functional tests for resources API - based on Swagger version: 1.0.0-rc6.7 (WIP)";
-
+    public static final String ACCOUNT_API_NAME = “Functional tests for accounts API - based on Swagger version: 1.0.0-rc6.7”;
+    public static final String CONSENTS_API_NAME = “Functional tests for consents API - based on Swagger version: 1.0.0";
+    public static final String CREDIT_CARDS_API_PLAN_NAME = “Functional tests for credit card API - based on Swagger version: 1.0.0-rc6.7 (WIP)“;
+    public static final String CREDIT_OPERATIONS_API_PLAN_NAME = “Functional tests for credit operations API - based on Swagger version: 1.0.0-rc6.7 (WIP)“;
+    public static final String REGISTRATION_DATA_API_PLAN_NAME = “Functional tests for registration data API - based on Swagger version: 1.0.0”;
+    public static final String RESOURCES_API_PLAN_NAME = “Functional tests for resources API - based on Swagger version: 1.0.0";
 }
