f0e57afe5580630c11fb495d4cfe9846a637951e
George McIntosh <george@georgemcintosh.com>
2021-07-07 15:08:03 +0100

    Removing an unused test module

--- src/main/java/net/openid/conformance/openbanking_brasil/plans/AccountsApiTestPlan.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/plans/AccountsApiTestPlan.java
@@ -2,7 +2,6 @@ package net.openid.conformance.openbanking_brasil.plans;
 
 import net.openid.conformance.openbanking_brasil.OBBProfile;
 import net.openid.conformance.openbanking_brasil.testmodules.AccountApiTestModule;
-import net.openid.conformance.openbanking_brasil.testmodules.AccountApiWrongPermissionsTestModule2;
 import net.openid.conformance.openbanking_brasil.testmodules.AccountsApiWrongPermissionsTestModule;
 import net.openid.conformance.plan.PublishTestPlan;
 import net.openid.conformance.plan.TestPlan;
@@ -14,8 +13,7 @@ import net.openid.conformance.plan.TestPlan;
 	summary = "Structural and logical tests for OpenBanking Brasil-conformant Account API",
 	testModules = {
 		AccountApiTestModule.class,
-		AccountsApiWrongPermissionsTestModule.class,
-		AccountApiWrongPermissionsTestModule2.class
+		AccountsApiWrongPermissionsTestModule.class
 	})
 public class AccountsApiTestPlan implements TestPlan {
 }
