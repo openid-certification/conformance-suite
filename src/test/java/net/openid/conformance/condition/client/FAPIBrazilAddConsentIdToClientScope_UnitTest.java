17af7a650bea130302e112e493be8da7ba37bd62
George McIntosh <george@georgemcintosh.com>
2021-07-08 15:48:37 +0100

    Fixing potentially unhandled NPE when setting up consents

---
+++ src/test/java/net/openid/conformance/condition/client/FAPIBrazilAddConsentIdToClientScope_UnitTest.java
@@ -0,0 +1,86 @@
+package net.openid.conformance.condition.client;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.ConditionError;
+import net.openid.conformance.logging.TestInstanceEventLog;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.testmodule.OIDFJSON;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FAPIBrazilAddConsentIdToClientScope_UnitTest {
+
+	@Spy
+	private Environment env = new Environment();
+
+	@Mock
+	private TestInstanceEventLog eventLog;
+
+	private FAPIBrazilAddConsentIdToClientScope condition;
+
+	@Test
+	public void addsConsentAsScopeIfProvided() {
+
+		JsonObject clientConfig = new JsonObject();
+		clientConfig.addProperty("scope", "openid");
+
+		env.putObject("client", clientConfig);
+		env.putString("consent_id", "consent_id");
+
+		condition.execute(env);
+		String scope = OIDFJSON.getString(clientConfig.get("scope"));
+
+		assertThat(scope, equalTo("openid consent:consent_id"));
+
+	}
+
+	@Test
+	public void errorsCorrectlyIfScopeMissing() {
+
+		JsonObject clientConfig = new JsonObject();
+
+		env.putObject("client", clientConfig);
+		env.putString("consent_id", "consent_id");
+
+		try {
+			condition.execute(env);
+		} catch(ConditionError ce) {
+			assertThat(ce.getMessage(), equalTo("FAPIBrazilAddConsentIdToClientScope: scope missing in client object"));
+		}
+
+	}
+
+	@Test
+	public void errorsCorrectlyIfScopeEmpty() {
+
+		JsonObject clientConfig = new JsonObject();
+		clientConfig.addProperty("scope", "");
+
+		env.putObject("client", clientConfig);
+		env.putString("consent_id", "consent_id");
+
+		try {
+			condition.execute(env);
+		} catch(ConditionError ce) {
+			assertThat(ce.getMessage(), equalTo("FAPIBrazilAddConsentIdToClientScope: scope empty in client object"));
+		}
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		condition = new FAPIBrazilAddConsentIdToClientScope();
+
+		condition.setProperties("UNIT-TEST", eventLog, Condition.ConditionResult.INFO);
+	}
+
+}
