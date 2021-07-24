c07a8d589e9408e056cf418c5909b63fea7de790
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-07-24 17:11:48 +0100

    Refactor CheckErrorFromDynamicRegistrationEndpoint conditions
    Push duplicated code up into a superclass.

--- src/main/java/net/openid/conformance/condition/client/CheckErrorFromDynamicRegistrationEndpointIsInvalidRedirectUriOrInvalidClientMetadata.java
+++ src/main/java/net/openid/conformance/condition/client/CheckErrorFromDynamicRegistrationEndpointIsInvalidRedirectUriOrInvalidClientMetadata.java
@@ -1,34 +1,14 @@
 package net.openid.conformance.condition.client;
 
-import java.util.List;
-
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
-import net.openid.conformance.condition.AbstractCondition;
-import net.openid.conformance.condition.PreEnvironment;
-import net.openid.conformance.testmodule.Environment;
+import java.util.List;
 
-public class CheckErrorFromDynamicRegistrationEndpointIsInvalidRedirectUriOrInvalidClientMetadata extends AbstractCondition {
-
-	private static final List<String> PERMITTED_ERRORS = ImmutableList.of("invalid_redirect_uri", "invalid_client_metadata");
+public class CheckErrorFromDynamicRegistrationEndpointIsInvalidRedirectUriOrInvalidClientMetadata extends AbstractCheckErrorFromDynamicRegistrationEndpoint {
 
 	@Override
-	@PreEnvironment(required = "dynamic_registration_endpoint_response")
-	public Environment evaluate(Environment env) {
-
-		String error = env.getString("dynamic_registration_endpoint_response", "error");
-
-		if (Strings.isNullOrEmpty(error)) {
-			throw error("'error' field not found in response from dynamic registration endpoint");
-		}
-
-		if (!PERMITTED_ERRORS.contains(error)) {
-			throw error("'error' field has unexpected value", args("permitted", PERMITTED_ERRORS, "actual", error));
-		}
-
-		logSuccess("Dynamic registration endpoint returned 'error'", args("permitted", PERMITTED_ERRORS, "error", error));
-
-		return env;
+	List<String> getPermittedErrors() {
+		return ImmutableList.of("invalid_redirect_uri", "invalid_client_metadata");
 	}
+
 }
