1fb37e84c32f8cc427286494b9bcf0d92f54a72e
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-08-04 16:07:08 +0100

    Update help for 'FAPI Response Mode'
    JARM is now accepted for certification, so tweak the text to make this
    clear.

--- src/main/java/net/openid/conformance/variant/FAPIResponseMode.java
+++ src/main/java/net/openid/conformance/variant/FAPIResponseMode.java
@@ -3,7 +3,7 @@ package net.openid.conformance.variant;
 @VariantParameter(
 	name = "fapi_response_mode",
 	displayName = "FAPI Response Mode",
-	description = "The response mode that will be used. 'Plain response' is required for FAPI certification. JARM responses are not currently part of the certification program. If in doubt, select 'plain_response'."
+	description = "The response mode that will be used. 'Plain response' is the most commonly used. JARM (JWT Secured Authorization Response Mode) is an option in the FAPI specifications. If in doubt, select 'plain_response'."
 )
 public enum FAPIResponseMode {
 
