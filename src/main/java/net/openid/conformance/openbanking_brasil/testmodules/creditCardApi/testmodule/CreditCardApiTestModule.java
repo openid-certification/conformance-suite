705c3d4f87e6ef4dbf17db4b8188c352fbbe442a
Ralph Bragg <ralph.bragg@raidiam.com>
2021-09-22 12:51:28 +0100

    Correcting a description typo

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/creditCardApi/testmodule/CreditCardApiTestModule.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/creditCardApi/testmodule/CreditCardApiTestModule.java
@@ -70,7 +70,7 @@ public class CreditCardApiTestModule extends AbstractOBBrasilFunctionalTestModul
 
 		callAndStopOnFailure(CardBillSelector.class);
 		callAndStopOnFailure(PrepareUrlForFetchingBillTransactionResource.class);
-		preCallProtectedResource("Fetch Credit Card bill transationc");
+		preCallProtectedResource("Fetch Credit Card bill transaction");
 		callAndContinueOnFailure(InvoiceCreditCardTransactionsValidator.class, Condition.ConditionResult.FAILURE);
 		callAndContinueOnFailure(EnsureResponseHasLinks.class, Condition.ConditionResult.FAILURE);
 		call(sequence(ValidateSelfEndpoint.class));
