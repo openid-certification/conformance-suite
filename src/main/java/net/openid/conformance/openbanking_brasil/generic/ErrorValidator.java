ad59fc5323368ca564f37f769560d7c8c53f13bf
Ross Haliburton <ross.haliburton@raidiam.com>
2021-09-30 11:15:28 +0100

    refactored error validator to handle it

--- src/main/java/net/openid/conformance/openbanking_brasil/generic/ErrorValidator.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/generic/ErrorValidator.java
@@ -1,29 +1,49 @@
 package net.openid.conformance.openbanking_brasil.generic;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.condition.client.AbstractJsonAssertingCondition;
 import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.util.JsonUtils;
 import net.openid.conformance.util.field.ArrayField;
 import net.openid.conformance.util.field.StringField;
+
+import java.util.Base64;
 
 public class ErrorValidator extends AbstractJsonAssertingCondition {
 
 	@Override
 	@PreEnvironment(strings = "resource_endpoint_response")
 	public Environment evaluate(Environment environment) {
+
+		boolean jwt;
+		String contentTypeStr = environment.getString("resource_endpoint_response_headers", "content-type");
+		jwt = contentTypeStr.contains("application/jwt");
 		JsonObject body;
-		if(environment.getString("resource_endpoint_response").equals("{}")) {
-			body = environment.getObject("errored_response");
-		} else {
-			body = bodyFrom(environment);
-		}
+
+		body = jwt ? getBodyFromJwt(environment) : getBodyFromJson(environment);
+
 		assertHasField(body, "$.errors");
 		assertOuterFields(body);
 		assertInnerFields(body);
 
 		return environment;
+	}
+
+	private JsonObject getBodyFromJwt(Environment environment) {
+		String response = environment.getString("resource_endpoint_response");
+		Base64.Decoder decoder = Base64.getDecoder();
+		String body = new String(decoder.decode(response.split("\\.")[1]));
+		Gson gson = JsonUtils.createBigDecimalAwareGson();
+		return gson.fromJson(body, JsonObject.class);
+	}
+
+	private JsonObject getBodyFromJson(Environment environment) {
+		if(environment.getString("resource_endpoint_response").equals("{}")) {
+			return environment.getObject("errored_response");
+		} else {
+			return bodyFrom(environment);
+		}
 	}
 
 	private void assertOuterFields(JsonObject body) {
@@ -41,25 +61,26 @@ public class ErrorValidator extends AbstractJsonAssertingCondition {
 	}
 	private void assertInnerFields(JsonObject body) {
 		JsonArray errors = findByPath(body, "$.errors").getAsJsonArray();
+		final String PATTERN = "[\\w\\W\\s]*";
 		errors.forEach(error -> {
 			assertField(error.getAsJsonObject(),
 				new StringField
 					.Builder("code")
-					.setPattern("[\\w\\W\\s]*")
+					.setPattern(PATTERN)
 					.setMaxLength(255)
 					.build());
 
 			assertField(error.getAsJsonObject(),
 				new StringField
 					.Builder("title")
-					.setPattern("[\\w\\W\\s]*")
+					.setPattern(PATTERN)
 					.setMaxLength(255)
 					.build());
 
 			assertField(error.getAsJsonObject(),
 				new StringField
 					.Builder("detail")
-					.setPattern("[\\w\\W\\s]*")
+					.setPattern(PATTERN)
 					.setMaxLength(2048)
 					.build());
 		});
