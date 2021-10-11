76a69bba8a0e4b343b95b4d91528eb3e8c0735fa
Michael Fraser <michael.fraser@raidiam.com>
2021-10-11 17:03:23 +0100

    Test for payment that isnt a pix

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/AbstractPaymentConsentPaymentTypeCondition.java
@@ -0,0 +1,21 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.testmodule.Environment;
+
+public abstract class AbstractPaymentConsentPaymentTypeCondition extends AbstractCondition {
+
+	@Override
+	public final Environment evaluate(Environment env) {
+		JsonObject obj = env.getObject("resource");
+		obj = obj.getAsJsonObject("brazilPaymentConsent");
+		obj = obj.getAsJsonObject("data");
+		obj = obj.getAsJsonObject("payment");
+		obj.addProperty("type", getPaymentType());
+		return env;
+	}
+
+	protected abstract String getPaymentType();
+
+}
