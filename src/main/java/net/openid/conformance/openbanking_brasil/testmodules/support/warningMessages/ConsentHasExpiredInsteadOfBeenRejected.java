5d26a34443085381dc9b351f243d48ad45dc5ebf
Michael Fraser <michael.fraser@raidiam.com>
2021-10-11 19:16:44 +0100

    added warning to check consent actually expired

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/warningMessages/ConsentHasExpiredInsteadOfBeenRejected.java
@@ -0,0 +1,16 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support.warningMessages;
+
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.client.AbstractJsonAssertingCondition;
+import net.openid.conformance.testmodule.Environment;
+
+public class ConsentHasExpiredInsteadOfBeenRejected extends AbstractJsonAssertingCondition {
+
+	@Override
+	@PostEnvironment(strings = "warning_message")
+	public Environment evaluate(Environment env) {
+		log("Setting warning message");
+		env.putString("warning_message", "To certifier: Check the consent actually expired instead of being manually rejected.");
+		return env;
+	}
+}
