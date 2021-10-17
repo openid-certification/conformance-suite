5287c15d4395a0d3087280058bf3d0e6ae643dd7
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-10-17 22:22:57 +0100

    FAPIBrazilDCR: Verify client id/config creds returned from config endpoint
    Note that we don't allow the registration access token to be rotated.
    This would seem a bad thing to do for the same reason refresh tokens
    shouldn't be rotated, i.e. there is a risk that the new token is not
    returned and client management is irretrievably lost.
    
    As requested from Brazil sandbox/security squad, row 18:
    
    https://docs.google.com/spreadsheets/d/15Wu1unFOIgZy6A-jmawc2QkYWLV9SZ0W/edit#gid=1277899644

--- src/main/java/net/openid/conformance/fapi1advancedfinal/AbstractFAPI1AdvancedFinalBrazilDCR.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/AbstractFAPI1AdvancedFinalBrazilDCR.java
@@ -14,6 +14,8 @@ import net.openid.conformance.condition.client.AddTlsClientAuthSubjectDnToDynami
 import net.openid.conformance.condition.client.AddTokenEndpointAuthMethodToDynamicRegistrationRequestFromEnvironment;
 import net.openid.conformance.condition.client.CallClientConfigurationEndpoint;
 import net.openid.conformance.condition.client.CallTokenEndpoint;
+import net.openid.conformance.condition.client.CheckClientConfigurationCredentialsFromClientConfigurationEndpoint;
+import net.openid.conformance.condition.client.CheckClientIdFromClientConfigurationEndpoint;
 import net.openid.conformance.condition.client.CheckForAccessTokenValue;
 import net.openid.conformance.condition.client.CheckIfTokenEndpointResponseError;
 import net.openid.conformance.condition.client.CheckRegistrationClientEndpointContentType;
@@ -196,6 +198,8 @@ public abstract class AbstractFAPI1AdvancedFinalBrazilDCR extends AbstractFAPI1A
 		callAndStopOnFailure(CallClientConfigurationEndpoint.class, "OIDCD-4.2");
 		callAndContinueOnFailure(CheckRegistrationClientEndpointContentTypeHttpStatus200.class, Condition.ConditionResult.FAILURE, "OIDCD-4.3");
 		callAndContinueOnFailure(CheckRegistrationClientEndpointContentType.class, Condition.ConditionResult.FAILURE, "OIDCD-4.3");
+		callAndContinueOnFailure(CheckClientIdFromClientConfigurationEndpoint.class, Condition.ConditionResult.FAILURE, "RFC7592-3");
+		callAndContinueOnFailure(CheckClientConfigurationCredentialsFromClientConfigurationEndpoint.class, Condition.ConditionResult.FAILURE, "RFC7592-3");
 
 		callAndContinueOnFailure(UnregisterDynamicallyRegisteredClient.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2.3");
 
