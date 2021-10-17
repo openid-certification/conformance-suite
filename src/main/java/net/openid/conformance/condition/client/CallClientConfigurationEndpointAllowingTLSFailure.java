334de75ef6df5dc367da4b0410f98a2f807e35e1
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-10-17 20:28:05 +0100

    FAPIBrazilDCR: Extend 'no MTLS' checks to config endpoint GET / DELETE

---
+++ src/main/java/net/openid/conformance/condition/client/CallClientConfigurationEndpointAllowingTLSFailure.java
@@ -0,0 +1,31 @@
+package net.openid.conformance.condition.client;
+
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
+
+import javax.net.ssl.SSLException;
+
+public class CallClientConfigurationEndpointAllowingTLSFailure extends CallClientConfigurationEndpoint {
+
+	public static final String RESPONSE_SSL_ERROR_KEY = "client_configuration_endpoint_response_ssl_error";
+
+	@Override
+	@PreEnvironment(required = "client")
+	public Environment evaluate(Environment env) {
+		env.putBoolean(RESPONSE_SSL_ERROR_KEY, false);
+		return super.evaluate(env);
+	}
+
+	@Override
+	protected Environment handleClientException(Environment env, String registrationClientUri, RestClientException e) {
+		if (e instanceof ResourceAccessException && e.getCause() instanceof SSLException) {
+			env.putBoolean(RESPONSE_SSL_ERROR_KEY, true);
+			logSuccess("Call to client configuration endpoint "+registrationClientUri+" failed due to a TLS issue", ex(e));
+			return env;
+		}
+		return super.handleClientException(env, registrationClientUri, e);
+	}
+
+}
--- src/main/java/net/openid/conformance/fapi1advancedfinal/FAPI1AdvancedFinalBrazilDCRNoMTLS.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/FAPI1AdvancedFinalBrazilDCRNoMTLS.java
@@ -1,10 +1,18 @@
 package net.openid.conformance.fapi1advancedfinal;
 
 import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.CallClientConfigurationEndpoint;
+import net.openid.conformance.condition.client.CallClientConfigurationEndpointAllowingTLSFailure;
 import net.openid.conformance.condition.client.CallDynamicRegistrationEndpointAllowingTLSFailure;
 import net.openid.conformance.condition.client.CheckDynamicRegistrationEndpointReturnedError;
+import net.openid.conformance.condition.client.CheckNoClientIdFromClientConfigurationEndpoint;
+import net.openid.conformance.condition.client.CheckRegistrationClientEndpointContentType;
+import net.openid.conformance.condition.client.CheckRegistrationClientEndpointContentTypeHttpStatus200;
 import net.openid.conformance.condition.client.EnsureContentTypeJson;
 import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs400;
+import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs401;
+import net.openid.conformance.condition.client.UnregisterDynamicallyRegisteredClient;
+import net.openid.conformance.condition.client.UnregisterDynamicallyRegisteredClientExpectingFailure;
 import net.openid.conformance.testmodule.PublishTestModule;
 
 @PublishTestModule(
@@ -34,7 +42,7 @@ public class FAPI1AdvancedFinalBrazilDCRNoMTLS extends AbstractFAPI1AdvancedFina
 
 	@Override
 	protected void callRegistrationEndpoint() {
-		env.removeObject("mutual_tls_authentication");
+		env.mapKey("mutual_tls_authentication", "none_existent_key");
 
 		callAndStopOnFailure(CallDynamicRegistrationEndpointAllowingTLSFailure.class);
 
@@ -49,10 +57,62 @@ public class FAPI1AdvancedFinalBrazilDCRNoMTLS extends AbstractFAPI1AdvancedFina
 			// an error to be returned in this case doesn't really seem to be defined anywhere, so allow any error
 			callAndContinueOnFailure(CheckDynamicRegistrationEndpointReturnedError.class, Condition.ConditionResult.FAILURE);
 		}
+
+		env.unmapKey("mutual_tls_authentication");
+
+		super.callRegistrationEndpoint();
+	}
+
+	@Override
+	protected void onPostAuthorizationFlowComplete() {
+		eventLog.startBlock("Call client configuration endpoint with no certificate");
+		env.mapKey("mutual_tls_authentication", "none_existent_key");
+
+		callAndStopOnFailure(CallClientConfigurationEndpoint.class, "OIDCD-4.2");
+		callAndContinueOnFailure(CheckRegistrationClientEndpointContentTypeHttpStatus200.class, Condition.ConditionResult.FAILURE, "OIDCD-4.3");
+		callAndContinueOnFailure(CheckRegistrationClientEndpointContentType.class, Condition.ConditionResult.FAILURE, "OIDCD-4.3");
+
+		callAndContinueOnFailure(UnregisterDynamicallyRegisteredClient.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2.3");
+
+		// we just deregistered the client, so prevent cleanup from trying to do so again
+		env.removeNativeValue("registration_client_uri");
+		env.removeNativeValue("registration_access_token");
+
+		super.onPostAuthorizationFlowComplete();
 	}
 
 	@Override
 	public void start() {
+		setStatus(Status.RUNNING);
+
+		eventLog.startBlock("Call client configuration endpoint with no certificate");
+		env.mapKey("mutual_tls_authentication", "none_existent_key");
+
+		callAndStopOnFailure(CallClientConfigurationEndpointAllowingTLSFailure.class, "OIDCD-4.2");
+		boolean sslError = env.getBoolean(CallClientConfigurationEndpointAllowingTLSFailure.RESPONSE_SSL_ERROR_KEY);
+		if (sslError) {
+			// the ssl connection was dropped; that's an acceptable way for a server to indicate that a TLS client cert
+			// is required, so there's no further checks to do
+		} else {
+			call(exec().mapKey("endpoint_response", "registration_client_endpoint_response"));
+
+			callAndContinueOnFailure(EnsureHttpStatusCodeIs401.class, Condition.ConditionResult.FAILURE);
+			callAndContinueOnFailure(CheckNoClientIdFromClientConfigurationEndpoint.class, Condition.ConditionResult.FAILURE);
+
+			call(exec().unmapKey("endpoint_response"));
+		}
+
+
+		callAndContinueOnFailure(UnregisterDynamicallyRegisteredClientExpectingFailure.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2.3");
+
+		env.unmapKey("mutual_tls_authentication");
+
+		callAndContinueOnFailure(UnregisterDynamicallyRegisteredClient.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2.3");
+
+		// we just deregistered the client, so prevent cleanup from trying to do so again
+		env.removeNativeValue("registration_client_uri");
+		env.removeNativeValue("registration_access_token");
+
 		fireTestFinished();
 	}
 }
