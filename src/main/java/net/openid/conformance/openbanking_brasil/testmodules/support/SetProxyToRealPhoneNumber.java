cec8a6f75cf27b0dba1677b19e314b1dc041c91c
Ross Haliburton <ross.haliburton@raidiam.com>
2021-10-18 11:22:12 +0100

    updated phone number, based on ticket 3305

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/SetProxyToRealPhoneNumber.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/SetProxyToRealPhoneNumber.java
@@ -13,7 +13,7 @@ public class SetProxyToRealPhoneNumber extends AbstractCondition {
 		obj = obj.getAsJsonObject("data");
 		obj = obj.getAsJsonObject("payment");
 		obj = obj.getAsJsonObject("details");
-		obj.addProperty("proxy", "+5511999998888");
+		obj.addProperty("proxy", "+5511986935789");
 
 		logSuccess("Added phone number as proxy to payment consent");
 
