490aee4e9f8ba3a4e0087829cdc0505549df059b
Ross Haliburton <ross.haliburton@raidiam.com>
2021-10-22 21:14:55 +0100

    Ensure proxy on both tests, update creditor

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/EnsureProxyPresentInConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/EnsureProxyPresentInConfig.java
@@ -12,9 +12,11 @@ public class EnsureProxyPresentInConfig extends AbstractCondition {
 		JsonObject obj = env.getObject("resource");
 		obj = obj.getAsJsonObject("brazilPaymentConsent");
 		obj = obj.getAsJsonObject("data");
+		log(obj);
 		obj = obj.getAsJsonObject("payment");
 		obj = obj.getAsJsonObject("details");
 		JsonElement proxy = obj.get("proxy");
+
 		if (proxy == null) {
 			logSuccess("Proxy not found, adding one");
 			obj.addProperty("proxy", "cliente-000000@pix.bcb.gov.br");
@@ -25,7 +27,8 @@ public class EnsureProxyPresentInConfig extends AbstractCondition {
 					.getAsJsonObject("payment")
 			);
 		} else {
-			logSuccess("Proxy found.");
+			logSuccess("Proxy found, still adding the correct proxy for the creditor account");
+			obj.addProperty("proxy", "cliente-000000@pix.bcb.gov.br");
 			logSuccess("Payment: ",
 				env.getObject("resource")
 					.getAsJsonObject("brazilPaymentConsent")
@@ -33,6 +36,28 @@ public class EnsureProxyPresentInConfig extends AbstractCondition {
 					.getAsJsonObject("payment")
 			);
 		}
+
+		log("Ensuring creditor account is correct and in config");
+		JsonObject creditorAccount = new JsonObject();
+		creditorAccount.addProperty("number", "22917383379");
+		creditorAccount.addProperty("accountType", "CACC");
+		creditorAccount.addProperty("ispb", "99999060");
+		creditorAccount.addProperty("issuer", "0024");
+
+		obj.add("creditorAccount", creditorAccount);
+		logSuccess("Added creditorAccount: ", obj);
+
+		log("Updating creditor");
+		JsonObject creditor = new JsonObject();
+		creditor.addProperty("personType", "PESSOA_NATURAL");
+		creditor.addProperty("cpfCnpj", "11122233300");
+		creditor.addProperty("name", "Joao Silva");
+
+		obj = env.getObject("resource").getAsJsonObject("brazilPaymentConsent").getAsJsonObject("data");
+		obj.add("creditor", creditor);
+		logSuccess("Updated creditor: ",
+			env.getObject("resource").getAsJsonObject("brazilPaymentConsent").getAsJsonObject("data")
+		);
 		return env;
 	}
 }
