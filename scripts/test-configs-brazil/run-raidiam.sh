8f3c81da335b422a7d42bee3ab4f64b044562405
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-10-31 12:58:19 +0000

    Update CI to run tests against raidiam mockbank

---
+++ scripts/test-configs-brazil/run-raidiam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TESTS=""
+
+TESTS+="fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=openbanking_brazil][fapi_auth_request_method=by_value][fapi_response_mode=plain_response] scripts/test-configs-brazil/brazil-raidiam-fapi-payments-automated.json "
+TESTS+="fapi1-advanced-final-brazil-dcr-test-plan[client_auth_type=private_key_jwt][fapi_auth_request_method=by_value][fapi_response_mode=plain_response] scripts/test-configs-brazil/brazil-raidiam-fapi-dcr-payments-automated.json "
+
+./scripts/run-test-plan.py $TESTS
