42cdacc4c64d460d5bd75dc7b3b7d11431f26772
joolsRaidiam <jools.houston@raidiam.com>
2021-10-31 18:54:41 +0000

    201-200-refactor

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureResponseCodeWas201or200.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureResponseCodeWas201or200.java
@@ -1,31 +1,20 @@
 package net.openid.conformance.openbanking_brasil.testmodules.support;
 
-import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
-import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
-import net.openid.conformance.testmodule.OIDFJSON;
-import org.springframework.http.HttpStatus;
 
-import java.util.Map;
 
 public class EnsureResponseCodeWas201or200 extends AbstractCondition {
 	@Override
-//	@PreEnvironment(required = "endpoint_response")
 	public Environment evaluate(Environment env) {
 		int statusCode = env.getInteger("resource_endpoint_response_status");
-//		String endpointName = env.getString("endpoint_response", "endpoint_name");
 
 		if(statusCode == org.apache.http.HttpStatus.SC_OK) {
 			logSuccess("endpoint returned an http status of 200 - ending test now", args("http_status", statusCode));
-		}
-
-		if(statusCode == org.apache.http.HttpStatus.SC_CREATED) {
+		} else if(statusCode == org.apache.http.HttpStatus.SC_CREATED) {
 			logSuccess("endpoint returned an http status of 2301 - proceeding with test now", args("http_status", statusCode));
 			env.putString("proceed_with_test", "proceed");
-		}
-
-		if (statusCode != org.apache.http.HttpStatus.SC_CREATED && statusCode != org.apache.http.HttpStatus.SC_UNPROCESSABLE_ENTITY) {
+		} else {
 			throw error("endpoint returned an unexpected http status - either 201 or 200 accepted", args("http_status", statusCode));
 		}
 
