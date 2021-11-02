41f28c00b2bec39aa5800a3421b59f274dff1eaa
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-11-02 19:58:26 +0000

    Update certificates used in payment API DCR tests
    There were issues with the cnpj previously being invalid.
    
    relates to #86

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OverrideClientWithPagtoClient.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OverrideClientWithPagtoClient.java
@@ -1,79 +1,80 @@
 package net.openid.conformance.openbanking_brasil.testmodules.support;
 
 public class OverrideClientWithPagtoClient extends AbstractOverrideClient {
+	// software id is 368bceef-6096-4e1a-a702-64c07d12c98a
 
 	@Override
 	String clientCert() {
 		return "-----BEGIN CERTIFICATE-----\n" +
-			"MIIG+zCCBeOgAwIBAgIUL2wvQ+efTa6gQJC5DI4g6cLOYfAwDQYJKoZIhvcNAQEL\n" +
+			"MIIHDDCCBfSgAwIBAgIUYvUFpecbRLx1Dt2CwJQpQW7x7AowDQYJKoZIhvcNAQEL\n" +
 			"BQAwcTELMAkGA1UEBhMCQlIxHDAaBgNVBAoTE09wZW4gQmFua2luZyBCcmFzaWwx\n" +
 			"FTATBgNVBAsTDE9wZW4gQmFua2luZzEtMCsGA1UEAxMkT3BlbiBCYW5raW5nIFNB\n" +
-			"TkRCT1ggSXNzdWluZyBDQSAtIEcxMB4XDTIxMTAyOTAxMTkwMFoXDTIyMTEyODAx\n" +
-			"MTkwMFowggEeMQswCQYDVQQGEwJCUjELMAkGA1UECBMCUkoxETAPBgNVBAcTCEJP\n" +
-			"VEFGT0dPMRwwGgYDVQQKExNPcGVuIEJhbmtpbmcgQnJhc2lsMS0wKwYDVQQLEyQ3\n" +
-			"NGU5MjlkOS0zM2I2LTRkODUtOGJhNy1jMTQ2Yzg2N2E4MTcxHzAdBgNVBAMTFm1v\n" +
-			"Y2stcGFndG8ucmFpZGlhbS5jb20xFjAUBgNVBAUTDTEzMzUzMjM2MDAxODkxHjAc\n" +
-			"BgNVBA8TFU5vbi1Db21tZXJjaWFsIEVudGl0eTETMBEGCysGAQQBgjc8AgEDEwJC\n" +
-			"UjE0MDIGCgmSJomT8ixkAQETJDM2OGJjZWVmLTYwOTYtNGUxYS1hNzAyLTY0YzA3\n" +
-			"ZDEyYzk4YTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANjjR7vOEU7E\n" +
-			"BkMU15SzsoABYqMQdkWwZfGWt2DFvYBvYC3I9jR9Fl4W5sVk9WX7YGr5uIviATYK\n" +
-			"finLLlsmIuhi2wSdGhi3a3aPACtYtrriOrRqKPfBmbes1b/GxxEcdXz+4gdMPXHy\n" +
-			"46Zs2NeqfjEI9mhOWV3O91lLlF/tGGjyCPqP2r45G+2nLOqIQfAI16TqI02z+Hmv\n" +
-			"/tQpBPfOd6LX9hEzCSNtDxYtLZ6MnxZsKDvCS7x2jSOmiBBzhR+U787MDhupnIZq\n" +
-			"qFM8w9dHFdM4NO9lUEYQpvcTdhxbmsbiXJHXl/gwRt+psfbJor1ESl0PoM5dWI7c\n" +
-			"JyTu5gvcf2sCAwEAAaOCAtowggLWMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFCkB\n" +
-			"Jgw37O5X3tWIdd3z6l6GKDA4MB8GA1UdIwQYMBaAFIZ/WK0X9YK2TrQFs/uwzhFD\n" +
-			"30y+MEwGCCsGAQUFBwEBBEAwPjA8BggrBgEFBQcwAYYwaHR0cDovL29jc3Auc2Fu\n" +
-			"ZGJveC5wa2kub3BlbmJhbmtpbmdicmFzaWwub3JnLmJyMEsGA1UdHwREMEIwQKA+\n" +
-			"oDyGOmh0dHA6Ly9jcmwuc2FuZGJveC5wa2kub3BlbmJhbmtpbmdicmFzaWwub3Jn\n" +
-			"LmJyL2lzc3Vlci5jcmwwIQYDVR0RBBowGIIWbW9jay1wYWd0by5yYWlkaWFtLmNv\n" +
-			"bTAOBgNVHQ8BAf8EBAMCBaAwEwYDVR0lBAwwCgYIKwYBBQUHAwIwggGhBgNVHSAE\n" +
-			"ggGYMIIBlDCCAZAGCisGAQQBg7ovZAEwggGAMIIBNgYIKwYBBQUHAgIwggEoDIIB\n" +
-			"JFRoaXMgQ2VydGlmaWNhdGUgaXMgc29sZWx5IGZvciB1c2Ugd2l0aCBSYWlkaWFt\n" +
-			"IFNlcnZpY2VzIExpbWl0ZWQgYW5kIG90aGVyIHBhcnRpY2lwYXRpbmcgb3JnYW5p\n" +
-			"c2F0aW9ucyB1c2luZyBSYWlkaWFtIFNlcnZpY2VzIExpbWl0ZWRzIFRydXN0IEZy\n" +
-			"YW1ld29yayBTZXJ2aWNlcy4gSXRzIHJlY2VpcHQsIHBvc3Nlc3Npb24gb3IgdXNl\n" +
-			"IGNvbnN0aXR1dGVzIGFjY2VwdGFuY2Ugb2YgdGhlIFJhaWRpYW0gU2VydmljZXMg\n" +
-			"THRkIENlcnRpY2ljYXRlIFBvbGljeSBhbmQgcmVsYXRlZCBkb2N1bWVudHMgdGhl\n" +
-			"cmVpbi4wRAYIKwYBBQUHAgEWOGh0dHA6Ly9jcHMuc2FuZGJveC5wa2kub3BlbmJh\n" +
-			"bmtpbmdicmFzaWwub3JnLmJyL3BvbGljaWVzMA0GCSqGSIb3DQEBCwUAA4IBAQAB\n" +
-			"LM2dOfVev4khfkpThUwHD6pRzUneEX8OZj62fJ2nqTt++LoPJZy+trsddYfuDp5Q\n" +
-			"Gys8RWB1f3+Dc26265KRERVMegLrBvyWZzP1ZSS3CHZ4Bx5LPqMEgMhd1jxqI/IE\n" +
-			"0sUoDiw77VJJv/I5IkYLD1iIIr0jnzE559QxrGe0fl8ca56P71+NFSlBUQJRYTA5\n" +
-			"UGQ09iZc+Cx3fzO7yuaY4uNtm5lP/06W5GNPCI5ZPNNRtSdXqJlBO50q9RUw/aZG\n" +
-			"DSnVogooknPIre67CLlS0mn9TFPhQ7l7ktPurSWSNSRU5HcOv4Zugwpaix0MNWKT\n" +
-			"eFXxTwSgH+mElLyWqSaZ\n" +
+			"TkRCT1ggSXNzdWluZyBDQSAtIEcxMB4XDTIxMTEwMjE4NTMwMFoXDTIyMTIwMjE4\n" +
+			"NTMwMFowggEnMQswCQYDVQQGEwJCUjELMAkGA1UECBMCU1AxEjAQBgNVBAcTCVNh\n" +
+			"byBQYXVsbzEcMBoGA1UEChMTT3BlbiBCYW5raW5nIEJyYXNpbDEtMCsGA1UECxMk\n" +
+			"NzRlOTI5ZDktMzNiNi00ZDg1LThiYTctYzE0NmM4NjdhODE3MScwJQYDVQQDEx50\n" +
+			"cHAuc2FuZGJveC5vcGVuYmFua2luZy5vcmcuYnIxFzAVBgNVBAUTDjQzMTQyNjY2\n" +
+			"MDAwMTk3MTQwMgYKCZImiZPyLGQBARMkMzY4YmNlZWYtNjA5Ni00ZTFhLWE3MDIt\n" +
+			"NjRjMDdkMTJjOThhMR0wGwYDVQQPExRQcml2YXRlIE9yZ2FuaXphdGlvbjETMBEG\n" +
+			"CysGAQQBgjc8AgEDEwJCUjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" +
+			"ALeG7Q28qLiQMnIhaOMVIQzkhPxy9ARpHs6Ro8wrZiagAu3KN7rYaOBiRxRKCa9B\n" +
+			"fCnjmM7e3bW956/+nfp0wque2FHeiw+xhYyRzkP0kqlwiS+ctj6XrO/fv6zBMdMZ\n" +
+			"Gg5qYo2WaKnovouVlmSsyXGKkf5PSBl0po2fc0/G4lKF5znKRItDlnEFJenUSqmo\n" +
+			"/fFOzP18WwefUHlF21eZWO9Zj7vUtbSoVV9wBB2yw3V8V2iU53oy/N5LKgxdauB2\n" +
+			"Q+71oZIterf83Cu4yLZ0qVZFRwAt7ITJjnkXh7kUXXztHSUxb3lzhp9GRXWMn0eB\n" +
+			"jZbyyn8SbkqUwfSGYYl9W0sCAwEAAaOCAuIwggLeMAwGA1UdEwEB/wQCMAAwHwYD\n" +
+			"VR0jBBgwFoAUhn9YrRf1grZOtAWz+7DOEUPfTL4wTAYIKwYBBQUHAQEEQDA+MDwG\n" +
+			"CCsGAQUFBzABhjBodHRwOi8vb2NzcC5zYW5kYm94LnBraS5vcGVuYmFua2luZ2Jy\n" +
+			"YXNpbC5vcmcuYnIwSwYDVR0fBEQwQjBAoD6gPIY6aHR0cDovL2NybC5zYW5kYm94\n" +
+			"LnBraS5vcGVuYmFua2luZ2JyYXNpbC5vcmcuYnIvaXNzdWVyLmNybDAOBgNVHQ8B\n" +
+			"Af8EBAMCBaAwEwYDVR0lBAwwCgYIKwYBBQUHAwIwHQYDVR0OBBYEFJtxiQ1FP67l\n" +
+			"hmQARlLzBHqW9yTzMCkGA1UdEQQiMCCCHnRwcC5zYW5kYm94Lm9wZW5iYW5raW5n\n" +
+			"Lm9yZy5icjCCAaEGA1UdIASCAZgwggGUMIIBkAYKKwYBBAGDui9kATCCAYAwggE2\n" +
+			"BggrBgEFBQcCAjCCASgMggEkVGhpcyBDZXJ0aWZpY2F0ZSBpcyBzb2xlbHkgZm9y\n" +
+			"IHVzZSB3aXRoIFJhaWRpYW0gU2VydmljZXMgTGltaXRlZCBhbmQgb3RoZXIgcGFy\n" +
+			"dGljaXBhdGluZyBvcmdhbmlzYXRpb25zIHVzaW5nIFJhaWRpYW0gU2VydmljZXMg\n" +
+			"TGltaXRlZHMgVHJ1c3QgRnJhbWV3b3JrIFNlcnZpY2VzLiBJdHMgcmVjZWlwdCwg\n" +
+			"cG9zc2Vzc2lvbiBvciB1c2UgY29uc3RpdHV0ZXMgYWNjZXB0YW5jZSBvZiB0aGUg\n" +
+			"UmFpZGlhbSBTZXJ2aWNlcyBMdGQgQ2VydGljaWNhdGUgUG9saWN5IGFuZCByZWxh\n" +
+			"dGVkIGRvY3VtZW50cyB0aGVyZWluLjBEBggrBgEFBQcCARY4aHR0cDovL2Nwcy5z\n" +
+			"YW5kYm94LnBraS5vcGVuYmFua2luZ2JyYXNpbC5vcmcuYnIvcG9saWNpZXMwDQYJ\n" +
+			"KoZIhvcNAQELBQADggEBANFOZt51FHjK64n5MbZkXqXef/9UCJx6QR+Hnu35tDmX\n" +
+			"7qhq5FE8c1JC4LUNbT5i2rmQSJctlab2nyTb89UsrLj+MIF7Do0jV3dSmfE9ACbk\n" +
+			"7QluLscNywUNDElBCMyR5J6OB4MpvfPMXuakDGjSuOUJzD/SLBj7dndaAEEdcVnU\n" +
+			"9koP/ogddAjT5zPmAEUh7/dD2Z8q7J6emW1wqlM6qkXiPQaPpvwhid07LK2/kM/d\n" +
+			"M4jV1Zm6m/5lacLiQmUJSf2rP94Ub29OXzJHjf+vvbhjdtReplWqNx4jzlZRbO1q\n" +
+			"qcJMtpaq+5UaVsptwcG5nwrp4goqNeDx12vO32+hbmo=\n" +
 			"-----END CERTIFICATE-----";
 	}
 	@Override
 	String clientKey() {
 		return "-----BEGIN PRIVATE KEY-----\n" +
-			"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDY40e7zhFOxAZD\n" +
-			"FNeUs7KAAWKjEHZFsGXxlrdgxb2Ab2AtyPY0fRZeFubFZPVl+2Bq+biL4gE2Cn4p\n" +
-			"yy5bJiLoYtsEnRoYt2t2jwArWLa64jq0aij3wZm3rNW/xscRHHV8/uIHTD1x8uOm\n" +
-			"bNjXqn4xCPZoTlldzvdZS5Rf7Rho8gj6j9q+ORvtpyzqiEHwCNek6iNNs/h5r/7U\n" +
-			"KQT3znei1/YRMwkjbQ8WLS2ejJ8WbCg7wku8do0jpogQc4UflO/OzA4bqZyGaqhT\n" +
-			"PMPXRxXTODTvZVBGEKb3E3YcW5rG4lyR15f4MEbfqbH2yaK9REpdD6DOXViO3Cck\n" +
-			"7uYL3H9rAgMBAAECggEAKzc6S2MXksS/ZK1p+VDG//eGsmwszN0Fqxo3Ztpv1IAc\n" +
-			"v3K8ECBejRfuGqXvucbZYudoTMMXL6/ujKBWF8ZlDT3pWyV8ljEyAfkjdP+jxWAf\n" +
-			"h2RhzmAuo2kWXDSc5L1xcZsN6ZtY4PQfRUGKAYsQwu+Ava7i4qzDH3FOWsDQ7NPz\n" +
-			"aVEEHd+BxWxE1Hf6HCaqiwHRLcFupCJYXjGE86rSLSdtqzF42QZdm2EKjDB9mx2A\n" +
-			"J3RNbXpTqBTb0Pf7oWMlKzBbbcWCwlOn6pdrtPQ0wgW37qjRCD9KP6WPA+7Nx2Ot\n" +
-			"+huOXyhTFCmpYd8dYMGTbqImgNkrpA1jGy4RR0MEAQKBgQDvDIj7z9jDbpfxvF5O\n" +
-			"2GEh2tgnhnYOs7QfrGurnYtElSvG8kUxJvqNyROSlEIlOEyCC2f/50yUNUjGYlve\n" +
-			"FEW+WZZW5ltgk4nOf8NlQISurnQkL3FgkJpxPhQq5M3Rg20bxBOCx35b2J9KPxjF\n" +
-			"jU+UsOrFN2f6EDY2AGKDujszawKBgQDoRHPwgiZJDLfFukIrPGrL4TcyvymsiehM\n" +
-			"BpnkZpaPH994ymS7XT0AwJq6ucazjPXqpbGf/M7opFTCol0AApSDHLTnmTCLDe8C\n" +
-			"k//O+MgS+pGL7LlG9KEung4hhO9K81jC3wvaj1v6mmtivMm6Frdu8lMy/BeyMyb+\n" +
-			"GYmK0wLkAQKBgGFsFWOyqF+1cb02DKVLcAKRIuiw1upU+IK0DUlqsnhn5uh0khhf\n" +
-			"D1u7Z/uj6nUqxZt2NJNJW59HO/qF2XTfhDD4Z9nQlcr9NftvfF2GdsyOEo0wvJ8j\n" +
-			"L54ZM7TrfKn3Sv824NC6ptedqAVlQ543YghyNavcUcbK1p0ZeQzAWCIzAoGBAL8M\n" +
-			"x10WgQ1nU6ZlrdXIjYQETZYFXe8ZRKV8bItNwxwZOIUv3amsaOg8hUmAVCwOj50i\n" +
-			"C7gwh7UersWmLd87QBOUwk0aWa6XmHjDahBB3LiXuK6i2ke+IvKoebEimn5JKkKo\n" +
-			"myR89UhuSlReO4RA+UNeOgdfCtK+dp1ePh+dr+wBAoGASP8/tivXPA5m2nSKSScf\n" +
-			"JuWW/3Vm4/CgFYqeIkD9fgY3VqrHwjTTjLdvvlAPD97cxODNbsPkJqdq8mjeVQ5g\n" +
-			"FXZQjGS8GmQ2AD0JSez4jPF+KBFcCANAe04jWgLr6CcNT+1aM6pa+VaPKo2coJM6\n" +
-			"dc7ydJkJRNosqFFYyqJTjTw=\n" +
+			"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3hu0NvKi4kDJy\n" +
+			"IWjjFSEM5IT8cvQEaR7OkaPMK2YmoALtyje62GjgYkcUSgmvQXwp45jO3t21veev\n" +
+			"/p36dMKrnthR3osPsYWMkc5D9JKpcIkvnLY+l6zv37+swTHTGRoOamKNlmip6L6L\n" +
+			"lZZkrMlxipH+T0gZdKaNn3NPxuJShec5ykSLQ5ZxBSXp1EqpqP3xTsz9fFsHn1B5\n" +
+			"RdtXmVjvWY+71LW0qFVfcAQdssN1fFdolOd6MvzeSyoMXWrgdkPu9aGSLXq3/Nwr\n" +
+			"uMi2dKlWRUcALeyEyY55F4e5FF187R0lMW95c4afRkV1jJ9HgY2W8sp/Em5KlMH0\n" +
+			"hmGJfVtLAgMBAAECggEBAKeNxrUI7dz6wSOTvwe7TZ5Qr90vMfso4dhj/PA1GyDe\n" +
+			"a24fdbvfQfjBoPU0Fx1rIum3gdY0gKQhifoS8rYrjWNUNVihXcJpJ21L3M4L5y5M\n" +
+			"sOLIJ1aD8X5af8s/4rZAr5pDpFBx5oJhHD2hx1mIqq0/fsR4K3IaHskjrwpBoHUH\n" +
+			"gEoOFZQbfTo3BBI+IZaRnfXg3mgitZ/28QH6uwtQvxTyGr2vTjElqzTNHuOZKkrh\n" +
+			"jjoFWR0dBNJky/FtXddeDzJCkTID3Rkr7wzWlntEw4dsR2jFct59VQ+i0x/KKyI8\n" +
+			"QqqGWrqZL4oxvv9C/TaJ1VQO4HA/bIe8IRJVGxWzU7kCgYEA6aDtpYw4nMp68Jyo\n" +
+			"/ymE3D+aB7JZOMwJL+x2ocHgeESGeZxq7MoNSGei7+zWR9aemNwqjL0ZSbi2A8ty\n" +
+			"pcFtcZ7EdlpB5VUG9eZI1pl3MtOAs6xcoHqEVm9dqGyAWw77ilj0rJqZyxgq+4y2\n" +
+			"7l5KlMnxuo9GEqXbGP8KsBr5pqUCgYEAyRnUTQJmuTmbNt3FcbSKO7f3Z2vqN4QK\n" +
+			"q9+l6THK7EWoJvoXTHfE8NAck3z9XlVh3tyzUQDArhZ30X+SOl9YnVN1J0eXqKcg\n" +
+			"bXZdRc6cHQYLP+3geGwLJLhph7sur0Gq0a055Ls6Ox4Wdi8mHiDAHRZ9+R9w2DAV\n" +
+			"jCmBb/uwRy8CgYBRJXdFl+WV9/IpnpBISDiAc4rhf33j72KFhcFSv89ilpKKrNyq\n" +
+			"sCda7bw3T3DB9PaTK22QAqJE6+Y0fTauAfgsjbO5U/ItYJIRWNIVz6oKOlIqlgR6\n" +
+			"LPPIPdgvBriyaArQa8NYLD6cvyE2zBJByvhmo5nVbawuD/OAxB0HO+dYjQKBgFvM\n" +
+			"87I332a8mlNAh6pxTQ0X3NOUWrX/C9QL4zQBq+2RmsI4NjyNDBUWG4VkcFEIzwWq\n" +
+			"YB0hJ9QQo5+6Caml7tDa0UceEmhF6rKtbsS4HWHOaYJEd8zhHXEk0d/JHfWZF35i\n" +
+			"AmSc52cnQ/+tZEwqjSh6JQV9SckWoi96nwDct0q3AoGASkv35OF4cVFvIM570m8v\n" +
+			"fMfALQWfafoJn/gNV9JIru1d4QsS40eaKMtbQkkgbIZoRkRd9bYrrqcLlOzg34CG\n" +
+			"Twh9NPEmbJUe7wxwcQuh9wrJ5cGCkNfOuwwK/dTkk2cPGdveCKzaqA9Wgo1lV3pK\n" +
+			"oWvBNoS85NEcmu9OX90rq3Q=\n" +
 			"-----END PRIVATE KEY-----\n";
 	}
 
