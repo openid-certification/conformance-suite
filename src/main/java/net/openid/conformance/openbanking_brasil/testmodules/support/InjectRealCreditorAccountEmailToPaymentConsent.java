445c0af4f064d8fd8064e623643283de945bbc0b
Ross Haliburton <ross.haliburton@raidiam.com>
2021-11-03 09:36:12 +0000

    Updated the email creditor cpf

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/InjectRealCreditorAccountEmailToPaymentConsent.java
@@ -0,0 +1,30 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.testmodule.Environment;
+
+public class InjectRealCreditorAccountEmailToPaymentConsent extends AbstractCondition {
+
+	@Override
+	public Environment evaluate(Environment env) {
+		JsonObject obj = (JsonObject) env.getElementFromObject("resource", "brazilPaymentConsent.data");
+
+		JsonObject creditor = obj.getAsJsonObject("creditor");
+		creditor.addProperty("name", "Joao Silva");
+		creditor.addProperty("cpfCnpj", "99991111140");
+		creditor.addProperty("personType", "PESSOA_NATURAL");
+
+		obj = obj.getAsJsonObject("payment");
+		obj = obj.getAsJsonObject("details");
+		obj = obj.getAsJsonObject("creditorAccount");
+
+		obj.addProperty("issuer", "0001");
+		obj.addProperty("number", "12345678");
+		obj.addProperty("accountType", "CACC");
+		obj.addProperty("ispb", "99999004");
+		logSuccess("Added real, working creditor account details to payment consent");
+
+		return env;
+	}
+}
