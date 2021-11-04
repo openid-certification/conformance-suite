e4f426ec562da223a26d2f11b205396b120f14a2
Erick  Domingues <erick.domingues@mirow.com.br>
2021-11-04 19:54:05 +0000

    Update DictHomologKeys.java

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/DictHomologKeys.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/DictHomologKeys.java
@@ -3,12 +3,12 @@ package net.openid.conformance.openbanking_brasil.testmodules.support.payments;
 public class DictHomologKeys {
 	public static final String PROXY_EMAIL = "cliente-a00001@pix.bcb.gov.br";
 	public static final String PROXY_EMAIL_BANK_CODE = "998";
-	public static final String PROXY_EMAIL_ACCOUNT_NUMBER = "11345678";
-	public static final String PROXY_EMAIL_BRANCH_NUMBER = "0002";
+	public static final String PROXY_EMAIL_ACCOUNT_NUMBER = "12345678";
+	public static final String PROXY_EMAIL_BRANCH_NUMBER = "0001";
 	public static final String PROXY_EMAIL_ISPB = "99999004";
-	public static final String PROXY_EMAIL_CPF = "11113333359";
-	public static final String PROXY_EMAIL_OWNER_NAME= "Joao Silva Silva";
+	public static final String PROXY_EMAIL_CPF = "99991111140";
+	public static final String PROXY_EMAIL_OWNER_NAME= "Joao Silva";
 	public static final String PROXY_EMAIL_PERSON_TYPE = "PESSOA_NATURAL";
 	public static final String PROXY_EMAIL_ACCOUNT_TYPE = "CACC";
-	public static final String PROXY_PHONE_NUMBER = "+5561990010002";
+	public static final String PROXY_PHONE_NUMBER = "+5561990010001";
 }
