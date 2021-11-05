ea32f3d0313bf482696768f262df57d3b6975b92
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-11-05 16:10:15 +0000

    SubjectDn test: Add a check that wrong cert can't be used
    Verify the cert from a different client fails at the token endpoint
    and payment consent creation endpoint.

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/PaymentsApiDcrSubjectDn.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/PaymentsApiDcrSubjectDn.java
@@ -1,12 +1,25 @@
 package net.openid.conformance.openbanking_brasil.testmodules;
 
-import net.openid.conformance.condition.client.AddTlsClientAuthSubjectDnToDynamicRegistrationRequest;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.AddIatToRequestObject;
+import net.openid.conformance.condition.client.AddJtiAsUuidToRequestObject;
 import net.openid.conformance.condition.client.AddTlsClientAuthSubjectDnWithBrazilShortnameToDynamicRegistrationRequest;
+import net.openid.conformance.condition.client.CallTokenEndpointAndReturnFullResponse;
+import net.openid.conformance.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidClient;
+import net.openid.conformance.condition.client.CheckTokenEndpointHttpStatus400or401;
+import net.openid.conformance.condition.client.CreateTokenEndpointRequestForClientCredentialsGrant;
+import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs400or401;
+import net.openid.conformance.condition.client.ExtractMTLSCertificatesFromConfiguration;
+import net.openid.conformance.condition.client.FAPIBrazilCallPaymentConsentEndpointWithBearerToken;
+import net.openid.conformance.condition.client.FAPIBrazilSignPaymentConsentRequest;
 import net.openid.conformance.condition.client.GetResourceEndpointConfiguration;
+import net.openid.conformance.condition.client.SetPaymentsScopeOnTokenEndpointRequest;
+import net.openid.conformance.condition.client.ValidateErrorFromTokenEndpointResponseError;
 import net.openid.conformance.fapi1advancedfinal.AbstractFAPI1AdvancedFinalBrazilDCR;
 import net.openid.conformance.openbanking_brasil.OBBProfile;
 import net.openid.conformance.openbanking_brasil.testmodules.support.EnsurePaymentDateIsToday;
 import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithPagtoClient;
+import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithPagtoClientThatHasClientSpecificJwks;
 import net.openid.conformance.openbanking_brasil.testmodules.support.SetDirectoryInfo;
 import net.openid.conformance.testmodule.PublishTestModule;
 import net.openid.conformance.variant.ClientAuthType;
@@ -44,6 +57,40 @@ public class PaymentsApiDcrSubjectDn extends AbstractFAPI1AdvancedFinalBrazilDCR
 		super.configureClient();
 
 		super.performPreAuthorizationSteps();
+
+		eventLog.startBlock("Trying using a different client's MTLS certificate with issued access token");
+
+		env.putObject("altconfig", env.getObject("config").deepCopy());
+		env.mapKey("config", "altconfig");
+		env.mapKey("mutual_tls_authentication", "altmtls");
+		callAndStopOnFailure(OverrideClientWithPagtoClientThatHasClientSpecificJwks.class);
+		callAndStopOnFailure(ExtractMTLSCertificatesFromConfiguration.class);
+
+		call(exec().mapKey("request_object_claims", "consent_endpoint_request"));
+		callAndStopOnFailure(AddJtiAsUuidToRequestObject.class, "BrazilOB-6.1");
+		callAndStopOnFailure(AddIatToRequestObject.class, "BrazilOB-6.1");
+		call(exec().unmapKey("request_object_claims"));
+		callAndStopOnFailure(FAPIBrazilSignPaymentConsentRequest.class);
+		callAndStopOnFailure(FAPIBrazilCallPaymentConsentEndpointWithBearerToken.class);
+
+		call(exec().mapKey("endpoint_response", "consent_endpoint_response_full"));
+		callAndContinueOnFailure(EnsureHttpStatusCodeIs400or401.class, Condition.ConditionResult.FAILURE);
+
+		env.unmapKey("config");
+
+		eventLog.startBlock("Trying using a different client's MTLS certificate to authenticate at token endpoint");
+
+		callAndStopOnFailure(CreateTokenEndpointRequestForClientCredentialsGrant.class);
+		callAndStopOnFailure(SetPaymentsScopeOnTokenEndpointRequest.class);
+		call(sequence(addTokenEndpointClientAuthentication));
+		callAndStopOnFailure(CallTokenEndpointAndReturnFullResponse.class);
+		callAndContinueOnFailure(ValidateErrorFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE);
+		callAndContinueOnFailure(CheckTokenEndpointHttpStatus400or401.class, Condition.ConditionResult.FAILURE);
+		callAndContinueOnFailure(CheckErrorFromTokenEndpointResponseErrorInvalidClient.class, Condition.ConditionResult.FAILURE);
+
+		env.unmapKey("mutual_tls_authentication");
+
+		eventLog.startBlock("Delete client");
 
 		deleteClient();
 
