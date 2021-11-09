4c6287d479645fea2d71123ccfc19e3630a6bc0c
GeorgeMc <george@georgemcintosh.com>
2021-11-09 12:22:32 +0000

    Oci 155

--- src/main/resources/static/js/fapi.ui.js
+++ src/main/resources/static/js/fapi.ui.js
@@ -46,7 +46,6 @@ var FAPI_UI = {
 					$.get('templates/userinfo.html', function(data) {
 						FAPI_UI.logTemplates.USER_INFO = _.template(data);
 					}),
-
 					$.get('templates/logHeader.html', function(data) {
 						FAPI_UI.logTemplates.LOG_START = _.template(data);
 					}),
@@ -131,7 +130,9 @@ var FAPI_UI = {
 					$.get('templates/configButton.html', function(data) {
 						FAPI_UI.logTemplates.CONFIG = _.template(data);
 					}),
-
+					$.get('templates/csvDigestButton.html', function(data) {
+						FAPI_UI.logTemplates.CSV_DIGEST_BUTTON = _.template(data);
+					}),
 					$.get('templates/date.html', function(data) {
 						FAPI_UI.logTemplates.DATE = _.template(data);
 					}),
@@ -288,6 +289,24 @@ var FAPI_UI = {
 			}).done(function(userInfo) {
 				this.currentUser = userInfo;
 				$('#userInfoHolder').html(FAPI_UI.logTemplates.USER_INFO({userInfo: userInfo}));
+				FAPI_UI.activeTooltip();
+			}).fail(function() {
+				// User is not logged in; don't fill in the user info holder
+			}).always(function() {
+				done.resolve();
+			});
+			return done.promise();
+		},
+
+		getCsvDigestButton : function() {
+			// get the current user info
+			var done = $.Deferred();
+			$.getJSON({
+				url: '/api/currentuser',
+				context: this
+			}).done(function(userInfo) {
+				this.currentUser = userInfo;
+				$('#csvButtonHolder').html(FAPI_UI.logTemplates.CSV_DIGEST_BUTTON({userInfo: userInfo}));
 				FAPI_UI.activeTooltip();
 			}).fail(function() {
 				// User is not logged in; don't fill in the user info holder
