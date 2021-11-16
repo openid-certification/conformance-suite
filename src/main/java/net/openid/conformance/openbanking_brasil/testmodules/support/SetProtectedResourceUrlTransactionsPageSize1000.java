9bd92850e25c4100281d4f8b39b7a28beda39c83
Alan Hughes <alan.hughes@raidiam.com>
2021-11-16 17:58:52 +0000

    Oci 505

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/SetProtectedResourceUrlTransactionsPageSize1000.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/SetProtectedResourceUrlTransactionsPageSize1000.java
@@ -1,19 +1,22 @@
 package net.openid.conformance.openbanking_brasil.testmodules.support;
 
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
-public class SetProtectedResourceUrlPageSize1001 extends AbstractCondition {
+public class SetProtectedResourceUrlTransactionsPageSize1000 extends AbstractCondition {
 
 	@Override
-	
+	@PreEnvironment(strings = "accountId") 
 	@PostEnvironment(strings = "protected_resource_url")
 	public Environment evaluate(Environment env) {
 
 		String resourceUrl = env.getString("protected_resource_url");
-		env.putString("protected_resource_url", resourceUrl.concat("?page-size=1001"));
-
+		String accountId = env.getString("accountId");
+		resourceUrl = String.format("%s/%s/transactions?page-size=1000", resourceUrl, accountId);
+		env.putString("protected_resource_url", resourceUrl);
+		logSuccess("URL for account transactions set up");
 		return env;
 	}
 }
