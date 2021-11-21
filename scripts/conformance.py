1e5721adc12100afa66633bda8fef7754d960b6a
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-11-21 18:37:06 +0000

    run-test-plan.py: Run in parallel where possible
    Use python's asyncio / await / async to run modules within one test plan
    in parallel if they don't have an alias, and to run plans in parallel
    where they use no / different aliases.
    
    The parallelism of tests within a plan should speed up the tests
    where we can freely use dcr and hence can proceed without
    any alias.
    
    Unfortunately the CIBA tests had to have parallelism
    disabled because Authlete's CIBA simulated authentication device
    doesn't seem to cope with multiple parallel requests.
    
    Reduces the time to run oidcc-basic-certification-test-plan from 313
    seconds to 63 seconds on my local machine - most of which comes from
    the test that sleeps for 30 seconds before reusing an auth code.
    
    The ability to run plans with different aliases in parallel means we
    can run some of the FAPI tests in parallel with other tests. There may
    be further potential to speed this up by tweaking the aliases and
    redirect urls we use in (say) half of the FAPI tests. (I changed a few
    recently.)
    
    It reduces the overall pipeline time from ~112 minutes to ~58 minutes,
    with the previous longest job (server_test) going from ~112 minutes to
    ~28 minutes. (And I think there's further scope to reduce both figures
    by carefully changing some aliases.)
    
    We add a retrying http transport, as it seems either the parallelisation
    or something in httpx means we get weird http / payload errors more
    often than before. The retry code is rather basic but seems to suite
    our needs.
    
    I initially tried aiohttp, but ran into quite a bit of randomness.
    I switched to httpx which feels a lot cleaner, but I still had issues
    when using it's async client so switched to it's sync client. (This
    has a bit of a speed penalty, but reliable is better than fast...)
    
    part of #783

--- scripts/conformance.py
+++ scripts/conformance.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # python wrapper for conformance suite API
 
@@ -6,117 +6,160 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import asyncio
 import json
 import re
+import httpx
 import os
 import shutil
 import time
+import traceback
 
+
+class RetryTransport(httpx.HTTPTransport):
+    def handle_request(
+        self,
+        request: httpx.Request,
+    ) -> httpx.Response:
+        retry = 0
+        while retry < 5:
+            retry += 1
+            if retry > 2:
+                time.sleep(1)
+            try:
+                resp = super().handle_request(request)
+            except Exception as e:
+                print("httpx {} exception {} caught - retrying".format(request.url, e))
+                continue
+            if resp.status_code >= 500 and resp.status_code < 600:
+                print("httpx {} 5xx response - retrying".format(request.url))
+                continue
+            content_type = resp.headers.get("Content-Type")
+            if content_type is not None:
+                mime_type, _, _ = content_type.partition(";")
+                if mime_type == 'application/json':
+                    try:
+                        resp.read()
+                        resp.json()
+                    except Exception as e:
+                        traceback.print_exc()
+                        print("httpx {} response not decodable as json '{}' - retrying".format(request.url, e))
+                        continue
+            break
+        return resp
 
 class Conformance(object):
-    def __init__(self, api_url_base, api_token, requests_session):
+    def __init__(self, api_url_base, api_token, verify_ssl):
         if not api_url_base.endswith('/'):
             api_url_base += "/"
         self.api_url_base = api_url_base
-        self.requests_session = requests_session
+        transport = RetryTransport(verify=verify_ssl)
+        self.httpclient = httpx.Client(verify=verify_ssl, transport=transport)
         headers = {'Content-Type': 'application/json'}
         if api_token is not None:
             headers['Authorization'] = 'Bearer {0}'.format(api_token)
-        self.requests_session.headers = headers
+        self.httpclient.headers = headers
 
-    def get_all_test_modules(self):
+    async def get_all_test_modules(self):
         """ Returns an array containing a dictionary per test module """
         api_url = '{0}api/runner/available'.format(self.api_url_base)
-        response = self.requests_session.get(api_url)
+        response = self.httpclient.get(api_url)
 
         if response.status_code != 200:
             raise Exception("get_all_test_modules failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def exporthtml(self, plan_id, path):
-        api_url = '{0}api/plan/exporthtml/{1}'.format(self.api_url_base, plan_id)
-        with self.requests_session.get(api_url, stream=True) as response:
-            if response.status_code != 200:
-                raise Exception("exporthtml failed - HTTP {:d} {}".format(response.status_code, response.content))
-            d = response.headers['content-disposition']
-            local_filename = re.findall("filename=\"(.+)\"", d)[0]
-            full_path = os.path.join(path, local_filename)
-            with open(full_path, 'wb') as f:
-                shutil.copyfileobj(response.raw, f)
-        return full_path
+    async def exporthtml(self, plan_id, path):
+        for i in range(5):
+            api_url = '{0}api/plan/exporthtml/{1}'.format(self.api_url_base, plan_id)
+            try:
+                with self.httpclient.stream("GET", api_url) as response:
+                    if response.status_code != 200:
+                        raise Exception("exporthtml failed - HTTP {:d} {}".format(response.status_code, response.content))
+                    d = response.headers['content-disposition']
+                    local_filename = re.findall("filename=\"(.+)\"", d)[0]
+                    full_path = os.path.join(path, local_filename)
+                    with open(full_path, 'wb') as f:
+                        for chunk in response.iter_bytes():
+                            f.write(chunk)
+                return full_path
+            except Exception as e:
+                print("httpx {} exception {} caught - retrying".format(api_url, e))
+                await asyncio.sleep(1)
+        raise Exception("exporthtml failed even after retries")
 
-    def create_test_plan(self, name, configuration, variant=None):
+    async def create_test_plan(self, name, configuration, variant=None):
         api_url = '{0}api/plan'.format(self.api_url_base)
         payload = {'planName': name}
         if variant != None:
             payload['variant'] = json.dumps(variant)
-        response = self.requests_session.post(api_url, params=payload, data=configuration)
+        response = self.httpclient.post(api_url, params=payload, data=configuration)
 
         if response.status_code != 201:
             raise Exception("create_test_plan failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def create_test(self, test_name, configuration):
+    async def create_test(self, test_name, configuration):
         api_url = '{0}api/runner'.format(self.api_url_base)
         payload = {'test': test_name}
-        response = self.requests_session.post(api_url, params=payload, data=configuration)
+        response = self.httpclient.post(api_url, params=payload, data=configuration)
 
         if response.status_code != 201:
             raise Exception("create_test failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def create_test_from_plan(self, plan_id, test_name):
+    async def create_test_from_plan(self, plan_id, test_name):
         api_url = '{0}api/runner'.format(self.api_url_base)
         payload = {'test': test_name, 'plan': plan_id}
-        response = self.requests_session.post(api_url, params=payload)
+        response = self.httpclient.post(api_url, params=payload)
 
         if response.status_code != 201:
             raise Exception("create_test_from_plan failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def create_test_from_plan_with_variant(self, plan_id, test_name, variant):
+    async def create_test_from_plan_with_variant(self, plan_id, test_name, variant):
         api_url = '{0}api/runner'.format(self.api_url_base)
         payload = {'test': test_name, 'plan': plan_id}
         if variant != None:
             payload['variant'] = json.dumps(variant)
-        response = self.requests_session.post(api_url, params=payload)
+        response = self.httpclient.post(api_url, params=payload)
 
         if response.status_code != 201:
             raise Exception("create_test_from_plan failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def get_module_info(self, module_id):
+    async def get_module_info(self, module_id):
         api_url = '{0}api/info/{1}'.format(self.api_url_base, module_id)
-        response = self.requests_session.get(api_url)
+        response = self.httpclient.get(api_url)
 
         if response.status_code != 200:
             raise Exception("get_module_info failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def get_test_log(self, module_id):
+    async def get_test_log(self, module_id):
         api_url = '{0}api/log/{1}'.format(self.api_url_base, module_id)
-        response = self.requests_session.get(api_url)
+        response = self.httpclient.get(api_url)
 
         if response.status_code != 200:
             raise Exception("get_test_log failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def start_test(self, module_id):
+    async def start_test(self, module_id):
         api_url = '{0}api/runner/{1}'.format(self.api_url_base, module_id)
-        response = self.requests_session.post(api_url)
+        response = self.httpclient.post(api_url)
 
         if response.status_code != 200:
             raise Exception("start_test failed - HTTP {:d} {}".format(response.status_code, response.content))
-        return json.loads(response.content.decode('utf-8'))
+        return response.json()
 
-    def wait_for_state(self, module_id, required_states, timeout=240):
+    async def wait_for_state(self, module_id, required_states, timeout=240):
         timeout_at = time.time() + timeout
         while True:
             if time.time() > timeout_at:
                 raise Exception("Timed out waiting for test module {} to be in one of states: {}".
                                 format(module_id, required_states))
 
-            info = self.get_module_info(module_id)
+            info = await self.get_module_info(module_id)
 
             status = info['status']
             print("module id {} status is {}".format(module_id, status))
@@ -125,4 +168,7 @@ class Conformance(object):
             if status == 'INTERRUPTED':
                 raise Exception("Test module {} has moved to INTERRUPTED".format(module_id))
 
-            time.sleep(1)
+            await asyncio.sleep(1)
+
+    async def close_client(self):
+        self.httpclient.close()
