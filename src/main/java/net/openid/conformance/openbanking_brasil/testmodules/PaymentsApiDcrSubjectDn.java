78807cbe716be4578b87a3da10f2e7b48ef33e6d
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-11-24 20:12:46 +0000

    Switch use of 'resources' scope to use all valid phase 2 scopes
    The resources scope is not technically valid as a standalone scope
    to request from the user; request all phase 2 scopes so that we're
    guaranteed to request at least one that the bank supports.
    
    closes #119

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/PaymentsApiDcrSubjectDn.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/PaymentsApiDcrSubjectDn.java
@@ -1,49 +1,9 @@
 package net.openid.conformance.openbanking_brasil.testmodules;
 
-import net.openid.conformance.condition.Condition;
-import net.openid.conformance.condition.client.AddIatToRequestObject;
-import net.openid.conformance.condition.client.AddJtiAsUuidToRequestObject;
-import net.openid.conformance.condition.client.AddTlsClientAuthSubjectDnWithBrazilShortnameToDynamicRegistrationRequest;
-import net.openid.conformance.condition.client.CallConsentEndpointWithBearerToken;
-import net.openid.conformance.condition.client.CallDynamicRegistrationEndpoint;
-import net.openid.conformance.condition.client.CallTokenEndpointAndReturnFullResponse;
-import net.openid.conformance.condition.client.CheckErrorFromDynamicRegistrationEndpointIsInvalidClientMetadata;
-import net.openid.conformance.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidClient;
-import net.openid.conformance.condition.client.CheckNoClientIdFromDynamicRegistrationEndpoint;
-import net.openid.conformance.condition.client.CheckNoErrorFromDynamicRegistrationEndpoint;
-import net.openid.conformance.condition.client.CheckTokenEndpointHttpStatus400or401;
-import net.openid.conformance.condition.client.ClientManagementEndpointAndAccessTokenRequired;
-import net.openid.conformance.condition.client.CopyOrgJwksFromDynamicRegistrationTemplateToClientConfiguration;
-import net.openid.conformance.condition.client.CopyScopeFromDynamicRegistrationTemplateToClientConfiguration;
-import net.openid.conformance.condition.client.CreateTokenEndpointRequestForClientCredentialsGrant;
-import net.openid.conformance.condition.client.DetectIfHttpStatusIsSuccessOrFailure;
-import net.openid.conformance.condition.client.EnsureContentTypeJson;
-import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs201;
-import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs400;
-import net.openid.conformance.condition.client.EnsureHttpStatusCodeIs400or401;
-import net.openid.conformance.condition.client.ExtractClientManagementCredentials;
-import net.openid.conformance.condition.client.ExtractDynamicRegistrationResponse;
-import net.openid.conformance.condition.client.ExtractMTLSCertificatesFromConfiguration;
-import net.openid.conformance.condition.client.FAPIBrazilCallPaymentConsentEndpointWithBearerToken;
-import net.openid.conformance.condition.client.FAPIBrazilSignPaymentConsentRequest;
-import net.openid.conformance.condition.client.GetResourceEndpointConfiguration;
-import net.openid.conformance.condition.client.SetConsentsScopeOnTokenEndpointRequest;
-import net.openid.conformance.condition.client.SetPaymentsScopeOnTokenEndpointRequest;
-import net.openid.conformance.condition.client.ValidateErrorFromTokenEndpointResponseError;
 import net.openid.conformance.openbanking_brasil.OBBProfile;
-import net.openid.conformance.openbanking_brasil.testmodules.support.EnsurePaymentDateIsToday;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithDadosClient;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithDadosClientThatHasClientSpecificJwks;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithPagtoClient;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideClientWithPagtoClientThatHasClientSpecificJwks;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideScopeWithOpenIdPayments;
-import net.openid.conformance.openbanking_brasil.testmodules.support.OverrideScopeWithOpenIdResources;
-import net.openid.conformance.openbanking_brasil.testmodules.support.SetDirectoryInfo;
 import net.openid.conformance.testmodule.PublishTestModule;
 import net.openid.conformance.variant.ClientAuthType;
 import net.openid.conformance.variant.VariantNotApplicable;
-
-import static net.openid.conformance.condition.client.DetectIfHttpStatusIsSuccessOrFailure.endpointResponseWas2xx;
 
 @PublishTestModule(
 	testName = "payments-api-dcr-subjectdn",
--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OverrideScopeWithAllDadosScopes.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OverrideScopeWithAllDadosScopes.java
@@ -4,11 +4,11 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 
-public class OverrideScopeWithOpenIdResources extends AbstractCondition {
+public class OverrideScopeWithAllDadosScopes extends AbstractCondition {
 	@Override
 	public Environment evaluate(Environment env) {
 		JsonObject client = (JsonObject) env.getElementFromObject("config", "client");
-		client.addProperty("scope", "openid resources");
+		client.addProperty("scope", "openid accounts credit-cards-accounts consents customers invoice-financings financings loans unarranged-accounts-overdraft resources");
 		return env;
 	}
 }
