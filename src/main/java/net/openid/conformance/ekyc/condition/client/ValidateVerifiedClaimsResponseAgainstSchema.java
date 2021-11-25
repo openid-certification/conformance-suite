729a5e3a67fd00c203f475b2b6784d23b7987b69
Joseph Heenan <joseph.heenan@fintechlabs.io>
2021-11-25 18:10:27 +0000

    Switch JSON schema validator
    The current one depends on the non-free org.json (see
    https://wiki.debian.org/qa.debian.org/jsonevil ), which also has an
    unfortunately clash with the alternative re-implementation of org.json
    used in spring (although my attempt to workaround the latter seems to
    have been successful).
    
    The networknt one was picked from:
    
    https://json-schema.org/implementations.html
    
    on the basis that it seems to be the next most popular java
    implementation based on github stars/forks.
    
    It's not clear why we had exclusions for jackson-databind (which the
    new schema validator depends on).
    
    They were originally added by Justin in commit
    3c932929ac2e65e265f3393ef9006c0b8b6d419f in September 2017, but no
    reason was given.
    
    Even once un-excluded, there was still an issue with version, I
    believe as maven was probably picking to use the version spring
    requested which was too old, resulting in an error when trying to run
    the unit tests:
    
    java.lang.NoSuchMethodError:
    com.fasterxml.jackson.core.JsonFactory.requiresPropertyOrdering()Z
    
    So an explicit dependency on a suitable version of jackson was added.
    
    The choice of version 2.12.1 is because that's what the validator
    currently seems to list in it's dependencies:
    
    https://github.com/networknt/json-schema-validator/blob/master/pom.xml

--- src/main/java/net/openid/conformance/ekyc/condition/client/ValidateVerifiedClaimsResponseAgainstSchema.java
+++ src/main/java/net/openid/conformance/ekyc/condition/client/ValidateVerifiedClaimsResponseAgainstSchema.java
@@ -1,19 +1,24 @@
 package net.openid.conformance.ekyc.condition.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
-import org.apache.xpath.operations.Bool;
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.InputStream;
+import java.util.Set;
 
 public class ValidateVerifiedClaimsResponseAgainstSchema extends AbstractCondition {
 
@@ -37,32 +42,40 @@ public class ValidateVerifiedClaimsResponseAgainstSchema extends AbstractConditi
 		}
 		//we add the outer {"verified_claims":...} here
 		String claimsJson = "{\"verified_claims\":" + claimsElement.toString() + "}";
-		try {
-			checkSchema(claimsJson);
-		} catch (ValidationException ex) {
-			throw error("Failed to validate verified_claims against schema", ex,
-				args("verified_claims", claimsJson,
-					"errors", new JsonParser().parse(ex.toJSON().toString())));
+
+		Set<ValidationMessage> errors = checkSchema(claimsJson);
+		if (!errors.isEmpty()) {
+			JsonArray jsonErrors = new JsonArray();
+			for (ValidationMessage error: errors) {
+				jsonErrors.add(error.toString());
+			}
+			throw error("Failed to validate verified_claims against schema",
+				args("verified_claims", new JsonParser().parse(claimsJson),
+					"errors", jsonErrors));
 		}
 		logSuccess("Verified claims are valid", args("location", location, "verified_claims", claimsElement));
 		return env;
 	}
 
-	protected void checkSchema(String verifiedClaimsJson) {
-		// This JSON Schema library has a dependency on org.json, which has license issues:
-		// https://wiki.debian.org/qa.debian.org/jsonevil
-		//
-		// It also conflicts with spring-boot-starter-test, see the comment in pom.xml.
-		//
-		// Please don't write any more code that uses it; an alternative library is suggested here:
-		// https://stackoverflow.com/a/56767636/292166
+	private ObjectMapper mapper = new ObjectMapper();
+
+	protected Set<ValidationMessage> checkSchema(String verifiedClaimsJson) {
 		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("json-schemas/ekyc/verified_claims-12.json");
 
-		JSONObject jsonSchema = new JSONObject(new JSONTokener(inputStream));
-		JSONObject jsonSubject = new JSONObject(new JSONTokener(verifiedClaimsJson));
-		Schema schema = SchemaLoader.load(jsonSchema);
-		schema.validate(jsonSubject);
+		// fixme load to node first then use         JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersionDetector.detect(jsonNode));
+		JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7); // current ekyc schemas use draft 7
+		JsonSchema schema = factory.getSchema(inputStream);
 
+		JsonNode node;
+		try {
+			node = mapper.readTree(verifiedClaimsJson);
+		} catch (JsonProcessingException e) {
+			throw error("Failed to parse JSON", e);
+		}
+
+		Set<ValidationMessage> errors = schema.validate(node);
+
+		return errors;
 	}
 
 }
