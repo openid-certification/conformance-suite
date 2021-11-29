444e12f23d7765aa5876dcdd205ba758f59e8b12
GeorgeMc <george@georgemcintosh.com>
2021-11-29 14:33:44 +0000

    Some stronger validation around QRDN test config

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/ValidateQrdnConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/ValidateQrdnConfig.java
@@ -11,16 +11,29 @@ import net.openid.conformance.testmodule.OIDFJSON;
 
 import java.util.Map;
 
-public class ValidateQrCodeOnQrdnConfig extends AbstractCondition {
+public class ValidateQrdnConfig extends AbstractCondition {
 
 	@Override
 	@PreEnvironment(required = "config" )
 	public Environment evaluate(Environment env) {
 		JsonObject paymentDetails = (JsonObject) env.getElementFromObject("resource", "brazilQrdnPaymentConsent.data.payment.details");
+		if(paymentDetails == null) {
+			throw error("QRDN consent config is missing or incomplete");
+		}
 		JsonElement qrCodeElement = paymentDetails.get("qrCode");
 		if(qrCodeElement == null) {
 			throw error("QRDN consent config does not contain a qr code");
 		}
+		JsonElement brazilQrdnCnpj = env.getElementFromObject("resource", "brazilQrdnCnpj");
+		if(brazilQrdnCnpj == null) {
+			throw error("QRDN consent config does not contain an initiators CNPJ");
+		}
+
+		JsonElement remittanceAdvice = env.getElementFromObject("resource", "brazilQrdnRemittance");
+		if(remittanceAdvice == null) {
+			throw error("QRDN config does not contain a remittance advice");
+		}
+
 		String qrCode = OIDFJSON.getString(qrCodeElement);
 		try {
 			DecoderMpm.decode(qrCode, MerchantPresentedMode.class);
