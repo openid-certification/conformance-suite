a1d29380043e97922315cd6bfdc93afe6372587f
Marcus Almgren <marcus@almgren.pt>
2021-12-06 19:16:34 +0000

    Fix the consentId URN for non-payment consents as well

--- src/test/java/net/openid/conformance/condition/rs/ConsentId_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/rs/ConsentId_UnitTest.java
@@ -17,6 +17,20 @@ public class ConsentId_UnitTest {
 		var envIn = new Environment();
 		envIn.putString("fapi_interaction_id", "04ddbb1b-19f6-4300-9072-775a9bd704c4");
 
+		var consentResponse = new FAPIBrazilGenerateNewConsentResponse();
+		consentResponse.setProperties(null, mock(TestInstanceEventLog.class), null);
+
+		Environment envOut = consentResponse.evaluate(envIn);
+		String consentId = envOut.getString("consent_id");
+
+		assertTrue(consentId.startsWith("urn:conformance:oidf:"));
+	}
+
+	@Test
+	public void test_paymentsConsentIdStartsWithOidfConformanceUrn() {
+		var envIn = new Environment();
+		envIn.putString("fapi_interaction_id", "04ddbb1b-19f6-4300-9072-775a9bd704c4");
+
 		var consentResponse = new FAPIBrazilGenerateNewPaymentsConsentResponse();
 		consentResponse.setProperties(null, mock(TestInstanceEventLog.class), null);
 
@@ -42,6 +56,20 @@ public class ConsentId_UnitTest {
 
 	@Test
 	public void test_consentIdCompliesWithRFC8141() throws URNSyntaxError {
+		var envIn = new Environment();
+		envIn.putString("fapi_interaction_id", "04ddbb1b-19f6-4300-9072-775a9bd704c4");
+
+		var consentResponse = new FAPIBrazilGenerateNewConsentResponse();
+		consentResponse.setProperties(null, mock(TestInstanceEventLog.class), null);
+
+		Environment envOut = consentResponse.evaluate(envIn);
+		String consentId = envOut.getString("consent_id");
+
+		URN.rfc8141().parse(consentId);
+	}
+
+	@Test
+	public void test_paymentsConsentIdCompliesWithRFC8141() throws URNSyntaxError {
 		var envIn = new Environment();
 		envIn.putString("fapi_interaction_id", "04ddbb1b-19f6-4300-9072-775a9bd704c4");
 
