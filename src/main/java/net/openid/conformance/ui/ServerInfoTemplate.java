e6f8da2cf25be8169358fac0cefd9f0152974321
Alan Hughes <alan.hughes@raidiam.com>
2021-12-10 15:35:36 +0000

    Opin support

--- src/main/java/net/openid/conformance/ui/ServerInfoTemplate.java
+++ src/main/java/net/openid/conformance/ui/ServerInfoTemplate.java
@@ -13,11 +13,21 @@ public class ServerInfoTemplate {
 
 	@Value("${fintechlabs.version}")
 	private String version;
+	
 	@Value("${fintechlabs.show_external_ip_address}")
 	private Boolean showExternalIpAddress;
-
+	
 	@Value("${fintechlabs.external_ip_service_uri}")
 	private String ipAddressServiceUri;
+
+	@Value("${fintechlabs.issuer}")
+	private String issuer;
+
+	@Value("${fintechlabs.logo}")
+	private String logo;
+
+	@Value("${fintechlabs.brand}")
+	private String brand;
 
 	private static final Map<String, String> SERVER_INFO = new HashMap<>();
 
@@ -27,6 +37,9 @@ public class ServerInfoTemplate {
 	public void initServerInfo() {
 		SERVER_INFO.put("version", version);
 		SERVER_INFO.put("external_ip", callServiceToGetExternalIpAddress());
+		SERVER_INFO.put("issuer", issuer);
+		SERVER_INFO.put("logo", logo);
+		SERVER_INFO.put("brand", brand);
 	}
 
 	/***
