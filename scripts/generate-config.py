3c19c104b78021e46be36864f86cf89d3a9385a9
George McIntosh <george@georgemcintosh.com>
2022-02-01 10:46:48 +0000

    Small typo

--- scripts/generate-config.py
+++ scripts/generate-config.py
@@ -15,12 +15,12 @@ def add_overrides(config, overrides):
         override_data = load_override_template()
         if 'pixPaymentAmount' in override_info:
             payment_amount = override_info['pixPaymentAmount']
-            print("Overriding pix payment amount in module {0} to {1}").format(module_name, payment_amount)
+            print('Overriding pix payment amount in module {0} to {1}'.format(module_name, payment_amount))
             override_data['resource']['brazilPaymentConsent']['data']['payment']['amount'] = payment_amount
             override_data['resource']['brazilPixPayment']['data']['payment']['amount'] = payment_amount
         if 'qrdnAmount' in override_info:
             payment_amount = override_info['qrdnAmount']
-            print("Overriding qrdn payment amount in module {0} to {1}").format(module_name, payment_amount)
+            print('Overriding qrdn payment amount in module {0} to {1}'.format(module_name, payment_amount))
             override_data['resource']['brazilQrdnRemittance'] = "$$EDITPAYMENT$$:" + payment_amount
         overrides_element[module_name] = override_data
 
@@ -38,5 +38,5 @@ if __name__ == '__main__':
 
     add_overrides(data, overrides)
 
-    with open('./scripts/configs/generated-payment-config.json', 'w', encoding='utf-8') as f:
+    with open('./scripts/configs/generated-payment-config.json', 'w+', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
