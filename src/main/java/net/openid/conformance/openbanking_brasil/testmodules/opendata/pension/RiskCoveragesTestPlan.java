7450d85d059c3c2d903cc3d94dbd89a4778ee8ed
Erick Domingues <erick.domingues@raidiam.com>
2022-02-03 15:31:01 +0000

    Update RiskCoveragesTestPlan.java

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/opendata/pension/RiskCoveragesTestPlan.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/opendata/pension/RiskCoveragesTestPlan.java
@@ -17,7 +17,7 @@ import java.util.List;
 @PublishTestPlan(
 	testPlanName = "Pension - Risk Coverages API test plan",
 	profile = OBBProfile.OBB_PROFIlE_PHASE4,
-	displayName = "Functional tests for Pension - Risk Coverages - based on Swagger version: 1.0.0",
+	displayName = "Functional tests for Pension - Risk Coverages - based on Swagger version: 1.0.0-rc1.0 (WIP)",
 	summary = "Structural and logical tests for Pension API"
 )
 public class RiskCoveragesTestPlan implements TestPlan {
