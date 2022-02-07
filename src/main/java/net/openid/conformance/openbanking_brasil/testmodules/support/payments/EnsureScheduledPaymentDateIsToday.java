d4ab99775aaec99f822b62397474c9081bd5afe4
George McIntosh <george@georgemcintosh.com>
2022-02-07 17:11:22 +0000

    Adding test module for today's date on scheduled payment

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/EnsureScheduledPaymentDateIsToday.java
@@ -0,0 +1,40 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support.payments;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.testmodule.Environment;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class EnsureScheduledPaymentDateIsToday extends AbstractCondition {
+
+	@Override
+	public Environment evaluate(Environment env) {
+		JsonObject obj = env.getObject("resource");
+		if(obj == null){
+			obj = env.getObject("config");
+			obj = obj.getAsJsonObject("resource");
+		}
+		if(obj == null){
+			logFailure("Cannot find resource object.");
+			return env;
+		}
+		obj = obj.getAsJsonObject("brazilPaymentConsent");
+		obj = obj.getAsJsonObject("data");
+		obj = obj.getAsJsonObject("payment");
+		obj.remove("date");
+		obj = obj.getAsJsonObject("schedule");
+		obj = obj.getAsJsonObject("single");
+
+		LocalDate currentDate = LocalDate.now(ZoneId.of("America/Sao_Paulo"));
+
+		log("Setting scheduled payment date to today: " + currentDate);
+		obj.addProperty("date", currentDate.toString());
+		logSuccess("Successfully added current date to payment schedule", obj);
+		logSuccess("Successfully removed standard payment date");
+
+		return env;
+	}
+
+}
