54729de4f7020d6d1b1abd8ecd147267fcc46c4f
Denis Podberiozkin <denis@Deniss-MBP.lan>
2022-02-28 15:53:12 +0000

    OCI-587-preventing-NPE

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/SelectQRDNCodeLocalInstrumentWithQrdnConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/SelectQRDNCodeLocalInstrumentWithQrdnConfig.java
@@ -14,12 +14,16 @@ public class SelectQRDNCodeLocalInstrumentWithQrdnConfig extends AbstractPayment
 	@Override
 	protected JsonObject getPaymentConsentObject(JsonObject resourceConfig) {
 		JsonElement brazilQrdnPaymentConsent = resourceConfig.get("brazilQrdnPaymentConsent");
-		if (brazilQrdnPaymentConsent.isJsonObject()) {
-			return brazilQrdnPaymentConsent.getAsJsonObject();
-		} else {
-			String massage = "Test cannot continue because brazilQrdnPaymentConsent cannot be " +
-				"parsed. -> Show View Button -> Display brazilQrdnPaymentConsent right below";
-			throw error(massage, args("brazilQrdnPaymentConsent", brazilQrdnPaymentConsent.toString()));
+		if (brazilQrdnPaymentConsent != null) {
+			if (brazilQrdnPaymentConsent.isJsonObject()) {
+				return brazilQrdnPaymentConsent.getAsJsonObject();
+			} else {
+				String massage = "Test cannot continue because brazilQrdnPaymentConsent cannot be " +
+					"parsed. -> Show View Button -> Display brazilQrdnPaymentConsent right below";
+				throw error(massage, args("brazilQrdnPaymentConsent", brazilQrdnPaymentConsent.toString()));
+			}
 		}
+		String massage = "brazilQrdnPaymentConsent must be present in the config not null";
+		throw error(massage);
 	}
 }
