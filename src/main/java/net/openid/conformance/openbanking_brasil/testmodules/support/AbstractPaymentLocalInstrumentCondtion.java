c1d784d60a83bdeeb59b634dd9d1135ab1b01aab
Denis Podberiozkin <denis@Deniss-MBP.lan>
2022-03-01 16:14:49 +0000

    OCI-587-full-path-check

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/AbstractPaymentLocalInstrumentCondtion.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/AbstractPaymentLocalInstrumentCondtion.java
@@ -4,19 +4,35 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class AbstractPaymentLocalInstrumentCondtion extends AbstractCondition {
 
 	@Override
 	public final Environment evaluate(Environment env) {
 		log("Setting local instrument to a new value");
 		JsonObject obj = env.getObject("resource");
-		obj = getPaymentConsentObject(obj);
-		obj = obj.getAsJsonObject("data");
-		obj = obj.getAsJsonObject("payment");
-		obj = obj.getAsJsonObject("details");
-		obj.addProperty("localInstrument", getLocalInstrument());
-		log(obj);
-		return env;
+
+		final String errorMessage = "Configuration is malformed. Missing an expected JSON field" +
+			"- brazilQrdnPaymentConsent > data > payment > details.";
+
+		if (obj.has("brazilQrdnPaymentConsent")) {
+			obj = getPaymentConsentObject(obj);
+			List<String> members = new LinkedList<>(Arrays.asList("data", "payment", "details"));
+			for (String member : members) {
+				if (obj.has(member)) {
+					obj = obj.getAsJsonObject(member);
+				} else {
+					throw error(errorMessage);
+				}
+			}
+			obj.addProperty("localInstrument", getLocalInstrument());
+			log(obj);
+			return env;
+		}
+		throw error(errorMessage);
 	}
 
 	protected JsonObject getPaymentConsentObject(JsonObject resourceConfig) {
