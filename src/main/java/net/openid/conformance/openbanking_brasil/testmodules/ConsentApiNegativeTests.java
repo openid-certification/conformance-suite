3a0a4a45b71da4e22c11579d3ab933c5700a86e6
Denis Podberiozkin <denis@Deniss-MBP.lan>
2022-03-03 17:53:35 +0000

    OCI-663

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/ConsentApiNegativeTests.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/ConsentApiNegativeTests.java
@@ -9,7 +9,29 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "consent-api-test-negative",
 	displayName = "Runs various negative tests",
-	summary = "Runs various negative tests",
+	summary = "Runs various negative tests\n" +
+		"\u2022 Creates a Consent with only RESOURCES_READ permissions\n" +
+		"\u2022 Expects a failure with a 400 Error\n" +
+		"\u2022 Creates consent with incomplete credit card permission group (\"CREDIT_CARDS_ACCOUNTS_BILLS_READ\",\"CREDIT_CARDS_ACCOUNTS_READ\",\"RESOURCES_READ\")\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent with incomplete account permission group 1 (\"ACCOUNTS_READ\")\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent with incomplete account permission group 2 (\"ACCOUNTS_READ\",\"RESOURCES_READ\")\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent with incomplete Limits & Credit Operations Contract Data permissions group (\"RESOURCES_READ\",\"LOANS_READ\",\"LOANS_WARRANTIES_READ\",\n" +
+		"\"LOANS_SCHEDULED_INSTALMENTS_READ\",\"LOANS_PAYMENTS_READ\",\"FINANCINGS_READ\",\"FINANCINGS_WARRANTIES_READ\",\"FINANCINGS_SCHEDULED_INSTALMENTS_READ\",\n" +
+		"\"FINANCINGS_PAYMENTS_READ\",\"UNARRANGED_ACCOUNTS_OVERDRAFT_READ\",\"UNARRANGED_ACCOUNTS_OVERDRAFT_WARRANTIES_READ\",\n" +
+		"\"UNARRANGED_ACCOUNTS_OVERDRAFT_SCHEDULED_INSTALMENTS_READ\",\"UNARRANGED_ACCOUNTS_OVERDRAFT_PAYMENTS_READ\",\"INVOICE_FINANCINGS_READ\",\n" +
+		"\"INVOICE_FINANCINGS_WARRANTIES_READ\",\"INVOICE_FINANCINGS_SCHEDULED_INSTALMENTS_READ\",\"INVOICE_FINANCINGS_PAYMENTS_READ\",\"RESOURCES_READ\")\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent with non existente permission group 2 (\"BAD_PERMISSION\")\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent api request for DateTime greater than 1 year from now\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent api request for DateTime in the past\n" +
+		"\u2022 Expects failure with a 400 Error\n" +
+		"\u2022 Creates consent api request for DateTime poorly formed.\n" +
+		"\u2022 Expects failure with a 400 Error",
 	profile = OBBProfile.OBB_PROFILE,
 	configurationFields = {
 		"server.discoveryUrl",
--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/ConsentsApiCrossClientTestModule.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/ConsentsApiCrossClientTestModule.java
@@ -11,7 +11,19 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "consent-api-test-client-limits",
 	displayName = "Validate that clients cannot obtain one another's consents",
-	summary = "Validate that clients cannot obtain one another's consents",
+	summary = "Validates that clients cannot obtain one another's consents\n" +
+		"\u2022 Creates a Consent with all of the existing permissions \n" +
+		"\u2022 Calls the GET Consents with the Consent ID that has been created\n" +
+		"\u2022 Checks all of the fields sent on the consent API are specification compliant\n" +
+		"\u2022 Calls the Token endpoint using the 2nd client provided on the configuration file\n" +
+		"\u2022 Calls the GET Consents with the first Consent ID created\n" +
+		"\u2022 Expects the test to return a 403 - Forbidden\n" +
+		"\u2022 Calls the DELETE Consents with the first Consent ID created, using the 2nd client\n" +
+		"\u2022 Expects the test to return a 403 - Forbidden\n" +
+		"\u2022 Calls the DELETE Consents with the first Consent ID created, using the 1st client\n" +
+		"\u2022 Expects a success on the Delete 20x\n" +
+		"\u2022 Calls the GET Consents with the 1st Consent ID created\n" +
+		"\u2022 Confirms that the Consent has been sent to a Rejected state",
 	profile = OBBProfile.OBB_PROFILE,
 	configurationFields = {
 		"server.discoveryUrl",
