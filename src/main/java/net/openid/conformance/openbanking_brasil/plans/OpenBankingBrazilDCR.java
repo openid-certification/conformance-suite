2288632efe1621a745aa68e32189e51ba4a613e6
Denis Podberiozkin <denis.podberiozkin@raidiam.com>
2022-03-14 10:59:39 +0000

    OCI-655

--- src/main/java/net/openid/conformance/openbanking_brasil/plans/OpenBankingBrazilDCR.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/plans/OpenBankingBrazilDCR.java
@@ -2,7 +2,7 @@ package net.openid.conformance.openbanking_brasil.plans;
 
 
 import net.openid.conformance.openbanking_brasil.OBBProfile;
-import net.openid.conformance.openbanking_brasil.testmodules.PreFlightCertCheckPaymentsModule;
+import net.openid.conformance.openbanking_brasil.testmodules.DCRWithAllScopesNoUnregistrationTestModule;
 import net.openid.conformance.plan.PublishTestPlan;
 import net.openid.conformance.plan.TestPlan;
 import net.openid.conformance.variant.FAPI1FinalOPProfile;
@@ -13,14 +13,16 @@ import java.util.List;
 	testPlanName = "Open Banking Brazil - DCR",
 	profile = OBBProfile.OBB_PROFIlE_PHASE3,
 	displayName = PlanNames.OBB_DCR,
-	summary = "summary"
+	summary = "Tests for client registration via Dynamic Client Registration without unregistering the client after the test. " +
+		"These tests are designed to help users execute the Conformance Suite locally against different Authorization Servers. " +
+		"So, they can do a DCR using the C.S. and then do functional tests with the client_id created."
 )
 public class OpenBankingBrazilDCR implements TestPlan {
 	public static List<ModuleListEntry> testModulesWithVariants() {
 		return List.of(
 			new ModuleListEntry(
 				List.of(
-					PreFlightCertCheckPaymentsModule.class
+					DCRWithAllScopesNoUnregistrationTestModule.class
 				),
 				List.of(
 					new Variant(FAPI1FinalOPProfile.class, "openbanking_brazil")
