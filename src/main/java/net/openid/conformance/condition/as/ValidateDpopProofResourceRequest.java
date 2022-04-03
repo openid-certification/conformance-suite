b2e8bc59ecab16868ddbcf725bcfbc2f208d967f
Edmund Jay <ejay@mgi1.com>
2022-04-03 15:51:37 -0700

    Validate DPoP Proof header and claims values. ValidateDpopProofResourceRequest differs from ValidateDpopProofTokenRequest in that it checks for existence of 'ath' claim.

---
+++ src/main/java/net/openid/conformance/condition/as/ValidateDpopProofResourceRequest.java
@@ -0,0 +1,13 @@
+package net.openid.conformance.condition.as;
+
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class ValidateDpopProofResourceRequest extends AbstractValidateDpopProof {
+
+	@Override
+	@PreEnvironment(required = {"incoming_dpop_proof", "incoming_request"})
+	public Environment evaluate(Environment env) {
+		return validateDpopProof(env, false);
+	}
+}
