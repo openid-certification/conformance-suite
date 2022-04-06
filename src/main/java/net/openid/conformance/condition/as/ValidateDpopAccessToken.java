85e5761d6de03966993b2e3246bf245fa836cbe4
Edmund Jay <ejay@mgi1.com>
2022-04-06 04:20:31 -0700

    Checks that the incoming DPoP access token is the expected value

---
+++ src/main/java/net/openid/conformance/condition/as/ValidateDpopAccessToken.java
@@ -0,0 +1,26 @@
+package net.openid.conformance.condition.as;
+
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class ValidateDpopAccessToken extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(strings =  "incoming_dpop_access_token", required = "dpop_access_token")
+	public Environment evaluate(Environment env) {
+
+		String dpopAccessToken = env.getString("dpop_access_token", "value");
+		String incomingDpopToken = env.getString("incoming_dpop_access_token");
+		if(dpopAccessToken.equals(incomingDpopToken)) {
+			logSuccess("DPoP Access Token is valid", args("DPoP Access Token", dpopAccessToken));
+			return env;
+		} else {
+			throw error("Invalid DPoP Access Token", args("expected", dpopAccessToken, "actual", incomingDpopToken));
+		}
+	}
+
+}
