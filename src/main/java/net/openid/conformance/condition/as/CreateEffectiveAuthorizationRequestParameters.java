1b52086162d37faae3ab3293f1dc9f6f62a22c4c
Edmund Jay <ejay@mgi1.com>
2022-04-08 16:24:59 -0700

    Add optional checks for PKCE in auth/token endpoints in OIDC Client tests

--- src/main/java/net/openid/conformance/condition/as/CreateEffectiveAuthorizationRequestParameters.java
+++ src/main/java/net/openid/conformance/condition/as/CreateEffectiveAuthorizationRequestParameters.java
@@ -25,6 +25,8 @@ public class CreateEffectiveAuthorizationRequestParameters extends AbstractCondi
 	public static final String CLIENT_ID = "client_id";
 	public static final String SCOPE = "scope";
 	public static final String NONCE = "nonce";
+	public static final String CODE_CHALLENGE = "code_challenge";
+	public static final String CODE_CHALLENGE_METHOD = "code_challenge_method";
 
 	//WARNING "authorization_request_object" is also used but it's not required
 	@Override
