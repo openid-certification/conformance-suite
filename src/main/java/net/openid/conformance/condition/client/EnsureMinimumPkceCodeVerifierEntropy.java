ea5980b8c717bd31ca9fd9f12282d621b91dbcc4
Edmund Jay <ejay@mgi1.com>
2022-04-08 16:20:03 -0700

    Check PKCE code_verifier for length and entropy

---
+++ src/main/java/net/openid/conformance/condition/client/EnsureMinimumPkceCodeVerifierEntropy.java
@@ -0,0 +1,43 @@
+package net.openid.conformance.condition.client;
+
+import com.google.common.base.Strings;
+import net.openid.conformance.condition.AbstractEnsureMinimumEntropy;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class EnsureMinimumPkceCodeVerifierEntropy extends AbstractEnsureMinimumEntropy {
+	/**
+	 * https://www.rfc-editor.org/rfc/rfc7636.html#section-7.1
+	 *
+	 *    The client SHOULD create a "code_verifier" with a minimum of 256 bits
+	 *    of entropy.  This can be done by having a suitable random number
+	 *    generator create a 32-octet sequence.  The octet sequence can then be
+	 *    base64url-encoded to produce a 43-octet URL safe string to use as a
+	 *    "code_challenge" that has the required entropy.
+	 *
+	 * The actual amount of required entropy is 256 bits, but we can't accurately measure entropy so a bit of
+	 * slop is allowed for.
+	 */
+	private final double requiredEntropy = 200;
+
+	/* (non-Javadoc)
+	 * @see Condition#evaluate(Environment)
+	 */
+	@Override
+	@PreEnvironment(required = "token_endpoint_request")
+	public Environment evaluate(Environment env) {
+
+		String codeVerifier = env.getString("token_endpoint_request", "body_form_params.code_verifier");
+
+		if (Strings.isNullOrEmpty(codeVerifier)) {
+			throw error("Couldn't find code_verifier in token request");
+		}
+
+		if (Strings.isNullOrEmpty(codeVerifier)) {
+			throw error("Can't find access token");
+		}
+
+		return ensureMinimumEntropy(env, codeVerifier, requiredEntropy);
+	}
+
+}
