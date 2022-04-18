f507df77f40ccf09150f5d3f13d74f11db680f90
Marcus Almgren <marcus@almgren.pt>
2022-04-18 12:42:56 +0100

    Addresses https://gitlab.com/openid/conformance-suite/-/merge_requests/1139#note_915316471

--- src/main/java/net/openid/conformance/fapiciba/rp/BackchannelRequestIsFormData.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/BackchannelRequestIsFormData.java
@@ -14,11 +14,15 @@ public class BackchannelRequestIsFormData extends AbstractCondition {
 	public Environment evaluate(Environment env) {
 
 		String contentType = env.getString("backchannel_endpoint_http_request", "headers.content-type");
-		List<String> allowedContentTypes =
-			List.of("application/x-www-form-urlencoded", "application/x-www-form-urlencoded;charset=utf-8");
+		String expected = "application/x-www-form-urlencoded";
 
-		if(allowedContentTypes.stream().noneMatch(s -> s.equalsIgnoreCase(contentType))) {
-			throw error("Content-Type must be one of '" + Joiner.on("', '").join(allowedContentTypes) + "'");
+		String mimeType = null;
+		try {
+			mimeType = contentType.split(";")[0].trim();
+		} catch (Exception ignored) { }
+
+		if (!expected.equals(mimeType)) {
+			throw error("content-type header  does not have the expected value", args("content_type", contentType, "expected", expected));
 		}
 
 		logSuccess("Backchannel authentication request has Content-Type 'application/x-www-form-urlencoded'");
