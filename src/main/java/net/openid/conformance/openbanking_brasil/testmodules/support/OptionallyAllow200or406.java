8dee77e00438c9c582658ce60dd0efe1ac4f0cfb
Alexandre Morais <alexandre.morais@raidiam.com>
2022-05-04 07:03:10 -0300

    (OCI-763) refactor: update OptionallyAllow200or406

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OptionallyAllow200or406.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/OptionallyAllow200or406.java
@@ -8,26 +8,25 @@ import org.apache.http.HttpStatus;
 public class OptionallyAllow200or406 extends AbstractCondition {
 
 	@Override
-	@PreEnvironment(required = "endpoint_response")
+	@PreEnvironment(required = "resource_endpoint_response_full")
 	public Environment evaluate(Environment env) {
 
-		int statusCode = env.getInteger("endpoint_response", "status");
-		String endpointName = env.getString("endpoint_response", "endpoint_name");
+		int statusCode = env.getInteger("resource_endpoint_response_full", "status");
 
 		if(statusCode == HttpStatus.SC_OK) {
-			logSuccess(endpointName + " endpoint returned an http status of 200 - proceeding with test now", args("http_status", statusCode));
+			logSuccess(" endpoint returned an http status of 200 - proceeding with test now", args("http_status", statusCode));
 			env.putString("proceed_with_test", "proceed");
 		}
 
 		if(statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
-			logSuccess(endpointName + " endpoint returned an http status of 422 - validating response and ending test now", args("http_status", statusCode));
+			logSuccess(" endpoint returned an http status of 422 - validating response and ending test now", args("http_status", statusCode));
 		}
 
 		if (statusCode != HttpStatus.SC_OK && statusCode != HttpStatus.SC_NOT_ACCEPTABLE) {
-			throw error(endpointName + " endpoint returned an unexpected http status - either 200 or 406 accepted", args("http_status", statusCode));
+			throw error(" endpoint returned an unexpected http status - either 200 or 406 accepted", args("http_status", statusCode));
 		}
 
-		logSuccess(endpointName + " endpoint returned the expected http status", args("http_status", statusCode));
+		logSuccess(" endpoint returned the expected http status", args("http_status", statusCode));
 
 		return env;
 	}
