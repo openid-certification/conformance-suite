ddfaa27eaf98bb488eb04555c36b1c61590eabb3
Alexandre Morais <alexandre.morais@raidiam.com>
2022-05-16 20:02:58 -0300

    (OCI-798) add unarranged api test v2

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/plans/v2/CreditOperationsAdvancesApiTestPlanV2.java
@@ -0,0 +1,37 @@
+package net.openid.conformance.openbanking_brasil.plans.v2;
+
+import net.openid.conformance.openbanking_brasil.OBBProfile;
+import net.openid.conformance.openbanking_brasil.plans.PlanNames;
+import net.openid.conformance.openbanking_brasil.testmodules.PreFlightCertCheckModule;
+import net.openid.conformance.openbanking_brasil.testmodules.creditOperations.advances.testmodules.CreditOperationsAdvancesApiResourcesTestModule;
+import net.openid.conformance.openbanking_brasil.testmodules.creditOperations.advances.testmodules.CreditOperationsAdvancesApiTestModule;
+import net.openid.conformance.openbanking_brasil.testmodules.creditOperations.advances.testmodules.CreditOperationsAdvancesApiWrongPermissionsTestModule;
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.variant.FAPI1FinalOPProfile;
+
+import java.util.List;
+
+@PublishTestPlan(
+	testPlanName = "Unarranged overdraft api test " + PlanNames.LATEST_VERSION_2,
+	profile = OBBProfile.OBB_PROFIlE_PHASE2,
+	displayName = PlanNames.CREDIT_OPERATIONS_ADVANCES_API_PLAN_NAME_V2,
+	summary = "Structural and logical tests for OpenBanking Brasil-conformant Unarranged Overdraft API"
+)
+public class CreditOperationsAdvancesApiTestPlanV2 implements TestPlan {
+	public static List<ModuleListEntry> testModulesWithVariants() {
+		return List.of(
+			new ModuleListEntry(
+				List.of(
+					PreFlightCertCheckModule.class,
+					CreditOperationsAdvancesApiTestModule.class,
+					CreditOperationsAdvancesApiWrongPermissionsTestModule.class,
+					CreditOperationsAdvancesApiResourcesTestModule.class
+				),
+				List.of(
+					new Variant(FAPI1FinalOPProfile.class, "openbanking_brazil")
+				)
+			)
+		);
+	}
+}
