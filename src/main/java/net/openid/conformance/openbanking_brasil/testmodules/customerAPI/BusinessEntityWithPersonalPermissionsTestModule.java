78e9c6c45f98df2aa46bf9b7b48d1da57e572e9a
Alexandre Morais <alexandre.morais@raidiam.com>
2022-05-19 09:55:49 -0300

    (OCI-803) feat: customer-api-businessentity-personal-permissions

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/customerAPI/BusinessEntityWithPersonalPermissionsTestModule.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/customerAPI/BusinessEntityWithPersonalPermissionsTestModule.java
@@ -15,9 +15,9 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "customer-api-businessentity-personal-permissions",
 	displayName = "Validate the structure of all consent API resources",
-	summary = "Validates the structure of all consent API resources\n" +
-		"\u2022 Creates a Consent with all of the existing permissions.\n" +
-		"\u2022 Checks all of the fields sent on the consent API are specification compliant\n",
+	summary = "This test makes sure that if businessEntity is sent on the consent creation together with personal customer data permission the server will reject the request\n" +
+		"\u2022 Make sure that the user has sent a data Payload that contains a BusinessEntity \n" +
+		"\u2022 Make a consent request with all the permissions but the Customer Business ones \n",
 	profile = OBBProfile.OBB_PROFILE,
 	configurationFields = {
 		"server.discoveryUrl",
