c3af474d4acc935b509f46d7696c3e94dba009dc
Alexandre Morais <alexandre.morais@raidiam.com>
2022-05-22 15:45:04 -0300

    (OCI-766) refactor: change how to build payment consents objects

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/AddDummyBrazilPaymentConsent.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/AddDummyBrazilPaymentConsent.java
@@ -5,52 +5,41 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.openbanking_brasil.testmodules.support.payments.DictHomologKeys;
 import net.openid.conformance.testmodule.Environment;
 import net.openid.conformance.testmodule.OIDFJSON;
 import net.openid.conformance.util.JsonObjectBuilder;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Map;
 
 public class AddDummyBrazilPaymentConsent extends AbstractCondition {
 	@Override
 	@PreEnvironment(required = "config" )
 	public Environment evaluate(Environment env) {
 
-		String brazilPaymentConsentString = "\n" +
-			"{\n" +
-			"  \"data\": {\n" +
-			"    \"loggedUser\": {\n" +
-			"      \"document\": {\n" +
-			"        \"identification\": \"11111111111\",\n" +
-			"        \"rel\": \"CPF\"\n" +
-			"      }\n" +
-			"    },\n" +
-			"    \"creditor\": {\n" +
-			"      \"name\": \"Pessoa Inexistente\",\n" +
-			"      \"cpfCnpj\": \"11111111111\",\n" +
-			"      \"personType\": \"PESSOA_NATURAL\"\n" +
-			"    },\n" +
-			"    \"payment\": {\n" +
-			"      \"date\": \"2022-06-18\",\n" +
-			"		\"type\": \"PIX\",\n"  +
-			"      \"amount\": \"0.01\",\n" +
-			"      \"currency\": \"BRL\",\n" +
-			"      \"details\": {\n" +
-			"        \"proxy\": \"11111111111\",\n" +
-			"        \"localInstrument\": \"DICT\",\n" +
-			"        \"creditorAccount\": {\n" +
-			"          \"number\": \"0012345678\",\n" +
-			"          \"accountType\": \"CACC\",\n" +
-			"          \"ispb\": \"99999008\",\n" +
-			"          \"issuer\": \"0001\"\n" +
-			"        }\n" +
-			"      }\n" +
-			"    }\n" +
-			"  }\n" +
-			"}";
+		JsonObjectBuilder brazilPaymentConsentObjectBuild = new JsonObjectBuilder()
+			.addFields( "data.loggedUser.document", Map.of("identification", "11111111111", "rel", "CPF"))
+			.addFields( "data.creditor",
+				Map.of("personType", "PESSOA_NATURAL",
+					"cpfCnpj", "11111111111",
+					"name", "Pessoa Inexistente"))
 
-		JsonObject brazilPaymentConsentObject = JsonParser.parseString(brazilPaymentConsentString).getAsJsonObject();
+			.addFields( "data.payment", Map.of("type", "PIX",
+				"currency", "BRL",
+				"amount", "0.01"))
+
+			.addFields( "data.payment.details", Map.of("localInstrument", "DICT",
+				"proxy", "11111111111"))
+
+			.addFields( "data.payment.details.creditorAccount",
+				Map.of("ispb", "99999008",
+					"issuer", "0001",
+					"number", "0012345678",
+					"accountType", "CACC"));
+
+		JsonObject brazilPaymentConsentObject = brazilPaymentConsentObjectBuild.build();
 
 		LocalDate scheduledDate = LocalDate.now(ZoneId.of("America/Sao_Paulo"));
 		JsonObjectBuilder.addField(brazilPaymentConsentObject, "data.payment.date", scheduledDate.toString());
