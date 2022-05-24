c5a1b3a2b5d27c673ad72371d543bb74e3048fd9
Marcus Almgren <marcus@almgren.pt>
2022-05-24 14:50:18 +0100

    The error reason should reflect the condition it is verifying https://gitlab.com/openid/conformance-suite/-/issues/1042

--- src/main/java/net/openid/conformance/condition/client/ExpectIdTokenHintRequiredErrorPage.java
+++ src/main/java/net/openid/conformance/condition/client/ExpectIdTokenHintRequiredErrorPage.java
@@ -10,7 +10,7 @@ public class ExpectIdTokenHintRequiredErrorPage extends AbstractCondition {
 	@PostEnvironment(strings = "id_token_hint_required_error")
 	public Environment evaluate(Environment env) {
 
-		String placeholder = createBrowserInteractionPlaceholder("The server must show an error page saying the request is invalid as the post_logout_redirect_uri is not a registered one - upload a screenshot of the error page.");
+		String placeholder = createBrowserInteractionPlaceholder("The server must show an error page saying the request is invalid as the id_token_hint is missing - upload a screenshot of the error page.");
 		env.putString("id_token_hint_required_error", placeholder);
 
 		return env;
