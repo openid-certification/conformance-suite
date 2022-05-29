a07920e44379483a5a3e50be0297f8f523ebd3a4
Alexandre Morais <alexandre.morais@raidiam.com>
2022-05-29 14:55:58 -0300

    (OCI-826) wip

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/customerAPI/testmodule/CustomerPersonalWrongPermissionsTestModule.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/customerAPI/testmodule/CustomerPersonalWrongPermissionsTestModule.java
@@ -1,14 +1,17 @@
 package net.openid.conformance.openbanking_brasil.testmodules.customerAPI.testmodule;
 
+import com.google.gson.JsonObject;
 import net.openid.conformance.openbanking_brasil.OBBProfile;
 import net.openid.conformance.openbanking_brasil.generic.ErrorValidator;
 import net.openid.conformance.openbanking_brasil.testmodules.AbstractPermissionsCheckingFunctionalTestModule;
+import net.openid.conformance.openbanking_brasil.testmodules.RememberOriginalScopes;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.AddScopesForCustomerApi;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.PrepareAllCustomerPersonalRelatedConsentsForHappyPathTest;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.PrepareToGetPersonalFinancialRelationships;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.PrepareToGetPersonalIdentifications;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.PrepareToGetPersonalQualifications;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.ProvideIncorrectPermissionsForCustomerPersonalApi;
+import net.openid.conformance.openbanking_brasil.testmodules.support.AddDummyPersonalProductTypeToConfig;
 import net.openid.conformance.openbanking_brasil.testmodules.support.CallProtectedResourceExpectingFailureSequence;
 import net.openid.conformance.openbanking_brasil.testmodules.support.EnsureResponseCodeWas403;
 import net.openid.conformance.testmodule.PublishTestModule;
@@ -41,6 +44,11 @@ import net.openid.conformance.condition.Condition;
 	}
 )
 public class CustomerPersonalWrongPermissionsTestModule extends AbstractPermissionsCheckingFunctionalTestModule {
+
+	protected void onConfigure(JsonObject config, String baseUrl) {
+		//Simples UI fix
+		callAndStopOnFailure(AddDummyPersonalProductTypeToConfig.class);
+	}
 
 	@Override
 	protected void preFetchResources() {
