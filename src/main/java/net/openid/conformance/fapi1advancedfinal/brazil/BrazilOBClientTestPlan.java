5f01cb2097ff6576c915e731130325e62c3424dd
Edmund Jay <ejay@mgi1.com>
2022-06-01 15:34:33 -0700

    Rename FAPIJARMType to FAPIClientType
    Replaced FAPIJARMType to FAPIClientType,  "fapi_jarm_type" to "fapi_client_type", "FAPI_JARM_TYPE" to "FAPI_CLIENT_TYPE", fapiJarmType to fapiClientType, jarmType = fapiClientType

--- src/main/java/net/openid/conformance/fapi1advancedfinal/brazil/BrazilOBClientTestPlan.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/brazil/BrazilOBClientTestPlan.java
@@ -55,7 +55,7 @@ public class BrazilOBClientTestPlan implements TestPlan {
 		Map<String, String> v = variant.getVariant();
 		String clientAuth = v.get("client_auth_type");
 		String responseMode = v.get("fapi_response_mode");
-		String jarmType = v.get("fapi_jarm_type");
+		String fapiClientType = v.get("fapi_client_type");
 
 		switch (clientAuth) {
 			case "private_key_jwt":
@@ -72,7 +72,7 @@ public class BrazilOBClientTestPlan implements TestPlan {
 				break;
 			case "jarm":
 				certProfile += ", JARM";
-				switch(jarmType) {
+				switch(fapiClientType) {
 					case "oidc":
 						certProfile += " (OpenID Connect)";
 						break;
@@ -81,7 +81,7 @@ public class BrazilOBClientTestPlan implements TestPlan {
 						break;
 					default:
 						throw new RuntimeException(String.format("Invalid configuration for %s: Unexpected jarm type value: %s",
-							MethodHandles.lookup().lookupClass().getSimpleName(), jarmType));
+							MethodHandles.lookup().lookupClass().getSimpleName(), fapiClientType));
 				}
 				break;
 		}
--- src/main/java/net/openid/conformance/fapi2baselineid2/FAPI2BaselineID2ClientTestValidAudAsArray.java
+++ src/main/java/net/openid/conformance/fapi2baselineid2/FAPI2BaselineID2ClientTestValidAudAsArray.java
@@ -3,7 +3,7 @@ package net.openid.conformance.fapi2baselineid2;
 import net.openid.conformance.condition.as.AddAudValueAsArrayToIdToken;
 import net.openid.conformance.condition.as.SignIdTokenBypassingNimbusChecks;
 import net.openid.conformance.testmodule.PublishTestModule;
-import net.openid.conformance.variant.FAPIJARMType;
+import net.openid.conformance.variant.FAPIClientType;
 import net.openid.conformance.variant.VariantNotApplicable;
 
 @PublishTestModule(
@@ -20,7 +20,7 @@ import net.openid.conformance.variant.VariantNotApplicable;
 		"client.jwks"
 	}
 )
-@VariantNotApplicable(parameter = FAPIJARMType.class, values = "plain_oauth")
+@VariantNotApplicable(parameter = FAPIClientType.class, values = "plain_oauth")
 public class FAPI2BaselineID2ClientTestValidAudAsArray extends AbstractFAPI2BaselineID2ClientTest {
 
 	@Override
