430dbb0b91c46d1fb0da33b70ac3ec7a22f9e769
Joseph Heenan <joseph.heenan@fintechlabs.io>
2022-06-06 17:14:04 +0100

    Fixup pre/post for AddTlsClientAuthSubjectDnToClientConfigurationRequest

--- src/main/java/net/openid/conformance/condition/client/AddTlsClientAuthSubjectDnToClientConfigurationRequest.java
+++ src/main/java/net/openid/conformance/condition/client/AddTlsClientAuthSubjectDnToClientConfigurationRequest.java
@@ -10,8 +10,7 @@ import net.openid.conformance.testmodule.Environment;
 public class AddTlsClientAuthSubjectDnToClientConfigurationRequest extends AbstractCondition {
 
 	@Override
-	@PreEnvironment(required = { "dynamic_registration_request", "certificate_subject" })
-	@PostEnvironment(required = "dynamic_registration_request")
+	@PreEnvironment(required = { "registration_client_endpoint_request_body", "certificate_subject" })
 	public Environment evaluate(Environment env) {
 
 		JsonObject request = env.getObject("registration_client_endpoint_request_body");
@@ -24,7 +23,7 @@ public class AddTlsClientAuthSubjectDnToClientConfigurationRequest extends Abstr
 		request.addProperty("tls_client_auth_subject_dn", subjectDn);
 
 		log("Added tls_client_auth_subject_dn to client configuration request",
-			args("dynamic_registration_request", request));
+			args("registration_client_endpoint_request_body", request));
 
 		return env;
 	}
