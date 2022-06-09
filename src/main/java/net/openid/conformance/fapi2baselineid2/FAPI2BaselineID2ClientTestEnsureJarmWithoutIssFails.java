b23ee0e035fb2cc9e4393772e7291130919b89f6
Edmund Jay <ejay@mgi1.com>
2022-06-09 02:34:46 -0700

    Add FAPI 2 RP JARM test for returning JARM response with invalid iss claim and without iss claim

---
+++ src/main/java/net/openid/conformance/fapi2baselineid2/FAPI2BaselineID2ClientTestEnsureJarmWithoutIssFails.java
@@ -0,0 +1,44 @@
+package net.openid.conformance.fapi2baselineid2;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.as.RemoveIssFromJarm;
+import net.openid.conformance.testmodule.PublishTestModule;
+import net.openid.conformance.variant.FAPIResponseMode;
+import net.openid.conformance.variant.VariantNotApplicable;
+
+
+@PublishTestModule(
+	testName = "fapi2-baseline-id2-client-test-ensure-jarm-without-iss-fails",
+	displayName = "FAPI2-Baseline-ID2: sends a JARM response without the iss claim.",
+	summary = "This test should end with the client displaying an error message that the JARM response is missing the iss claim",
+	profile = "FAPI2-Baseline-ID2",
+	configurationFields = {
+		"server.jwks",
+		"client.client_id",
+		"client.scope",
+		"client.redirect_uri",
+		"client.certificate",
+		"client.jwks"
+	}
+)
+
+@VariantNotApplicable(parameter = FAPIResponseMode.class, values = { "plain_response" })
+public class FAPI2BaselineID2ClientTestEnsureJarmWithoutIssFails extends AbstractFAPI2BaselineID2ClientExpectNothingAfterAuthorizationResponse {
+
+
+	@Override
+	protected void generateJARMResponseClaims() {
+		super.generateJARMResponseClaims();
+		callAndContinueOnFailure(RemoveIssFromJarm.class);
+	}
+
+	@Override
+	protected void addCustomValuesToIdToken(){
+		//Do nothing
+	}
+
+	@Override
+	protected String getAuthorizationResponseErrorMessage() {
+		return "Removed iss from JARM response";
+	}
+}
