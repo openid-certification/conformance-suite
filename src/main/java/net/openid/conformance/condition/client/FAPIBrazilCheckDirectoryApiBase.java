9b6ddf53c38c5139286b60ec1e9f6d6e690d4596
George McIntosh <george@georgemcintosh.com>
2022-06-10 14:01:58 +0100

    Also allow apis to be directory prod

--- src/main/java/net/openid/conformance/condition/client/FAPIBrazilCheckDirectoryApiBase.java
+++ src/main/java/net/openid/conformance/condition/client/FAPIBrazilCheckDirectoryApiBase.java
@@ -4,8 +4,14 @@ import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class FAPIBrazilCheckDirectoryApiBase extends AbstractCondition {
-	private final String BRAZIL_DIRECTORY_API_BASE = "https://matls-api.sandbox.directory.openbankingbrasil.org.br/";
+	private final String BRAZIL_SANDBOX_DIRECTORY_API_BASE = "https://matls-api.sandbox.directory.openbankingbrasil.org.br/";
+	private final String BRAZIL_DIRECTORY_API_BASE = "https://matls-api.directory.openbankingbrasil.org.br/";
+	private final Set<String> ACCEPTABLE_API_URLS = Set.of(BRAZIL_SANDBOX_DIRECTORY_API_BASE, BRAZIL_DIRECTORY_API_BASE);
+	private final String suggestion = ACCEPTABLE_API_URLS.stream().collect(Collectors.joining(" or "));
 
 	@Override
 	@PreEnvironment(required = "config")
@@ -14,10 +20,10 @@ public class FAPIBrazilCheckDirectoryApiBase extends AbstractCondition {
 		String apiBase = getStringFromEnvironment(env, "config", "directory.apibase",
 			"Directory API base in test configuration");
 
-		if (!apiBase.equals(BRAZIL_DIRECTORY_API_BASE)) {
+		if (!ACCEPTABLE_API_URLS.contains(apiBase)) {
 			throw error("Testing for Brazil certification must be done using the Brazil directory. If you do not have access to the directory an example client is available in the conformance suite instructions.",
 				args("directory_api_base", apiBase,
-					"expected", BRAZIL_DIRECTORY_API_BASE));
+					"expected", suggestion));
 		}
 
 		logSuccess("Directory API base matches the Brazil directory.", args("actual", apiBase));
