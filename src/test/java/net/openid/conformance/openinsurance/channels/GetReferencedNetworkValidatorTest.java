63339e3c4c7a59e0cfc0e963acc80351683f70a5
omnigon java delivery <conformance.delivery@omnigon.biz>
2022-06-10 10:44:45 +0300

    delivery-20220610-OPIN
    - CF-321 OPIN: Two APIS changed from products-services to channels

--- src/test/java/net/openid/conformance/openinsurance/channels/GetReferencedNetworkValidatorTest.java
+++ src/test/java/net/openid/conformance/openinsurance/channels/GetReferencedNetworkValidatorTest.java
@@ -1,6 +1,7 @@
-package net.openid.conformance.openinsurance.validator.productsServices;
+package net.openid.conformance.openinsurance.channels;
 
 import net.openid.conformance.apis.AbstractJsonResponseConditionUnitTest;
+import net.openid.conformance.openinsurance.validator.channels.GetReferencedNetworkValidator;
 import net.openid.conformance.util.UseResurce;
 import org.junit.Test;
 
