ee58f51e149fda6e924cf308fd821b837bc358b9
Edmund Jay <ejay@mgi1.com>
2022-06-13 03:16:04 -0700

    Add FAPI 2 RP tests for missing and invalid state in the authorization response

---
+++ src/main/java/net/openid/conformance/condition/as/RemoveStateFromAuthorizationEndpointResponseParams.java
@@ -0,0 +1,24 @@
+package net.openid.conformance.condition.as;
+
+import com.google.common.base.Strings;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class RemoveStateFromAuthorizationEndpointResponseParams extends AbstractCondition {
+
+
+	@Override
+	@PreEnvironment(required = CreateAuthorizationEndpointResponseParams.ENV_KEY)
+	@PostEnvironment(required = CreateAuthorizationEndpointResponseParams.ENV_KEY)
+	public Environment evaluate(Environment env) {
+		JsonObject response = env.getObject(CreateAuthorizationEndpointResponseParams.ENV_KEY);
+		response.remove(CreateAuthorizationEndpointResponseParams.STATE);
+		env.putObject(CreateAuthorizationEndpointResponseParams.ENV_KEY, response);
+		logSuccess("Removed state from authorization endpoint response params", args( CreateAuthorizationEndpointResponseParams.ENV_KEY, response));
+		return env;
+	}
+
+}
