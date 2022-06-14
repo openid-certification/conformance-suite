ecb27f25246a5cd46eefaa31bfe09de0e384c159
nathan <nathan.deguara@raidiam.com>
2022-06-14 13:43:57 +0100

    OCI-802 Multiple client DCR test

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/DCRMultipleClientTest.java
@@ -0,0 +1,71 @@
+package net.openid.conformance.openbanking_brasil.testmodules;
+
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.ClientManagementEndpointAndAccessTokenRequired;
+import net.openid.conformance.fapi1advancedfinal.AbstractFAPI1AdvancedFinalBrazilDCR;
+import net.openid.conformance.openbanking_brasil.testmodules.support.CheckScopesFromDynamicRegistrationEndpointContainsConsentsOrPayments;
+import net.openid.conformance.sequence.client.CallDynamicRegistrationEndpointAndVerifySuccessfulResponse;
+import net.openid.conformance.testmodule.PublishTestModule;
+import net.openid.conformance.variant.ClientAuthType;
+import net.openid.conformance.variant.FAPI1FinalOPProfile;
+import net.openid.conformance.variant.VariantHidesConfigurationFields;
+
+@PublishTestModule(
+	testName = "dcr-test-multiple-clients",
+	displayName = "DCR test: Ensure server accepts multiple clients with the same credentials",
+	summary = "Performs DCR against the target server\n" +
+		"\u2022 Expect success 201 returned, first client_id created for this set of credentials\n" +
+		"\u2022 Perform second DCR against the target server \n" +
+		"\u2022 Expect success 201 returned, second client_id created for this set of credentials\n" +
+		"\u2022 Unregister both created clients -> This step cannot be skipped even if only one client is created",
+	profile = "FAPI1-Advanced-Final",
+	configurationFields = {
+		"server.discoveryUrl",
+		"client.jwks",
+		"mtls.key",
+		"mtls.cert",
+		"mtls.ca",
+		"directory.discoveryUrl",
+		"directory.client_id",
+		"directory2.client_id",
+		"directory.apibase",
+		"client2.jwks",
+		"mtls2.key",
+		"mtls2.cert",
+		"mtls2.ca"
+	}
+)
+
+@VariantHidesConfigurationFields(parameter = FAPI1FinalOPProfile.class, value = "openbanking_brazil", configurationFields = {
+	"client.org_jwks",
+	"resource.brazilOrganizationId",
+	"resource.brazilPaymentConsent",
+	"resource.brazilPixPayment",
+	"resource.consentUrl",
+	"resource.brazilCpf",
+	"resource.brazilCnpj"
+})
+
+public class DCRMultipleClientTest extends AbstractFAPI1AdvancedFinalBrazilDCR {
+	protected ClientAuthType clientAuthType;
+	@Override
+	protected void configureClient() {
+		clientAuthType = getVariant(ClientAuthType.class);
+		super.configureClient();
+	}
+
+	@Override
+	protected void callRegistrationEndpoint() {
+		//Create DCR
+		eventLog.startBlock("Create First Client");
+		call(sequence(CallDynamicRegistrationEndpointAndVerifySuccessfulResponse.class)); //URL
+		callAndContinueOnFailure(ClientManagementEndpointAndAccessTokenRequired.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2"); //Access token
+		callAndStopOnFailure(CheckScopesFromDynamicRegistrationEndpointContainsConsentsOrPayments.class); //Scopes
+		eventLog.endBlock();
+		eventLog.startBlock("Create Second Client");
+		call(sequence(CallDynamicRegistrationEndpointAndVerifySuccessfulResponse.class)); //URL
+		callAndContinueOnFailure(ClientManagementEndpointAndAccessTokenRequired.class, Condition.ConditionResult.FAILURE, "BrazilOBDCR-7.1", "RFC7592-2"); //Access token
+		callAndStopOnFailure(CheckScopesFromDynamicRegistrationEndpointContainsConsentsOrPayments.class); //Scopes
+		eventLog.endBlock();
+	}
+}
