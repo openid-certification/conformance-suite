c90a17585bf596aa4c633e322aef85bc130f5ba1
Edmund Jay <ejay@mgi1.com>
2022-06-17 03:32:33 -0700

    Add condition to check that authorization request does not request identity claims for the userinfo endpoint for IDMVP profile

---
+++ src/main/java/net/openid/conformance/condition/as/IdmvpEnsureAuthorizationRequestContainsNoUserinfoIdentityClaims.java
@@ -0,0 +1,32 @@
+package net.openid.conformance.condition.as;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.ConditionError;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.condition.rs.OIDCCLoadUserInfo;
+import net.openid.conformance.testmodule.Environment;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IdmvpEnsureAuthorizationRequestContainsNoUserinfoIdentityClaims extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = {CreateEffectiveAuthorizationPARRequestParameters.ENV_KEY})
+	public Environment evaluate(Environment env) {
+		JsonElement requestedUserinfoClaimsElement = env.getElementFromObject(CreateEffectiveAuthorizationPARRequestParameters.ENV_KEY, "claims.userinfo");
+		if((null != requestedUserinfoClaimsElement) && requestedUserinfoClaimsElement.isJsonObject()) {
+			JsonObject requestedUserinfoClaims = requestedUserinfoClaimsElement.getAsJsonObject();
+			List<String> userinfoClaims = Arrays.asList(OIDCCLoadUserInfo.SUPPORTED_CLAIMS);
+			for (String key : requestedUserinfoClaims.keySet()) {
+				if (userinfoClaims.contains(key)) {
+					throw error("Authorization request claims request contains identity claims for the UserInfo endpoint", args("claims userinfo request", requestedUserinfoClaims));
+				}
+			}
+		}
+		logSuccess("Authorization request claims request do not contain identity claims for the UserInfo endpoint");
+		return env;
+	}
+}
