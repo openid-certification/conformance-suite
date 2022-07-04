e53321617d122e4910a826377af1d7168b769dc7
joolsRaidiam <jools.houston@raidiam.com>
2022-07-04 17:31:15 +0100

    OCI-895

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureConsentAspspRevoked.java
@@ -0,0 +1,15 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support;
+
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class EnsureConsentAspspRevoked extends AbstractConsentRejectionValidation {
+	@Override
+	@PreEnvironment(strings = "resource_endpoint_response")
+	public Environment evaluate(Environment env) {
+		if (validateResponse(env.getString("resource_endpoint_response"),"CUSTOMER_MANUALLY_REVOKED","ASPSP")){
+			env.putBoolean("code_returned", true);
+		}
+		return env;
+	}
+}
