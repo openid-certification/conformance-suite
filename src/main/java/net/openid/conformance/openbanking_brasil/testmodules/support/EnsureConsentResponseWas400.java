4cafd366c00785c08c650c654d643d585497d54a
Alexandre Morais <alexandre.morais@raidiam.com>
2022-07-07 12:13:29 -0300

    (OCI-944) fix: PMD failures

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureConsentResponseWas400.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureConsentResponseWas400.java
@@ -7,12 +7,8 @@ import org.springframework.http.HttpStatus;
 public class EnsureConsentResponseWas400 extends AbstractCondition {
 	@Override
 	public Environment evaluate(Environment env) {
-		int status;
-		try {
-			status = env.getInteger("resource_endpoint_response_status");
-		}catch (NullPointerException e) {
-			status = env.getInteger("consent_endpoint_response_full", "status");
-		}
+		int status = env.getInteger("consent_endpoint_response_full", "status");
+
 		if(status != HttpStatus.BAD_REQUEST.value() ) {
 			throw error("Was expecting a 400 response");
 		} else {
