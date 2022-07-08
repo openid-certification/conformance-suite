e687064b72c41a2ae4768f521adf4d85112c08c2
Joseph Heenan <joseph.heenan@fintechlabs.io>
2022-07-08 15:04:50 +0100

    BrazilDCR: Update clients + add accounts tests to CI
    The keys and certs are now ones from the openid org on openbanking
    brazil sandbox directory, so ones we can more directly manage.
    
    This hopefully also sorts out the fact that the existing clients were
    missing one or two urls for one or two of the review environments.

--- scripts/test-configs-brazil/run-raidiam.sh
+++ scripts/test-configs-brazil/run-raidiam.sh
@@ -5,6 +5,8 @@ TESTS=""
 # The non-DCR tests currently only work when run locally as the redirect urls for our review apps aren't registered
 # TESTS="$TESTS fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=openbanking_brazil][fapi_auth_request_method=by_value][fapi_response_mode=plain_response] ../conformance-suite/scripts/test-configs-brazil/brazil-raidiam-fapi-payments-automated.json"
 
+TESTS="$TESTS fapi1-advanced-final-brazil-dcr-test-plan[client_auth_type=mtls][fapi_auth_request_method=pushed][fapi_response_mode=jarm][fapi_profile=openbanking_brazil] ../conformance-suite/scripts/test-configs-brazil/brazil-raidiam-fapi-dcr-accounts-automated.json"
+
 TESTS="$TESTS fapi1-advanced-final-brazil-dcr-test-plan[client_auth_type=private_key_jwt][fapi_auth_request_method=by_value][fapi_response_mode=plain_response][fapi_profile=openbanking_brazil] ../conformance-suite/scripts/test-configs-brazil/brazil-raidiam-fapi-dcr-payments-automated.json"
 
 TESTS="${TESTS} --export-dir ../conformance-suite"
