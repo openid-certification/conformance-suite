ac04eb0ca10c1779dec4184f5b7301b1709aa600
Alexandre Morais <alexandre.morais@raidiam.com>
2022-07-12 18:13:05 -0300

    (OCI-945) feat: new pre-flight for consents V2

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/CpfCnpjValidator.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/CpfCnpjValidator.java
@@ -4,9 +4,10 @@ public class CpfCnpjValidator {
 	private static final int[] weightCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
 	private static final int[] weightCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
-	private static int calculateDigit(String str, int[] weight) {
+	private static int calculateDigit(String str, int... weight) {
 		int sum = 0;
-		for (int index=str.length()-1, digit; index >= 0; index-- ) {
+		int digit;
+		for (int index=str.length()-1; index >= 0; index-- ) {
 			digit = Integer.parseInt(str.substring(index,index+1));
 			sum += digit*weight[weight.length-str.length()+index];
 		}
@@ -15,7 +16,9 @@ public class CpfCnpjValidator {
 	}
 
 	public static boolean isValidCPF(String cpf) {
-		if ((cpf==null) || (cpf.length()!=11)) return false;
+		if ((cpf==null) || (cpf.length()!=11)) {
+			return false;
+		}
 
 		Integer firstDigit = calculateDigit(cpf.substring(0,9), weightCPF);
 		Integer secondDigit = calculateDigit(cpf.substring(0,9) + firstDigit, weightCPF);
@@ -23,15 +26,12 @@ public class CpfCnpjValidator {
 	}
 
 	public static boolean isValidCNPJ(String cnpj) {
-		if ((cnpj==null)||(cnpj.length()!=14)) return false;
+		if ((cnpj==null)||(cnpj.length()!=14)) {
+			return false;
+		}
 
 		Integer firstDigit = calculateDigit(cnpj.substring(0,12), weightCNPJ);
 		Integer secondDigit = calculateDigit(cnpj.substring(0,12) + firstDigit, weightCNPJ);
 		return cnpj.equals(cnpj.substring(0,12) + firstDigit.toString() + secondDigit.toString());
-	}
-
-	public static void main(String[] args) {
-		System.out.printf("CPF Valido:%s \n", CpfCnpjValidator.isValidCPF("0711821306"));
-		System.out.printf("CNPJ Valido:%s \n", CpfCnpjValidator.isValidCNPJ("13642634756318"));
 	}
 }
