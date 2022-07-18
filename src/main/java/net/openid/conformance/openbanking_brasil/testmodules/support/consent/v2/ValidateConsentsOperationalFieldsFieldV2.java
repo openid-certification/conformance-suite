c8d47e69f82864a182b619b93a9792b939263238
Alexandre Morais <alexandre.morais@raidiam.com>
2022-07-18 06:48:05 -0300

    (OCI-945) refactor: use super.evaluate in ValidateConsentsOperational

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/consent/v2/ValidateConsentsOperationalFieldsFieldV2.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/consent/v2/ValidateConsentsOperationalFieldsFieldV2.java
@@ -14,22 +14,6 @@ public class ValidateConsentsOperationalFieldsFieldV2 extends ValidateConsentsFi
 	public Environment evaluate(Environment env) {
 
 		JsonElement config = env.getObject("config");
-		validateMainFields(config);
-
-		JsonElement productTypeElement = findElementOrThrowError(config, "$.consent.productType");
-		String productType = OIDFJSON.getString(productTypeElement);
-		if (Strings.isNullOrEmpty(productType)) {
-			logFailure("Product type (Business or Personal) must be specified in the test configuration");
-		}
-
-		if(productType.equals("business")) {
-			JsonElement brazilCnpjElement = findElementOrThrowError(config, "$.resource.brazilCnpj");
-			String brazilCnpj = OIDFJSON.getString(brazilCnpjElement);
-			if(Strings.isNullOrEmpty(brazilCnpj) || brazilCnpj.length() != super.cnpjLength) {
-				logFailure("brazilCnpj is not valid", args("brazilCnpj", brazilCnpj));
-			}
-		}
-
 		//Extra checks for Operational Limits tests
 		JsonElement brazilCpfOperationalElement = findElementOrThrowError(config, "$.resource.brazilCpfOperational");
 		String brazilCpfOperational = OIDFJSON.getString(brazilCpfOperationalElement);
@@ -43,6 +27,6 @@ public class ValidateConsentsOperationalFieldsFieldV2 extends ValidateConsentsFi
 			logFailure("client ID for Operational Limits not found");
 		}
 
-        return env;
+        return super.evaluate(env);
 	}
 }
