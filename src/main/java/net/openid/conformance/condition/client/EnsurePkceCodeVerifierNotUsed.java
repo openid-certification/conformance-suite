158730961880e6750c583e562a8c9b8c1317d943
Edmund Jay <ejay@mgi1.com>
2022-07-20 18:31:50 -0700

    Changed EnsurePkceCodeVerifierNotUsed to use static list to keep track of code verifiers.

--- src/main/java/net/openid/conformance/condition/client/EnsurePkceCodeVerifierNotUsed.java
+++ src/main/java/net/openid/conformance/condition/client/EnsurePkceCodeVerifierNotUsed.java
@@ -2,25 +2,35 @@ package net.openid.conformance.condition.client;
 
 import com.google.common.base.Strings;
 import net.openid.conformance.condition.AbstractCondition;
-import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
+import java.util.List;
+import java.util.Vector;
+
 public class EnsurePkceCodeVerifierNotUsed extends AbstractCondition {
 
-	public static final String ENV_KEY = "code_verifier_list";
+	private static final int CACHE_SIZE = 256;
+	private static final List<String> cachedCodes = new Vector<>(CACHE_SIZE) ;
 
 	@Override
 	@PreEnvironment(required = "token_endpoint_request")
-	@PostEnvironment(required = ENV_KEY)
 	public Environment evaluate(Environment env) {
 		String codeVerifier = env.getString("token_endpoint_request", "body_form_params.code_verifier");
-		String usedCodeVerifier = env.getString(ENV_KEY, codeVerifier);
-		if(Strings.isNullOrEmpty(usedCodeVerifier)) {
-			logSuccess("Code verifier has not been used", args("code_verifier", codeVerifier));
-			env.putString(ENV_KEY, codeVerifier, "1");
-		} else {
-			throw error("code verifier has been used", args("code verifier", codeVerifier));
+		if(Strings.isNullOrEmpty(codeVerifier)) {
+			throw error("Code Verifier not found in request");
+		}
+		synchronized (cachedCodes) {
+			if(cachedCodes.contains(codeVerifier)) {
+				throw error("code verifier has been used", args("code verifier", codeVerifier));
+			} else {
+				if(cachedCodes.size() >= CACHE_SIZE) {
+					cachedCodes.remove(0);
+				}
+				cachedCodes.add(codeVerifier);
+				log("Cached List obj", args("cache", cachedCodes));
+				logSuccess("Code verifier has not been used", args("code_verifier", codeVerifier));
+			}
 		}
 		return env;
 	}
