7ff0634e692dedd28ad688af98a1fc9f001531b5
DenisRaidiam <denis.podberiozkin@raidiam.com>
2022-07-25 15:37:24 +0100

    OCI-1004

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/v2/resources/ResourcesApiTestModuleCorrect404V2.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/v2/resources/ResourcesApiTestModuleCorrect404V2.java
@@ -5,18 +5,14 @@ import net.openid.conformance.condition.Condition;
 import net.openid.conformance.openbanking_brasil.OBBProfile;
 import net.openid.conformance.openbanking_brasil.generic.ErrorValidator;
 import net.openid.conformance.openbanking_brasil.testmodules.customerAPI.AddScopesForCustomerApi;
-import net.openid.conformance.openbanking_brasil.testmodules.support.AbstractOBBrasilFunctionalTestModuleOptionalErrors;
-import net.openid.conformance.openbanking_brasil.testmodules.support.BuildCreditOperationsAdvancesConfigResourceUrlFromConsentUrl;
-import net.openid.conformance.openbanking_brasil.testmodules.support.EnsureResponseCodeWas404;
-import net.openid.conformance.openbanking_brasil.testmodules.support.IgnoreResponseError;
-import net.openid.conformance.openbanking_brasil.testmodules.support.PrepareAllCustomerRelatedConsentsForResource404HappyPathTest;
+import net.openid.conformance.openbanking_brasil.testmodules.support.*;
 import net.openid.conformance.testmodule.PublishTestModule;
 
 @PublishTestModule(
 	testName = "resources-api-test-404-customer-data-v2",
 	displayName = "Validate correct response when only request customer data permissions",
 	summary = "Validates correct response when only requesting customer data permissions\n" +
-		"\u2022 Creates a Consent with the customer personal and business permissions (\"CUSTOMERS_PERSONAL_IDENTIFICATIONS_READ\", \"CUSTOMERS_PERSONAL_ADITTIONALINFO_READ\", \"CUSTOMERS_BUSINESS_IDENTIFICATIONS_READ\", \"CUSTOMERS_BUSINESS_ADITTIONALINFO_READ\", \"RESOURCES_READ\")\n" +
+		"\u2022 Creates a Consent with either the customer personal or business permissions (\"CUSTOMERS_PERSONAL_IDENTIFICATIONS_READ\", \"CUSTOMERS_PERSONAL_ADITTIONALINFO_READ\", \"RESOURCES_READ\") or (\"CUSTOMERS_BUSINESS_IDENTIFICATIONS_READ\", \"CUSTOMERS_BUSINESS_ADITTIONALINFO_READ\", \"RESOURCES_READ\")\n" +
 		"\u2022 Expects a success 201 - Expects a success on Redirect as well \n" +
 		"\u2022 Calls GET Resources API V2\n" +
 		"\u2022 Expects a 404 response as neither customer personal nor customer business support the resources API V2",
@@ -37,7 +33,7 @@ public class ResourcesApiTestModuleCorrect404V2 extends AbstractOBBrasilFunction
 
 	@Override
 	protected void configureClient(){
-		callAndStopOnFailure(BuildCreditOperationsAdvancesConfigResourceUrlFromConsentUrl.class);
+		callAndStopOnFailure(BuildResourcesConfigResourceUrlFromConsentUrl.class);
 		super.configureClient();
 	}
 
