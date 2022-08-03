2f8626cbc6765321cf019852337f43d046a92224
Marcus Almgren <marcus@almgren.pt>
2022-08-03 12:34:46 +0100

    Missing expires_in
    The client must detect that the backchannel endpoint response
    does not contain required property expires_in.

--- src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponseWithoutExpiresIn.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponseWithoutExpiresIn.java
@@ -4,14 +4,13 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.condition.util.RFC6749AppendixASyntaxUtils;
 import net.openid.conformance.testmodule.Environment;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 
-public class CreateBackchannelEndpointResponseWithoutAuthReqId extends AbstractCondition {
-
-	public static final int EXPIRES_IN = 180;
+public class CreateBackchannelEndpointResponseWithoutExpiresIn extends AbstractCondition {
 
 	@Override
 	@PreEnvironment(required = { "backchannel_endpoint_http_request", "backchannel_request_object" })
@@ -19,7 +18,9 @@ public class CreateBackchannelEndpointResponseWithoutAuthReqId extends AbstractC
 	public Environment evaluate(Environment env) {
 		JsonObject backchannelResponse = new JsonObject();
 
-		backchannelResponse.addProperty("expires_in", EXPIRES_IN);
+		String authReqId = RFC6749AppendixASyntaxUtils.generateVSChar(40, 10, 0);
+		env.putString("auth_req_id", authReqId); // Needed for the ping
+		backchannelResponse.addProperty("auth_req_id", authReqId);
 
 		env.putObject("backchannel_endpoint_response", backchannelResponse);
 		logSuccess("Created backchannel response", backchannelResponse);
