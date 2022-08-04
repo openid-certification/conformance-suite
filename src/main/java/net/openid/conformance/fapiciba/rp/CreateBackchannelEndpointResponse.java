bdeeb27ecbcd4f0132186cf86891fef8e068819c
Marcus Almgren <marcus@almgren.pt>
2022-08-04 12:31:50 +0100

    Refactor

--- src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponse.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponse.java
@@ -9,7 +9,6 @@ import net.openid.conformance.testmodule.Environment;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.function.BiConsumer;
 
 public class CreateBackchannelEndpointResponse extends AbstractCondition {
 
@@ -20,21 +19,29 @@ public class CreateBackchannelEndpointResponse extends AbstractCondition {
 	@PostEnvironment(required = "backchannel_endpoint_response", strings = { "auth_req_id", "auth_req_id_expiration"})
 	public Environment evaluate(Environment env) {
 		JsonObject backchannelResponse = new JsonObject();
-		String authReqId = RFC6749AppendixASyntaxUtils.generateVSChar(40, 10, 0);
-		env.putString("auth_req_id", authReqId); // Needed for the ping
-		backchannelResponse.addProperty("auth_req_id", authReqId);
 
+		addAuthReqId(env, backchannelResponse);
+		addExpiresIn(env, backchannelResponse);
+
+		env.putObject("backchannel_endpoint_response", backchannelResponse);
+		logSuccess("Created backchannel response", backchannelResponse);
+
+		return env;
+	}
+
+	protected void addExpiresIn(Environment env, JsonObject backchannelResponse) {
 		Integer requestedExpiry = env.getInteger("backchannel_request_object", "claims.requested_expiry");
 		int expiresIn = requestedExpiry != null ? requestedExpiry : EXPIRES_IN;
 		backchannelResponse.addProperty("expires_in", expiresIn);
 
 		String authReqIdExpiration = DateTimeFormatter.ISO_INSTANT.format(Instant.now().plusSeconds(expiresIn));
 		env.putString("auth_req_id_expiration", authReqIdExpiration);
+	}
 
-		env.putObject("backchannel_endpoint_response", backchannelResponse);
-		logSuccess("Created backchannel response", backchannelResponse);
-
-		return env;
+	protected void addAuthReqId(Environment env, JsonObject backchannelResponse) {
+		String authReqId = RFC6749AppendixASyntaxUtils.generateVSChar(40, 10, 0);
+		env.putString("auth_req_id", authReqId);
+		backchannelResponse.addProperty("auth_req_id", authReqId);
 	}
 
 }
--- src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponseWithoutExpiresIn.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/CreateBackchannelEndpointResponseWithoutExpiresIn.java
@@ -1,16 +1,11 @@
 package net.openid.conformance.fapiciba.rp;
 
 import com.google.gson.JsonObject;
-import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
-import net.openid.conformance.condition.util.RFC6749AppendixASyntaxUtils;
 import net.openid.conformance.testmodule.Environment;
 
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-
-public class CreateBackchannelEndpointResponseWithoutExpiresIn extends AbstractCondition {
+public class CreateBackchannelEndpointResponseWithoutExpiresIn extends CreateBackchannelEndpointResponse {
 
 	@Override
 	@PreEnvironment(required = { "backchannel_endpoint_http_request", "backchannel_request_object" })
@@ -18,9 +13,7 @@ public class CreateBackchannelEndpointResponseWithoutExpiresIn extends AbstractC
 	public Environment evaluate(Environment env) {
 		JsonObject backchannelResponse = new JsonObject();
 
-		String authReqId = RFC6749AppendixASyntaxUtils.generateVSChar(40, 10, 0);
-		env.putString("auth_req_id", authReqId); // Needed for the ping
-		backchannelResponse.addProperty("auth_req_id", authReqId);
+		addAuthReqId(env, backchannelResponse);
 
 		env.putObject("backchannel_endpoint_response", backchannelResponse);
 		logSuccess("Created backchannel response", backchannelResponse);
