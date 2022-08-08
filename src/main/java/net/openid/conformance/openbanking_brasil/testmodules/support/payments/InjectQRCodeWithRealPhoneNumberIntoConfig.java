6dc1cf0b1a00730a0a28dbf5a733571a19df25a4
DenisRaidiam <denis.podberiozkin@raidiam.com>
2022-08-08 18:03:38 +0100

    OCI-1040 prepared pipeline config for mb changes

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealPhoneNumberIntoConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealPhoneNumberIntoConfig.java
@@ -4,11 +4,8 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.openbanking_brasil.testmodules.support.payments.pixqrcode.PixQRCode;
 import net.openid.conformance.testmodule.Environment;
-import net.openid.conformance.testmodule.OIDFJSON;
 
-import java.util.Random;
-
-public class InjectQRCodeWithRealPhoneNumberIntoConfig  extends AbstractCondition {
+public class InjectQRCodeWithRealPhoneNumberIntoConfig extends AbstractCondition {
 
 	@Override
 	public Environment evaluate(Environment env) {
@@ -20,26 +17,12 @@ public class InjectQRCodeWithRealPhoneNumberIntoConfig  extends AbstractConditio
 		//Add proxy as the phone number
 		qrCode.setProxy(DictHomologKeys.PROXY_PHONE_NUMBER);
 
-		//Generate random amount
-		Random r = new Random();
-		float random = r.nextFloat() * 100;
-		String amount = String.format("%.02f", random);
-
-		qrCode.setTransactionAmount(amount);
-
-		JsonObject obj = (JsonObject) env.getElementFromObject("resource", "brazilPaymentConsent.data.payment");
-		obj.addProperty("amount", amount);
-
-		obj = (JsonObject) env.getElementFromObject("resource", "brazilPixPayment.data.payment");
-		obj.addProperty("amount", amount);
-
-
 		JsonObject consentPaymentDetails = (JsonObject) env.getElementFromObject("resource", "brazilPaymentConsent.data.payment.details");
 		JsonObject paymentInitiation = (JsonObject) env.getElementFromObject("resource", "brazilPixPayment.data");
 		consentPaymentDetails.addProperty("qrCode", qrCode.toString());
 		paymentInitiation.addProperty("qrCode", qrCode.toString());
 
-		logSuccess(String.format("Added new QRes to payment consent and payment initiation with amount %s BRL", amount), args("QRes", qrCode.toString()));
+		logSuccess("Added new QRes to payment consent and payment initiation", args("QRes", qrCode.toString()));
 
 		return env;
 	}
