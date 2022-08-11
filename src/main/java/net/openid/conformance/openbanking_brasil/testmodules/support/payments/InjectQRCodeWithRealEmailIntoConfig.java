f99772c5d682af767a5066ead8bfd856611b602d
DenisRaidiam <denis.podberiozkin@raidiam.com>
2022-08-11 11:26:29 +0100

    OCI-1040 changed config

--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealEmailIntoConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealEmailIntoConfig.java
@@ -2,17 +2,23 @@ package net.openid.conformance.openbanking_brasil.testmodules.support.payments;
 
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.openbanking_brasil.testmodules.support.payments.pixqrcode.PixQRCode;
 import net.openid.conformance.testmodule.Environment;
 
 public class InjectQRCodeWithRealEmailIntoConfig extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(required = "resource")
 	public Environment evaluate(Environment env) {
 		JsonObject consentPaymentDetails = (JsonObject) env.getElementFromObject("resource", "brazilPaymentConsent.data.payment.details");
 		JsonObject paymentInitiation = (JsonObject) env.getElementFromObject("resource", "brazilPixPayment.data");
 
-		consentPaymentDetails.addProperty("qrCode", QrCodeKeys.QRES_EMAIL);
-		paymentInitiation.addProperty("qrCode", QrCodeKeys.QRES_EMAIL);
+		PixQRCode qrCode = new PixQRCode();
+		qrCode.useStandardConfig();
+		qrCode.setTransactionAmount(env.getString("resource", "brazilPaymentConsent.data.payment.amount"));
+		consentPaymentDetails.addProperty("qrCode", qrCode.toString());
+		paymentInitiation.addProperty("qrCode", qrCode.toString());
 
 		logSuccess("Added qr code to payment consent and payment initiation");
 
--- src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealPhoneNumberIntoConfig.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/payments/InjectQRCodeWithRealPhoneNumberIntoConfig.java
@@ -2,21 +2,23 @@ package net.openid.conformance.openbanking_brasil.testmodules.support.payments;
 
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.openbanking_brasil.testmodules.support.payments.pixqrcode.PixQRCode;
 import net.openid.conformance.testmodule.Environment;
 
 public class InjectQRCodeWithRealPhoneNumberIntoConfig extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(required = "resource")
 	public Environment evaluate(Environment env) {
 
 		//Build the QRes
 		PixQRCode qrCode = new PixQRCode();
 		qrCode.useStandardConfig();
+		qrCode.setTransactionAmount(env.getString("resource", "brazilPaymentConsent.data.payment.amount"));
 
 		//Add proxy as the phone number
 		qrCode.setProxy(DictHomologKeys.PROXY_PHONE_NUMBER);
-
 		JsonObject consentPaymentDetails = (JsonObject) env.getElementFromObject("resource", "brazilPaymentConsent.data.payment.details");
 		JsonObject paymentInitiation = (JsonObject) env.getElementFromObject("resource", "brazilPixPayment.data");
 		consentPaymentDetails.addProperty("qrCode", qrCode.toString());
