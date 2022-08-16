f20581509965cb613a1aaf6978aaf1748b63ee48
Alexandre Morais <alexandre.morais@raidiam.com>
2022-08-16 08:18:09 -0300

    (OCI-1071) feat: add EnsureTokenResponseWas401

---
+++ src/main/java/net/openid/conformance/openbanking_brasil/testmodules/support/EnsureTokenResponseWas401.java
@@ -0,0 +1,25 @@
+package net.openid.conformance.openbanking_brasil.testmodules.support;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+import org.apache.http.HttpStatus;
+
+public class EnsureTokenResponseWas401 extends AbstractCondition {
+
+	@Override
+	@PostEnvironment(strings = "token_endpoint_response_http_status")
+	public Environment evaluate(Environment env) {
+		int statusCode = env.getInteger("token_endpoint_response_http_status");
+
+		if(statusCode != HttpStatus.SC_UNAUTHORIZED) {
+			throw error("Was expecting a 401 in the token response" , args("status", statusCode));
+		}
+
+		logSuccess("The status  code was 401, as expected", args("status", statusCode));
+		return env;
+	}
+
+}
