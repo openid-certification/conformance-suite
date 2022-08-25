1039a34b46a363236179d1b79f8fc891731087ee
Alexandre Morais <alexandre.morais@raidiam.com>
2022-08-24 22:27:20 -0300

    (OICS-66) refactor

--- src/main/java/net/openid/conformance/openinsurance/testmodule/support/OpinConsentPermissionsBuilder.java
+++ src/main/java/net/openid/conformance/openinsurance/testmodule/support/OpinConsentPermissionsBuilder.java
@@ -9,17 +9,13 @@ import net.openid.conformance.testmodule.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class OpinConsentPermissionsBuilder extends AbstractFAPI1AdvancedFinalServerTestModule {
 
 	private final Logger logger = LoggerFactory.getLogger(ConditionSequenceRepeater.class);
-
 	private Set<String> permissionsSet;
-	private Set<String> includedPermissionsGroup;
-
+	private Set<PermissionsGroup> includedPermissionsGroups;
 	private boolean setsSet = false;
 
 	public OpinConsentPermissionsBuilder(Environment env, String id, TestInstanceEventLog eventLog,
@@ -32,12 +28,9 @@ public class OpinConsentPermissionsBuilder extends AbstractFAPI1AdvancedFinalSer
 	}
 
 	public OpinConsentPermissionsBuilder resetPermissions() {
+
 		this.permissionsSet = new HashSet<>();
-		this.includedPermissionsGroup = new HashSet<>();
-
-		//All calls to Consents API needs RESOURCES_READ permission
-		includedPermissionsGroup.add(PermissionsGroup.RESOURCES_ALL);
-
+		this.includedPermissionsGroups = new HashSet<>();
 		setsSet = true;
 		logger.info("permissionsSet and includedPermissionsGroup sets were correctly instantiated");
 		return this;
@@ -47,103 +40,57 @@ public class OpinConsentPermissionsBuilder extends AbstractFAPI1AdvancedFinalSer
 		return setsSet;
 	}
 
-	public OpinConsentPermissionsBuilder addAllPermissions() {
-		if(!isSetsSet()) {
-			resetPermissions();
-		}
-
-		String[] permissions = {"CUSTOMERS_PERSONAL_IDENTIFICATIONS_READ", "RESOURCES_READ", "CUSTOMERS_PERSONAL_QUALIFICATION_READ",
-			"CUSTOMERS_PERSONAL_ADITTIONALINFO_READ", "CUSTOMERS_BUSINESS_IDENTIFICATIONS_READ","CUSTOMERS_BUSINESS_QUALIFICATION_READ",
-			"CUSTOMERS_BUSINESS_ADITTIONALINFO_READ", "CAPITALIZATION_TITLES_READ", "CAPITALIZATION_TITLES_POLICYINFO_READ",
-		"CAPITALIZATION_TITLES_PREMIUM_READ", "CAPITALIZATION_TITLES_CLAIM_READ", "PENSION_RISK_READ", "PENSION_RISK_CONTRACTINFO_READ",
-			"PENSION_RISK_CONTRIBUTIONS_READ", "DAMAGES_AND_PEOPLE_PATRIMONIAL_READ", "DAMAGES_AND_PEOPLE_PATRIMONIAL_POLICYINFO_READ",
-			"DAMAGES_AND_PEOPLE_PATRIMONIAL_PREMIUM_READ", "DAMAGES_AND_PEOPLE_PATRIMONIAL_CLAIM_READ", "DAMAGES_AND_PEOPLE_AERONAUTICAL_READ",
-			"DAMAGES_AND_PEOPLE_AERONAUTICAL_POLICYINFO_READ", "DAMAGES_AND_PEOPLE_AERONAUTICAL_PREMIUM_READ", "DAMAGES_AND_PEOPLE_AERONAUTICAL_CLAIM_READ",
-			"DAMAGES_AND_PEOPLE_NAUTICAL_READ", "DAMAGES_AND_PEOPLE_NAUTICAL_POLICYINFO_READ", "DAMAGES_AND_PEOPLE_NAUTICAL_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_NAUTICAL_CLAIM_READ", "DAMAGES_AND_PEOPLE_OIL_READ", "DAMAGES_AND_PEOPLE_OIL_POLICYINFO_READ", "DAMAGES_AND_PEOPLE_OIL_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_OIL_CLAIM_READ", "DAMAGES_AND_PEOPLE_RESPONSIBILITY_READ","DAMAGES_AND_PEOPLE_RESPONSIBILITY_POLICYINFO_READ",
-			"DAMAGES_AND_PEOPLE_RESPONSIBILITY_PREMIUM_READ", "DAMAGES_AND_PEOPLE_RESPONSIBILITY_CLAIM_READ","DAMAGES_AND_PEOPLE_TRANSPORT_READ",
-			"DAMAGES_AND_PEOPLE_TRANSPORT_POLICYINFO_READ","DAMAGES_AND_PEOPLE_TRANSPORT_PREMIUM_READ", "DAMAGES_AND_PEOPLE_TRANSPORT_CLAIM_READ",
-			"DAMAGES_AND_PEOPLE_FINANCIAL_RISKS_READ", "DAMAGES_AND_PEOPLE_FINANCIAL_RISKS_POLICYINFO_READ","DAMAGES_AND_PEOPLE_FINANCIAL_RISKS_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_FINANCIAL_RISKS_CLAIM_READ","DAMAGES_AND_PEOPLE_RURAL_READ", "DAMAGES_AND_PEOPLE_RURAL_POLICYINFO_READ", "DAMAGES_AND_PEOPLE_RURAL_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_RURAL_CLAIM_READ","DAMAGES_AND_PEOPLE_AUTO_READ", "DAMAGES_AND_PEOPLE_AUTO_POLICYINFO_READ","DAMAGES_AND_PEOPLE_AUTO_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_AUTO_CLAIM_READ","DAMAGES_AND_PEOPLE_HOUSING_READ","DAMAGES_AND_PEOPLE_HOUSING_POLICYINFO_READ","DAMAGES_AND_PEOPLE_HOUSING_PREMIUM_READ",
-			"DAMAGES_AND_PEOPLE_HOUSING_CLAIM_READ","DAMAGES_AND_PEOPLE_ACCEPTANCE_AND_BRANCHES_ABROAD_READ","DAMAGES_AND_PEOPLE_ACCEPTANCE_AND_BRANCHES_ABROAD_POLICYINFO_READ",
-			"DAMAGES_AND_PEOPLE_ACCEPTANCE_AND_BRANCHES_ABROAD_PREMIUM_READ","DAMAGES_AND_PEOPLE_ACCEPTANCE_AND_BRANCHES_ABROAD_CLAIM_READ",
-			"DAMAGES_AND_PEOPLE_PERSON_READ", "DAMAGES_AND_PEOPLE_PERSON_POLICYINFO_READ", "DAMAGES_AND_PEOPLE_PERSON_PREMIUM_READ","DAMAGES_AND_PEOPLE_PERSON_CLAIM_READ"
-		};
-
-		permissionsSet.addAll(List.of(permissions));
-		includedPermissionsGroup.add(PermissionsGroup.ALL);
-
-		logger.info("All Permissions were correctly added");
-		return this;
-	}
-	public OpinConsentPermissionsBuilder addAllCustomersPersonalPermissions() {
-		if(!isSetsSet()) {
-			resetPermissions();
-		}
-
-		String[] permissions = {"CUSTOMERS_PERSONAL_IDENTIFICATIONS_READ", "RESOURCES_READ", "CUSTOMERS_PERSONAL_QUALIFICATION_READ",
-			"CUSTOMERS_PERSONAL_ADITTIONALINFO_READ" };
-
-		permissionsSet.addAll(List.of(permissions));
-		includedPermissionsGroup.add(PermissionsGroup.CUSTOMERS_PERSONAL);
-
-		logger.info("All Customers Personal Permissions were correctly added");
-		return this;
-	}
-
-	public OpinConsentPermissionsBuilder addAllCustomersBusinessPermissions() {
-		if(!isSetsSet()) {
-			resetPermissions();
-		}
-
-		String[] permissions = {"CUSTOMERS_BUSINESS_IDENTIFICATIONS_READ", "RESOURCES_READ", "CUSTOMERS_BUSINESS_QUALIFICATION_READ",
-			"CUSTOMERS_BUSINESS_ADITTIONALINFO_READ" };
-
-		permissionsSet.addAll(List.of(permissions));
-		includedPermissionsGroup.add(PermissionsGroup.CUSTOMERS_BUSINESS);
-
-		logger.info("All Customers Business Permissions were correctly added");
-		return this;
-	}
-
-	public OpinConsentPermissionsBuilder addAllCustomersPermissions() {
-		if(!isSetsSet()) {
-			resetPermissions();
-		}
-
-		addAllCustomersPersonalPermissions();
-		addAllCustomersBusinessPermissions();
-
-		logger.info("All Customers Business Permissions were correctly set");
-		return this;
-	}
-
-	public OpinConsentPermissionsBuilder removePermissions(String... permissionGroup) {
-		for (String permission : permissionGroup) {
-			permissionsSet.removeIf(s -> s.startsWith(permission));
-			includedPermissionsGroup.removeIf(s -> s.startsWith(permission));
-		}
-
-		return this;
-	}
-
 	public OpinConsentPermissionsBuilder set(String... permissions) {
+
 		resetPermissions();
 		permissionsSet.addAll(List.of(permissions));
-		//No group associated, adding all permissions
-		includedPermissionsGroup.addAll(List.of(permissions));
 		return this;
 	}
+
+	public OpinConsentPermissionsBuilder addPermissionsGroup(PermissionsGroup... permissionsGroups) {
+
+		if(!isSetsSet()) {
+			resetPermissions();
+		}
+
+		for (PermissionsGroup permissionsGroup : permissionsGroups) {
+			permissionsSet.addAll(List.of(permissionsGroup.getPermissions()));
+			includedPermissionsGroups.add(permissionsGroup);
+		}
+		return  this;
+	}
+	public OpinConsentPermissionsBuilder removePermissionsGroups(PermissionsGroup... permissionGroups) {
+
+		for (PermissionsGroup permissionsGroup : permissionGroups) {
+			List<String> permissions = new ArrayList<>(List.of(permissionsGroup.getPermissions()));
+			permissions.remove("RESOURCES_READ");
+			permissionsSet.removeAll(permissions);
+		}
+		return this;
+	}
+
+	public OpinConsentPermissionsBuilder removePermission(String... permissions) {
+
+		for (String permission : permissions) {
+			includedPermissionsGroups.removeIf(permissionsGroup -> permissionsGroup.name().startsWith(permission));
+		}
+		permissionsSet.removeAll(List.of(permissions));
+		return this;
+	}
+
 
 	public String getLogMessage() {
 
-		String result = String.join(" ", includedPermissionsGroup) ;
-		if (includedPermissionsGroup.contains(PermissionsGroup.ALL)) {
-			result = "ALL PERMISSIONS";
+		String result;
+		if (includedPermissionsGroups.isEmpty()) {
+			result = "CUSTOM";
+		} else {
+			result = String.join(" ", includedPermissionsGroups.toString()) ;
+			if (includedPermissionsGroups.contains(PermissionsGroup.ALL)) {
+				result = "ALL PERMISSIONS";
+			}
 		}
+
 		String msg = String.format("Providing permissions %s\n", result);
 
 		return msg;
--- src/main/java/net/openid/conformance/openinsurance/testmodule/support/PermissionsGroup.java
+++ src/main/java/net/openid/conformance/openinsurance/testmodule/support/PermissionsGroup.java
@@ -1,9 +1,75 @@
 package net.openid.conformance.openinsurance.testmodule.support;
 
-public class PermissionsGroup {
-	public static final String ALL = "READ";
-	public static final String CUSTOMERS_ALL = "CUSTOMERS";
-	public static final String CUSTOMERS_PERSONAL = "CUSTOMERS_PERSONAL";
-	public static final String CUSTOMERS_BUSINESS = "CUSTOMERS_BUSINESS";
-	public static final String RESOURCES_ALL = "RESOURCES_READ";
+import org.apache.commons.lang3.ArrayUtils;
+
+public enum PermissionsGroup {
+	ALL,
+	RESOURCES,
+	CUSTOMERS_PERSONAL,
+	CUSTOMERS_BUSINESS,
+	CAPITALIZATION_TITLES,
+	PENSION_RISK,
+	DAMAGES_AND_PEOPLE_PATRIMONIAL,
+	DAMAGES_AND_PEOPLE_AERONAUTICAL,
+	DAMAGES_AND_PEOPLE_NAUTICAL,
+	DAMAGES_AND_PEOPLE_NUCLEAR,
+	DAMAGES_AND_PEOPLE_OIL,
+	DAMAGES_AND_PEOPLE_RESPONSIBILITY,
+	DAMAGES_AND_PEOPLE_TRANSPORT,
+	DAMAGES_AND_PEOPLE_FINANCIAL_RISKS,
+	DAMAGES_AND_PEOPLE_RURAL,
+	DAMAGES_AND_PEOPLE_AUTO,
+	DAMAGES_AND_PEOPLE_HOUSING,
+	DAMAGES_AND_PEOPLE_ACCEPTANCE_AND_BRANCHES_ABROAD,
+	DAMAGES_AND_PEOPLE_PERSON
+	;
+
+
+	public String[] getPermissions() {
+		String[] permissions = {};
+
+		if(this.equals(ALL)) {
+			for (PermissionsGroup permissionsGroup : PermissionsGroup.values()) {
+				if (permissionsGroup.equals(ALL)) {
+					continue;
+				}
+				permissions = ArrayUtils.addAll(permissions, getIndividualPermissions(permissionsGroup));
+			}
+		} else {
+			permissions = getIndividualPermissions(this);
+		}
+
+		return permissions;
+	}
+
+	private String[] getIndividualPermissions(PermissionsGroup permissionsGroup) {
+		String[] permissions;
+		switch (permissionsGroup) {
+			case CUSTOMERS_PERSONAL:
+				permissions = new String[]{permissionsGroup.name()+"_IDENTIFICATIONS_READ", permissionsGroup.name()+"_QUALIFICATION_READ",
+					permissionsGroup.name()+"_ADITTIONALINFO_READ", "RESOURCES_READ"};
+				break;
+
+			case CUSTOMERS_BUSINESS:
+				permissions = new String[]{permissionsGroup.name()+"_IDENTIFICATIONS_READ", permissionsGroup.name()+"_QUALIFICATION_READ",
+					permissionsGroup.name()+"_ADITTIONALINFO_READ", "RESOURCES_READ"};
+				break;
+
+			case PENSION_RISK:
+				permissions = new String[]{permissionsGroup.name() + "_READ", permissionsGroup.name()+"_CONTRACTINFO_READ",
+					permissionsGroup.name()+"_CONTRIBUTIONS_READ", "RESOURCES_READ"};
+				break;
+
+			case RESOURCES:
+				permissions = new String[]{"RESOURCES_READ"};
+				break;
+
+			default:
+				permissions = new String[]{permissionsGroup.name() + "_READ", permissionsGroup.name() + "_POLICYINFO_READ",
+					permissionsGroup.name() + "_PREMIUM_READ", permissionsGroup.name() + "_CLAIM_READ", "RESOURCES_READ"};
+				break;
+		}
+
+		return permissions;
+	}
 }
