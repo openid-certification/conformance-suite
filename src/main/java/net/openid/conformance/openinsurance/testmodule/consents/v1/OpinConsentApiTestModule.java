a33f033703de74187b841c9d145a35c4e5a765c8
Daniel Simoes <daniel.simoes@raidiam.com>
2022-08-25 17:59:55 -0300

    fix: Set permission builder to choose between personal/business

--- src/main/java/net/openid/conformance/openinsurance/testmodule/consents/v1/OpinConsentApiTestModule.java
+++ src/main/java/net/openid/conformance/openinsurance/testmodule/consents/v1/OpinConsentApiTestModule.java
@@ -1,5 +1,6 @@
 package net.openid.conformance.openinsurance.testmodule.consents.v1;
 
+import com.google.common.base.Strings;
 import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.client.CheckItemCountHasMin1;
 import net.openid.conformance.condition.client.FAPIBrazilAddExpirationToConsentRequest;
@@ -17,8 +18,20 @@ import net.openid.conformance.openinsurance.testmodule.support.OpinConsentPermis
 	testName = "opin-consent-api-test",
 	displayName = "Validate the structure of all consent API resources V1",
 	summary = "Validates the structure of all consent API resources V1\n" +
-		"\u2022 Creates a Consent with alSet l of the existing permissions.\n" +
-		"\u2022 Checks all of the fields sent on the consent API are specification compliant\n",
+		"\u2022 Confirm if we are already selecting it to send either business or personal permissions \n" +
+		"\u2022 If the Consent is for Business Account, call the POST consent APIs with permission CUSTOMERS_BUSINESS_IDENTIFICATIONS_READ, CUSTOMERS_BUSINESS_QUALIFICATION_READ, and CUSTOMERS_BUSINESS_ADITTIONALINFO_READ, plus all aditional permissions \n" +
+		"\u2022 If the Consent is for Personal Account, call the POST consent APIs with permission CUSTOMERS_PERSONAL_IDENTIFICATIONS_READ, CUSTOMERS_PERSONAL_QUALIFICATION_READ, and CUSTOMERS_PERSONAL_ADITTIONALINFO_READ, plus all aditional permissions  \n" +
+		"\u2022 Creates Consent with all of the Business/ Personal, plus all existing additional permissions \n" +
+		"\u2022 Checks all of the fields sent on the consent API are specification compliant\n" +
+		"\u2022 Calls the GET Consents with the Consent ID that has been created\n" +
+		"\u2022 Checks all of the fields sent on the consent API are specification compliant\n"+
+		"\u2022 Calls the DELETE Consents with the Consent ID created\n" +
+		"\u2022 Expects the test to return a 204 - No Content\n" +
+		"\u2022 Calls the GET Consents with the 1st Consent ID created\n" +
+		"\u2022 Confirms that the Consent has been sent to a Rejected state",
+
+
+
 	profile = OBBProfile.OBB_PROFILE_OPEN_INSURANCE_PHASE2,
 	configurationFields = {
 		"server.discoveryUrl",
@@ -42,6 +55,14 @@ public class OpinConsentApiTestModule extends AbstractClientCredentialsGrantFunc
 
 		permissionsBuilder = new OpinConsentPermissionsBuilder(env,getId(),eventLog,testInfo,executionManager);
 		permissionsBuilder.addPermissionsGroup(PermissionsGroup.ALL).build();
+		String productType = env.getString("config", "consent.productType");
+		if (!Strings.isNullOrEmpty(productType) && productType.equals("business")) {
+			permissionsBuilder.removePermissionsGroups(PermissionsGroup.CUSTOMERS_PERSONAL);
+		}
+		if (!Strings.isNullOrEmpty(productType) && productType.equals("personal")) {
+			permissionsBuilder.removePermissionsGroups(PermissionsGroup.CUSTOMERS_BUSINESS);
+		}
+
 
 		runInBlock("Validating create consent response", () -> {
 
