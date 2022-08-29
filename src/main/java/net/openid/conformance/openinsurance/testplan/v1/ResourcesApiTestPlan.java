6a837014fed3a86aa6ba2fe7836670e71a3b5cca
Alexandre Morais <alexandre.morais@raidiam.com>
2022-08-29 12:44:03 -0300

    (OICS-15) feat: opin-resources-api-test

---
+++ src/main/java/net/openid/conformance/openinsurance/testplan/v1/ResourcesApiTestPlan.java
@@ -0,0 +1,31 @@
+package net.openid.conformance.openinsurance.testplan.v1;
+
+import net.openid.conformance.openbanking_brasil.OBBProfile;
+import net.openid.conformance.openinsurance.testmodule.resources.v1.OpinResourcesApiTestModule;
+import net.openid.conformance.openinsurance.testplan.utils.PlanNames;
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+
+import java.util.List;
+
+@PublishTestPlan(
+	testPlanName = "Insurance consents api test",
+	profile = OBBProfile.OBB_PROFILE_OPEN_INSURANCE_PHASE2,
+	displayName = PlanNames.RESOURCES_API_TEST_PLAN,
+	summary = "Structural and logical tests for OpenInsurance Brasil-conformant consents API"
+)
+
+public class ResourcesApiTestPlan implements TestPlan {
+	public static List<ModuleListEntry> testModulesWithVariants() {
+		return List.of(
+			new ModuleListEntry(
+				List.of(
+					OpinResourcesApiTestModule.class
+				),
+				List.of(
+
+				)
+			)
+		);
+	}
+}
