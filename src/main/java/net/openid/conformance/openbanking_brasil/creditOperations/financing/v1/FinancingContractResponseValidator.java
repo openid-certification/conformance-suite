719c792aa0ae1c988bd6a10f172ab623dadc0344
DenisRaidiam <denis.podberiozkin@raidiam.com>
2022-08-30 12:18:09 +0100

    OCI-1132 ISS-367 Updated ippCode regex

--- src/main/java/net/openid/conformance/openbanking_brasil/creditOperations/financing/v1/FinancingContractResponseValidator.java
+++ src/main/java/net/openid/conformance/openbanking_brasil/creditOperations/financing/v1/FinancingContractResponseValidator.java
@@ -59,6 +59,7 @@ public class FinancingContractResponseValidator extends AbstractJsonAssertingCon
 		assertField(data,
 			new StringField
 				.Builder("ipocCode")
+				.setPattern("^[\\w\\W]{22,67}$")
 				.setMaxLength(67)
 				.build());
 
