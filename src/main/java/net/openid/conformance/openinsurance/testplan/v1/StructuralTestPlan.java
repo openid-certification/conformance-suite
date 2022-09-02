8084278122b0c74e455bb7e3cf943d31447e6693
Daniel Simoes <daniel.simoes@raidiam.com>
2022-09-02 07:37:55 -0300

    feat:Implement OpinCustomerPersonalStructuralTestModule

--- src/main/java/net/openid/conformance/openinsurance/testplan/v1/StructuralTestPlan.java
+++ src/main/java/net/openid/conformance/openinsurance/testplan/v1/StructuralTestPlan.java
@@ -1,6 +1,7 @@
 package net.openid.conformance.openinsurance.testplan.v1;
 
 import net.openid.conformance.openbanking_brasil.OBBProfile;
+import net.openid.conformance.openinsurance.testmodule.structural.v1.OpinCustomerPersonalStructuralTestModule;
 import net.openid.conformance.openinsurance.testplan.utils.PlanNames;
 import net.openid.conformance.plan.PublishTestPlan;
 import net.openid.conformance.plan.TestPlan;
@@ -19,7 +20,7 @@ public class StructuralTestPlan implements TestPlan{
 		return List.of(
 			new TestPlan.ModuleListEntry(
 				List.of(
-
+					OpinCustomerPersonalStructuralTestModule.class
 				),
 				List.of(
 
