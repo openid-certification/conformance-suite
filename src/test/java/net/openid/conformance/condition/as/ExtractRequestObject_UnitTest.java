5a9cc7c155e71632c054f9efff18e4f6887d7d01
Marcus Almgren <marcus@almgren.pt>
2022-09-03 13:07:54 +0100

    Fixing NPE when deriving key from algorithm
    This commit refactors and updates the alg parsing code to be up to date
    with the current code in https://github.com/panva/node-openid-client/blob/main/lib/client.js#L1301-L1307.
    
    Some effort is spent on tests in order to document what is supported and how.

---
+++ src/test/java/net/openid/conformance/condition/as/ExtractRequestObject_UnitTest.java
@@ -0,0 +1,48 @@
+package net.openid.conformance.condition.as;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.logging.TestInstanceEventLog;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.testmodule.OIDFJSON;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ExtractRequestObject_UnitTest {
+
+	String request = "eyJraWQiOiJkZXJpdmVkLWRlZmF1bHRQcm9wZXJ0aWVzQ2xpZW50U2VjcmV0IiwiY3R5IjoiSldUIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciJ9..4x-FeDrAHbVpXhV7.gW6Dmx0TACWDhOdFNKV_uU8XHVqBMMR3UVRdvG919ugjnt_6SH7XcBuBAMVKsTHcUQFwQiEny3WwPWfKLVNSHLEAMAFOvIToUXkMvVaAjqCXqN3fKH2KH2l7ak6Jj2sLE_K4dl4cWPkYKaMTH4gJupbOVGzB_91FTRCsfRlRJU_p68Gl0kaQfW4PYCdG0aGWOSxqStuGLPP8KpT659J3QexCj94AKzWCGMC42xKH4vQILjQGPHwdEgOpUxYafRigkQHNFhjQI2ozbQGydGunwr0lmfMh9tueRTRsVHYBGyCic862phHy1xKWAj8Gvn_rwGOoK3YIZui_PBcxKv8fhnjvKYZymvURJ4QIGFb1LIgtwTWC_iwtko40RY0TZGi6XtFItByjrdpofdtkCR8EDittJTtUtv8dbf2xjU_6xoYR0CGy-Uajpq3EOfQARo6X862uKFsMtAUQUfVEeC1knC6lBnk7v_UNqQ8UOYJvN7tmBlD-EZ_czAqvalP9cEWgS4nwu_t9Uz64YauPn9NBAgRkOI011WT31UJyD3TnKrnW_XLktYddkrngeEjca-Gn-qq1b0hq6qljWgS7Yv4GFpszuoh6WjQzkqB1un8OUyrzhri9o1cRB6zAPriSYsu_HTOedGkaPMUHtAOTTX_OEdaDVcCr0UYplgSpKP6_oHe0neHHONqPmqgUHhONF-vl4jz-dUNfTfxjEG3yt26JAc3MNjrzY0IuIC_iZ4H1j1Q1wgT0rwd7fz2Ht7Y5xk9pDDeRr_dHqDyLcjR6G_5fjUqcMmhQQwJbcyp84TYAal_ZR6von4I6MUI286X89xLLdvS1e55d-Tu8vJjmvtNteueDBAevhogb-jXvTd8uf9GOplcqDiqxHvpG95-9q2sbA2g2IA3-rS95mzDNcZZrvnk4QKFkAjFvGGr6FlFdcOOGsOZPTNy_zZYVn5yyyG-d4IOT32t65Uft5KlL388xhqh9oWjY5ZsyCk0EIATdcebZDFoD7RB_SwMrcyqYr8gk4HzS63kpiZnp_n7uTm2GvqCacJxudbar5pCIOQrM6oDMj-v728_NjLCoIG0u-5xgTTAItfSZQ8vPIc_VyLIMzofkVp5KmMetk63B7EZkf0f8-ZL1E4LCZ6nn5fhsZYrxX-8cD4jDCEVyrzJRWIuyjaqlFH7Dq1FMoxdcn1YDRuKbIP9RvsAZcGGx4_8FNGTzursfFMAhLCmHoAQoAQR_R_-jj2sDdRMiyuw9f59Zvd1Ob9stK9QMs4YH.jSR2ddjij2me04XHT_Obew";
+
+	public JsonObject getClient() {
+		String clientConfig = "{ \"client_secret\": \"UjWnZr4u7x!A%D*G-KaPdSgVkYp2s5v8\" }";
+		return JsonParser.parseString(clientConfig).getAsJsonObject();
+	}
+
+	@Test
+	public void succeeds_in_extracting_request_object_when_payload_is_a_direct_symmetric_A256GCM_encrypted_payload() {
+		JsonObject jsonObjectIrrelevantForTheTest = new JsonObject();
+
+		Environment env = mock(Environment.class);
+		when(env.getString("authorization_endpoint_http_request_params", "request")).thenReturn(request);
+		when(env.getObject("client")).thenReturn(getClient());
+		when(env.getObject("server_encryption_keys")).thenReturn(jsonObjectIrrelevantForTheTest);
+
+		ArgumentCaptor<JsonObject> extractedRequestObjectCaptor = ArgumentCaptor.forClass(JsonObject.class);
+		when(env.putObject(anyString(), extractedRequestObjectCaptor.capture())).thenReturn(jsonObjectIrrelevantForTheTest);
+
+		ExtractRequestObject condition = new ExtractRequestObject();
+		condition.setProperties("testId", mock(TestInstanceEventLog.class), Condition.ConditionResult.FAILURE);
+		condition.evaluate(env);
+
+		JsonObject extractedRequestedObject = extractedRequestObjectCaptor.getValue();
+
+		assertEquals("{\"kid\":\"derived-defaultPropertiesClientSecret\",\"cty\":\"JWT\",\"enc\":\"A256GCM\",\"alg\":\"dir\"}", extractedRequestedObject.get("jwe_header").toString());
+		assertEquals("mytestclient", OIDFJSON.getString(extractedRequestedObject.get("claims").getAsJsonObject().get("client_id")));
+
+	}
+}
---
+++ src/test/java/net/openid/conformance/util/JWEUTil_UnitTest.java
@@ -0,0 +1,76 @@
+package net.openid.conformance.util;
+
+import com.nimbusds.jose.util.Base64URL;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JWEUTil_UnitTest {
+
+	String clientSecret = "secure";
+
+	@Test
+	public void supported_cbc_enc_algorithms_from_rfc7518_section_5_1_produce_the_expected_derived_key() {
+		byte[] A128CBC_HS256_derivedKey = JWEUtil.deriveEncryptionKey("A128CBC-HS256", clientSecret);
+
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznNzaklH6lgmx4", Base64URL.encode(A128CBC_HS256_derivedKey).toString());
+	}
+
+	@Test
+	public void supported_gcm_enc_algorithms_from_rfc7518_section_5_1_produce_the_expected_derived_key() {
+		byte[] A128GCM_derivedKey = JWEUtil.deriveEncryptionKey("A128GCM", clientSecret);
+		byte[] A192GCM_derivedKey = JWEUtil.deriveEncryptionKey("A192GCM", clientSecret);
+		byte[] A256GCM_derivedKey = JWEUtil.deriveEncryptionKey("A256GCM", clientSecret);
+
+		assertEquals("apNLRRRON1iRHvop7Wj7LQ", Base64URL.encode(A128GCM_derivedKey).toString());
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznN", Base64URL.encode(A192GCM_derivedKey).toString());
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznNzaklH6lgmx4", Base64URL.encode(A256GCM_derivedKey).toString());
+	}
+
+	@Test
+	public void supported_key_wrap_algorithms_produce_the_expected_derived_key() {
+		byte[] A128KW_derivedKey = JWEUtil.deriveEncryptionKey("A128KW", clientSecret);
+		byte[] A128GCMKW_derivedKey = JWEUtil.deriveEncryptionKey("A128GCMKW", clientSecret);
+
+		byte[] A192KW_derivedKey = JWEUtil.deriveEncryptionKey("A192KW", clientSecret);
+		byte[] A192GCMKW_derivedKey = JWEUtil.deriveEncryptionKey("A192GCMKW", clientSecret);
+
+		byte[] A256KW_derivedKey = JWEUtil.deriveEncryptionKey("A256KW", clientSecret);
+		byte[] A256GCMKW_derivedKey = JWEUtil.deriveEncryptionKey("A256GCMKW", clientSecret);
+
+		assertEquals("apNLRRRON1iRHvop7Wj7LQ", Base64URL.encode(A128KW_derivedKey).toString());
+		assertEquals("apNLRRRON1iRHvop7Wj7LQ", Base64URL.encode(A128GCMKW_derivedKey).toString());
+
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznN", Base64URL.encode(A192KW_derivedKey).toString());
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznN", Base64URL.encode(A192GCMKW_derivedKey).toString());
+
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznNzaklH6lgmx4", Base64URL.encode(A256KW_derivedKey).toString());
+		assertEquals("apNLRRRON1iRHvop7Wj7LUIPp71WhznNzaklH6lgmx4", Base64URL.encode(A256GCMKW_derivedKey).toString());
+	}
+
+	@Test
+	public void unsupported_algorithms_from_rfc7518_section_5_1_includes_A192CBC_HS384() {
+		Exception expectedException = assertThrows(RuntimeException.class,
+			() -> JWEUtil.deriveEncryptionKey("A192CBC-HS384", clientSecret));
+
+		assertEquals("Unexpected algorithm:A192CBC-HS384", expectedException.getMessage());
+	}
+
+	@Test
+	public void unsupported_algorithms_from_rfc7518_section_5_1_includes_A256CBC_HS512() {
+		Exception expectedException = assertThrows(RuntimeException.class,
+			() -> JWEUtil.deriveEncryptionKey("A256CBC-HS512", clientSecret));
+
+		assertEquals("Unexpected algorithm:A256CBC-HS512", expectedException.getMessage());
+	}
+
+	@Test
+	public void throws_when_unable_to_parse_the_algorithm_for_the_key_length() {
+		Exception expectedException = assertThrows(RuntimeException.class,
+			() -> JWEUtil.deriveEncryptionKey("none", clientSecret));
+
+		assertEquals("Unable to parse key bit length from algorithm none", expectedException.getMessage());
+	}
+
+}
