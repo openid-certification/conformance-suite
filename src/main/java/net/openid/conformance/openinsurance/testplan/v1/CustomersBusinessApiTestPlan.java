0dcc9af6b306576a7674b51e8e037213eab13367
Daniel Simoes <daniel.simoes@raidiam.com>
2022-09-09 14:30:04 -0300

    Fix: possible future merge conflicts

--- src/main/java/net/openid/conformance/openinsurance/testplan/v1/CustomersBusinessApiTestPlan.java
+++ src/main/java/net/openid/conformance/openinsurance/testplan/v1/CustomersBusinessApiTestPlan.java
@@ -20,7 +20,7 @@ public class CustomersBusinessApiTestPlan implements TestPlan {
 		return List.of(
 			new ModuleListEntry(
 				List.of(
-					OpinCustomerBusinessDataApiTestModule.class
+
 				),
 				List.of(
 
