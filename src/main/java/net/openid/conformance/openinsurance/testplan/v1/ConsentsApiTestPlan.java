eca9f87859d2f7d350dca57705e4753f106193e3
Alexandre Morais <alexandre.morais@raidiam.com>
2022-09-09 10:10:22 -0300

    (OICS-10) refactor: remove from testplan

--- src/main/java/net/openid/conformance/openinsurance/testplan/v1/ConsentsApiTestPlan.java
+++ src/main/java/net/openid/conformance/openinsurance/testplan/v1/ConsentsApiTestPlan.java
@@ -1,8 +1,6 @@
 package net.openid.conformance.openinsurance.testplan.v1;
 
 import net.openid.conformance.openbanking_brasil.OBBProfile;
-import net.openid.conformance.openinsurance.testmodule.v1.consents.OpinConsentsApiPermissionGroupsTestModule;
-import net.openid.conformance.openinsurance.testmodule.v1.consents.OpinConsentsApiConsentStatusTestModule;
 import net.openid.conformance.openinsurance.testplan.utils.PlanNames;
 import net.openid.conformance.plan.PublishTestPlan;
 import net.openid.conformance.plan.TestPlan;
@@ -21,8 +19,6 @@ public class ConsentsApiTestPlan implements TestPlan {
 		return List.of(
 			new ModuleListEntry(
 				List.of(
-					OpinConsentsApiPermissionGroupsTestModule.class
-					OpinConsentsApiConsentStatusTestModule.class
 				),
 				List.of(
 
