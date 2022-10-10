979191cddb95131b6ea27626ad0f767237ec6ce6
George McIntosh <george@georgemcintosh.com>
2022-10-10 12:28:52 +0100

    Test Plan Names have to be unique.
    This adds a unit test to scan the classpath for test plans and ensures they are all uniquely named

---
+++ src/test/java/net/openid/conformance/CorrectnessTests.java
@@ -0,0 +1,50 @@
+package net.openid.conformance;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class CorrectnessTests {
+
+	@Test
+	public void ensureTestPlanNamesAreUnique() {
+
+		Stream<Class<?>> classStream = findTestPlanClasses();
+		List<Class<?>> collect = classStream.collect(Collectors.toList());
+		Set<String> found = new HashSet<>();
+		for(Class<?> clazz: collect) {
+			PublishTestPlan publishTestPlan = clazz.getDeclaredAnnotation(PublishTestPlan.class);
+			String planName = publishTestPlan.testPlanName();
+			if(found.contains(planName)) {
+				fail(String.format("The test plan %s is not unique - this is not allowed", planName));
+			}
+			found.add(planName);
+		}
+
+	}
+
+	private static Stream<Class<?>> findTestPlanClasses() {
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(PublishTestPlan.class));
+		Stream.Builder<Class<?>> builder = Stream.builder();
+		try {
+			for (BeanDefinition bd : scanner.findCandidateComponents("net.openid")) {
+				builder.accept(Class.forName(bd.getBeanClassName()));
+			}
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Error loading class", e);
+		}
+		return builder.build();
+	}
+
+}
