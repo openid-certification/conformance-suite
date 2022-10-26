openid-certification/conformance-suite

commit 41ef64b20a51f2f19b8801f388f39cbd0d6eecb6

Small improvements to AbstractSignJWT
Use the signer factory in the new version of nimbus jose,
use a JWSHeader builder instead of the massive initialiser.

src/main/java/net/openid/conformance/condition/client/AbstractSignJWT.java
@@ -1,7 +1,6 @@
 package net.openid.conformance.condition.client;
 
 import com.google.gson.JsonObject;
-import com.nimbusds.jose.Algorithm;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
@@ -11,6 +10,7 @@ import com.nimbusds.jose.crypto.ECDSASigner;
 import com.nimbusds.jose.crypto.Ed25519Signer;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.factories.DefaultJWSSignerFactory;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -19,10 +19,10 @@ import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.OctetKeyPair;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.produce.JWSSignerFactory;
 import com.nimbusds.jose.util.Base64URL;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 import net.openid.conformance.testmodule.OIDFJSON;
 import net.openid.conformance.util.JWKUtil;
@@ -53,30 +53,20 @@ public abstract class AbstractSignJWT extends AbstractGetSigningKey {
 		try {
 			JWTClaimsSet claimSet = JWTClaimsSet.parse(claims.toString());
 			JWK signingJwk = getSigningKey("signing", jwks);
-
-			JWSSigner signer = null;
-			// JWSSignerFactory
-			if (signingJwk.getKeyType().equals(KeyType.RSA)) {
-				signer = new RSASSASigner((RSAKey) signingJwk);
-			} else if (signingJwk.getKeyType().equals(KeyType.EC)) {
-				signer = new ECDSASigner((ECKey) signingJwk);
-			} else if (signingJwk.getKeyType().equals(KeyType.OCT)) {
-				signer = new MACSigner((OctetSequenceKey) signingJwk);
-			}
-
-			if (signer == null) {
-				throw error("Couldn't create signer from key; kty must be one of 'oct', 'rsa', 'ec'", args("jwk", signingJwk.toJSONString()));
-			}
-
-			Algorithm alg = signingJwk.getAlgorithm();
+			JWSAlgorithm alg = JWSAlgorithm.parse(signingJwk.getAlgorithm().getName());
 			if (alg == null) {
-				throw error("No 'alg' field specified in key; please add 'alg' field in the configuration", args("jwk", signingJwk.toJSONString()));
+				throw error("No 'alg' field specified in key; please add 'alg' field in the configuration", args("jwk", signingJwk));
 			}
 
-			JWSHeader header = new JWSHeader(JWSAlgorithm.parse(alg.getName()),
-				includeTyp ? JOSEObjectType.JWT : null,
-				null, null, null, null, null, null, null, null,
-				signingJwk.getKeyID(), true,null, null);
+			JWSSignerFactory jwsSignerFactory = new DefaultJWSSignerFactory();
+			JWSSigner signer = jwsSignerFactory.createJWSSigner(signingJwk, alg);
+
+			JWSHeader.Builder builder = new JWSHeader.Builder(alg);
+			if (includeTyp) {
+				builder.type(JOSEObjectType.JWT);
+			}
+			builder.keyID(signingJwk.getKeyID());
+			JWSHeader header = builder.build();
 
 			String jws = performSigning(header, claims, signer);
 

commit 0fec74043ff65d64acdfd017f772344ca5fd100e

Avoid null exception when alg missing from signing key
I think this use to work fine; I suspect it was a regression I caused
in commit 41ef64b2.

Also add a unit test for this case...

relates to #1112

src/main/java/net/openid/conformance/condition/client/AbstractSignJWT.java
@@ -1,6 +1,7 @@
 package net.openid.conformance.condition.client;
 
 import com.google.gson.JsonObject;
+import com.nimbusds.jose.Algorithm;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
@@ -53,10 +54,11 @@ public abstract class AbstractSignJWT extends AbstractGetSigningKey {
 		try {
 			JWTClaimsSet claimSet = JWTClaimsSet.parse(claims.toString());
 			JWK signingJwk = getSigningKey("signing", jwks);
-			JWSAlgorithm alg = JWSAlgorithm.parse(signingJwk.getAlgorithm().getName());
-			if (alg == null) {
+			Algorithm algorithm = signingJwk.getAlgorithm();
+			if (algorithm == null) {
 				throw error("No 'alg' field specified in key; please add 'alg' field in the configuration", args("jwk", signingJwk));
 			}
+			JWSAlgorithm alg = JWSAlgorithm.parse(algorithm.getName());
 
 			JWSSignerFactory jwsSignerFactory = MultiJWSSignerFactory.getInstance();
 			JWSSigner signer = jwsSignerFactory.createJWSSigner(signingJwk, alg);

src/test/java/net/openid/conformance/condition/client/SignClientAuthenticationAssertion_UnitTest.java
@@ -142,8 +142,29 @@ public class SignClientAuthenticationAssertion_UnitTest {
 
 	}
 
-	/**
-	 */
+	@Test(expected = ConditionError.class)
+	public void testEvaluate_noAlg() {
+
+		env.putObject("client_assertion_claims", claims);
+
+		env.putObjectFromJsonString("client_jwks", "{" +
+			"  \"keys\": [" +
+			"    {" +
+			"      \"kty\": \"RSA\"," +
+			"      \"d\": \"ePRRljk3VGde-JOxwH5LMAY4dERXTLCs7x1NcbuVXF7dyggrbQpDMhCrdH2UJQyvsb4c7H6w9LvosC7UnDgcTzrdd-rccz02XtiTCjP4mXqDUv1qZkYoy-HUq0UH7-WdRJNnx9sZTmWox-yucUbtfdmVRVuI355vNfG3ldsl6kC3DRNKH8nLay0eiS1x62p5S00Fz7TY_hzRqgexsjKu_UNQYDpQcOBVR9qi_mp3Yp8MdsJ2DoBrFJnDTTrG9uNlos1n1kBw-MVQQIPyHKIcElS5b1D-uc53MyxmWUAhiSnf0gpYgk3bYHccAmi_vsRqajLTt0w6hbNozhFht94mq3aLjGQJjt78zSu42hrUqTR-FLHwSoIGilS9SbdYypyYku0ocinwVb51IraDIwJY0_b42aHxE5an9HNaephGvPYPq0jA0xXehLSHqtS9D49OlxcnAvRkyn3TnPbxrdPAaZ6sJEHC8jpdYgKZ9LM7QRAFxwIk3Bxrf4rcEoC_mk5f5in4JYlYmx7VfXSzmSTXLfAWbqpLYRgtaCTFZ-HBLU1VJiAr2nkzw6cewwpxUCrowADO_w0GYRIU3BWamY2i--9wbI2nRUlxaROSMYJx5-A4sWDNPKJWQpX1SUyBpgTO82tHicZ2wzVEqMA4-np_leFBXFvAaMSuIxJ-aQlkLyE\","
+			+
+			"      \"e\": \"AQAB\"," +
+			"      \"use\": \"sig\"," +
+			"      \"kid\": \"fapi-test-suite\"," +
+			"      \"n\": \"qVGMeRQ8KG9ZVQFXa2YxCiapfesToXILzIvmHqNOJ8EbVwZKcwZ1iHpAzaZhnrSHJBU7Fgz9PEeTAqY-2FrRLE2xz3pW_LCg3y4fb1Sfi5GBSl1es0e4UD4Ie0g5SfQeZIpHfGM9zZx8rmsiDLRUyBsB8kHyUG6Ul5pHpAUHhrtghCpzL5lvnVvCM3-apeSyOdquS09tjwGUPUWifrKr0X1xEoNm7rsChZXQbB-MwqJG-ZGw51udn5-3mMunqp-B37jXdo4tYYOg0kKrpZSqQdRPANmTbge3LIN7uYprFVmGDMvXwiLMF9Unkeudox6nuIy3MWSeFRWpcrSQ_7s5p4mSupZxWHWrN_qSM-H9mTosvPg32m7XjQvlEJhuXqn13VYd8ZhmYSqV7s_x8bveZJLBnNPAkjPDktz8eUsdX3S8X6MNoYh4Ch7rsE_FO21B3FcMwtFql11z3_3PCAQOB3lzggy1hs-4Zb1As84R1Fmy0WKHTPAKPlJtLNFKz_bVf7xRbrt8BlPbkF4HduQixi-twkWRkUz8_-zCzWhz6s9y5ltQgs0_rl9aYSAt5z8UarP1PNfQIYGBmhwqGHi2LmrYFRjBEHRIsDXJcUGWgkfKMsiECs-mT-6AIw2L5DZIMIo8aroEnVpS24Ln2fwpdrRjO-b65VW-5cFOM6gERCc\""
+			+
+			"    }" +
+			"  ]" +
+			"}");
+
+		cond.execute(env);
+
+	}
 	@Test(expected = ConditionError.class)
 	public void testEvaluate_noClaims() {
 
@@ -153,8 +174,6 @@ public class SignClientAuthenticationAssertion_UnitTest {
 
 	}
 
-	/**
-	 */
 	@Test(expected = ConditionError.class)
 	public void testEvaluate_noJwks() {
 