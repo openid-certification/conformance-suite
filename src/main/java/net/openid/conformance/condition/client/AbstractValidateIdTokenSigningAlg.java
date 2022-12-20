2449bdf7b3eca5c0732cfaa6b2da531d6c2b49ff
Joseph Heenan <joseph.heenan@fintechlabs.io>
2022-12-20 16:25:36 +0000

    Replace some uses of spaces for indentation with tabs
    Done using IntelliJ's convert indents to tabs feature.
    
    They were causing checkstyle to complain in various ways.

--- src/main/java/net/openid/conformance/condition/client/AbstractValidateIdTokenSigningAlg.java
+++ src/main/java/net/openid/conformance/condition/client/AbstractValidateIdTokenSigningAlg.java
@@ -13,29 +13,29 @@ public abstract class AbstractValidateIdTokenSigningAlg extends AbstractConditio
 	abstract Set<String> getPermitted();
 
 	@Override
-    @PreEnvironment(required = "id_token")
-    public Environment evaluate(Environment env) {
+	@PreEnvironment(required = "id_token")
+	public Environment evaluate(Environment env) {
 		final Set<String> permitted = getPermitted();
 
-        String idToken = env.getString("id_token", "value");
+		String idToken = env.getString("id_token", "value");
 
-        try {
-            // translate stored items into nimbus objects
-            SignedJWT jwt = SignedJWT.parse(idToken);
-            String alg = jwt.getHeader().getAlgorithm().getName();
+		try {
+			// translate stored items into nimbus objects
+			SignedJWT jwt = SignedJWT.parse(idToken);
+			String alg = jwt.getHeader().getAlgorithm().getName();
 
-            if (!permitted.contains(alg)) {
-                throw error("id_token must be signed with a permitted alg",
+			if (!permitted.contains(alg)) {
+				throw error("id_token must be signed with a permitted alg",
 					args("alg", alg, "permitted", permitted));
-            }
+			}
 
-            logSuccess("id_token was signed with a permitted algorithm",
+			logSuccess("id_token was signed with a permitted algorithm",
 				args("alg", alg, "permitted", permitted));
-            return env;
+			return env;
 
-        } catch (ParseException e) {
-            throw error("Error parsing ID Token", e);
-        }
+		} catch (ParseException e) {
+			throw error("Error parsing ID Token", e);
+		}
 
-    }
+	}
 }
--- src/main/java/net/openid/conformance/condition/common/AbstractFAPI2CheckKeyAlgInClientJWKs.java
+++ src/main/java/net/openid/conformance/condition/common/AbstractFAPI2CheckKeyAlgInClientJWKs.java
@@ -14,55 +14,55 @@ public abstract class AbstractFAPI2CheckKeyAlgInClientJWKs extends AbstractCondi
 
 	abstract Set<String> getPermitted();
 
-		@Override
-    @PreEnvironment(required = "client_jwks")
-    public Environment evaluate(Environment env) {
-        final Set<String> permitted = getPermitted();
-        JsonElement keys = env.getElementFromObject("client_jwks", "keys");
-        if (keys == null || !keys.isJsonArray()) {
-            throw error("keys array not found in client JWKs");
-        }
+	@Override
+	@PreEnvironment(required = "client_jwks")
+	public Environment evaluate(Environment env) {
+		final Set<String> permitted = getPermitted();
+		JsonElement keys = env.getElementFromObject("client_jwks", "keys");
+		if (keys == null || !keys.isJsonArray()) {
+			throw error("keys array not found in client JWKs");
+		}
 
-        for (JsonElement key : keys.getAsJsonArray()) {
-            if (!key.isJsonObject()) {
-                throw error("invalid key in client JWKs", args("key", key));
-            }
-            JsonObject keyObj = key.getAsJsonObject();
+		for (JsonElement key : keys.getAsJsonArray()) {
+			if (!key.isJsonObject()) {
+				throw error("invalid key in client JWKs", args("key", key));
+			}
+			JsonObject keyObj = key.getAsJsonObject();
 
-            if (!keyObj.has("alg")) {
-                throw error("'alg' not found in client JWKS provided in the test configuration - this is "+
-                        "required to set the request object signing algorithm the conformance suite will use, and "+
-                        "should be set to a permitted alg",
-                    args("key", key, "permitted", permitted));
-            }
+			if (!keyObj.has("alg")) {
+				throw error("'alg' not found in client JWKS provided in the test configuration - this is "+
+						"required to set the request object signing algorithm the conformance suite will use, and "+
+						"should be set to a permitted alg",
+					args("key", key, "permitted", permitted));
+			}
 
-            String use = keyObj.has("use") ? OIDFJSON.getString(keyObj.getAsJsonPrimitive("use")) : null;
-            if (use == null || use.equals("sig")) {
-                String alg = OIDFJSON.getString(keyObj.getAsJsonPrimitive("alg"));
-                if (!permitted.contains(alg)) {
-                    throw error("client jwks contains a signing key with a non-permitted alg",
-                        args("key", keyObj,
-                            "permitted", permitted));
-                }
+			String use = keyObj.has("use") ? OIDFJSON.getString(keyObj.getAsJsonPrimitive("use")) : null;
+			if (use == null || use.equals("sig")) {
+				String alg = OIDFJSON.getString(keyObj.getAsJsonPrimitive("alg"));
+				if (!permitted.contains(alg)) {
+					throw error("client jwks contains a signing key with a non-permitted alg",
+						args("key", keyObj,
+							"permitted", permitted));
+				}
 
-                if("EdDSA".equals(alg)){
-                	if(!keyObj.has("crv")) {
+				if("EdDSA".equals(alg)){
+					if(!keyObj.has("crv")) {
 						throw error("client jwks contains EdDSA alg with a missing crv parameter",
 							args("key", keyObj));
 					}
-                	String curve = OIDFJSON.getString(keyObj.getAsJsonPrimitive("crv"));
-                	if(!Curve.Ed25519.getName().equals(curve)) {
+					String curve = OIDFJSON.getString(keyObj.getAsJsonPrimitive("crv"));
+					if(!Curve.Ed25519.getName().equals(curve)) {
 						throw error("client jwks contains EdDSA alg with an unsupported curve",
 							args("key", keyObj));
 					}
 				}
-            }
+			}
 
-        }
+		}
 
-        logSuccess("Keys in client JWKS all have permitted 'alg'",
-            args("permitted", permitted));
+		logSuccess("Keys in client JWKS all have permitted 'alg'",
+			args("permitted", permitted));
 
-        return env;
-    }
+		return env;
+	}
 }
