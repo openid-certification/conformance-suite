7301b52a6515f839164dac56a44f92f7e52ac9eb
Alan Smillie <alans@emobix.co.uk>
2023-02-17 16:00:51 +0000

    Check ID Token For Non-Requested Claims
    Issue 1144: "Ensure identity claims aren't returned when not requested"
    required checking the 'id_token' for claims that were not requested.
    
    This commit adds such a test to FAPI2SPID2HappyFlow.

---
+++ src/main/java/net/openid/conformance/condition/client/EnsureIdTokenDoesNotContainNonRequestedClaims.java
@@ -0,0 +1,53 @@
+package net.openid.conformance.condition.client;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+import java.util.List;
+
+public class EnsureIdTokenDoesNotContainNonRequestedClaims extends AbstractCondition {
+	public static List<String> idTokenValidClaims = List.of(
+		// as per https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+		"iss",
+		"sub",
+		"aud",
+		"exp",
+		"iat",
+		"auth_time",
+		"nonce",
+		"acr",
+		"amr",
+		"azp",
+		// as per https://openid.net/specs/openid-connect-core-1_0.html#CodeIDToken
+		"at_hash"
+	);
+
+	@Override
+	@PreEnvironment(required = "id_token")
+	public Environment evaluate(Environment env) {
+
+		JsonObject idTokenClaims = env.getElementFromObject("id_token", "claims").getAsJsonObject().deepCopy();
+
+		boolean failure = false;
+
+		for (String key : idTokenClaims.keySet()) {
+			if (idTokenValidClaims.contains(key)) {
+			    continue;
+			}
+
+			failure = true;
+
+			logFailure("id_token contains non-requested claim '" + key + "'");
+		}
+
+		if (failure) {
+			throw error("id_token contains non-requested claims");
+		}
+
+		logSuccess("no non-requested id_token claims found");
+
+		return env;
+	}
+}
