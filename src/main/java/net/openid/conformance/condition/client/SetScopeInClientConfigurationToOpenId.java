66a0b20e6637ea8e3c7319f1b97e7f05c22714e0
Joseph Heenan <joseph.heenan@fintechlabs.io>
2023-02-26 10:28:46 +0000

    FAPI2 ConnectID: Allow overriding of scope so we can add tests to CI
    The ConnectID tests use only the 'openid' scope, but expect specific
    fapi2sp/ms behaviours. This makes it hard for Authlete to configure the
    server they provide for us to use in our CI to pass these tests,
    as the interactions for some of the negative tests look
    exactly like those expected to pass in the OpenID Connect Core
    tests.
    
    To solve this, we add a way to override the scope used in the idmvp
    tests so that our test config for this test can specify an extra scope
    that triggers the necessary fapi2 behaviours.

--- src/main/java/net/openid/conformance/condition/client/SetScopeInClientConfigurationToOpenId.java
+++ src/main/java/net/openid/conformance/condition/client/SetScopeInClientConfigurationToOpenId.java
@@ -8,7 +8,18 @@ public class SetScopeInClientConfigurationToOpenId extends AbstractSetScopeInCli
 	@Override
 	@PreEnvironment(required = "client")
 	public Environment evaluate(Environment env) {
-		return setScopeInClientConfiguration(env, "openid");
+		String scope = "openid";
+
+		// This is necessary to allow the Australian ConnectID tests to run in our CI against Authlete;
+		// in particular as the same Authlete server is supports multiple use cases just requesting
+		// the 'openid' scope doesn't trigger FAPI2 behaviour; this override allows us to request
+		// an additional scope that does enable FAPI2 behaviour
+		String overrideScope = env.getString("client", "override_openid_scope");
+		if (overrideScope != null) {
+			scope = overrideScope;
+		}
+
+		return setScopeInClientConfiguration(env, scope);
 	}
 
 }
