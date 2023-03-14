6a058c875077f1e13978177070b4cba900b8424c
Alan Smillie <alans@emobix.co.uk>
2023-03-14 16:11:53 +0000

    FAPI2 OP: Validate aud values that are accepted for private_key_jwt
    FAPI2 requires that the authorization server: issuer identifier, token
    endpoint URL or pushed authorization request endpoint URL be accepted
    as the PAR JWT client assertion intended audience.
    
    The FAPI2MessageSigningID1TestPlan PAR tests use the authorization server
    issuer identifier  by default. The pushed authorization request endpoint is
    tested by FAPI2SPID2PARCheckAudienceForJWTClientAssertion.
    
    This commit adds a new test, FAPI2SPID2PARCheckAudienceForJWTClientAssertion1,
    to test the token endpoint.

---
+++ src/main/java/net/openid/conformance/condition/client/AddTokenEndpointAsAudToClientAuthenticationAssertionClaims.java
@@ -0,0 +1,36 @@
+package net.openid.conformance.condition.client;
+
+import com.google.common.base.Strings;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+
+//Sets the Token Endpoint URL as the Audience claim for Client Authentication Assertion.
+public class AddTokenEndpointAsAudToClientAuthenticationAssertionClaims extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = "client_assertion_claims")
+	@PostEnvironment(required = "client_assertion_claims")
+	public Environment evaluate(Environment env) {
+		JsonObject claims = env.getObject("client_assertion_claims").getAsJsonObject();
+
+		addAudience(claims, env);
+
+		logSuccess("add audience in client assertion claims", claims);
+
+		env.putObject("client_assertion_claims", claims);
+
+		return env;
+	}
+
+	private void addAudience(JsonObject claims, Environment env) {
+		String audience = env.getString("server", "token_endpoint");
+		if (Strings.isNullOrEmpty(audience)) {
+			throw error("Couldn't find required configuration element", args( "audience", audience));
+		}
+		claims.addProperty("aud", audience);
+	}
+}
