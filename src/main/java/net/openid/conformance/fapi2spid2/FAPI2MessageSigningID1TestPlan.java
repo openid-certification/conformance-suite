853b966df978eca6fbe678fb733476bfab6d94ff
Joseph Heenan <joseph.heenan@fintechlabs.io>
2023-03-18 10:04:31 +0000

    Remove BETA tags from FAPI2 test plans in preparation for launch

--- src/main/java/net/openid/conformance/fapi2spid2/FAPI2MessageSigningID1TestPlan.java
+++ src/main/java/net/openid/conformance/fapi2spid2/FAPI2MessageSigningID1TestPlan.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @PublishTestPlan (
 	testPlanName = "fapi2-message-signing-id1-test-plan",
-	displayName = "FAPI2-Message-Signing-ID1: Authorization server test - BETA; subject to change, no certification programme yet",
+	displayName = "FAPI2-Message-Signing-ID1: Authorization server test",
 	profile = TestPlan.ProfileNames.optest
 )
 public class FAPI2MessageSigningID1TestPlan implements TestPlan {
