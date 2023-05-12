f92712048c7a729cb25ddd2b2f2bdcf449447221
Alan Smillie <alans@emobix.co.uk>
2023-05-12 09:12:01 +0100

    Fix False Warnings When Validating ID Token Claims
    The EnsureIdTokenDoesNotContainNonRequestedClaims condition checks
    the claims received in the ID token against a fixed list of valid claims.
    
    This commit modifies this logic to allow for the fact that OpenID Connect
    allows claims to be specified via scopes in the authorization request.
    
    The condition now, in the OpenID Connect case, converts scopes in the
    authorization request into a set of claims and adds them to the valid
    claims list thus allowing them to be contained in the Id Token.

--- src/main/java/net/openid/conformance/condition/client/EnsureIdTokenDoesNotContainNonRequestedClaims.java
+++ src/main/java/net/openid/conformance/condition/client/EnsureIdTokenDoesNotContainNonRequestedClaims.java
@@ -1,14 +1,16 @@
 package net.openid.conformance.condition.client;
 
+import com.google.common.base.Strings;
 import com.google.gson.JsonObject;
-import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class EnsureIdTokenDoesNotContainNonRequestedClaims extends AbstractCondition {
-	public static List<String> idTokenValidClaims = List.of(
+public class EnsureIdTokenDoesNotContainNonRequestedClaims extends AbstractVerifyScopesReturnedInClaims {
+	public static List<String> idTokenValidClaims = new ArrayList<>(Arrays.asList(
 		// as per https://openid.net/specs/openid-connect-core-1_0.html#IDToken
 		"iss",
 		"sub",
@@ -24,11 +26,38 @@ public class EnsureIdTokenDoesNotContainNonRequestedClaims extends AbstractCondi
 		"at_hash",
 		// as per https://openid.net/specs/openid-financial-api-part-2-1_0.html#id-token-as-detached-signature
 		"s_hash"
-	);
+	));
 
 	@Override
-	@PreEnvironment(required = "id_token")
+	@PreEnvironment(required = {"id_token", "authorization_endpoint_request"})
 	public Environment evaluate(Environment env) {
+
+		// For OpenID Connect, scopes can be used to request that specific sets of information be made available
+		// as claim values.
+		//
+		// Here we map the scopes included in the authorization request to a set of claims and add them to the
+		// valid claims list.
+		String scopeStr = env.getString("authorization_endpoint_request", "scope");
+
+		if (! Strings.isNullOrEmpty(scopeStr)) {
+			List<String> scopeList = Arrays.asList(scopeStr.split(" "));
+
+			if (scopeList.contains("openid")) {
+				for (String scope: scopeList) {
+					List<String> claimsList = SCOPE_STANDARD_CLAIMS.get(scope);
+
+					if (claimsList == null) {
+						continue;
+					}
+
+					for (String claim: claimsList) {
+						if (! idTokenValidClaims.contains(claim)) {
+							idTokenValidClaims.add(claim);
+						}
+					}
+				}
+			}
+		}
 
 		JsonObject idTokenClaims = env.getElementFromObject("id_token", "claims").getAsJsonObject().deepCopy();
 
