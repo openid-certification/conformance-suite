984e0e80e47722d602362653779213764cb01fbe
Edmund Jay <ejay@mgi1.com>
2023-06-02 19:27:13 -0700

    Disable checking for KID in encrypted ID Tokens and Request Objects using symmetric algs
    Related to #1157 - Kid in JWE using Key Wrapping mode is unknown

--- src/main/java/net/openid/conformance/condition/as/ValidateEncryptedRequestObjectHasKid.java
+++ src/main/java/net/openid/conformance/condition/as/ValidateEncryptedRequestObjectHasKid.java
@@ -4,17 +4,23 @@ import com.google.common.base.Strings;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.util.JWEUtil;
 
 public class ValidateEncryptedRequestObjectHasKid extends AbstractCondition {
 
 	@Override
 	@PreEnvironment(required = "authorization_request_object")
 	public Environment evaluate(Environment env) {
-		String kid = env.getString("authorization_request_object", "jwe_header.kid");
-		if (Strings.isNullOrEmpty(kid)) {
-			throw error("kid was not found in the encrypted request object header");
+		String alg = env.getString("authorization_request_object", "jwe_header.alg");
+		if(JWEUtil.isSymmetricJWEAlgorithm(alg)) {
+			logSuccess("skipping KID check for symmetric alg " + alg);
+		} else {
+			String kid = env.getString("authorization_request_object", "jwe_header.kid");
+			if (Strings.isNullOrEmpty(kid)) {
+				throw error("kid was not found in the encrypted request object header");
+			}
+			logSuccess("kid was found in the encrypted request object header", args("kid", kid));
 		}
-		logSuccess("kid was found in the encrypted request object header", args("kid", kid));
 		return env;
 	}
 
