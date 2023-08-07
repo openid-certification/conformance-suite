8e38da94cdf2ce973bcd2f2ed2df09bff10bc1fa
Alan Smillie <alans@emobix.co.uk>
2023-08-07 11:29:26 +0100

    RP Tests: Extend Request Object Validation To FAPI1

--- src/main/java/net/openid/conformance/fapi1advancedfinal/AbstractFAPI1AdvancedFinalClientTest.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/AbstractFAPI1AdvancedFinalClientTest.java
@@ -21,6 +21,11 @@ import net.openid.conformance.condition.as.CheckClientIdMatchesOnTokenRequestIfP
 import net.openid.conformance.condition.as.CheckForClientCertificate;
 import net.openid.conformance.condition.as.CheckForFAPIInteractionIdInResourceRequest;
 import net.openid.conformance.condition.as.CheckPkceCodeVerifier;
+import net.openid.conformance.condition.as.CheckForUnexpectedClaimsInClaimsParameter;
+import net.openid.conformance.condition.as.CheckForUnexpectedClaimsInRequestObject;
+import net.openid.conformance.condition.as.CheckForUnexpectedOpenIdClaims;
+import net.openid.conformance.condition.as.CheckRequestObjectClaimsParameterMemberValues;
+import net.openid.conformance.condition.as.CheckRequestObjectClaimsParameterValues;
 import net.openid.conformance.condition.as.CopyAccessTokenToClientCredentialsField;
 import net.openid.conformance.condition.as.CreateAuthorizationCode;
 import net.openid.conformance.condition.as.CreateAuthorizationEndpointResponseParams;
@@ -1042,6 +1047,20 @@ public abstract class AbstractFAPI1AdvancedFinalClientTest extends AbstractTestM
 		}
 
 		skipIfElementMissing("authorization_request_object", "jwe_header", ConditionResult.INFO, ValidateEncryptedRequestObjectHasKid.class, ConditionResult.FAILURE, "OIDCC-10.2", "OIDCC-10.2.1");
+
+		skipIfElementMissing("authorization_request_object", "claims", ConditionResult.INFO,
+			CheckForUnexpectedClaimsInRequestObject.class, ConditionResult.WARNING, "RFC6749-4.1.1", "OIDCC-3.1.2.1", "RFC7636-4.3", "OAuth2-RT-2.1", "RFC7519-4.1", "DPOP-10", "RFC8485-4.1", "RFC8707-2.1", "RFC9396-2");
+
+		if (fapiClientType == FAPIClientType.OIDC) {
+			skipIfElementMissing("authorization_request_object", "claims.claims", ConditionResult.INFO,
+				CheckForUnexpectedClaimsInClaimsParameter.class, ConditionResult.WARNING, "OIDCC-5.5");
+			skipIfElementMissing("authorization_request_object", "claims.claims", ConditionResult.INFO,
+				CheckForUnexpectedOpenIdClaims.class, ConditionResult.WARNING, "OIDCC-5.1");
+			skipIfElementMissing("authorization_request_object", "claims.claims", ConditionResult.INFO,
+				CheckRequestObjectClaimsParameterValues.class, ConditionResult.FAILURE, "OIDCC-5.5");
+			skipIfElementMissing("authorization_request_object", "claims.claims", ConditionResult.INFO,
+				CheckRequestObjectClaimsParameterMemberValues.class, ConditionResult.FAILURE, "OIDCC-5.5.1");
+		}
 
 		//CreateEffectiveAuthorizationRequestParameters call must be before endTestIfRequiredParametersAreMissing
 		callAndStopOnFailure(CreateEffectiveAuthorizationRequestParameters.class);
