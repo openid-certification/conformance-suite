75fa02f0a70c1c3e25cd22045933ade6c750a6fe
Edmund Jay <ejay@mgi1.com>
2023-11-27 02:44:19 -0800

    Validate Token endpoint DPoP proof nonce

--- src/main/java/net/openid/conformance/sequence/as/PerformDpopProofTokenRequestChecks.java
+++ src/main/java/net/openid/conformance/sequence/as/PerformDpopProofTokenRequestChecks.java
@@ -3,6 +3,7 @@ package net.openid.conformance.sequence.as;
 import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.as.ValidateDpopProofSignature;
 import net.openid.conformance.condition.as.ValidateDpopProofTokenRequest;
+import net.openid.conformance.condition.as.ValidateTokenEndpointDpopProofNonce;
 import net.openid.conformance.sequence.AbstractConditionSequence;
 
 public class PerformDpopProofTokenRequestChecks extends AbstractConditionSequence {
@@ -10,6 +11,7 @@ public class PerformDpopProofTokenRequestChecks extends AbstractConditionSequenc
 	@Override
 	public void evaluate() {
 		callAndContinueOnFailure(ValidateDpopProofTokenRequest.class, Condition.ConditionResult.FAILURE, "DPOP-4.3");
+		callAndContinueOnFailure(ValidateTokenEndpointDpopProofNonce.class, Condition.ConditionResult.WARNING);
 		callAndContinueOnFailure(ValidateDpopProofSignature.class, Condition.ConditionResult.FAILURE, "DPOP-4.3-6");
 	}
 }
