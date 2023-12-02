5fa65e077c56db9f1d4134a61872fdc1ecf9267d
Edmund Jay <ejay@mgi1.com>
2023-12-01 17:44:38 -0800

    Check whether the authorization code in the token endpoint request is bound to the a DPoP proof

--- src/main/java/net/openid/conformance/sequence/as/PerformDpopProofTokenRequestChecks.java
+++ src/main/java/net/openid/conformance/sequence/as/PerformDpopProofTokenRequestChecks.java
@@ -1,6 +1,7 @@
 package net.openid.conformance.sequence.as;
 
 import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.as.ValidateAuthorizationCodeDpopBindingKey;
 import net.openid.conformance.condition.as.ValidateDpopProofSignature;
 import net.openid.conformance.condition.as.ValidateDpopProofTokenRequest;
 import net.openid.conformance.condition.as.ValidateTokenEndpointDpopProofNonce;
@@ -15,5 +16,6 @@ public class PerformDpopProofTokenRequestChecks extends AbstractConditionSequenc
 		callAndContinueOnFailure(EnsureDpopProofJtiNotUsed.class, Condition.ConditionResult.FAILURE, "DPOP-4.2", "DPOP-11");
 		callAndContinueOnFailure(ValidateTokenEndpointDpopProofNonce.class, Condition.ConditionResult.WARNING);
 		callAndContinueOnFailure(ValidateDpopProofSignature.class, Condition.ConditionResult.FAILURE, "DPOP-4.3-6");
+		callAndContinueOnFailure(ValidateAuthorizationCodeDpopBindingKey.class, Condition.ConditionResult.FAILURE,"DPOP-10");
 	}
 }
