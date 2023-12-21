8b1c95794664d94da32ad4b56575b42871193551
Joseph Heenan <joseph.heenan@fintechlabs.io>
2023-12-21 17:07:10 +0900

    Add expected failures for mock bank

--- scripts/test-configs-brazil/run-raidiam.sh
+++ scripts/test-configs-brazil/run-raidiam.sh
@@ -7,6 +7,10 @@ TESTS=""
 
 TESTS="$TESTS fapi1-advanced-final-brazil-dcr-test-plan[client_auth_type=private_key_jwt][fapi_auth_request_method=pushed][fapi_response_mode=plain_response][fapi_profile=openbanking_brazil] ../conformance-suite/scripts/test-configs-brazil/brazil-raidiam-fapi-dcr-payments-automated.json"
 
+EXPECTED_FAILURES_FILE="../conformance-suite/.gitlab-ci/expected-failures-raidiam.json"
+TESTS="${TESTS} --expected-failures-file ${EXPECTED_FAILURES_FILE}"
+TESTS="${TESTS} --verbose"
+
 TESTS="${TESTS} --export-dir ../conformance-suite"
 
 ../conformance-suite/scripts/run-test-plan.py $TESTS
