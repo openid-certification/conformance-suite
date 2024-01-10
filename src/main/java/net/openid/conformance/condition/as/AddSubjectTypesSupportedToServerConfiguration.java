c5147743860c4bd1be5c1fedee1fd0c4278aff19
Alan Smillie <alans@emobix.co.uk>
2024-01-10 11:49:19 +0000

    FAPI OP/RP: Discovery Metadata - Subject Types Supported
    FAPI RP: Return 'subject_types_supported' in the discovery metadata
    when open id is configured..
    
    FAPI OP: Verify that the discovery metadata contains 'subject_types_supported'
    when open id is configured.

---
+++ src/main/java/net/openid/conformance/condition/as/AddSubjectTypesSupportedToServerConfiguration.java
@@ -0,0 +1,30 @@
+package net.openid.conformance.condition.as;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class AddSubjectTypesSupportedToServerConfiguration extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = {"server"})
+	@PostEnvironment(required = {"server"})
+	public Environment evaluate(Environment env) {
+
+		JsonObject server = env.getObject("server");
+
+		JsonArray claimsSupported = new JsonArray();
+
+		claimsSupported.add("public");
+		claimsSupported.add("pairwise");
+
+		server.add("subject_types_supported", claimsSupported);
+
+		log("Added public/pairwise to subject_types_supported in server metadata", args("server", server));
+
+		return env;
+	}
+}
