7c4a5114c192bf2df634ca6f0e2a73981e5309ad
Edmund Jay <ejay@mgi1.com>
2024-04-16 18:49:15 -0700

    Generate DPOP nonces using allowed characters

--- src/main/java/net/openid/conformance/condition/as/CreateAuthorizationServerDpopNonce.java
+++ src/main/java/net/openid/conformance/condition/as/CreateAuthorizationServerDpopNonce.java
@@ -2,15 +2,16 @@ package net.openid.conformance.condition.as;
 
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.util.RFC6749AppendixASyntaxUtils;
 import net.openid.conformance.testmodule.Environment;
-import org.apache.commons.lang3.RandomStringUtils;
 
 public class CreateAuthorizationServerDpopNonce extends AbstractCondition {
 
 	@Override
 	@PostEnvironment(strings = "authorization_server_dpop_nonce")
 	public Environment evaluate(Environment env) {
-		String nonce = RandomStringUtils.randomAlphanumeric(20);
+		// DPOP spec does not define max length for nonce value
+		String nonce = RFC6749AppendixASyntaxUtils.generateNQChar(50, 10, 30);
 		env.putString("authorization_server_dpop_nonce", nonce);
 		logSuccess("Created Authorization Server DPoP nonce", args("authorization_server_dpop_nonce", nonce));
 		return env;
--- src/main/java/net/openid/conformance/condition/rs/CreateResourceServerDpopNonce.java
+++ src/main/java/net/openid/conformance/condition/rs/CreateResourceServerDpopNonce.java
@@ -2,15 +2,15 @@ package net.openid.conformance.condition.rs;
 
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.util.RFC6749AppendixASyntaxUtils;
 import net.openid.conformance.testmodule.Environment;
-import org.apache.commons.lang3.RandomStringUtils;
 
 public class CreateResourceServerDpopNonce extends AbstractCondition {
 
 	@Override
 	@PostEnvironment(strings = "resource_server_dpop_nonce")
 	public Environment evaluate(Environment env) {
-		String nonce = RandomStringUtils.randomAlphanumeric(20);
+		String nonce = RFC6749AppendixASyntaxUtils.generateNQChar(50, 10, 30);
 		env.putString("resource_server_dpop_nonce", nonce);
 		logSuccess("Created Resource Server nonce", args("resource_server_dpop_nonce", nonce));
 		return env;
