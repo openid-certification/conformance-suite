8a1cf56b3ba69cd00ce025bd78e60468cead728d
Edmund Jay <ejay@mgi1.com>
2024-06-20 12:48:12 -0700

    Add test to ensure that DPOP proofs with iat of 10 seconds in the future are accepted
    addresses #1038

--- src/main/java/net/openid/conformance/fapi2spid2/FAPI2MessageSigningID1TestPlan.java
+++ src/main/java/net/openid/conformance/fapi2spid2/FAPI2MessageSigningID1TestPlan.java
@@ -31,6 +31,7 @@ public class FAPI2MessageSigningID1TestPlan implements TestPlan {
 		// DPoP tests
 		FAPI2SPID2CheckDpopProofNbfExp.class,
 		FAPI2SPID2EnsureDpopProofWithIat10SecondsBeforeSucceeds.class,
+		FAPI2SPID2EnsureDpopProofWithIat10SecondsAfterSucceeds.class,
 
 		// DPop Authorization Code Binding negative tests
 		FAPI2SPID2EnsureMismatchedDpopJktFails.class,
