5e5a4daca2f307df9bc6d7f5da4610112f7f0559
Edmund Jay <ejay@mgi1.com>
2024-06-20 12:07:27 -0700

    Refactor RefreshTokenRequestExpectingErrorSteps to  use the CreateDpopProofSteps sequence
    fixes #1335 - Refactor DPoP case in RefreshTokenRequestSteps

--- src/main/java/net/openid/conformance/sequence/client/RefreshTokenRequestExpectingErrorSteps.java
+++ src/main/java/net/openid/conformance/sequence/client/RefreshTokenRequestExpectingErrorSteps.java
@@ -1,23 +1,20 @@
 package net.openid.conformance.sequence.client;
 
+import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.Condition.ConditionResult;
-import net.openid.conformance.condition.client.AddDpopHeaderForTokenEndpointRequest;
 import net.openid.conformance.condition.client.AddScopeToTokenEndpointRequest;
 import net.openid.conformance.condition.client.CallTokenEndpointAllowingDpopNonceErrorAndReturnFullResponse;
 import net.openid.conformance.condition.client.CallTokenEndpointAndReturnFullResponse;
 import net.openid.conformance.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidGrant;
 import net.openid.conformance.condition.client.CheckTokenEndpointHttpStatus400;
 import net.openid.conformance.condition.client.CheckTokenEndpointReturnedJsonContentType;
-import net.openid.conformance.condition.client.CreateDpopClaims;
-import net.openid.conformance.condition.client.CreateDpopHeader;
 import net.openid.conformance.condition.client.CreateRefreshTokenRequest;
-import net.openid.conformance.condition.client.EnsureDpopNonceContainsAllowedCharactersOnly;
-import net.openid.conformance.condition.client.SetDpopHtmHtuForTokenEndpoint;
-import net.openid.conformance.condition.client.SetDpopProofNonceForTokenEndpoint;
-import net.openid.conformance.condition.client.SignDpopProof;
 import net.openid.conformance.condition.client.ValidateErrorFromTokenEndpointResponseError;
 import net.openid.conformance.sequence.AbstractConditionSequence;
 import net.openid.conformance.sequence.ConditionSequence;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RefreshTokenRequestExpectingErrorSteps extends AbstractConditionSequence {
 
@@ -45,46 +42,26 @@ public class RefreshTokenRequestExpectingErrorSteps extends AbstractConditionSeq
 		call(sequence(addClientAuthenticationToTokenEndpointRequest));
 
 		if (isDpop) {
-			callAndStopOnFailure(CreateDpopHeader.class);
-			callAndStopOnFailure(CreateDpopClaims.class);
-			callAndStopOnFailure(SetDpopHtmHtuForTokenEndpoint.class);
-			callAndContinueOnFailure(SetDpopProofNonceForTokenEndpoint.class, ConditionResult.INFO);
-			callAndContinueOnFailure(EnsureDpopNonceContainsAllowedCharactersOnly.class, ConditionResult.FAILURE, "DPOP-8.1");
-			callAndStopOnFailure(SignDpopProof.class);
-			callAndStopOnFailure(AddDpopHeaderForTokenEndpointRequest.class);
+			call(CreateDpopProofSteps.createTokenEndpointDpopSteps());
 			callAndStopOnFailure(CallTokenEndpointAllowingDpopNonceErrorAndReturnFullResponse.class);
 
 			// retry request if token_endpoint_dpop_nonce_error is found
-			exec().startBlock("Token endpoint DPoP nonce retry");
-			call(condition(CreateDpopHeader.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO));
-			call(condition(CreateDpopClaims.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO));
-			call(condition(SetDpopHtmHtuForTokenEndpoint.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO));
-			call(condition(SetDpopProofNonceForTokenEndpoint.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO)
-				.dontStopOnFailure());
-			call(condition(EnsureDpopNonceContainsAllowedCharactersOnly.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO)
-				.requirement("DPOP-8.1")
-				.onFail(ConditionResult.WARNING)
-				.dontStopOnFailure());
-			call(condition(SignDpopProof.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO));
-			call(condition(AddDpopHeaderForTokenEndpointRequest.class)
-				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
-				.onSkip(ConditionResult.INFO));
+			call(exec().startBlock("Token endpoint DPoP nonce retry"));
+
+			// repeat conditions in CreateDpopProofSteps.createTokenEndpointDpopSteps() only if token_endpoint_dpop_nonce_error is found
+			ConditionSequence seq = CreateDpopProofSteps.createTokenEndpointDpopSteps();
+			seq.evaluate();
+			List<Class<?extends Condition>> condList = seq.getTestExecutionUnits().stream().map(actionToConditionClass).collect(Collectors.toList());
+			condList.forEach((Class<?extends Condition> cond) -> {
+				call(condition(cond)
+					.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
+					.onSkip(ConditionResult.INFO));
+			});
+
 			call(condition(CallTokenEndpointAllowingDpopNonceErrorAndReturnFullResponse.class)
 				.skipIfStringsMissing("token_endpoint_dpop_nonce_error")
 				.onSkip(ConditionResult.INFO));
-			exec().endBlock();
+			call(exec().endBlock());
 		} else {
 			callAndStopOnFailure(CallTokenEndpointAndReturnFullResponse.class);
 		}
