3b0facf4a5c49b7126aad418280b2e258c805dcb
Alan Smillie <alans@emobix.co.uk>
2024-07-02 16:08:09 +0100

    UI - Fix FAPI2-SP Section Links

--- src/main/java/net/openid/conformance/export/LogEntryHelper.java
+++ src/main/java/net/openid/conformance/export/LogEntryHelper.java
@@ -40,7 +40,7 @@ public class LogEntryHelper {
 		specLinks.put("FAPI-RW-", "https://openid.net/specs/openid-financial-api-part-2-ID2.html#rfc.section.");
 		specLinks.put("FAPI1-BASE-", "https://openid.net/specs/openid-financial-api-part-1-1_0-final.html#rfc.section.");
 		specLinks.put("FAPI1-ADV-", "https://openid.net/specs/openid-financial-api-part-2-1_0-final.html#rfc.section.");
-		specLinks.put("FAPI2-SP-ID2-", "https://openid.net/specs/fapi-2_0-security-profile-ID2.html#section.");
+		specLinks.put("FAPI2-SP-ID2-", "https://openid.net/specs/fapi-2_0-security-profile-ID2.html#section-");
 		specLinks.put("FAPI2-MS-ID1-", "https://openid.net/specs/fapi-2_0-message-signing-ID1.html#section..");
 		specLinks.put("CIBA-", "https://openid.net/specs/openid-client-initiated-backchannel-authentication-core-1_0.html#rfc.section.");
 		specLinks.put("FAPI-CIBA-", "https://openid.net/specs/openid-financial-api-ciba.html#rfc.section.");
