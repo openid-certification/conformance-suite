e3d3a6804d4d80848a9a33bf308727f4e32488ea
Thomas Darimont <thomas.darimont@oidf.org>
2024-07-15 12:20:10 +0200

    Improve detection of incorrect error reporting in conditions
    We now throw error(..) instead of of simply calling logFailure(..) to
    avoid generating multiple warning messages.
    
    Revised the error message in case of errors were logged without
    throwing error(..).
    
    Fixes #1355
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/condition/AbstractCondition.java
+++ src/main/java/net/openid/conformance/condition/AbstractCondition.java
@@ -126,7 +126,7 @@ public abstract class AbstractCondition implements Condition, DataUtils {
 			if (errorsLogged > 0) {
 				// the condition has logged a warning/failure so must throw an error, otherwise the test result will
 				// not be updated
-				throw error("Test logged a non-success result but did not throw an error");
+				throw new ConditionError(testId, "Test condition needs to throw error() if it logs failures/warnings");
 			}
 
 			// check the environment to make sure the condition did what it claimed to
