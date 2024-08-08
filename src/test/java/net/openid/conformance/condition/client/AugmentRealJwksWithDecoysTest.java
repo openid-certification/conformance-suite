97fa17b61c0e1c050618f86e1a461d54739f04c3
Thomas Darimont <thomas.darimont@oidf.org>
2024-08-08 12:02:23 +0200

    Add unit test for AugmentRealJwksWithDecoys
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

---
+++ src/test/java/net/openid/conformance/condition/client/AugmentRealJwksWithDecoysTest.java
@@ -0,0 +1,72 @@
+package net.openid.conformance.condition.client;
+
+import com.google.gson.JsonObject;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.gen.JWKGenerator;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.logging.TestInstanceEventLog;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.util.JWKUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AugmentRealJwksWithDecoysTest {
+
+	@Spy
+	private Environment env = new Environment();
+
+	@Mock
+	private TestInstanceEventLog eventLog;
+
+	private AugmentRealJwksWithDecoys augmentRealJwksWithDecoys;
+
+	@Before
+	public void setUp() throws Exception {
+		JsonObject server = new JsonObject();
+		server.addProperty("jwks_uri", "https://as/jwks");
+		env.putObject("server", server);
+		env.putString("base_url", "https://as/");
+
+		var keyGenerator = new AbstractGenerateKey() {
+			@Override
+			protected Environment evaluate(Environment env) {
+				return null;
+			}
+
+			@Override
+			protected JWKGenerator<? extends JWK> onConfigure(JWKGenerator<? extends JWK> generator) {
+				// use same kid for all keys
+				generator.keyID("testKey");
+				return generator;
+			}
+		};
+
+		JWK ps256 = keyGenerator.createJwkForAlg("PS256");
+		JWKSet jwks = new JWKSet(ps256);
+
+		JsonObject publicJwksAsJsonObject = JWKUtil.getPublicJwksAsJsonObject(jwks);
+		env.putObject("server_public_jwks", publicJwksAsJsonObject);
+
+		augmentRealJwksWithDecoys = new AugmentRealJwksWithDecoys();
+		augmentRealJwksWithDecoys.setProperties("UNIT-TEST", eventLog, Condition.ConditionResult.INFO, "server", "base_url", "server_public_jwks");
+	}
+
+	@Test
+	public void shouldGenerateDecoyKeysAndUpdateJwksUri() {
+
+		augmentRealJwksWithDecoys.evaluate(env);
+
+		JsonObject serverPublicJwksDecoy = env.getObject("server_public_jwks_decoy");
+		assertThat(serverPublicJwksDecoy).isNotNull();
+		String jwksUri = env.getString("server", "jwks_uri");
+		assertThat(jwksUri).isEqualTo("https://as/jwks_decoy");
+	}
+}
