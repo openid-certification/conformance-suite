788af210bc0f56dd2bdee30c15d875ec3b910430
Edmund Jay <ejay@mgi1.com>
2024-09-20 02:41:35 -0700

    Refactor FAPI1AdvancedFainalBrV1 tests that subclasses AbstractFAPI1AdvancedFinalBrV1PerformTokenEndpoint to override the processTokenEndpointResponse method

--- src/main/java/net/openid/conformance/fapi1advancedfinalfapibrv1/FAPI1AdvancedFinalBrV1PAREnsurePKCECodeVerifierRequired.java
+++ src/main/java/net/openid/conformance/fapi1advancedfinalfapibrv1/FAPI1AdvancedFinalBrV1PAREnsurePKCECodeVerifierRequired.java
@@ -1,7 +1,6 @@
 package net.openid.conformance.fapi1advancedfinalfapibrv1;
 
 import net.openid.conformance.condition.Condition;
-import net.openid.conformance.condition.client.CallTokenEndpointAndReturnFullResponse;
 import net.openid.conformance.condition.client.CheckErrorDescriptionFromTokenEndpointResponseErrorContainsCRLFTAB;
 import net.openid.conformance.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidGrant;
 import net.openid.conformance.condition.client.CheckTokenEndpointHttpStatus400;
@@ -45,9 +44,8 @@ public class FAPI1AdvancedFinalBrV1PAREnsurePKCECodeVerifierRequired extends Abs
 	}
 
 	@Override
-	protected void requestAuthorizationCode() {
+	protected void processTokenEndpointResponse() {
 		/* expect an 'invalid_grant' error */
-		callAndStopOnFailure(CallTokenEndpointAndReturnFullResponse.class, Condition.ConditionResult.FAILURE, "FAPI1-BASE-5.2.2-19");
 		callAndContinueOnFailure(CheckTokenEndpointHttpStatus400.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
 		callAndContinueOnFailure(CheckTokenEndpointReturnedJsonContentType.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.3.4");
 		callAndContinueOnFailure(CheckErrorFromTokenEndpointResponseErrorInvalidGrant.class, Condition.ConditionResult.FAILURE, "RFC7636-4.6", "RFC6749-5.2");
