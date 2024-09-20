9433e5be94a39086e82ecb2883e03b94a90b7961
Alan Smillie <alans@emobix.co.uk>
2024-09-20 16:30:13 +0100

    FAPI (PAR): Ensure The 'request_uri' Can Be Reused Prior To Authentication Completion
    As per https://bitbucket.org/openid/fapi/issues/635/one-time-use-of-request_uri-causing-error
    
    Tests have been added to issue a GET request, including the 'request_uri', to
    the authentication endpoint prior to executing a successful authentication flow.
    
    This should succeed.
    
    NB. The HttpClient can now be configured to handle redirection. This is enabled
        in the new tests.

--- src/test/java/net/openid/conformance/condition/AbstractCondition_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/AbstractCondition_UnitTest.java
@@ -1564,9 +1564,9 @@ public class AbstractCondition_UnitTest {
 		 * @see AbstractCondition#createHttpClient(Environment)
 		 */
 		@Override
-		protected HttpClient createHttpClient(Environment env, boolean restrictAllowedTLSVersions) throws CertificateException, InvalidKeySpecException, NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, KeyManagementException {
+		protected HttpClient createHttpClient(Environment env, boolean restrictAllowedTLSVersions, boolean disableRedirectHandling) throws CertificateException, InvalidKeySpecException, NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, KeyManagementException {
 			// TODO Auto-generated method stub
-			return super.createHttpClient(env, restrictAllowedTLSVersions);
+			return super.createHttpClient(env, restrictAllowedTLSVersions, disableRedirectHandling);
 
 		}
 
