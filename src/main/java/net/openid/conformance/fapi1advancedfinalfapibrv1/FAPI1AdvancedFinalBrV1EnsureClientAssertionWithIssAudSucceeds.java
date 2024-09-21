5e21fab05c9af9b2663db47f602dfd0b283d0911
Edmund Jay <ejay@mgi1.com>
2024-09-20 17:18:05 -0700

    Add FAPI1BrV1 OP test to check if AS accepts Issuer ID as aud claim in client assertion

---
+++ src/main/java/net/openid/conformance/fapi1advancedfinalfapibrv1/FAPI1AdvancedFinalBrV1EnsureClientAssertionWithIssAudSucceeds.java
@@ -0,0 +1,73 @@
+package net.openid.conformance.fapi1advancedfinalfapibrv1;
+
+import com.google.common.base.Strings;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.AddClientAssertionToTokenEndpointRequest;
+import net.openid.conformance.condition.client.CheckErrorDescriptionFromTokenEndpointResponseErrorContainsCRLFTAB;
+import net.openid.conformance.condition.client.CheckErrorFromTokenEndpointResponseErrorInvalidClient;
+import net.openid.conformance.condition.client.CheckIfTokenEndpointResponseError;
+import net.openid.conformance.condition.client.CheckTokenEndpointHttpStatusIs400Allowing401ForInvalidClientError;
+import net.openid.conformance.condition.client.CheckTokenEndpointReturnedJsonContentType;
+import net.openid.conformance.condition.client.CreateClientAuthenticationAssertionClaims;
+import net.openid.conformance.condition.client.SignClientAuthenticationAssertion;
+import net.openid.conformance.condition.client.UpdateClientAuthenticationAssertionClaimsWithISSAud;
+import net.openid.conformance.condition.client.ValidateErrorDescriptionFromTokenEndpointResponseError;
+import net.openid.conformance.condition.client.ValidateErrorFromTokenEndpointResponseError;
+import net.openid.conformance.condition.client.ValidateErrorUriFromTokenEndpointResponseError;
+import net.openid.conformance.testmodule.PublishTestModule;
+import net.openid.conformance.variant.ClientAuthType;
+import net.openid.conformance.variant.VariantNotApplicable;
+
+@PublishTestModule(
+	testName = "fapi1-advanced-final-br-v1-ensure-client-assertion-with-iss-aud-succeeds",
+	displayName = "FAPI1-Advanced-Final-Br-v1: ensure client_assertion with AS issuer ID succeeds at the token endpoint",
+	summary = "This test passes a client assertion where 'aud' is the Authorization Server's Issuer ID instead of the token endpoint. The server should accept this value.",
+	profile = "FAPI1-Advanced-Final-Br-v1",
+	configurationFields = {
+		"server.discoveryUrl",
+		"client.client_id",
+		"client.scope",
+		"client.jwks",
+		"mtls.key",
+		"mtls.cert",
+		"mtls.ca",
+		"client2.client_id",
+		"client2.scope",
+		"client2.jwks",
+		"mtls2.key",
+		"mtls2.cert",
+		"mtls2.ca",
+		"resource.resourceUrl"
+	}
+)
+@VariantNotApplicable(parameter = ClientAuthType.class, values = { "mtls" })
+public class FAPI1AdvancedFinalBrV1EnsureClientAssertionWithIssAudSucceeds extends AbstractFAPI1AdvancedFinalBrV1PerformTokenEndpoint {
+
+	@Override
+	protected void addClientAuthenticationToTokenEndpointRequest() {
+		callAndStopOnFailure(CreateClientAuthenticationAssertionClaims.class);
+		callAndStopOnFailure(UpdateClientAuthenticationAssertionClaimsWithISSAud.class);
+		callAndStopOnFailure(SignClientAuthenticationAssertion.class);
+		callAndStopOnFailure(AddClientAssertionToTokenEndpointRequest.class);
+	}
+
+	@Override
+	protected void processTokenEndpointResponse() {
+		/* If we get an error back from the token endpoint server:
+		 * - It must be a 'invalid_client' error
+		 */
+		if(!Strings.isNullOrEmpty(env.getString("token_endpoint_response", "error"))) {
+			callAndContinueOnFailure(CheckIfTokenEndpointResponseError.class, Condition.ConditionResult.WARNING);
+			callAndContinueOnFailure(CheckTokenEndpointReturnedJsonContentType.class, Condition.ConditionResult.FAILURE, "OIDCC-3.1.3.4");
+			callAndContinueOnFailure(ValidateErrorFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+			callAndContinueOnFailure(CheckErrorDescriptionFromTokenEndpointResponseErrorContainsCRLFTAB.class, Condition.ConditionResult.WARNING, "RFC6749-5.2");
+			callAndContinueOnFailure(ValidateErrorDescriptionFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE,"RFC6749-5.2");
+			callAndContinueOnFailure(ValidateErrorUriFromTokenEndpointResponseError.class, Condition.ConditionResult.FAILURE,"RFC6749-5.2");
+			callAndContinueOnFailure(CheckTokenEndpointHttpStatusIs400Allowing401ForInvalidClientError.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+			callAndContinueOnFailure(CheckErrorFromTokenEndpointResponseErrorInvalidClient.class, Condition.ConditionResult.FAILURE, "RFC6749-5.2");
+		} else {
+			super.processTokenEndpointResponse();
+		}
+		fireTestFinished();
+	}
+}
