840615f4904cb58ebec23ecae8119a113d3e5768
Thomas Darimont <thomas.darimont@oidf.org>
2024-10-04 17:16:47 +0200

    Initial support for SSF Tests
    Fixes #1386

---
+++ src/main/java/net/openid/conformance/openid/ssf/OpenIdSsfReceiverTestPlan.java
@@ -0,0 +1,20 @@
+package net.openid.conformance.openid.ssf;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.variant.VariantSelection;
+
+@PublishTestPlan(
+	testPlanName = "openid-ssf-receiver-test-plan",
+	displayName = "OpenID Shared Signals Framework: Receiver test (alpha - INCOMPLETE/INCORRECT, please email certification team if interested)",
+	profile = TestPlan.ProfileNames.ssftest,
+	testModules = {
+	}
+)
+public class OpenIdSsfReceiverTestPlan implements TestPlan {
+
+	public static String certificationProfileName(VariantSelection variant) {
+		return "OpenID SSF Receiver";
+	}
+
+}
