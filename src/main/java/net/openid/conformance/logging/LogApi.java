e6f94e9616e5b40fe4adc14f58dd989111f1f4d4
Joseph Heenan <joseph.heenan@oidf.org>
2024-10-05 20:01:30 +0100

    Remove some unneeded @SuppressWarnings
    As pointed out by IntelliJ

--- src/main/java/net/openid/conformance/logging/LogApi.java
+++ src/main/java/net/openid/conformance/logging/LogApi.java
@@ -272,7 +272,6 @@ public class LogApi {
 
 						String sigFileName = "test-log-" + testLogInfoExport.get("testModuleName") + "-" + testLogInfoExport.get("testId") + ".sig";
 
-						@SuppressWarnings("unchecked")
 						TestExportInfo infoExport = (TestExportInfo) testLogInfoExport.get("export");
 
 						addFilesToZip(archiveOutputStream, jsonFileName, sigFileName, infoExport);
