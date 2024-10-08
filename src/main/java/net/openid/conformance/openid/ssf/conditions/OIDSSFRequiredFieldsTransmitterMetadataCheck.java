e63b4dd08348f23ecc373bf38c0c3193bca2fe72
Thomas Darimont <thomas.darimont@oidf.org>
2024-10-08 12:11:06 +0200

    [ci skip]
    Refactor SSF checks
    - Use OIDSSF prefix for tests and conditions
    - Split up checks into multiple smaller conditions

---
+++ src/main/java/net/openid/conformance/openid/ssf/conditions/OIDSSFRequiredFieldsTransmitterMetadataCheck.java
@@ -0,0 +1,32 @@
+package net.openid.conformance.openid.ssf.conditions;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OIDSSFRequiredFieldsTransmitterMetadataCheck extends AbstractCondition {
+
+	private static final Set<String> REQUIRED_FIELDS = Set.of(
+		"issuer",
+		"authorization_schemes"
+	);
+
+	@Override
+	@PreEnvironment(required = {"transmitter_metadata"})
+	public Environment evaluate(Environment env) {
+
+		JsonObject transmitterMetadata = env.getObject("transmitter_metadata");
+		Set<String> missingFields = REQUIRED_FIELDS.stream().filter(field -> !transmitterMetadata.has(field)).collect(Collectors.toCollection(LinkedHashSet::new));
+
+		if (!missingFields.isEmpty()) {
+			throw error("Couldn't find required fields in transmitter_metadata", args("missing_fields", missingFields));
+		}
+
+		return env;
+	}
+}
