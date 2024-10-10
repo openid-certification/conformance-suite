039b18135b23c04ab2f5fda7adafbece68a6c2af
Thomas Darimont <thomas.darimont@oidf.org>
2024-10-11 00:59:08 +0200

    [ci skip]
    Initial support for Stream management tests
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

---
+++ src/main/java/net/openid/conformance/openid/ssf/conditions/OIDSSFObtainTransmitterAccessToken.java
@@ -0,0 +1,26 @@
+package net.openid.conformance.openid.ssf.conditions;
+
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class OIDSSFObtainTransmitterAccessToken extends AbstractCondition {
+
+	@PreEnvironment(required = {"config"})
+	@Override
+	public Environment evaluate(Environment env) {
+
+		// TODO add auth variant (dynamic / static)
+		String accessTokenFromConfig = env.getString("config", "ssf.transmitter.access_token");
+
+		if (accessTokenFromConfig != null) {
+			log("Obtained transmitter access token from config");
+			env.putString("transmitter_access_token", accessTokenFromConfig);
+		}
+
+		logSuccess("Added transmitter access token");
+
+		return env;
+	}
+}
