3a6531c487c83807215476e2441771fcd95a4243
Thomas Darimont <thomas.darimont@oidf.org>
2024-10-21 09:46:13 +0200

    Revise SSF Stream tests
    - Add support for testing subject add / deletion
    - Move Receiver tests to stream tests

--- src/main/java/net/openid/conformance/openid/ssf/OIDSSFStreamControlErrorResponseTest.java
+++ src/main/java/net/openid/conformance/openid/ssf/OIDSSFStreamControlErrorResponseTest.java
@@ -16,12 +16,21 @@ import net.openid.conformance.variant.ServerMetadata;
 import net.openid.conformance.variant.VariantConfigurationFields;
 import net.openid.conformance.variant.VariantParameters;
 
-@PublishTestModule(testName = "openid-ssf-receiver-stream-control-error-path", displayName = "OpenID Shared Signals Framework: Validate Receiver Stream Control (Error Paths)", summary = "This test verifies the behavior of the receiver stream control.", profile = "OIDSSF", configurationFields = {"ssf.transmitter.issuer", "ssf.transmitter.metadata_suffix", // see: https://openid.net/specs/openid-sharedsignals-framework-1_0.html#section-6.2.1
-	"ssf.transmitter.access_token"})
+@PublishTestModule(
+	testName = "openid-ssf-stream-control-error-path",
+	displayName = "OpenID Shared Signals Framework: Validate Stream Control (Error Paths)",
+	summary = "This test verifies the behavior of the stream control.",
+	profile = "OIDSSF",
+	configurationFields = {
+		"ssf.transmitter.issuer",
+		"ssf.transmitter.metadata_suffix", // see: https://openid.net/specs/openid-sharedsignals-framework-1_0.html#section-6.2.1
+		"ssf.transmitter.access_token"
+	}
+)
 @VariantParameters({ServerMetadata.class, SsfDeliveryMode.class,})
 @VariantConfigurationFields(parameter = ServerMetadata.class, value = "static", configurationFields = {"ssf.transmitter.configuration_metadata_endpoint",})
 @VariantConfigurationFields(parameter = ServerMetadata.class, value = "discovery", configurationFields = {"ssf.transmitter.issuer", "ssf.transmitter.metadata_suffix",})
-public class OIDSSFReceiverStreamControlErrorResponseTest extends AbstractOIDSSFTest {
+public class OIDSSFStreamControlErrorResponseTest extends AbstractOIDSSFTest {
 
 	@Override
 	public void start() {
