2a0d5d86fc3c86e3eba3f46190736919407526c0
Thomas Darimont <thomas.darimont@oidf.org>
2024-10-22 11:13:32 +0200

    [ci skip]
    - Add TLS connection check
    - Extracted EnsureTLS12OrLater from EnsureTLS12WithFAPICiphers
    - Introduced EnsureTLS130OrLater
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/openid/ssf/OIDSSFTransmitterMetadataTest.java
+++ src/main/java/net/openid/conformance/openid/ssf/OIDSSFTransmitterMetadataTest.java
@@ -4,6 +4,7 @@ import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.client.CheckJwksUri;
 import net.openid.conformance.condition.client.FetchServerKeys;
 import net.openid.conformance.condition.client.ValidateServerJWKs;
+import net.openid.conformance.openid.ssf.conditions.CheckTransmitterMetadataIssuer;
 import net.openid.conformance.openid.ssf.conditions.OIDSSFAuthorizationSchemesTransmitterMetadataCheck;
 import net.openid.conformance.openid.ssf.conditions.OIDSSFDefaultSubjectsTransmitterMetadataCheck;
 import net.openid.conformance.openid.ssf.conditions.OIDSSFEnsureHttpsUrlsTransmitterMetadataCheck;
@@ -28,28 +29,36 @@ public class OIDSSFTransmitterMetadataTest extends AbstractOIDSSFTest {
 
 		setStatus(Status.RUNNING);
 
-		fetchTransmitterMetadata();
+		eventLog.runBlock("Fetch Transmitter Metadata", () -> {
+			fetchTransmitterMetadata();
+		});
 
-		validateTransmitterMetadata();
+		eventLog.runBlock("Validate TLS Connection", () -> {
+			validateTlsConnection();
+		});
+
+		eventLog.runBlock("Validate Transmitter Metadata", () -> {
+			validateTransmitterMetadata();
+		});
 
 		fireTestFinished();
 	}
 
 	private void validateTransmitterMetadata() {
 
-		eventLog.runBlock("Validate Transmitter Metadata", () -> {
-			callAndStopOnFailure(OIDSSFEnsureHttpsUrlsTransmitterMetadataCheck.class, "OIDSSF-6.1", "CAEPIOP-2.3.7");
-			callAndContinueOnFailure(OIDSSFSpecVersionTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "CAEPIOP-2.3.1");
-			callAndStopOnFailure(OIDSSFRequiredFieldsTransmitterMetadataCheck.class, "OIDSSF-6.1");
-			callAndContinueOnFailure(OIDSSFDefaultSubjectsTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "OIDSSF-6.1");
-			callAndContinueOnFailure(OIDSSFAuthorizationSchemesTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "OIDSSF-6.1.1", "CAEPIOP-2.3.7");
 
-			// treat transmitter_metadata as "server" metadata to leverage existing checks
-			env.mapKey("server", "transmitter_metadata");
+		callAndContinueOnFailure(CheckTransmitterMetadataIssuer.class, Condition.ConditionResult.WARNING, "OIDSSF-6.2");
+		callAndStopOnFailure(OIDSSFEnsureHttpsUrlsTransmitterMetadataCheck.class, "OIDSSF-6.1", "CAEPIOP-2.3.7");
+		callAndContinueOnFailure(OIDSSFSpecVersionTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "CAEPIOP-2.3.1");
+		callAndStopOnFailure(OIDSSFRequiredFieldsTransmitterMetadataCheck.class, "OIDSSF-6.1");
+		callAndContinueOnFailure(OIDSSFDefaultSubjectsTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "OIDSSF-6.1");
+		callAndContinueOnFailure(OIDSSFAuthorizationSchemesTransmitterMetadataCheck.class, Condition.ConditionResult.WARNING, "OIDSSF-6.1.1", "CAEPIOP-2.3.7");
 
-			callAndStopOnFailure(CheckJwksUri.class);
-			callAndStopOnFailure(FetchServerKeys.class);
-			callAndStopOnFailure(ValidateServerJWKs.class, "RFC7517-1.1");
-		});
+		// treat transmitter_metadata as "server" metadata to leverage existing checks
+		env.mapKey("server", "transmitter_metadata");
+
+		callAndStopOnFailure(CheckJwksUri.class);
+		callAndStopOnFailure(FetchServerKeys.class);
+		callAndStopOnFailure(ValidateServerJWKs.class, "RFC7517-1.1");
 	}
 }
