b329fc709d7b4ffa52f743eb7ff593e701408ca7
Marcus Almgren <marcus@almgren.pt>
2024-11-05 17:21:26 +0000

    Use jwtStringToJsonObjectForEnvironment to follow existing patterns.

--- src/main/java/net/openid/conformance/openid/federation/ExtractOpenIDConnectRelyingPartyMetadata.java
+++ src/main/java/net/openid/conformance/openid/federation/ExtractOpenIDConnectRelyingPartyMetadata.java
@@ -9,10 +9,10 @@ import net.openid.conformance.testmodule.Environment;
 public class ExtractOpenIDConnectRelyingPartyMetadata extends AbstractCondition {
 
 	@Override
-	@PreEnvironment(required = "federation_response_body")
+	@PreEnvironment(required = "federation_response_jwt")
 	public Environment evaluate(Environment env) {
 
-		JsonElement openidRelyingPartyMetadataElement = env.getElementFromObject("federation_response_body", "metadata.openid_relying_party");
+		JsonElement openidRelyingPartyMetadataElement = env.getElementFromObject("federation_response_jwt", "claims.metadata.openid_relying_party");
 		if (openidRelyingPartyMetadataElement == null) {
 			logSuccess("Entity statement does not contain the openid_relying_party metadata claim");
 		} else {
