49af7842f10087c1da960de59cab3b6f1062c0b4
Thomas Darimont <thomas.darimont@oidf.org>
2024-11-12 10:34:12 +0100

    Move metadata conditions to metadata package
    [ci skip]
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/openid/ssf/conditions/metadata/OIDSSFGetDynamicTransmitterConfiguration.java
+++ src/main/java/net/openid/conformance/openid/ssf/conditions/metadata/OIDSSFGetDynamicTransmitterConfiguration.java
@@ -1,4 +1,4 @@
-package net.openid.conformance.openid.ssf.conditions;
+package net.openid.conformance.openid.ssf.conditions.metadata;
 
 import com.google.common.base.Strings;
 import com.google.gson.JsonObject;
