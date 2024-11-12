2b5446c849d5a807f43cacf1b9538dd40725ec82
Thomas Darimont <thomas.darimont@oidf.org>
2024-11-12 11:19:22 +0100

    Revise OIDSSFInjectDummyStreamId
    [ci skip]
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/openid/ssf/conditions/OIDSSFInjectDummyStreamId.java
+++ src/main/java/net/openid/conformance/openid/ssf/conditions/OIDSSFInjectDummyStreamId.java
@@ -8,7 +8,7 @@ public class OIDSSFInjectDummyStreamId extends AbstractCondition {
 	@Override
 	public Environment evaluate(Environment env) {
 
-		env.putString("ssf","stream_id", "dummy");
+		env.putString("ssf","stream.stream_id", "dummy");
 
 		return env;
 	}
