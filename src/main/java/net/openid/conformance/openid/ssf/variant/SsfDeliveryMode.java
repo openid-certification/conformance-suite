22d673bd0687870c7422dab1fc001f2515ebf7e3
Thomas Darimont <thomas.darimont@oidf.org>
2024-11-18 12:00:48 +0100

    Add support for delivery mode push
    [ci skip]
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/openid/ssf/variant/SsfDeliveryMode.java
+++ src/main/java/net/openid/conformance/openid/ssf/variant/SsfDeliveryMode.java
@@ -12,15 +12,25 @@ public enum SsfDeliveryMode {
 	/**
 	 * The Poll delivery mode, see: https://openid.net/specs/openid-sharedsignals-framework-1_0-ID3.html#section-10.3.1.2
 	 */
-	PUSH,
+	PUSH("urn:ietf:rfc:8935"),
 
 	/**
 	 * The PUSH delivery mode, see: https://openid.net/specs/openid-sharedsignals-framework-1_0-ID3.html#section-10.3.1.1
 	 */
-	POLL;
+	POLL("urn:ietf:rfc:8936");
 
 	@Override
 	public String toString() {
 		return name().toLowerCase();
+	}
+
+	private final String alias;
+
+	SsfDeliveryMode(String alias) {
+		this.alias = alias;
+	}
+
+	public String getAlias() {
+		return alias;
 	}
 }
