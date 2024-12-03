6d6d03879632232862d60be8be9c4ac375b13677
Thomas Darimont <thomas.darimont@oidf.org>
2024-12-03 10:26:11 +0100

    Warn on fail for clock skew in nbf for FAPI2 ID2 tests
    Add missing summary.
    
    Fixes #1436
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2EnsureRequestObjectWithNbf8SecondsInTheFutureIsAccepted.java
+++ src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2EnsureRequestObjectWithNbf8SecondsInTheFutureIsAccepted.java
@@ -11,7 +11,7 @@ import net.openid.conformance.variant.VariantNotApplicable;
 @PublishTestModule(
 	testName = "fapi2-security-profile-id2-ensure-request-object-with-nbf-8-seconds-in-the-future-is-accepted",
 	displayName = "FAPI2-Security-Profile-ID2: ensure request object with nbf 8 seconds in the future is accepted",
-	summary = "",
+	summary = "This test ensures the Authorization Server accepts request objects with a nbf claim slightly in the future (e.g., 8 seconds) to account for clock skew. It verifies robustness against minor time discrepancies, avoiding unnecessary request rejections.",
 	profile = "FAPI2-Security-Profile-ID2",
 	configurationFields = {
 		"server.discoveryUrl",
