d76517ac7f5e180c39e2b4169681ab449659eadb
Marcus Almgren <marcus@almgren.pt>
2024-12-05 18:12:21 +0000

    checkstyle

--- src/main/java/net/openid/conformance/openid/federation/ValidateTrustAnchor.java
+++ src/main/java/net/openid/conformance/openid/federation/ValidateTrustAnchor.java
@@ -1,13 +1,8 @@
 package net.openid.conformance.openid.federation;
 
 import com.google.gson.JsonElement;
-import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
-import net.openid.conformance.testmodule.OIDFJSON;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ValidateTrustAnchor extends ValidateUrlRequirements {
 
