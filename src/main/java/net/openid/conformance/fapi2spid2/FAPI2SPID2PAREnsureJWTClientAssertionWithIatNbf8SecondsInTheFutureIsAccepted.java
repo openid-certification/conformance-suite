8833d9cf603f78ff3e7b87ddba8c881a33ea7563
Thomas Darimont <thomas.darimont@oidf.org>
2024-12-11 10:42:02 +0100

    Skip fapi2-security-profile-id2 test execution with PAR if backend doesn't support clock skew
    Fixes #1436
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2PAREnsureJWTClientAssertionWithIatNbf8SecondsInTheFutureIsAccepted.java
+++ src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2PAREnsureJWTClientAssertionWithIatNbf8SecondsInTheFutureIsAccepted.java
@@ -1,11 +1,15 @@
 package net.openid.conformance.fapi2spid2;
 
+import net.openid.conformance.condition.Condition;
 import net.openid.conformance.condition.client.AddIatNbf8SecondsInTheFutureToClientAuthenticationAssertionClaims;
+import net.openid.conformance.condition.client.CallPAREndpoint;
+import net.openid.conformance.condition.client.CheckPAREndpointResponse201WithNoError;
 import net.openid.conformance.condition.client.CreateClientAuthenticationAssertionClaims;
 import net.openid.conformance.sequence.client.CreateJWTClientAuthenticationAssertionAndAddToPAREndpointRequest;
 import net.openid.conformance.testmodule.PublishTestModule;
 import net.openid.conformance.variant.ClientAuthType;
 import net.openid.conformance.variant.VariantNotApplicable;
+import org.springframework.http.HttpStatus;
 
 @PublishTestModule(
 	testName = "fapi2-security-profile-id2-par-ensure-jwt-client-assertions-nbf-8-seconds-in-the-future-is-accepted",
@@ -38,5 +42,20 @@ public class FAPI2SPID2PAREnsureJWTClientAssertionWithIatNbf8SecondsInTheFutureI
 		call(new CreateJWTClientAuthenticationAssertionAndAddToPAREndpointRequest().insertAfter(
 			CreateClientAuthenticationAssertionClaims.class,
 			condition(AddIatNbf8SecondsInTheFutureToClientAuthenticationAssertionClaims.class).requirements("PAR-2", "RFC7519-4.1.5",  "RFC7519-4.1.6")));
+	}
+
+	@Override
+	protected void processParResponse() {
+
+		// if response code is not 201 then skip test
+		Integer status = env.getInteger(CallPAREndpoint.RESPONSE_KEY, "status");
+		if (status != HttpStatus.CREATED.value()) {
+			callAndContinueOnFailure(CheckPAREndpointResponse201WithNoError.class, Condition.ConditionResult.WARNING,"PAR-2.2", "PAR-2.3");
+			eventLog.log(executionManager.getTestId(), "PAR endpoint doesn't seem to support clock skew, finishing test prematurely.");
+			fireTestFinished();
+			return;
+		}
+
+		super.processParResponse();
 	}
 }
