d4c369b3425b7b8b78e5b9d9f4a73d40513fc394
Edmund Jay <ejay@mgi1.com>
2024-12-14 01:54:51 -0800

    eKYC: Update spec links

--- src/main/java/net/openid/conformance/export/LogEntryHelper.java
+++ src/main/java/net/openid/conformance/export/LogEntryHelper.java
@@ -87,7 +87,8 @@ public class LogEntryHelper {
 		specLinks.put("JAR-", "https://www.rfc-editor.org/rfc/rfc9101.html#section-");
 		specLinks.put("SDJWT-", "https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-14.html#section-");
 		specLinks.put("SDJWTVC-", "https://datatracker.ietf.org/doc/draft-ietf-oauth-sd-jwt-vc/#section-");
-		specLinks.put("IA-", "https://openid.net/specs/openid-connect-4-identity-assurance-1_0-ID3.html#section-");
+		specLinks.put("IA-", "https://openid.net/specs/openid-connect-4-identity-assurance-1_0.htm#section-");
+		specLinks.put("IAVC-", "https://openid.net/specs/openid-ida-verified-claims-1_0.htm#section-");
 		specLinks.put("DPOP-", "https://www.rfc-editor.org/rfc/rfc9449#section-");
 		specLinks.put("KSA", "https://ksaob.atlassian.net/wiki/spaces/KS20221101finalerrata1/pages/61014862/API+Security");
 		specLinks.put("CID-SP-", "https://cdn.connectid.com.au/specifications/digitalid-fapi-profile-01.html#section");
