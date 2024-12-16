ac0a34f1634ddd86750fef046ec1512b26947b2e
Edmund Jay <ejay@mgi1.com>
2024-12-15 16:28:10 -0800

    eKYC: modify EnsureIdTokenDoesNotContainVerifiedClaims to also check that an empty claims objec is still valid

--- src/main/java/net/openid/conformance/ekyc/condition/client/EnsureIdTokenDoesNotContainVerifiedClaims.java
+++ src/main/java/net/openid/conformance/ekyc/condition/client/EnsureIdTokenDoesNotContainVerifiedClaims.java
@@ -1,6 +1,7 @@
 package net.openid.conformance.ekyc.condition.client;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
@@ -12,10 +13,15 @@ public class EnsureIdTokenDoesNotContainVerifiedClaims extends AbstractCondition
 	public Environment evaluate(Environment env) {
 		JsonElement verifiedClaimsElement = env.getElementFromObject("id_token", "claims.verified_claims");
 		if(verifiedClaimsElement!=null) {
-			throw error("id_token unexpectedly contains verified_claims",
-				args("verified_claims", verifiedClaimsElement));
+			JsonObject verifiedClaims = verifiedClaimsElement.getAsJsonObject();
+			if(verifiedClaims.has("claims")) { // look in the claims object
+				JsonObject claims = verifiedClaims.getAsJsonObject("claims");
+				if(!claims.isEmpty()) {
+					throw error("id_token unexpectedly contains verified_claims",
+						args("verified_claims", verifiedClaimsElement));
+				}
+			}
 		}
-
 		logSuccess("id_token does not contain verified_claims as expected");
 		return env;
 	}
