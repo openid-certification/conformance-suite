2c41baac18b64deecc1ea8a4935df09de8318539
Alan Smillie <alans@emobix.co.uk>
2024-12-23 14:26:55 +0000

    Commonise processParResponse() into AbstractFAPI1AdvancedFinalInvalidRequestObject

---
+++ src/main/java/net/openid/conformance/fapi1advancedfinal/AbstractFAPI1AdvancedFinalInvalidRequestObject.java
@@ -0,0 +1,22 @@
+package net.openid.conformance.fapi1advancedfinal;
+
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.CallPAREndpoint;
+import net.openid.conformance.condition.client.EnsurePARInvalidRequestObjectError;
+
+public class AbstractFAPI1AdvancedFinalInvalidRequestObject extends AbstractFAPI1AdvancedFinalExpectingAuthorizationEndpointPlaceholderOrCallback {
+
+	@Override
+	protected void processParResponse() {
+		// the server could reject this at the par endpoint, or at the authorization endpoint
+		Integer http_status = env.getInteger(CallPAREndpoint.RESPONSE_KEY, "status");
+		if (http_status >= 200 && http_status < 300) {
+			super.processParResponse();
+			return;
+		}
+
+		callAndContinueOnFailure(EnsurePARInvalidRequestObjectError.class, Condition.ConditionResult.FAILURE, "PAR-2.3");
+
+		fireTestFinished();
+	}
+}
