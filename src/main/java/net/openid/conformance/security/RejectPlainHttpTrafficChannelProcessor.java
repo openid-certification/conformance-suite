82d9106ef3838f5d8f716debbc787cc2f4f89515
Joseph Heenan <joseph.heenan@oidf.org>
2025-02-01 20:56:40 +0000

    Improve logging when incorrectly proxied requests arrive
    When browsing logs trying to diagnose #1458 I noticed some of these
    runtime exceptions being logged - I'm not clear why and without the url
    it's hard to diagnose, so include the url in the exception.

--- src/main/java/net/openid/conformance/security/RejectPlainHttpTrafficChannelProcessor.java
+++ src/main/java/net/openid/conformance/security/RejectPlainHttpTrafficChannelProcessor.java
@@ -16,7 +16,8 @@ public class RejectPlainHttpTrafficChannelProcessor implements ChannelProcessor 
 
 		if (!invocation.getHttpRequest().getScheme().equals("https")) {
 			// It's important that the reverse proxy settings are correct - if we receive a request that appears to be http here, then, e.g., we will send the user to the http version of the login page when they logout.
-			throw new RuntimeException("A non-https request has been received by the conformance suite. The external interface should always use https; if https is in use then there may be a problem with the reverse-proxy apache in front of the suite not setting the X-Forwarded-Proto (etc) http headers correctly.");
+			throw new RuntimeException("A non-https request has been received by the conformance suite. The external interface should always use https; if https is in use then there may be a problem with the reverse-proxy apache in front of the suite not setting the X-Forwarded-Proto (etc) http headers correctly. URL: "+
+				invocation.getHttpRequest().getRequestURI()+"    "+invocation.getHttpRequest().getRequestURL().toString());
 		}
 	}
 
