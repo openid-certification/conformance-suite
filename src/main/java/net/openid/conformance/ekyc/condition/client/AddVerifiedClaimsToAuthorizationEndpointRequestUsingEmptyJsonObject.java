65321736066db9d73636252935b2a8a57a2fb3c6
Edmund Jay <ejay@mgi1.com>
2025-02-04 14:38:41 -0800

    eKYC: rename AddOnlyOneSimpleVerified* classes to AddVerified* to reflect that more than one verified claim may be requested

--- src/main/java/net/openid/conformance/ekyc/condition/client/AddVerifiedClaimsToAuthorizationEndpointRequestUsingEmptyJsonObject.java
+++ src/main/java/net/openid/conformance/ekyc/condition/client/AddVerifiedClaimsToAuthorizationEndpointRequestUsingEmptyJsonObject.java
@@ -3,7 +3,7 @@ package net.openid.conformance.ekyc.condition.client;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-public class AddOnlyOneSimpleVerifiedClaimToAuthorizationEndpointRequestUsingEmptyJsonObject extends AbstractAddOnlyOneSimpleVerifiedClaimToAuthorizationEndpointRequest {
+public class AddVerifiedClaimsToAuthorizationEndpointRequestUsingEmptyJsonObject extends AbstractAddVerifiedClaimsToAuthorizationEndpointRequest {
 	@Override
 	protected JsonElement getClaimValue() {
 		return new JsonObject();
