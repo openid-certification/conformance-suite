5ac015e881955cad8381edb9b9390fed057030c1
Alan Smillie <alans@emobix.co.uk>
2025-02-07 17:03:47 +0000

    Commonise processParResponse() into AbstractFAPI2SPID2PARExpectingAuthorizationEndpointPlaceholderOrCallback

--- src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2EnsureRequestObjectWithoutRedirectUriFails.java
+++ src/main/java/net/openid/conformance/fapi2spid2/FAPI2SPID2EnsureRequestObjectWithoutRedirectUriFails.java
@@ -1,7 +1,6 @@
 package net.openid.conformance.fapi2spid2;
 
 import net.openid.conformance.condition.Condition;
-import net.openid.conformance.condition.client.CallPAREndpoint;
 import net.openid.conformance.condition.client.CheckErrorFromAuthorizationEndpointErrorInvalidRequestOrInvalidRequestObject;
 import net.openid.conformance.condition.client.CheckForUnexpectedParametersInErrorResponseFromAuthorizationEndpoint;
 import net.openid.conformance.condition.client.CheckStateInAuthorizationResponse;
@@ -35,7 +34,7 @@ import net.openid.conformance.testmodule.PublishTestModule;
 		"resource.resourceUrl"
 	}
 )
-public class FAPI2SPID2EnsureRequestObjectWithoutRedirectUriFails extends AbstractFAPI2SPID2ExpectingAuthorizationEndpointPlaceholderOrCallback {
+public class FAPI2SPID2EnsureRequestObjectWithoutRedirectUriFails extends AbstractFAPI2SPID2PARExpectingAuthorizationEndpointPlaceholderOrCallback {
 
 	@Override
 	protected void createPlaceholder() {
@@ -58,17 +57,8 @@ public class FAPI2SPID2EnsureRequestObjectWithoutRedirectUriFails extends Abstra
 	}
 
 	@Override
-	protected void processParResponse() {
-		// the server could reject this at the par endpoint, or at the authorization endpoint
-		Integer http_status = env.getInteger(CallPAREndpoint.RESPONSE_KEY, "status");
-		if (http_status >= 200 && http_status < 300) {
-			super.processParResponse();
-			return;
-		}
-
+	protected void processParErrorResponse() {
 		callAndContinueOnFailure(EnsurePARInvalidRequestOrInvalidRequestObjectError.class, Condition.ConditionResult.FAILURE, "JAR-6.2");
-
-		fireTestFinished();
 	}
 
 	@Override
