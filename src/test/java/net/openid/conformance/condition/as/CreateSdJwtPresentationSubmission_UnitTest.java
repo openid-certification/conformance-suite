5546a3c26051a896ace8cdb86c97cfefe9cd5d48
Joseph Heenan <joseph.heenan@oidf.org>
2025-02-11 07:50:34 +0000

    OID4VP: Fix AddVpTokenToAuthorizationEndpoint unit test
    A previous refactor had moved the code in this condition to two separate
    conditions for the sdjwt & presentation_submission creation, but had
    left the unit test behind in a pretty useless state. Use that unittest
    to create unittests for the two new conditions.

--- src/test/java/net/openid/conformance/condition/as/CreateSdJwtPresentationSubmission_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/as/CreateSdJwtPresentationSubmission_UnitTest.java
@@ -2,7 +2,6 @@ package net.openid.conformance.condition.as;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import net.openid.conformance.condition.Condition.ConditionResult;
 import net.openid.conformance.logging.TestInstanceEventLog;
 import net.openid.conformance.testmodule.Environment;
@@ -14,7 +13,7 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class AddPeVpTokenToAuthorizationEndpointResponseParams_UnitTest {
+public class CreateSdJwtPresentationSubmission_UnitTest {
 
 	@Spy
 	private Environment env = new Environment();
@@ -22,11 +21,11 @@ public class AddPeVpTokenToAuthorizationEndpointResponseParams_UnitTest {
 	@Mock
 	private TestInstanceEventLog eventLog;
 
-	private AddPeVpTokenToAuthorizationEndpointResponseParams cond;
+	private CreateSdJwtPresentationSubmission cond;
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		cond = new AddPeVpTokenToAuthorizationEndpointResponseParams();
+		cond = new CreateSdJwtPresentationSubmission();
 		cond.setProperties("UNIT-TEST", eventLog, ConditionResult.INFO);
 	}
 
@@ -98,22 +97,7 @@ public class AddPeVpTokenToAuthorizationEndpointResponseParams_UnitTest {
 			      }
 			    ]
 			  }""";
-		env.putObject(CreateAuthorizationEndpointResponseParams.ENV_KEY, new JsonObject());
 		env.putObjectFromJsonString("authorization_request_object", "claims.presentation_definition", pd);
-		String key = "{\n" +
-				"    \"kty\": \"EC\",\n" +
-				"    \"d\": \"y2NSNIvlRAEBMFk2bjQcSKbjS1y_NBJQ6jRzIfuIxS0\",\n" +
-				"    \"use\": \"sig\",\n" +
-				"    \"x5c\": [\n" +
-				"        \"MIIB+DCCAZ6gAwIBAgIUSy80Ezru1eOPrGW88uSFC8H8lVYwCgYIKoZIzj0EAwIwITELMAkGA1UEBhMCR0IxEjAQBgNVBAMMCU9JREYgVGVzdDAeFw0yNDExMTkwOTMwMzNaFw0zNDExMTcwOTMwMzNaMCExCzAJBgNVBAYTAkdCMRIwEAYDVQQDDAlPSURGIFRlc3QwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATT/dLsd51LLBrGV6R23o6vymRxHXeFBoI8yq31y5kFV2VV0gi9x5ZzEFiq8DMiAHucLACFndxLtZorCha9zznQo4GzMIGwMB0GA1UdDgQWBBS5cbdgAeMBi5wxpbpwISGhShAWETAfBgNVHSMEGDAWgBS5cbdgAeMBi5wxpbpwISGhShAWETAPBgNVHRMBAf8EBTADAQH/MF0GA1UdEQRWMFSCEHd3dy5oZWVuYW4ubWUudWuCHWRlbW8uY2VydGlmaWNhdGlvbi5vcGVuaWQubmV0gglsb2NhbGhvc3SCFmxvY2FsaG9zdC5lbW9iaXguY28udWswCgYIKoZIzj0EAwIDSAAwRQIhAPQtPciRiOPkw4ZMfmP1ov3LXlhG8wizrJ9Oyu+QPWAEAiBJn30EEuhhFyS7nqOhZok+M0XNbbxhNB0i7KxKSEsITA==\"\n" +
-				"    ],\n" +
-				"    \"crv\": \"P-256\",\n" +
-				"    \"kid\": \"5H1WLeSx55tMW6JNlvqMfg3O_E0eQPqB8jDSoUn6oiI\",\n" +
-				"    \"x\": \"0_3S7HedSywaxlekdt6Or8pkcR13hQaCPMqt9cuZBVc\",\n" +
-				"    \"y\": \"ZVXSCL3HlnMQWKrwMyIAe5wsAIWd3Eu1misKFr3POdA\",\n" +
-				"    \"alg\": \"ES256\"\n" +
-				"}";
-		env.putObjectFromJsonString("config", "credential.signing_jwk", key);
 
 		cond.execute(env);
 
