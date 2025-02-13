13500f0a45fc85236997fade71c65f1d5fc3a874
Joseph Heenan <joseph.heenan@oidf.org>
2025-02-13 14:07:28 +0000

    Remove some unnecessary @Spy
    As spotted by Thomas here:
    
    https://gitlab.com/openid/conformance-suite/-/merge_requests/1587#note_2346183136

--- src/test/java/net/openid/conformance/condition/client/ExtractVpTokenDCQL_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/client/ExtractVpTokenDCQL_UnitTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ExtendWith(MockitoExtension.class)
 public class ExtractVpTokenDCQL_UnitTest {
 
-	@Spy
 	private Environment env = new Environment();
 
 	@Mock
