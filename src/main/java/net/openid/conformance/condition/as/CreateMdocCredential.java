e109edfc4b60b3eebe4c3ee7fe5cf6671e1a5845
Alan Smillie <alans@emobix.co.uk>
2025-02-20 10:57:06 +0000

    VP OP Tests: Include All Available Credentials In VP Response
    Update CreateMdocCredential to call generateEncodedDeviceResponse() with
    a 'consentFields' list containing all available credentials from the
    test document.

--- src/main/java/net/openid/conformance/condition/as/CreateMdocCredential.java
+++ src/main/java/net/openid/conformance/condition/as/CreateMdocCredential.java
@@ -3,17 +3,55 @@ package net.openid.conformance.condition.as;
 import com.android.identity.appsupport.ui.consent.MdocConsentField;
 import com.android.identity.cbor.Cbor;
 import com.android.identity.cbor.DiagnosticOption;
+import com.android.identity.document.NameSpacedData;
+import com.android.identity.documenttype.knowntypes.DrivingLicense;
+import com.android.identity.mdoc.credential.MdocCredential;
 import com.android.identity.testapp.TestAppUtils;
+import com.android.identity.util.ApplicationData;
 import com.nimbusds.jose.util.Base64URL;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import kotlin.Pair;
+
 public class CreateMdocCredential extends AbstractMdocSessionTranscript {
+
+	private List<MdocConsentField> createConsentList(MdocCredential mdocCredential) {
+		// A map from namespace into a list of data elements where each pair is the data element name and whether the data element will be retained.
+		Map<String, List<Pair<String, Boolean>>> requestedData = new HashMap<>();
+
+		ApplicationData applicationData = mdocCredential.getDocument().getApplicationData();
+
+		if (! applicationData.keyExists("documentData")) {
+			return new ArrayList<MdocConsentField>();
+		}
+
+		NameSpacedData documentData = applicationData.getNameSpacedData("documentData");
+
+		for (String namespace: documentData.getNameSpaceNames()) {
+			// List of credentials within the current namespace.
+			ArrayList<Pair<String, Boolean>> credentials = new ArrayList<>();
+
+			for (String credential: documentData.getDataElementNames(namespace)) {
+				Pair<String, Boolean> credentialPair = new Pair<>(credential, true);
+				credentials.add(credentialPair);
+			}
+
+			requestedData.put(namespace, credentials);
+		}
+
+		return MdocConsentField.Companion.generateConsentFields(DrivingLicense.INSTANCE.getDocumentType().getMdocDocumentType().getDocType(),
+									requestedData,
+									TestAppUtils.documentTypeRepository,
+									mdocCredential);
+	}
 
 	@Override
 	@PostEnvironment(strings = "credential")
@@ -29,11 +67,13 @@ public class CreateMdocCredential extends AbstractMdocSessionTranscript {
 
 		byte[] sessionTranscript = createSessionTranscript(clientId, responseUri, nonce, mdocGeneratedNonce);
 
-		// This will result in an mdoc without any fields (no name/etc). We should perhaps attempt to parse the PE/DCQL.
-		List<MdocConsentField> mdocConsentFields = new ArrayList<>();
+		// Ensure the test document is provisioned.
+		TestAppUtils testAppUtils = TestAppUtils.INSTANCE;
 
-		byte[] mdoc = TestAppUtils.INSTANCE.generateEncodedDeviceResponse(mdocConsentFields, sessionTranscript);
+		// Create a list of consent fields containded in the credential.
+		List<MdocConsentField> mdocConsentFields = createConsentList(TestAppUtils.mdocCredential);
 
+		byte[] mdoc = testAppUtils.generateEncodedDeviceResponse(mdocConsentFields, sessionTranscript);
 		env.putString("credential", Base64URL.encode(mdoc).toString());
 
 		String diagnostics = Cbor.INSTANCE.toDiagnostics(mdoc,
