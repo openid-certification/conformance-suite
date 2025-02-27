0d4aa07c5e0bbf47f53479fe5b0b2f7418a71d81
Thomas Darimont <thomas.darimont@oidf.org>
2025-02-27 10:38:16 +0000

    Ease extensibility of CheckDiscEndpointIssuer
    Fixes #1486
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/condition/client/CheckDiscEndpointIssuer.java
+++ src/main/java/net/openid/conformance/condition/client/CheckDiscEndpointIssuer.java
@@ -2,10 +2,10 @@ package net.openid.conformance.condition.client;
 
 import com.google.gson.JsonElement;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.ConditionError;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.testmodule.Environment;
 import net.openid.conformance.testmodule.OIDFJSON;
-import org.jetbrains.annotations.NotNull;
 
 public class CheckDiscEndpointIssuer extends AbstractCondition {
 
@@ -24,6 +24,20 @@ public class CheckDiscEndpointIssuer extends AbstractCondition {
 
 		String issuerUrl = OIDFJSON.getString(issuerElement);
 
+		String discoveryUrl = getExpectedIssuerUrl(env);
+
+		//Remove slash character endpoint url before comparing
+		if (!removeSlashEndpointURL(issuerUrl).equals(removeSlashEndpointURL(discoveryUrl))) {
+
+			throw createIssuerMismatchError(env, issuerUrl, discoveryUrl);
+		}
+
+		logSuccess("issuer is consistent with the " + endpointLabel + " endpoint", args("issuer", issuerUrl));
+
+		return env;
+	}
+
+	protected String getExpectedIssuerUrl(Environment env) {
 		String discoveryUrl = getConfigurationUrl(env);
 
 		final String removingPartInUrl = getConfigurationEndpoint();
@@ -31,16 +45,11 @@ public class CheckDiscEndpointIssuer extends AbstractCondition {
 
 			discoveryUrl = discoveryUrl.substring(0, discoveryUrl.length() - removingPartInUrl.length());
 		}
+		return discoveryUrl;
+	}
 
-		//Remove slash character endpoint url before comparing
-		if (!removeSlashEndpointURL(issuerUrl).equals(removeSlashEndpointURL(discoveryUrl))) {
-
-			throw error("issuer listed in the discovery document is not consistent with the location the discovery document was retrieved from. These must match to prevent impersonation attacks.", args("discovery_url", discoveryUrl, "issuer", issuerUrl));
-		}
-
-		logSuccess("issuer is consistent with the " + endpointLabel + " endpoint");
-
-		return env;
+	protected ConditionError createIssuerMismatchError(Environment env, String issuerUrl, String discoveryUrl) {
+		return error("issuer listed in the discovery document is not consistent with the location the discovery document was retrieved from. These must match to prevent impersonation attacks.", args("discovery_url", discoveryUrl, "issuer", issuerUrl));
 	}
 
 	protected String getEndpointLabel() {
@@ -55,12 +64,11 @@ public class CheckDiscEndpointIssuer extends AbstractCondition {
 		return env.getElementFromObject("server", "issuer");
 	}
 
-	@NotNull
 	protected String getConfigurationEndpoint() {
 		return ".well-known/openid-configuration";
 	}
 
-	private String removeSlashEndpointURL(String url) {
+	protected String removeSlashEndpointURL(String url) {
 		if (url.endsWith("/")) {
 
 			return url.substring(0, url.length() - 1);
