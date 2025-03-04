66ff874aaeaeb7e0b0ca9a3ef58c97687a10b770
Marcus Almgren <marcus@almgren.pt>
2025-03-04 17:43:23 +0000

    work

---
+++ src/main/java/net/openid/conformance/openid/federation/client/OpenIDFederationClientRegistrationRPTestPlan.java
@@ -0,0 +1,22 @@
+package net.openid.conformance.openid.federation.client;
+
+import net.openid.conformance.openid.federation.OpenIDFederationEntityConfigurationTest;
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.variant.VariantSelection;
+
+@PublishTestPlan(
+	testPlanName = "openid-federation-client-registration-rptest-plan",
+	displayName = "OpenID Federation: Client Registration RP test (alpha - INCOMPLETE/INCORRECT, please email certification team if interested)",
+	profile = TestPlan.ProfileNames.federationTest,
+	testModules = {
+		OpenIDFederationClientHappyPathTest.class,
+	}
+)
+public class OpenIDFederationClientRegistrationRPTestPlan implements TestPlan {
+
+	public static String certificationProfileName(VariantSelection variant) {
+		return "OpenID Federation Client Registration RP";
+	}
+
+}
