1e2410408ab46602b325a60948b860eb6379f29f
Marcus Almgren <marcus@almgren.pt>
2025-03-13 15:08:36 +0000

    [ci skip]

--- src/main/java/net/openid/conformance/openid/federation/client/AddMetadataToEntityConfiguration.java
+++ src/main/java/net/openid/conformance/openid/federation/client/AddMetadataToEntityConfiguration.java
@@ -1,6 +1,7 @@
 package net.openid.conformance.openid.federation.client;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
@@ -10,15 +11,25 @@ import net.openid.conformance.testmodule.OIDFJSON;
 public class AddMetadataToEntityConfiguration extends AbstractCondition {
 
 	@Override
-	@PreEnvironment(required = "server")
+	@PreEnvironment(required = "server", strings = {"base_url"})
 	public Environment evaluate(Environment env) {
 
+		String baseUrl = env.getString("base_url");
 		JsonObject server = env.getObject("server");
 
 		JsonObject metadata = new JsonObject();
 
 		JsonObject federationEntity = new JsonObject();
 		metadata.add("federation_entity", federationEntity);
+
+		JsonElement immediateSubordinatesElement = env.getElementFromObject("config", "federation.immediate_subordinates");
+		if (immediateSubordinatesElement != null) {
+			if (!immediateSubordinatesElement.isJsonArray()) {
+				throw error("immediate_subordinates must be an array of strings");
+			}
+			federationEntity.addProperty("federation_fetch_endpoint", baseUrl + "/fetch");
+			federationEntity.addProperty("federation_list_endpoint", baseUrl + "/list");
+		}
 
 		JsonObject openIdProvider = new JsonObject();
 		metadata.add("openid_provider", openIdProvider);
