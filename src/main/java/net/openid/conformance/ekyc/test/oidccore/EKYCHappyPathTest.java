6781459930c4c5413a3efcc43b512d83d54a1172
Edmund Jay <ejay@mgi1.com>
2025-03-14 14:14:21 -0700

    eKYC: add input configuration for additional request and response validation schemas

--- src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCHappyPathTest.java
+++ src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCHappyPathTest.java
@@ -26,7 +26,9 @@ import net.openid.conformance.testmodule.PublishTestModule;
 		"trust_framework",
 		"verified_claim_names",
 		"ekyc.unverified_claims_names",
-		"ekyc.verified_claims_names"
+		"ekyc.verified_claims_names",
+		"ekyc.request_schemas",
+		"ekyc.response_schemas"
 	}
 )
 public class EKYCHappyPathTest extends AbstractEKYCTestWithOIDCCore {
