97ded4d182f847d68cd14c041b83afb8e0002f6f
Edmund Jay <ejay@mgi1.com>
2025-03-14 14:14:21 -0700

    eKYC: add input configuration for additional request and response validation schemas

--- src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCRequestClaimWithRandomValueMustBeOmitted.java
+++ src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCRequestClaimWithRandomValueMustBeOmitted.java
@@ -19,7 +19,9 @@ import net.openid.conformance.testmodule.PublishTestModule;
 	profile = "OIDCC",
 	configurationFields = {
 		"ekyc.unverified_claims_names",
-		"ekyc.verified_claims_names"
+		"ekyc.verified_claims_names",
+		"ekyc.request_schemas",
+		"ekyc.response_schemas"
 	}
 )
 public class EKYCRequestClaimWithRandomValueMustBeOmitted extends AbstractEKYCTestWithOIDCCore {
