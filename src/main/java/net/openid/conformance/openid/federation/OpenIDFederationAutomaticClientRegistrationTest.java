a57b46712ec7d18f1dba5c57ce5cb47a7431a8ab
Marcus Almgren <marcus@almgren.pt>
2025-03-19 15:27:58 +0000

    Add openid_provider and openid_relying_party

--- src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationTest.java
+++ src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationTest.java
@@ -1,12 +1,22 @@
 package net.openid.conformance.openid.federation;
 
 import com.google.gson.JsonObject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.as.SignIdToken;
 import net.openid.conformance.condition.client.ExtractJWKsFromStaticClientConfiguration;
 import net.openid.conformance.condition.client.GetStaticClientConfiguration;
 import net.openid.conformance.condition.client.SignRequestObject;
 import net.openid.conformance.condition.client.ValidateClientJWKsPrivatePart;
+import net.openid.conformance.openid.federation.client.GenerateEntityConfiguration;
 import net.openid.conformance.testmodule.PublishTestModule;
+import net.openid.conformance.testmodule.TestFailureException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @PublishTestModule(
 		testName = "openid-federation-automatic-client-registration",
@@ -25,12 +35,26 @@ public class OpenIDFederationAutomaticClientRegistrationTest extends AbstractOpe
 	@Override
 	public void additionalConfiguration() {
 		eventLog.startBlock("Additional configuration");
+
+		String baseUrl = env.getString("base_url");
+
 		JsonObject clientConfig = env.getElementFromObject("config", "client").getAsJsonObject();
-		clientConfig.addProperty("client_id", env.getString("base_url"));
+		clientConfig.addProperty("client_id", baseUrl);
+
+		callAndStopOnFailure(GenerateEntityConfiguration.class);
+		callAndStopOnFailure(AddFederationEntityMetadataToEntityConfiguration.class);
+		callAndStopOnFailure(AddOpenIDRelyingPartyMetadataToEntityConfiguration.class);
 
 		callAndStopOnFailure(GetStaticClientConfiguration.class);
 		callAndStopOnFailure(ValidateClientJWKsPrivatePart.class, "RFC7517-1.1");
 		callAndStopOnFailure(ExtractJWKsFromStaticClientConfiguration.class);
+
+		env.putString("entity_identifier", baseUrl);
+		exposeEnvString("entity_identifier");
+
+		env.putString("entity_configuration_url", baseUrl + "/.well-known/openid-federation");
+		exposeEnvString("entity_configuration_url");
+
 		eventLog.endBlock();
 	}
 
@@ -40,6 +64,8 @@ public class OpenIDFederationAutomaticClientRegistrationTest extends AbstractOpe
 
 		callAndContinueOnFailure(CreateRequestObjectClaims.class, Condition.ConditionResult.FAILURE);
 		callAndContinueOnFailure(SignRequestObject.class, Condition.ConditionResult.FAILURE);
+		callAndContinueOnFailure(EncryptRequestObject.class, Condition.ConditionResult.FAILURE);
+
 		callAndContinueOnFailure(CallAuthorizationEndpointWithGetAndReturnFullResponse.class, Condition.ConditionResult.FAILURE);
 		//callAndContinueOnFailure(CallAuthorizationEndpointWithPostAndReturnFullResponse.class, Condition.ConditionResult.FAILURE);
 
@@ -47,4 +73,46 @@ public class OpenIDFederationAutomaticClientRegistrationTest extends AbstractOpe
 		//fireTestFinished();
 	}
 
+	@Override
+	public Object handleHttp(String path, HttpServletRequest req, HttpServletResponse res, HttpSession session, JsonObject requestParts) {
+		String requestId = "incoming_request_" + RandomStringUtils.randomAlphanumeric(37);
+		env.putObject(requestId, requestParts);
+		return switch (path) {
+			case ".well-known/openid-federation" -> entityConfigurationResponse();
+			case "jwks" -> clientJwksResponse();
+			default ->
+				throw new TestFailureException(getId(), "Got an HTTP request to '" + path + "' that wasn't expected");
+		};
+	}
+
+	protected Object entityConfigurationResponse() {
+		setStatus(Status.RUNNING);
+
+		env.mapKey("id_token_claims", "server");
+		env.mapKey("server_jwks", "client_jwks");
+		callAndStopOnFailure(SignIdToken.class);
+		env.unmapKey("server_jwks");
+		env.unmapKey("id_token_claims");
+		String entityConfiguration = env.getString("id_token");
+
+		setStatus(Status.WAITING);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.contentType(EntityUtils.ENTITY_STATEMENT_JWT)
+			.body(entityConfiguration);
+	}
+
+	protected Object clientJwksResponse() {
+		setStatus(Status.RUNNING);
+
+		JsonObject jwks = env.getObject("client_public_jwks");
+
+		setStatus(Status.WAITING);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(jwks);
+	}
 }
