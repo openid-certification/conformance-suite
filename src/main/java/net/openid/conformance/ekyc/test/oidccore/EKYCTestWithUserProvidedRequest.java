c3e22a8070bddba890763e4c08cea7e8a85825f6
Edmund Jay <ejay@mgi1.com>
2025-03-28 19:08:04 -0700

    eKYC: modify EKYCTestWithUserProvidedRequest to work with list of user provided request data instead of a single item. The test will perform the full authorization flow for each request data.

--- src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCTestWithUserProvidedRequest.java
+++ src/main/java/net/openid/conformance/ekyc/test/oidccore/EKYCTestWithUserProvidedRequest.java
@@ -1,9 +1,12 @@
 package net.openid.conformance.ekyc.test.oidccore;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import net.openid.conformance.condition.Condition;
 import net.openid.conformance.ekyc.condition.client.AddUserProvidedClaimsRequestToAuthorizationEndpointRequest;
+import net.openid.conformance.testmodule.OIDFJSON;
 import net.openid.conformance.testmodule.PublishTestModule;
+import net.openid.conformance.testmodule.TestFailureException;
 
 @PublishTestModule(
 	testName = "ekyc-server-testuserprovidedrequest",
@@ -12,7 +15,7 @@ import net.openid.conformance.testmodule.PublishTestModule;
 		"and expects a happy path flow, i.e the request must succeed.",
 	profile = "OIDCC",
 	configurationFields = {
-		"ekyc.verified_claims_request",
+		"ekyc.verified_claims_request_list",
 		"ekyc.unverified_claims_names",
 		"ekyc.verified_claims_names",
 		"ekyc.request_schemas",
@@ -20,6 +23,44 @@ import net.openid.conformance.testmodule.PublishTestModule;
 	}
 )
 public class EKYCTestWithUserProvidedRequest extends AbstractEKYCTestWithOIDCCore {
+
+	private JsonArray requestList;
+
+	@Override
+	public void start() {
+		// fetch request list which can be single object or list (in which case, we will perform the flow multiple times)
+		// stuff the config into an array if it's a single object
+		this.requestList = OIDFJSON.packJsonElementIntoJsonArray(env.getElementFromObject("config", "ekyc.verified_claims_request_list"));
+		if(requestList.isEmpty()) { // make sure list is not empty
+			throw new TestFailureException(getId(), "ekyc.verified_claims_request_list is not configured");
+		}
+		super.start();
+	}
+
+	@Override
+	protected void performAuthorizationFlow() {
+		// perform authorization flow using requestList as a queue
+		if(!requestList.isEmpty()) {
+			JsonElement requestElement = requestList.remove(0); // dequeue and save the first item
+			if(requestElement.isJsonObject()) {  // each element must be a JSON object
+				env.putObject("config", "ekyc.verified_claims_request", requestElement.getAsJsonObject());
+				super.performAuthorizationFlow();
+			} else {
+				throw new TestFailureException(getId(), "ekyc.verified_claims_request items must be JSON objects");
+			}
+		}
+	}
+
+	@Override
+	protected void onPostAuthorizationFlowComplete() {
+		if(this.requestList.isEmpty()) {
+			// end the test
+			super.onPostAuthorizationFlowComplete();
+		} else {
+			// perform flow again
+			performAuthorizationFlow();
+		}
+	}
 
 	@Override
 	protected void addVerifiedClaimsToAuthorizationRequest() {
