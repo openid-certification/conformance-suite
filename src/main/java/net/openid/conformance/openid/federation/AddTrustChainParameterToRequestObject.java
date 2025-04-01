dcf3b24acce6917e248999ce1dc0d52abc6bf787
Marcus Almgren <marcus@almgren.pt>
2025-04-01 18:27:42 +0100

    [ci skip]

---
+++ src/main/java/net/openid/conformance/openid/federation/AddTrustChainParameterToRequestObject.java
@@ -0,0 +1,29 @@
+package net.openid.conformance.openid.federation;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class AddTrustChainParameterToRequestObject extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = { "config", "request_object_claims" })
+	public Environment evaluate(Environment env) {
+
+		JsonElement trustChainElement = env.getElementFromObject("config", "client.trust_chain");
+		JsonArray trustChain = trustChainElement.getAsJsonObject().getAsJsonArray("trust_chain");
+		env.putArray("request_object_claims", "trust_chain", trustChain);
+
+		logSuccess("Added trust chain to request object");
+
+		return env;
+	}
+
+}
