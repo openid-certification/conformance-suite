cfd525bcb200e6798e7c65e143b49333264c0810
Thomas Darimont <thomas.darimont@oidf.org>
2025-04-02 11:43:15 +0200

    Refactor AbstractSchemaBasedMetadataValidation
    Revise names
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vciid2issuer/condition/VCICredentialIssuerMetadataValidation.java
+++ src/main/java/net/openid/conformance/vciid2issuer/condition/VCICredentialIssuerMetadataValidation.java
@@ -3,16 +3,16 @@ package net.openid.conformance.vciid2issuer.condition;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
-import net.openid.conformance.vciid2issuer.util.MetadataValidationInput;
+import net.openid.conformance.vciid2issuer.util.JsonSchemaValidationInput;
 
-public class VCICredentialIssuerMetadataValidation extends AbstractSchemaBasedMetadataValidation {
+public class VCICredentialIssuerMetadataValidation extends AbstractJsonSchemaBasedValidation {
 
 	@Override
-	protected MetadataValidationInput createMetadataValidationInput(Environment env) {
+	protected JsonSchemaValidationInput createJsonSchemaValidationInput(Environment env) {
 		JsonObject metadata = env.getElementFromObject("vci", "credential_issuer_metadata").getAsJsonObject();
 		String schemaResource = "json-schemas/oid4vci/credential_issuer_metadata_ID2_15.json";
-		String metadataName = "OID4VCI Credential Issuer";
-		return new MetadataValidationInput(metadataName, schemaResource, metadata);
+		String metadataName = "OID4VCI Credential Issuer metadata";
+		return new JsonSchemaValidationInput(metadataName, schemaResource, metadata);
 	}
 
 	@Override
