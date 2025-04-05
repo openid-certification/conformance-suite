2320c6761df4a2d32bdedc4fb7324fd832a4c9b0
Joseph Heenan <joseph.heenan@oidf.org>
2025-04-05 22:31:40 +0100

    OID4VCI Issuer: Extract/validate credential response

--- src/main/java/net/openid/conformance/vciid2issuer/condition/VCIValidateNoUnknownKeysInCredentialResponse.java
+++ src/main/java/net/openid/conformance/vciid2issuer/condition/VCIValidateNoUnknownKeysInCredentialResponse.java
@@ -4,22 +4,28 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 
+import java.util.Set;
+
 /**
  * See: 8.3. Credential Response https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-8.3
  */
-public class VCIValidateCredentialResponse extends AbstractCondition {
+public class VCIValidateNoUnknownKeysInCredentialResponse extends AbstractCondition {
 
 	@SuppressWarnings("unused")
 	@Override
 	public Environment evaluate(Environment env) {
 
-		JsonObject endpointResponse = env.getObject("endpoint_response").getAsJsonObject();
-
 		JsonObject credentialResponseBodyJson = env.getElementFromObject("endpoint_response", "body_json").getAsJsonObject();
+		Set<String> expected = Set.of("credentials", "transaction_id", "notification_id");
 
-		// TODO validate credential response
+		Set<String> unexpected = credentialResponseBodyJson.keySet();
+		unexpected.removeAll(expected);
 
-		logSuccess("Extracted credential response", args("credential", credentialResponseBodyJson));
+		if (!unexpected.isEmpty()) {
+			throw error("Unexpected keys in credential response", args("unexpected", unexpected, "expected", expected));
+		}
+
+		logSuccess("No unexpected keys in credential response", args("expected", expected));
 
 		return env;
 	}
