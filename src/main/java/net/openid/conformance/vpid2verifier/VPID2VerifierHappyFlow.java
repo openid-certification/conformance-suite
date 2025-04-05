553c9986375f7cfba415e0f758311265b7777dca
Joseph Heenan <joseph.heenan@oidf.org>
2025-04-05 14:47:38 +0100

    OID4VP Verifier: Try and better explain how the tests work

--- src/main/java/net/openid/conformance/vpid2verifier/VPID2VerifierHappyFlow.java
+++ src/main/java/net/openid/conformance/vpid2verifier/VPID2VerifierHappyFlow.java
@@ -12,7 +12,7 @@ import net.openid.conformance.testmodule.PublishTestModule;
 @PublishTestModule(
 	testName = "oid4vp-id2-verifier-happy-flow",
 	displayName = "OID4VPID2 Verifier: Happy flow test ",
-	summary = "Expects the verifier to make a valid OID4VP request that matches the configuration, creates and returns an SD-JWT VC credential using the https://example.bmi.bund.de/credential/pid/1.0 VCT.\n\nThe presentation_definition must contain only one input_descriptor.",
+	summary = "Expects the verifier to make a valid OID4VP request that matches the configuration, creates and returns an SD-JWT VC credential using the https://example.bmi.bund.de/credential/pid/1.0 VCT.\n\nThe presentation_definition must contain only one input_descriptor.\n\nThe conformance suite acts as a mock web wallet. You must configure your verifier to use the authorization endpoint url below instead of 'openid4vp://' and then start the flow in your verifier as normal.",
 	profile = "OID4VP-ID2",
 	configurationFields = {
 		"client.client_id",
