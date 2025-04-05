ccbd63d3f0543af2668e5ca37332bd43ed4f7e16
Joseph Heenan <joseph.heenan@oidf.org>
2025-04-05 16:45:30 +0100

    Refactor: replace string concatentation with text block

--- src/test/java/net/openid/conformance/condition/as/CreateSdJwtKbCredential_UnitTest.java
+++ src/test/java/net/openid/conformance/condition/as/CreateSdJwtKbCredential_UnitTest.java
@@ -31,19 +31,20 @@ public class CreateSdJwtKbCredential_UnitTest {
 	@Test
 	public void testEvaluate_noError() {
 		env.putObject(CreateAuthorizationEndpointResponseParams.ENV_KEY, new JsonObject());
-		String key = "{\n" +
-				"    \"kty\": \"EC\",\n" +
-				"    \"d\": \"y2NSNIvlRAEBMFk2bjQcSKbjS1y_NBJQ6jRzIfuIxS0\",\n" +
-				"    \"use\": \"sig\",\n" +
-				"    \"x5c\": [\n" +
-				"        \"MIIB+DCCAZ6gAwIBAgIUSy80Ezru1eOPrGW88uSFC8H8lVYwCgYIKoZIzj0EAwIwITELMAkGA1UEBhMCR0IxEjAQBgNVBAMMCU9JREYgVGVzdDAeFw0yNDExMTkwOTMwMzNaFw0zNDExMTcwOTMwMzNaMCExCzAJBgNVBAYTAkdCMRIwEAYDVQQDDAlPSURGIFRlc3QwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATT/dLsd51LLBrGV6R23o6vymRxHXeFBoI8yq31y5kFV2VV0gi9x5ZzEFiq8DMiAHucLACFndxLtZorCha9zznQo4GzMIGwMB0GA1UdDgQWBBS5cbdgAeMBi5wxpbpwISGhShAWETAfBgNVHSMEGDAWgBS5cbdgAeMBi5wxpbpwISGhShAWETAPBgNVHRMBAf8EBTADAQH/MF0GA1UdEQRWMFSCEHd3dy5oZWVuYW4ubWUudWuCHWRlbW8uY2VydGlmaWNhdGlvbi5vcGVuaWQubmV0gglsb2NhbGhvc3SCFmxvY2FsaG9zdC5lbW9iaXguY28udWswCgYIKoZIzj0EAwIDSAAwRQIhAPQtPciRiOPkw4ZMfmP1ov3LXlhG8wizrJ9Oyu+QPWAEAiBJn30EEuhhFyS7nqOhZok+M0XNbbxhNB0i7KxKSEsITA==\"\n" +
-				"    ],\n" +
-				"    \"crv\": \"P-256\",\n" +
-				"    \"kid\": \"5H1WLeSx55tMW6JNlvqMfg3O_E0eQPqB8jDSoUn6oiI\",\n" +
-				"    \"x\": \"0_3S7HedSywaxlekdt6Or8pkcR13hQaCPMqt9cuZBVc\",\n" +
-				"    \"y\": \"ZVXSCL3HlnMQWKrwMyIAe5wsAIWd3Eu1misKFr3POdA\",\n" +
-				"    \"alg\": \"ES256\"\n" +
-				"}";
+		String key = """
+			{
+			    "kty": "EC",
+			    "d": "y2NSNIvlRAEBMFk2bjQcSKbjS1y_NBJQ6jRzIfuIxS0",
+			    "use": "sig",
+			    "x5c": [
+			        "MIIB+DCCAZ6gAwIBAgIUSy80Ezru1eOPrGW88uSFC8H8lVYwCgYIKoZIzj0EAwIwITELMAkGA1UEBhMCR0IxEjAQBgNVBAMMCU9JREYgVGVzdDAeFw0yNDExMTkwOTMwMzNaFw0zNDExMTcwOTMwMzNaMCExCzAJBgNVBAYTAkdCMRIwEAYDVQQDDAlPSURGIFRlc3QwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATT/dLsd51LLBrGV6R23o6vymRxHXeFBoI8yq31y5kFV2VV0gi9x5ZzEFiq8DMiAHucLACFndxLtZorCha9zznQo4GzMIGwMB0GA1UdDgQWBBS5cbdgAeMBi5wxpbpwISGhShAWETAfBgNVHSMEGDAWgBS5cbdgAeMBi5wxpbpwISGhShAWETAPBgNVHRMBAf8EBTADAQH/MF0GA1UdEQRWMFSCEHd3dy5oZWVuYW4ubWUudWuCHWRlbW8uY2VydGlmaWNhdGlvbi5vcGVuaWQubmV0gglsb2NhbGhvc3SCFmxvY2FsaG9zdC5lbW9iaXguY28udWswCgYIKoZIzj0EAwIDSAAwRQIhAPQtPciRiOPkw4ZMfmP1ov3LXlhG8wizrJ9Oyu+QPWAEAiBJn30EEuhhFyS7nqOhZok+M0XNbbxhNB0i7KxKSEsITA=="
+			    ],
+			    "crv": "P-256",
+			    "kid": "5H1WLeSx55tMW6JNlvqMfg3O_E0eQPqB8jDSoUn6oiI",
+			    "x": "0_3S7HedSywaxlekdt6Or8pkcR13hQaCPMqt9cuZBVc",
+			    "y": "ZVXSCL3HlnMQWKrwMyIAe5wsAIWd3Eu1misKFr3POdA",
+			    "alg": "ES256"
+			}""";
 		env.putObjectFromJsonString("config", "credential.signing_jwk", key);
 
 		cond.execute(env);
