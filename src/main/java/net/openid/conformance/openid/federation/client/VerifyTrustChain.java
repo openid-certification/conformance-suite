44ecf5c0bd2d5f8160d6289ea148f11b1079a1e7
Marcus Almgren <marcus@almgren.pt>
2025-04-16 15:16:22 +0100

    Trust chain verification

---
+++ src/main/java/net/openid/conformance/openid/federation/client/VerifyTrustChain.java
@@ -0,0 +1,33 @@
+package net.openid.conformance.openid.federation.client;
+
+import com.google.gson.JsonArray;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.openid.federation.TrustChainVerifier;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.testmodule.OIDFJSON;
+
+public class VerifyTrustChain extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = {"trust_chain"})
+	public Environment evaluate(Environment env) {
+		String trustChainSubject = env.getString("trust_chain", "subject");
+		String trustChainTrustAnchor = env.getString("trust_chain", "trust_anchor");
+		JsonArray trustChain = env.getElementFromObject("trust_chain", "trust_chain").getAsJsonArray();
+
+		boolean ignoreExpAndIatValidation = "true".equals(env.getString("config", "internal.op_to_rp_mode"));
+
+		TrustChainVerifier.VerificationResult result =
+			TrustChainVerifier.verifyTrustChain(trustChainSubject, trustChainTrustAnchor, OIDFJSON.convertJsonArrayToList(trustChain), ignoreExpAndIatValidation);
+
+		if (!result.isVerified()) {
+			throw error("Could not verify the trust chain from the sub %s to trust anchor %s".formatted(trustChainSubject, trustChainTrustAnchor),
+				args("error", result.getError(), "trust_chain", trustChain));
+		}
+
+		logSuccess("Trust chain verified successfully", args("trust_chain", trustChain));
+
+		return env;
+	}
+}
