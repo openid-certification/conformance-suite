48ff00486bdd98979d60f1d0379cef2519d94eb7
Marcus Almgren <marcus@almgren.pt>
2025-04-17 17:33:53 +0100

    Test invalid trust chains in request objects

--- src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationWithJarAndGetAndTrustChainTest.java
+++ src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationWithJarAndGetAndTrustChainTest.java
@@ -1,6 +1,9 @@
 package net.openid.conformance.openid.federation;
 
 import com.google.gson.JsonElement;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.client.CheckErrorFromAuthorizationEndpointErrorInvalidRequestOrInvalidRequestObjectOrInvalidClient;
+import net.openid.conformance.condition.common.ExpectInvalidRequestOrInvalidRequestObjectErrorPage;
 import net.openid.conformance.testmodule.PublishTestModule;
 import net.openid.conformance.variant.FAPIAuthRequestMethod;
 import org.springframework.http.HttpMethod;
@@ -30,9 +33,27 @@ public class OpenIDFederationAutomaticClientRegistrationWithJarAndGetAndTrustCha
 	protected void verifyTestConditions() {
 		JsonElement trustChain = env.getElementFromObject("config", "client.trust_chain");
 		if (trustChain == null) {
-			fireTestSkipped("The client trust_chain is not provided in the test configuration");
+			fireTestSkipped("A client trust_chain is not provided in the test configuration");
 		}
 		includeTrustChainInAuthorizationRequest = true;
 	}
 
+	@Override
+	protected void createPlaceholder() {
+		callAndContinueOnFailure(ExpectInvalidRequestOrInvalidRequestObjectErrorPage.class, Condition.ConditionResult.FAILURE);
+		env.putString("error_callback_placeholder", env.getString("invalid_request_error"));
+	}
+
+	@Override
+	protected void redirect(HttpMethod method) {
+		performRedirectAndWaitForPlaceholdersOrCallback("error_callback_placeholder", method.name());
+	}
+
+	@Override
+	protected void processCallback() {
+		env.mapKey("authorization_endpoint_response", "callback_query_params");
+		performGenericAuthorizationEndpointErrorResponseValidation();
+		callAndContinueOnFailure(CheckErrorFromAuthorizationEndpointErrorInvalidRequestOrInvalidRequestObjectOrInvalidClient.class, Condition.ConditionResult.WARNING);
+		fireTestFinished();
+	}
 }
