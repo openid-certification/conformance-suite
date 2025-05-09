08fdfc0547ce708131d0fbaca8d77916850b49a3
Thomas Darimont <thomas.darimont@oidf.org>
2025-05-09 23:06:08 +0200

    Check expected credential information for token_endpoint_auth_method in DCR response
    Adds VerifyClientTokenEndpointAuthCredentials check that is included in CallDynamicRegistrationEndpointAndVerifySuccessfulResponse.
    We currently check for client_secret and client_secret_expires_at if token_endpoint_auth_method
    is client_secret_basic or client_secret_post.
    
    Fixes #1512
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

---
+++ src/main/java/net/openid/conformance/condition/client/VerifyClientTokenEndpointAuthCredentials.java
@@ -0,0 +1,39 @@
+package net.openid.conformance.condition.client;
+
+import com.google.common.base.Strings;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.testmodule.Environment;
+import net.openid.conformance.testmodule.OIDFJSON;
+
+public class VerifyClientTokenEndpointAuthCredentials extends AbstractCondition {
+
+	@Override
+	public Environment evaluate(Environment env) {
+
+		String authMethod = env.getString("dynamic_registration_request", "token_endpoint_auth_method");
+		if ("client_secret_basic".equalsIgnoreCase(authMethod) || "client_secret_post".equalsIgnoreCase(authMethod)) {
+			// client secret only necessary for client_secret_basic and client_secret_post
+			JsonObject clientObject = env.getObject("client");
+			JsonElement clientSecretEl = clientObject.get("client_secret");
+			String clientSecret = clientSecretEl == null ? null : OIDFJSON.getString(clientSecretEl);
+			if (Strings.isNullOrEmpty(clientSecret)) {
+				throw error("Missing client_secret for token_endpoint_auth_method in client registration response", args("token_endpoint_auth_method", authMethod));
+			}
+
+			JsonElement clientSecretExpiresAtEl = clientObject.get("client_secret_expires_at");
+			Long clientSecretExpiresAt = clientSecretExpiresAtEl == null ? null : OIDFJSON.getLong(clientSecretExpiresAtEl);
+			if (clientSecretExpiresAt == null) {
+				throw error("Missing client_secret_expires_at for token_endpoint_auth_method in client registration response", args("token_endpoint_auth_method", authMethod));
+			}
+
+			logSuccess("Found required credential information for token_endpoint_auth_method in client registration response", args("token_endpoint_auth_method", authMethod,
+				"client_secret", clientSecret, "client_secret_expires_at", clientSecretExpiresAt));
+		} else {
+			log("Skipped check for valid credential information for token_endpoint_auth_method in client registration response", args("token_endpoint_auth_method", authMethod));
+		}
+
+		return env;
+	}
+}
