44e84cf5c0eb763365ce40c40ace6a556f004a17
Edmund Jay <ejay@mgi1.com>
2025-05-13 15:45:30 -0700

    eKYC: Remove deprecated items

--- src/main/java/net/openid/conformance/ekyc/test/oidccore/AbstractEKYCTestWithOIDCCore.java
+++ src/main/java/net/openid/conformance/ekyc/test/oidccore/AbstractEKYCTestWithOIDCCore.java
@@ -110,41 +110,5 @@ public abstract class AbstractEKYCTestWithOIDCCore extends AbstractOIDCCServerTe
 	protected void validateUserinfoVerifiedClaimsAgainstRequested() {
 		callAndContinueOnFailure(ValidateVerifiedClaimsInUserinfoResponseAgainstRequest.class, Condition.ConditionResult.FAILURE, "IA-6");
 	}
-	protected void validateIdDocument() {
-		//5.1.1.1. id_document
-		//type: REQUIRED. Value MUST be set to "id_document".
-		//method: The method used to verify the ID document. For information on predefined verification method values see Section 12.
-		//verifier: JSON object denoting the legal entity that performed the identity verification on behalf of the OP. This object SHOULD only be included if the OP did not perform the identity verification itself. This object consists of the following properties:
-		//
-		//organization: String denoting the organization which performed the verification on behalf of the OP.
-		//
-		//txn: Identifier referring to the identity verification transaction. This transaction identifier can be resolved into transaction details during an audit.
-		//time: Time stamp in ISO 8601:2004 [ISO8601-2004] YYYY-MM-DDThh:mm[:ss]TZD format representing the date when this ID document was verified.
-		//document: JSON object representing the ID document used to perform the identity verification. It consists of the following properties:
-			//type: REQUIRED. String denoting the type of the ID document. For information on predefined identity document values see Section 12. The OP MAY use other than the predefined values in which case the RPs will either be unable to process the assertion, just store this value for audit purposes, or apply bespoken business logic to it.
-			//number: String representing the number of the identity document.
-			//issuer: JSON object containing information about the issuer of this identity document. This object consists of the following properties:
-			//name: Designation of the issuer of the identity document.
-			//country: String denoting the country or organization that issued the document as ICAO 2-letter code [ICAO-Doc9303], e.g. "JP". ICAO 3-letter codes MAY be used when there is no corresponding ISO 2-letter code, such as "UNO".
-			//date_of_issuance: The date the document was issued as ISO 8601:2004 YYYY-MM-DD format.
-			//date_of_expiry: The date the document will expire as ISO 8601:2004 YYYY-MM-DD format.
-	}
 
-	protected void validateUtilityBill() {
-		//5.1.1.2. utility_bill
-		//The following elements are contained in a utility_bill evidence sub-element.
-		//	type: REQUIRED. Value MUST be set to "utility_bill".
-		//	provider: JSON object identifying the respective provider that issued the bill. The object consists of the following properties:
-		//	name: String designating the provider.
-		//	All elements of the OpenID Connect address Claim ([OpenID])
-		//	date: String in ISO 8601:2004 YYYY-MM-DD format containing the date when this bill was issued.
-	}
-
-	protected void validateQes() {
-		//The following elements are contained in a qes evidence sub-element.
-		//	type: REQUIRED. Value MUST be set to "qes".
-		//	issuer: String denoting the certification authority that issued the signer's certificate.
-		//	serial_number: String containing the serial number of the certificate used to sign.
-		//	created_at: The time the signature was created as ISO 8601:2004 YYYY-MM-DDThh:mm[:ss]TZD format.
-	}
 }
