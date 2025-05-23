c950112fde2b00e08a0e3f21641c7628e064510f
Thomas Darimont <thomas.darimont@oidf.org>
2025-05-23 10:32:10 +0200

    Next iteration
    - Support for wallet_initiated and issuer_initiated flow
    - Support for Authorization_Code grant
    - Support for Credential offer by value and by reference
    
    Fixes #1511
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vciid2issuer/condition/VCIAddIssuerStateToAuthorizationRequest.java
+++ src/main/java/net/openid/conformance/vciid2issuer/condition/VCIAddIssuerStateToAuthorizationRequest.java
@@ -2,23 +2,20 @@ package net.openid.conformance.vciid2issuer.condition;
 
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
 public class VCIAddIssuerStateToAuthorizationRequest extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(required = {"vci", "authorization_endpoint_request"})
 	public Environment evaluate(Environment env) {
 
-		String issuerState = "dummy_issuer_state"; // env.getString("vci","issuer_state");
+		String issuerState = env.getString("vci", "issuer_state");
 		JsonObject authorizationEndpointRequest = env.getObject("authorization_endpoint_request");
 		authorizationEndpointRequest.addProperty("issuer_state", issuerState);
-		env.putObject("authorization_endpoint_request", authorizationEndpointRequest);
 
-		// TODO check if we have issuer_state in the credential offer,
-		//  add it to the authorization request Check if credential_configuration_ids
-		//  needs to be added to RAR object
-
-		logSuccess("Added issuer_state to authorization_endpoint_request", args("isser_state", issuerState));
+		logSuccess("Added issuer_state to authorization_endpoint_request", args("issuer_state", issuerState));
 
 		return env;
 	}
