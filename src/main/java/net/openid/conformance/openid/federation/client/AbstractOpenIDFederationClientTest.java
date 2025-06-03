4d68a0bab73f80c08ab7ba88ba342083a2844626
Marcus Almgren <marcus@almgren.pt>
2025-06-03 18:06:08 +0100

    Verify pkjwt client assertion in client test

--- src/main/java/net/openid/conformance/openid/federation/client/AbstractOpenIDFederationClientTest.java
+++ src/main/java/net/openid/conformance/openid/federation/client/AbstractOpenIDFederationClientTest.java
@@ -1,9 +1,13 @@
 package net.openid.conformance.openid.federation.client;
 
+import net.openid.conformance.condition.Condition;
 import net.openid.conformance.openid.federation.AbstractOpenIDFederationTest;
+import net.openid.conformance.sequence.ConditionSequence;
+import net.openid.conformance.sequence.as.ValidateClientAuthenticationWithPrivateKeyJWT;
 import net.openid.conformance.variant.ServerMetadata;
 import net.openid.conformance.variant.VariantConfigurationFields;
 import net.openid.conformance.variant.VariantParameters;
+import net.openid.conformance.variant.VariantSetup;
 
 @VariantParameters({
 	ServerMetadata.class,
@@ -13,4 +17,15 @@ import net.openid.conformance.variant.VariantParameters;
 	"federation.entity_configuration",
 })
 public abstract class AbstractOpenIDFederationClientTest extends AbstractOpenIDFederationTest {
+
+	protected Class<? extends Condition> addTokenEndpointAuthMethodSupported;
+	protected Class<? extends ConditionSequence> validateClientAuthenticationSteps;
+
+	@VariantSetup(parameter = ClientRegistration.class, value = "automatic")
+	public void setupPrivateKeyJwt() {
+		addTokenEndpointAuthMethodSupported = SetTokenEndpointAuthMethodsSupportedToPrivateKeyJWTOnly.class;
+		validateClientAuthenticationSteps = ValidateClientAuthenticationWithPrivateKeyJWT.class;
+		//clientRegistrationSteps = OIDCCRegisterClientWithPrivateKeyJwt.class;
+	}
+
 }
