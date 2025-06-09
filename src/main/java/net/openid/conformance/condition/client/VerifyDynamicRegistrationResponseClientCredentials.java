9e1057aa8d0207fb0788d9e26c9656ee7bce35e2
Thomas Darimont <thomas.darimont@oidf.org>
2025-06-09 17:19:25 +0200

    Revise VerifyClientTokenEndpointAuthCredentials
    - Rename to VerifyDynamicRegistrationResponseClientCredentials
    - Require client in preenv
    
    Fixes #1512
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/condition/client/VerifyDynamicRegistrationResponseClientCredentials.java
+++ src/main/java/net/openid/conformance/condition/client/VerifyDynamicRegistrationResponseClientCredentials.java
@@ -3,12 +3,14 @@ package net.openid.conformance.condition.client;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 import net.openid.conformance.testmodule.OIDFJSON;
 
-public class VerifyClientTokenEndpointAuthCredentials extends AbstractCondition {
+public class VerifyDynamicRegistrationResponseClientCredentials extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(required = "client")
 	public Environment evaluate(Environment env) {
 
 		JsonObject clientObject = env.getObject("client");
