b8889d90c56662594ec0fd942e90c1552614b5e0
Marcus Almgren <marcus@almgren.pt>
2025-06-10 09:24:09 +0100

    Utility method

--- src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationExpectEntityConfigurationRequestTest.java
+++ src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationExpectEntityConfigurationRequestTest.java
@@ -58,7 +58,7 @@ public class OpenIDFederationAutomaticClientRegistrationExpectEntityConfiguratio
 	@Override
 	public void configure(JsonObject config, String baseUrl, String externalUrlOverride, String baseMtlsUrl) {
 
-		String hostOverride = OIDFJSON.getString(config.get("federation").getAsJsonObject().get("entity_identifier_host_override"));
+		String hostOverride = OIDFJSON.getStringOrNull(config.get("federation").getAsJsonObject().get("entity_identifier_host_override"));
 		if (!Strings.isNullOrEmpty(hostOverride)) {
 			baseUrl = EntityUtils.replaceHostnameInUrl(baseUrl, hostOverride);
 		}
