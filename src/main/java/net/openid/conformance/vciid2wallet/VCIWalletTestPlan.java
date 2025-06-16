0bb0002c827ac39e955547983496bc3bdf4d5837
Thomas Darimont <thomas.darimont@oidf.org>
2025-06-16 21:24:02 +0200

    WIP Add credential_identifier to authorization_details in token response
    Fixes #1511
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vciid2wallet/VCIWalletTestPlan.java
+++ src/main/java/net/openid/conformance/vciid2wallet/VCIWalletTestPlan.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @PublishTestPlan (
 	testPlanName = "oid4vci-id2-wallet-test-plan",
-	displayName = "OpenID for Verifiable Credential Issuance ID2: Test a wallet (alpha version - may be incomplete or incorrect, please email certification@oidf.org)",
+	displayName = "OpenID for Verifiable Credential Issuance ID2: Test a wallet - alpha tests (not currently part of certification program)",
 	profile = TestPlan.ProfileNames.vciwallet
 )
 public class VCIWalletTestPlan implements TestPlan {
