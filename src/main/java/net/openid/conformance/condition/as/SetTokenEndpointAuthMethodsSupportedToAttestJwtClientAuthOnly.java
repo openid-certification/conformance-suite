29252f9d2a51560ca8470df39c4668f529850139
Thomas Darimont <thomas.darimont@oidf.org>
2025-06-19 14:39:14 +0200

    Add missing attest_jwt_client_auth to oauth AS metadata if client attestation is used
    Fixes #1511
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>
    (cherry picked from commit 0b9e1b5ac8f57a42d126563e28bee7d51cf4b768)

---
+++ src/main/java/net/openid/conformance/condition/as/SetTokenEndpointAuthMethodsSupportedToAttestJwtClientAuthOnly.java
@@ -0,0 +1,29 @@
+package net.openid.conformance.condition.as;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class SetTokenEndpointAuthMethodsSupportedToAttestJwtClientAuthOnly extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = "server")
+	@PostEnvironment(required = "server")
+	public Environment evaluate(Environment env) {
+
+		JsonArray data = new JsonArray();
+		data.add("attest_jwt_client_auth");
+
+		JsonObject server = env.getObject("server");
+		server.add("token_endpoint_auth_methods_supported", data);
+		env.putObject("server", server);
+
+		log("Changed token_endpoint_auth_methods_supported to attest_jwt_client_auth only in server configuration",
+			args("server_configuration", server));
+
+		return env;
+	}
+}
