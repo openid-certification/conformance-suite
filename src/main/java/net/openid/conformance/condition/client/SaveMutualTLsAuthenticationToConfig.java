4e7e399cc65daeb078e326d4db2c3752493e68e0
Edmund Jay <ejay@mgi1.com>
2025-06-30 13:56:05 -0700

    Save generated keys and certs from mutual_tls_authentication to config.mtls

---
+++ src/main/java/net/openid/conformance/condition/client/SaveMutualTLsAuthenticationToConfig.java
@@ -0,0 +1,42 @@
+package net.openid.conformance.condition.client;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.testmodule.Environment;
+
+public class SaveMutualTLsAuthenticationToConfig extends AbstractCondition {
+
+	@Override
+	@PreEnvironment(required = "mutual_tls_authentication")
+	@PostEnvironment(required = "config")
+	public Environment evaluate(Environment env) {
+
+		// these are not PEM encoded, need to be careful if
+		String certString = env.getString("mutual_tls_authentication", "cert");
+		String keyString = env.getString("mutual_tls_authentication", "key");
+		String caString = env.getString("mutual_tls_authentication", "ca");
+		String kty = env.getString("mutual_tls_authentication", "kty");
+
+
+		JsonObject mtls = new JsonObject();
+		mtls.addProperty("cert", certString);
+		mtls.addProperty("key", keyString);
+		if (caString != null) {
+			mtls.addProperty("ca", caString);
+		}
+		if (kty != null) {
+			mtls.addProperty("kty", kty);
+		}
+
+		// these are not PEM encoded, need to be careful if expecting config.mtls to be PEM format
+		// ExtractMTLSCertificatesFromConfiguration alread ytakes this into account
+		env.putObject("config", "mtls", mtls);
+
+		logSuccess("Mutual TLS authentication credentials saved to config", mtls);
+
+		return env;
+	}
+
+}
