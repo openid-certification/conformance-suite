68c12ede24642daa4193d1c6945591670dc5606b
Edmund Jay <ejay@mgi1.com>
2025-07-07 12:13:04 -0700

    revert Disallow FAPI2SenderConstrainMethod.NONE as variant for FAPI2ID2 tests

--- src/main/java/net/openid/conformance/fapi2spid2/AbstractFAPI2SPID2ServerTestModule.java
+++ src/main/java/net/openid/conformance/fapi2spid2/AbstractFAPI2SPID2ServerTestModule.java
@@ -220,9 +220,6 @@ import java.util.function.Supplier;
 @VariantNotApplicable(parameter = ClientAuthType.class, values = {
 	"none", "client_secret_basic", "client_secret_post", "client_secret_jwt"
 })
-@VariantNotApplicable(parameter = AccessTokenSenderConstrainMethod.class, values = {
-	"none"
-})
 @VariantHidesConfigurationFields(parameter = FAPI2ID2OPProfile.class, value = "connectid_au", configurationFields = {
 	"resource.resourceUrl", // the userinfo endpoint is always used
 	"client.scope", // scope is always openid
