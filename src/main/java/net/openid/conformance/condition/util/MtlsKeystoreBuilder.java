2ac90b5a07f2f801c35f9290ee9ecf6d72600893
Edmund Jay <ejay@mgi1.com>
2025-07-16 01:57:49 -0700

    Fixes #1538 for MtlsKeyStoreBuilder to handle EC and RSA keys which can be PKCS1/PKCS8 format

--- src/main/java/net/openid/conformance/condition/util/MtlsKeystoreBuilder.java
+++ src/main/java/net/openid/conformance/condition/util/MtlsKeystoreBuilder.java
@@ -3,23 +3,28 @@ package net.openid.conformance.condition.util;
 import com.google.common.collect.Lists;
 import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
 import net.openid.conformance.testmodule.Environment;
-import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -27,7 +32,7 @@ import java.util.List;
 
 public class MtlsKeystoreBuilder {
 
-	public static KeyManager[] configureMtls(Environment env) throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException {
+	public static KeyManager[] configureMtls(Environment env) throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, InvalidKeySpecException {
 		String clientCert = env.getString("mutual_tls_authentication", "cert");
 		String clientKey = env.getString("mutual_tls_authentication", "key");
 		String clientCa = env.getString("mutual_tls_authentication", "ca");
@@ -36,7 +41,13 @@ public class MtlsKeystoreBuilder {
 		byte[] keyBytes = Base64.getDecoder().decode(clientKey);
 
 		X509Certificate cert = generateCertificateFromDER(certBytes);
-		PrivateKey key = generatePrivateKeyFromDER(keyBytes);
+
+		// use public key from cert to aid in decoding the private key
+		// which can be EC or RSA keys in PKCS#1 or PKCS#8 format
+		PublicKey publicKey = cert.getPublicKey();
+		String alg = publicKey.getAlgorithm();
+
+		PrivateKey key = generateAlgPrivateKeyFromDER(alg, keyBytes);
 
 		ArrayList<X509Certificate> chain = Lists.newArrayList(cert);
 		if (clientCa != null) {
@@ -55,18 +66,27 @@ public class MtlsKeystoreBuilder {
 		return  keyManagerFactory.getKeyManagers();
 	}
 
-	protected static PrivateKey generatePrivateKeyFromDER(byte[] keyBytes) throws IOException {
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
 
-		/*
-		 * https://stackoverflow.com/questions/51951185/create-any-privatekey-instance-rsa-or-dsa-or-ec-from-pkcs8-encoded-data/52074721#comment112948749_52074721
-		 * Get PrivateKeyInfo from PKCS#8 PrivateKeyInfo structure
-		 */
-		ASN1InputStream bIn = new ASN1InputStream(new ByteArrayInputStream(spec.getEncoded()));
-		PrivateKeyInfo pki = PrivateKeyInfo.getInstance(bIn.readObject());
-
-		return new JcaPEMKeyConverter().setProvider(BouncyCastleProviderSingleton.getInstance()).getPrivateKey(pki);
+	protected static PrivateKey generateAlgPrivateKeyFromDER(String alg, byte[] keyBytes) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+		try {
+			// try to generate private key using PKCS8, works for both RSA and EC alg
+			// RSA alg will handle both PKCS1 and PKCS8 format here
+			// EC alg will throw exception for PKCS1
+			KeySpec kspec = new PKCS8EncodedKeySpec(keyBytes);
+			return KeyFactory.getInstance(alg, BouncyCastleProviderSingleton.getInstance()).generatePrivate(kspec);
+		} catch (InvalidKeySpecException e) {
+			if("EC".equals(alg)) {
+				// try to generate private key using PKCS1, code from ValidateMTLSCertificatesAsX509.verifyECPrivateKey
+				ASN1Sequence seq = ASN1Sequence.getInstance(keyBytes);
+				org.bouncycastle.asn1.sec.ECPrivateKey pKey = org.bouncycastle.asn1.sec.ECPrivateKey.getInstance(seq);
+				AlgorithmIdentifier algId = new AlgorithmIdentifier(X9ObjectIdentifiers.id_ecPublicKey, pKey.getParametersObject());
+				byte[] server_pkcs8 = new PrivateKeyInfo(algId, pKey).getEncoded();
+				return KeyFactory.getInstance(alg, BouncyCastleProviderSingleton.getInstance()).generatePrivate(new PKCS8EncodedKeySpec(server_pkcs8));
+			}
+			throw e;
+		}
 	}
+
 
 	protected static X509Certificate generateCertificateFromDER(byte[] certBytes) throws CertificateException {
 		CertificateFactory factory = CertificateFactory.getInstance("X.509");
