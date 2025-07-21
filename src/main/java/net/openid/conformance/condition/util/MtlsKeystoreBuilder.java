ee165afd4f262bf4de5179ab38949f268116c1ec
Edmund Jay <ejay@mgi1.com>
2025-07-21 12:24:21 -0700

    Generate MTLS certs without BC 2

--- src/main/java/net/openid/conformance/condition/util/MtlsKeystoreBuilder.java
+++ src/main/java/net/openid/conformance/condition/util/MtlsKeystoreBuilder.java
@@ -73,7 +73,8 @@ public class MtlsKeystoreBuilder {
 			// RSA alg will handle both PKCS1 and PKCS8 format here
 			// EC alg will throw exception for PKCS1, Ed25519 not possible with PKCS1
 			KeySpec kspec = new PKCS8EncodedKeySpec(keyBytes);
-			return KeyFactory.getInstance(alg, BouncyCastleProviderSingleton.getInstance()).generatePrivate(kspec);
+			return KeyFactory.getInstance(alg).generatePrivate(kspec);
+//			return KeyFactory.getInstance(alg, BouncyCastleProviderSingleton.getInstance()).generatePrivate(kspec);
 		} catch (InvalidKeySpecException e) {
 			if("EC".equals(alg)) {
 				// try to generate private key using PKCS1, code from ValidateMTLSCertificatesAsX509.generateAlgPrivateKeyFromDER
