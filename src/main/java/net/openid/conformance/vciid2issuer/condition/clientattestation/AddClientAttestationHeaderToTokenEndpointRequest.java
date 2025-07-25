6dddd65f46438d36a40c4b730fc25b9d0191e41e
Thomas Darimont <thomas.darimont@oidf.org>
2025-07-25 14:02:08 +0200

    Create a new Client Attestation Proof Jwt for every PAR / token request
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>
    (cherry picked from commit f7289fbeff490c8e7898d12c2f0f78bdf1994add)

--- src/main/java/net/openid/conformance/vciid2issuer/condition/clientattestation/AddClientAttestationHeaderToTokenEndpointRequest.java
+++ src/main/java/net/openid/conformance/vciid2issuer/condition/clientattestation/AddClientAttestationHeaderToTokenEndpointRequest.java
@@ -1,13 +1,9 @@
 package net.openid.conformance.vciid2issuer.condition.clientattestation;
 
-import net.openid.conformance.sequence.AbstractConditionSequence;
-
-public class AddClientAttestationHeaderToTokenEndpointRequest extends AbstractConditionSequence {
+public class AddClientAttestationHeaderToTokenEndpointRequest extends AddClientAttestationClientAuthToEndpointRequest {
 
 	@Override
 	public void evaluate() {
-
-		callAndStopOnFailure(AddClientAttestationHeaderToRequest.class, "OAuth2-ATCA05-6.1");
-		callAndStopOnFailure(AddClientAttestationProofHeaderToRequest.class, "OAuth2-ATCA05-6.1");
+		super.evaluate();
 	}
 }
