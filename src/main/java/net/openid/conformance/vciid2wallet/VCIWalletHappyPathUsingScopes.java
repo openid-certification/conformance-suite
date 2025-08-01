0ba42d674d0a2c41416924ff4ce978382b08d5f8
Thomas Darimont <thomas.darimont@oidf.org>
2025-08-01 17:26:40 +0200

    VCI: Add note about tx_code for vci_grant_type=pre_authorization_code for the VCI wallet test.
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vciid2wallet/VCIWalletHappyPathUsingScopes.java
+++ src/main/java/net/openid/conformance/vciid2wallet/VCIWalletHappyPathUsingScopes.java
@@ -14,7 +14,8 @@ import net.openid.conformance.vciid2wallet.condition.VCIInjectRequestScopePreAut
 	displayName = "OID4VCIID2: Wallet happy path test with Scopes",
 	summary = "This test case validates the standard credential issuance flow for a wallet using an emulated issuer, as defined in the OpenID for Verifiable Credential Issuance (OpenID4VCI) specification. It begins by emulating a Credential Issuer and the OAuth 2.0 Authorization Server. See the 'issuer' in the exported variables. In the wallet-initiated flow, it expects an authorization request using Pushed Authorization Requests (PAR) and a token request for which an access token is returned. " +
 		"In the issuer-initiated flow, the emulated issuer generates a URL (and a QR code) for a credential offer with an issuer state. That URL is expected to be visited by the wallet. It expects an authorization request containing the issuer state using Pushed Authorization Requests (PAR) and a token request for which an access token is returned. After both variants, it expects a token. The test then expects a call to the nonce and the Credential Endpoint. After successfully validating the credential request, a credential will be generated." +
-		"The credential configurations must be requested using scopes.",
+		"The credential configurations must be requested using scopes. " +
+		"If the vci_grant_type=pre_authorization_code is used, you can use 123456 as the tx_code.",
 	profile = "OID4VCI-ID2",
 	configurationFields = {
 		"server.jwks",
