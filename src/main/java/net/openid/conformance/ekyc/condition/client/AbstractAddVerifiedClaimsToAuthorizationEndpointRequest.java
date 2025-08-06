205958f27d4c05e3b684735862db9c99be790b7d
Edmund Jay <ejay@mgi1.com>
2025-08-06 00:52:51 -0700

    eKYC: request verified claims using configured claim names instead of first one from server supported list

--- src/main/java/net/openid/conformance/ekyc/condition/client/AbstractAddVerifiedClaimsToAuthorizationEndpointRequest.java
+++ src/main/java/net/openid/conformance/ekyc/condition/client/AbstractAddVerifiedClaimsToAuthorizationEndpointRequest.java
@@ -14,36 +14,15 @@ public abstract class AbstractAddVerifiedClaimsToAuthorizationEndpointRequest ex
 	@PreEnvironment(required = {"server", "authorization_endpoint_request", "config"})
 	@PostEnvironment(required = "authorization_endpoint_request")
 	public Environment evaluate(Environment env) {
-		JsonElement verifiedClaimsSupportedElement = env.getElementFromObject("server", "claims_in_verified_claims_supported");
-		if(verifiedClaimsSupportedElement==null) {
-			throw error("claims_in_verified_claims_supported element in server configuration is required for this test");
-		}
-		JsonArray verifiedClaimsSupportedList = verifiedClaimsSupportedElement.getAsJsonArray();
 		JsonObject verifiedClaims = new JsonObject();
 		JsonObject verification = new JsonObject();
 		verification.add("trust_framework", JsonNull.INSTANCE);
 		verifiedClaims.add("verification", verification);
 		JsonObject claims = new JsonObject();
 
-		JsonElement verifiedClaimsNamesElement = env.getElementFromObject("config","ekyc.verified_claims_names");
-		JsonArray requestedVerifiedClaimsList;
-		if(null != verifiedClaimsNamesElement) {
-			if(verifiedClaimsNamesElement.isJsonArray()) {
-				requestedVerifiedClaimsList = verifiedClaimsNamesElement.getAsJsonArray();
-			} else if(verifiedClaimsNamesElement.isJsonPrimitive()) {
-				requestedVerifiedClaimsList = new JsonArray();
-				requestedVerifiedClaimsList.add(verifiedClaimsNamesElement);
-			} else {
-				throw error("ekyc.verified_claims_names is not JSON array or primitive", args("ekyc.verified_claims_names", verifiedClaimsNamesElement));
-			}
-			for(JsonElement claimName : requestedVerifiedClaimsList) {
-				if(!verifiedClaimsSupportedList.contains(claimName)) {
-					continue;
-				}
-				claims.add(OIDFJSON.getString(claimName), getClaimValue());
-			}
-		} else {
-			claims.add(OIDFJSON.getString(verifiedClaimsSupportedList.get(0)), getClaimValue());
+		JsonArray requestedVerifiedClaimsList = getVerifiedClaimsRequestList(env);
+		for(JsonElement claimName : requestedVerifiedClaimsList) {
+			claims.add(OIDFJSON.getString(claimName), getClaimValue());
 		}
 
 		verifiedClaims.add("claims", claims);
