e4fe2cb4345654a410a171a5d520c618751aebe4
Marcus Almgren <marcus@almgren.pt>
2025-08-20 16:17:28 +0100

    Add ci tests

--- src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationTest.java
+++ src/main/java/net/openid/conformance/openid/federation/OpenIDFederationAutomaticClientRegistrationTest.java
@@ -3,27 +3,6 @@ package net.openid.conformance.openid.federation;
 import net.openid.conformance.variant.FAPIAuthRequestMethod;
 import org.springframework.http.HttpMethod;
 
-/*
-@PublishTestModule(
-	testName = "openid-federation-automatic-client-registration-test",
-	displayName = "openid-federation-automatic-client-registration-test",
-	summary = "openid-federation-automatic-client-registration-test",
-	profile = "OIDFED",
-	configurationFields = {
-		"federation.authority_hints",
-		"federation.immediate_subordinates",
-		"federation_trust_anchor.immediate_subordinates",
-		"federation_trust_anchor.trust_anchor_jwks",
-		"federation.entity_identifier_host_override",
-		"client.entity_identifier",
-		"client.trust_anchor",
-		"client.jwks",
-		"server.jwks",
-		"internal.op_to_rp_mode",
-		"internal.ignore_exp_iat"
-	}
-)
-*/
 public class OpenIDFederationAutomaticClientRegistrationTest extends AbstractOpenIDFederationAutomaticClientRegistrationTest {
 
 	@Override
