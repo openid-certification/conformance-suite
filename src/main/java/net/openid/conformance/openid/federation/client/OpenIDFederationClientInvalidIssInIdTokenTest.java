734a2b020e5f79d6b4516c8febfa36a4b02a1a16
Marcus Almgren <marcus@almgren.pt>
2025-08-29 10:50:20 +0100

    Add invalid id token iss RP test

---
+++ src/main/java/net/openid/conformance/openid/federation/client/OpenIDFederationClientInvalidIssInIdTokenTest.java
@@ -0,0 +1,35 @@
+package net.openid.conformance.openid.federation.client;
+
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.as.AddInvalidAudValueToIdToken;
+import net.openid.conformance.condition.as.AddInvalidIssValueToIdToken;
+import net.openid.conformance.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "openid-federation-client-invalid-iss-in-id-token",
+	displayName = "openid-federation-client-invalid-iss-in-id-token",
+	summary = "openid-federation-client-invalid-iss-in-id-token",
+	profile = "OIDFED",
+	configurationFields = {
+		"federation.authority_hints",
+		"federation.immediate_subordinates",
+		"federation_trust_anchor.immediate_subordinates",
+		"federation_trust_anchor.trust_anchor_jwks",
+		"federation.entity_identifier_host_override",
+		"client.entity_identifier",
+		"client.trust_anchor",
+		"client.jwks",
+		"server.jwks",
+		"internal.op_to_rp_mode",
+		"internal.ignore_exp_iat"
+	}
+)
+@SuppressWarnings("unused")
+public class OpenIDFederationClientInvalidIssInIdTokenTest extends OpenIDFederationClientTest {
+
+	@Override
+	protected void beforeSigningIdToken() {
+		callAndContinueOnFailure(AddInvalidIssValueToIdToken.class, Condition.ConditionResult.FAILURE);
+	}
+
+}
