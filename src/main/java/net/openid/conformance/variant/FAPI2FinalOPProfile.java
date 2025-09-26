4856623c63143e193ee03d9dd8f1c5e23d59579a
Alan Smillie <alans@emobix.co.uk>
2025-09-26 14:34:07 +0100

    WIP : Add new variant

--- src/main/java/net/openid/conformance/variant/FAPI2FinalOPProfile.java
+++ src/main/java/net/openid/conformance/variant/FAPI2FinalOPProfile.java
@@ -17,7 +17,9 @@ public enum FAPI2FinalOPProfile {
 	// https://connectid.com.au
 	CONNECTID_AU,
 	// https://openfinanceuae.atlassian.net/wiki/spaces/StandardsDraft01/pages/39158001/Security+Profile+-+FAPI
-	CBUAE;
+	CBUAE,
+	// PLAIN_FAPI utilising client credentials grant only.
+	CLIENT_CREDENTIALS_ONLY;
 
 	@Override
 	public String toString() {
