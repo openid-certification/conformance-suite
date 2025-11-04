d9827307eeb0968527d42a99503cdb24a3ce254c
Joseph Heenan <joseph.heenan@oidf.org>
2025-11-04 12:11:19 +0000

    WIP: DCQL query schema
    Unit tests and some changes generated by Claude
    
    Still to do:
    
    Reject unknown parameters too
    Check this works (by trying invalid parameters in actual test runs and see that they fail)
    Add to verifier test for incoming DCQL

--- src/main/java/net/openid/conformance/condition/client/AddDcqlToAuthorizationEndpointRequest.java
+++ src/main/java/net/openid/conformance/condition/client/AddDcqlToAuthorizationEndpointRequest.java
@@ -12,11 +12,11 @@ public class AddDcqlToAuthorizationEndpointRequest extends AbstractCondition {
 	@PreEnvironment(required = { "client", "authorization_endpoint_request" })
 	@PostEnvironment(required = "authorization_endpoint_request")
 	public Environment evaluate(Environment env) {
-		JsonObject presentationDefinition = (JsonObject) env.getElementFromObject("client", "dcql");
+		JsonObject dcql = (JsonObject) env.getElementFromObject("client", "dcql");
 
 		JsonObject authorizationEndpointRequest = env.getObject("authorization_endpoint_request");
 
-		authorizationEndpointRequest.add("dcql_query", presentationDefinition);
+		authorizationEndpointRequest.add("dcql_query", dcql);
 
 		logSuccess("Added dcql_query parameter to request", authorizationEndpointRequest);
 
