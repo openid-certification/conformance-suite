f3e6c14e030d884ec23899d44c6d322d48ebe887
Thomas Darimont <thomas.darimont@oidf.org>
2025-11-06 18:58:36 +0100

    VCI: PoC support for key attestation
    #ciskip
    
    Fixes #1567
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/export/LogEntryHelper.java
+++ src/main/java/net/openid/conformance/export/LogEntryHelper.java
@@ -88,6 +88,7 @@ public class LogEntryHelper {
 		specLinks.put("OID4VCI-1FINAL-", "https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#section-");
 		specLinks.put("OID4VCI-1FINALA-", "https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#appendix-");
 		specLinks.put("HAIP-", "https://openid.github.io/OpenID4VC-HAIP/openid4vc-high-assurance-interoperability-profile-wg-draft.html#section-");
+		specLinks.put("HAIPA-", "https://openid.github.io/OpenID4VC-HAIP/openid4vc-high-assurance-interoperability-profile-wg-draft.html#appendix-");
 		specLinks.put("OIDCD-", "https://openid.net/specs/openid-connect-discovery-1_0.html#rfc.section.");
 		specLinks.put("OIDCBCL-", "https://openid.net/specs/openid-connect-backchannel-1_0.html#rfc.section.");
 		specLinks.put("OIDCFCL-", "https://openid.net/specs/openid-connect-frontchannel-1_0.html#rfc.section.");
