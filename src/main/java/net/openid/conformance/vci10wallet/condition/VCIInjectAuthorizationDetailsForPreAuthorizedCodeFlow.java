87a6eddd934efdadcfbcb631b576973323f8cc5a
Thomas Darimont <thomas.darimont@oidf.org>
2025-11-12 12:00:09 +0100

    VCI: Make credential_configuration_id configurable for issuer initiated wallet test
    Users can provide a credential_configuration_id to use in the wallet test configuration. If empty we use the configured fallback credential_configuration_id `eu.europa.ec.eudi.pid.1`.
    
    This allows users to specify the desired credential_configuration_id for issuer initiated flows.
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vci10wallet/condition/VCIInjectAuthorizationDetailsForPreAuthorizedCodeFlow.java
+++ src/main/java/net/openid/conformance/vci10wallet/condition/VCIInjectAuthorizationDetailsForPreAuthorizedCodeFlow.java
@@ -3,16 +3,21 @@ package net.openid.conformance.vci10wallet.condition;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
 public class VCIInjectAuthorizationDetailsForPreAuthorizedCodeFlow extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(strings = {"credential_configuration_id_hint"})
 	public Environment evaluate(Environment env) {
+
+		String credentialConfigurationIdHint = env.getString("credential_configuration_id_hint");
+		log("Using credential_configuration_id " + credentialConfigurationIdHint, args("credential_configuration_id", credentialConfigurationIdHint));
 
 		JsonObject credentialConfiguration = new JsonObject();
 		credentialConfiguration.addProperty("type", "openid_credential");
-		credentialConfiguration.addProperty("credential_configuration_id", "eu.europa.ec.eudi.pid.1");
+		credentialConfiguration.addProperty("credential_configuration_id", credentialConfigurationIdHint);
 
 		JsonArray rarElements = new JsonArray();
 		rarElements.add(credentialConfiguration);
