8b5790b90d28effe62be9e6963fd2b68452fb6c5
Marcus Almgren <marcus@almgren.pt>
2026-01-08 13:13:48 +0000

    Change from poll to ping required

--- src/main/java/net/openid/conformance/condition/as/CheckCIBAModeIsPing.java
+++ src/main/java/net/openid/conformance/condition/as/CheckCIBAModeIsPing.java
@@ -5,12 +5,12 @@ import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 import net.openid.conformance.variant.CIBAMode;
 
-public class CheckCIBAModeIsPoll extends AbstractCondition {
+public class CheckCIBAModeIsPing extends AbstractCondition {
 	@Override
 	@PreEnvironment(strings = { "ciba_mode"})
 	public Environment evaluate(Environment env) {
 		CIBAMode cibaMode = Enum.valueOf(CIBAMode.class, env.getString("ciba_mode"));
-		if (cibaMode != CIBAMode.POLL) {
+		if (cibaMode != CIBAMode.PING) {
 			throw error("Unexpected or disallowed mode", args("mode", cibaMode));
 		}
 
