c700f37646c374dd4d8b8a966c94ca3397ce4aa5
Thomas Darimont <thomas.darimont@oidf.org>
2026-02-02 18:17:14 +0100

    VCI: Revise VCIValidateFormatOfCredentialConfigurationsInMetadata
    We now also allow non-haip credentials in HAIP mode, if there is at least one credential that supports the credential format used by HAIP.
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vci10issuer/condition/VCIValidateFormatOfCredentialConfigurationsInMetadata.java
+++ src/main/java/net/openid/conformance/vci10issuer/condition/VCIValidateFormatOfCredentialConfigurationsInMetadata.java
@@ -8,6 +8,7 @@ import net.openid.conformance.testmodule.OIDFJSON;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class VCIValidateFormatOfCredentialConfigurationsInMetadata extends AbstractCondition {
 
@@ -45,21 +46,40 @@ public class VCIValidateFormatOfCredentialConfigurationsInMetadata extends Abstr
 
 		Map<String, String> credentialKeysWithUnsupportedFormat = new LinkedHashMap<>();
 
-		Set<String> formatsToCheck = useHaip ? HAIP_CREDENTIAL_FORMATS : VCI_1_0_CREDENTIAL_FORMATS;
+		Set<String> foundCredentialFormats = new TreeSet<>();
 
 		JsonObject metadata = env.getElementFromObject("vci", "credential_issuer_metadata").getAsJsonObject();
 		JsonObject credentialConfigurationsSupported = metadata.getAsJsonObject("credential_configurations_supported");
 		for (String credentialKey : credentialConfigurationsSupported.keySet()) {
 			JsonObject credentialConfiguration = credentialConfigurationsSupported.getAsJsonObject(credentialKey);
 			String credentialFormat = OIDFJSON.getString(credentialConfiguration.get("format"));
-			if (!formatsToCheck.contains(credentialFormat)) {
+			if (!VCI_1_0_CREDENTIAL_FORMATS.contains(credentialFormat)) {
 				credentialKeysWithUnsupportedFormat.put(credentialKey, credentialFormat);
 			}
+			foundCredentialFormats.add(credentialFormat);
 		}
 
 		if (!credentialKeysWithUnsupportedFormat.isEmpty()) {
 			throw error("Found credential configurations with unsupported formats",
 				args("unsupported_credentials", credentialKeysWithUnsupportedFormat));
+		}
+
+		if (useHaip) {
+
+			Set<String> foundHaipFormats = new TreeSet<>();
+			for (String haipFormat : HAIP_CREDENTIAL_FORMATS) {
+				if (foundCredentialFormats.contains(haipFormat)) {
+					foundHaipFormats.add(haipFormat);
+				}
+			}
+
+			if (foundHaipFormats.isEmpty()) {
+				throw error("Could not find any credential configuration with a format supported by HAIP",
+					args("found_credential_formats", foundCredentialFormats, "standard_haip_credential_formats", HAIP_CREDENTIAL_FORMATS));
+			}
+
+			log("Found credential configuration with a format supported by HAIP",
+				args("found_haip_credential_formats", foundHaipFormats, "standard_haip_credential_formats", HAIP_CREDENTIAL_FORMATS));
 		}
 
 		logSuccess("Found only credential configurations with supported formats");
