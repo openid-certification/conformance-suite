6f17067c596d1f56fc1312509bb74f6fb6833774
Thomas Darimont <thomas.darimont@oidf.org>
2026-02-04 12:47:04 +0100

    VCI: Add dedicated test module for happy path without notification
    Fixes #1671
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/vci10issuer/VCIIssuerTestPlanHaip.java
+++ src/main/java/net/openid/conformance/vci10issuer/VCIIssuerTestPlanHaip.java
@@ -37,6 +37,7 @@ public class VCIIssuerTestPlanHaip implements TestPlan {
 					// positive tests
 					VCIIssuerHappyFlow.class,
 					VCIIssuerEnsureServerAcceptsRequestObjectWithMultipleAud.class, // may not be that useful but currently used for op-against-rp tests in our CI - maybe we should add a simple 'only one authorization' test in the test plan before the happy flow that uses two clients (as using two clients doesn't work with rp tests)
+					VCIIssuerHappyFlowWithSkipNotification.class,
 					// negative tests
 					VCIIssuerFailOnInvalidNonce.class,
 					VCIIssuerFailOnReplayNonce.class,
