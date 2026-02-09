fed6103d6feb472383ceccb2c9043e2853575133
Marcus Almgren <marcus@almgren.pt>
2026-02-09 15:56:45 +0000

    Remove flaky federation tests

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -556,9 +556,6 @@ makeFederationTests() {
 
     TESTS="${TESTS} openid-federation-deployed-entity-test-plan[server_metadata=discovery][client_registration=automatic] ../conformance-suite/scripts/test-configs-federation/authlete-federation-fapidev-as.json"
     TESTS="${TESTS} openid-federation-deployed-entity-test-plan[server_metadata=discovery][client_registration=automatic] ../conformance-suite/scripts/test-configs-federation/authlete-federation-trust-anchor.json"
-    TESTS="${TESTS} openid-federation-deployed-entity-test-plan[server_metadata=static][client_registration=automatic] ../conformance-suite/scripts/test-configs-federation/sweden-federation-bankid.json"
-    TESTS="${TESTS} openid-federation-deployed-entity-test-plan[server_metadata=discovery][client_registration=automatic] ../conformance-suite/scripts/test-configs-federation/sweden-federation-intermediate.json"
-    TESTS="${TESTS} openid-federation-deployed-entity-test-plan[server_metadata=discovery][client_registration=automatic] ../conformance-suite/scripts/test-configs-federation/sweden-federation-trust-anchor.json"
 }
 
 makeSsfTests() {
