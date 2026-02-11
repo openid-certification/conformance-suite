717ebeba3d8b69f58853d8968688eb9b8add791b
Thomas Darimont <thomas.darimont@oidf.org>
2026-02-11 12:03:54 +0100

    VP: HAIP Testplan variants for testing a Wallet and Verifier
    Fixes #1678
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

---
+++ src/main/java/net/openid/conformance/vp1finalverifier/VP1FinalVerifierTestPlanHaip.java
@@ -0,0 +1,49 @@
+package net.openid.conformance.vp1finalverifier;
+
+import net.openid.conformance.plan.PublishTestPlan;
+import net.openid.conformance.plan.TestPlan;
+import net.openid.conformance.variant.VPProfile;
+import net.openid.conformance.variant.VariantSelection;
+
+import java.util.List;
+import java.util.Map;
+
+@PublishTestPlan(
+	testPlanName = "oid4vp-1final-verifier-haip-test-plan",
+	displayName = "OpenID for Verifiable Presentations 1.0 Final/HAIP: Test a verifier - alpha tests (not currently part of certification program)",
+	profile = TestPlan.ProfileNames.verifierTest
+)
+public class VP1FinalVerifierTestPlanHaip implements TestPlan {
+
+	public static List<ModuleListEntry> testModulesWithVariants() {
+		return List.of(
+			new ModuleListEntry(
+				List.of(
+					// positive tests
+					VP1FinalVerifierHappyFlow.class
+				),
+				List.of(
+					new Variant(VPProfile.class, "haip"),
+					new Variant(VP1FinalVerifierClientIdPrefix.class, "x509_hash"),
+					new Variant(VP1FinalVerifierRequestMethod.class, "request_uri_signed")
+				)
+			)
+		);
+	}
+	public static String certificationProfileName(VariantSelection variant) {
+
+		Map<String, String> v = variant.getVariant();
+		String vpProfile = v.get("vp_profile");
+		String responseMode = v.get("response_mode");
+		String credentialFormat = v.get("credential_format");
+		String requestMethod = v.get("request_method");
+		String clientIdPrefix = v.get("client_id_prefix");
+
+		String certProfile = "OID4VP-1.0-FINAL Verifier";
+
+		certProfile += " " + vpProfile + " " + credentialFormat + " " + requestMethod + " " + clientIdPrefix + " " + responseMode;
+
+		return certProfile;
+	}
+
+}
