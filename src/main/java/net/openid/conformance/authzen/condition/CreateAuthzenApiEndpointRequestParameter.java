dc541d9c81486a0cb2400a74d225b964193decf2
Edmund Jay <ejay@mgi1.com>
2026-03-25 18:31:04 -0700

    AuthZen: refactor CreateAuthzenApiEndpointRequestParameter to use a JsonElement as the request parameter instead of JsonObject

--- src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestParameter.java
+++ src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestParameter.java
@@ -1,47 +1,42 @@
 package net.openid.conformance.authzen.condition;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.testmodule.Environment;
 
 public abstract class CreateAuthzenApiEndpointRequestParameter extends AbstractCondition {
 	protected String requestParameterName;
-	protected JsonObject requestParameter;
+	protected JsonElement requestParameter;
 	protected String[] requiredProperties; // assume to be string
 	protected String[] optionalObjects; // assume to be objects
-	public CreateAuthzenApiEndpointRequestParameter(String requestParameterName, JsonObject requestParameter) {
+
+	public CreateAuthzenApiEndpointRequestParameter(String requestParameterName, JsonElement requestParameter) {
 		this.requestParameterName = requestParameterName;
 		this.requestParameter = requestParameter;
 	}
 
-	protected JsonObject createAuthzenApiEndpointRequestParameter(Environment env) {
+	protected JsonElement createAuthzenApiEndpointRequestParameter(Environment env) {
 		JsonObject authzenApiEndpointRequestParameter = new JsonObject();
-		if(requestParameter != null) {
-			if(requiredProperties != null) {
-				for(String requiredProperty : requiredProperties) {
-					if(requestParameter.has(requiredProperty)) {
-						authzenApiEndpointRequestParameter.add(requiredProperty, requestParameter.get(requiredProperty));
+		if (requestParameter != null && requestParameter.isJsonObject()) {
+			JsonObject requestObject = requestParameter.getAsJsonObject();
+			if (requiredProperties != null) {
+				for (String requiredProperty : requiredProperties) {
+					if (requestObject.has(requiredProperty)) {
+						authzenApiEndpointRequestParameter.add(requiredProperty, requestObject.get(requiredProperty));
 					} else {
-						throw error(requestParameterName + " parameter is missing required property", args(requiredProperty, requestParameter));
+						throw error(requestParameterName + " parameter is missing required property", args(requiredProperty, requestObject));
 					}
 				}
 			}
-			if(optionalObjects != null) {
-				for(String optionalObject : optionalObjects) {
-					if(requestParameter.has(optionalObject)) {
-						authzenApiEndpointRequestParameter.add(optionalObject, requestParameter.get(optionalObject));
+			if (optionalObjects != null) {
+				for (String optionalObject : optionalObjects) {
+					if (requestObject.has(optionalObject)) {
+						authzenApiEndpointRequestParameter.add(optionalObject, requestObject.get(optionalObject));
 					}
 				}
 			}
 		}
 		return authzenApiEndpointRequestParameter;
 	}
-
-	protected JsonObject createAuthzenApiEndpointRequestWithAllParameters(Environment env) {
-		if(requestParameter != null) {
-			return requestParameter.deepCopy();
-		}
-		return new JsonObject();
-	}
-
 }
