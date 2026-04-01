04e9d7a46bd71c72b73317509c938ad948b68dab
Edmund Jay <ejay@mgi1.com>
2026-04-01 16:12:49 -0700

    AuthZen: separate extraction and validation of decision responses to separate conditions

--- src/main/java/net/openid/conformance/authzen/condition/ExtractAuthzenApiEndpointDecisionResponse.java
+++ src/main/java/net/openid/conformance/authzen/condition/ExtractAuthzenApiEndpointDecisionResponse.java
@@ -1,6 +1,5 @@
 package net.openid.conformance.authzen.condition;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
@@ -13,22 +12,13 @@ public class ExtractAuthzenApiEndpointDecisionResponse extends AbstractCondition
 	@PreEnvironment(required = "authzen_api_endpoint_response")
 	@PostEnvironment(required = "authzen_api_endpoint_decision")
 	public Environment evaluate(Environment env) {
-
 		JsonObject decision = (JsonObject) env.getElementFromObject("authzen_api_endpoint_response", "body_json");
 		if (decision == null) {
 			throw error("No json response from Authzen API endpoint");
 		}
-
-		env.putObject("authzen_api_endpoint_decision", decision.deepCopy());
-
-		JsonElement decisionVal = decision.get("decision");
-		if (decisionVal == null) {
-			throw error("no decision in decision response");
-		}
-
+		env.putObject("authzen_api_endpoint_decision", decision);
 		logSuccess("Extracted decision from API endpoint response",
 			args("decision", decision));
-
 		return env;
 	}
 
