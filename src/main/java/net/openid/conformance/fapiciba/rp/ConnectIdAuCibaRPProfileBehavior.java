e5406be1d4bd4f0493babf1bcd1b01ed6c4dd65b
Marcus Almgren <marcus@almgren.pt>
2026-04-01 15:23:57 +0100

    - Added AustraliaConnectIdEnsureRequestObjectSigningAlgIsPS256 to ensure backchannel request objects are signed using PS256. - Added AustraliaConnectIdValidateRequestObjectPurpose to enforce presence and length constraints on the purpose parameter. - Added AustraliaConnectIdEnsureRequestObjectContainsNoAcrClaims to prevent acr or acr_values from being requested. - Added AustraliaConnectIdEnsureRequestObjectContainsTrustFramework to ensure trust_framework is set to au_connectid when verified_claims are requested. - Overrode applyProfileSpecificBackchannelRequestChecks in ConnectIdAuCibaRPProfileBehavior to apply the new and existing validations to the backchannel request object.

--- src/main/java/net/openid/conformance/fapiciba/rp/ConnectIdAuCibaRPProfileBehavior.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/ConnectIdAuCibaRPProfileBehavior.java
@@ -1,10 +1,32 @@
 package net.openid.conformance.fapiciba.rp;
 
+import net.openid.conformance.condition.as.AustraliaConnectIdCheckForFAPI2ClaimsInRequestObject;
+import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObjectContainsNoAcrClaims;
+import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObjectContainsTrustFramework;
+import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObjectSigningAlgIsPS256;
+import net.openid.conformance.condition.as.AustraliaConnectIdEnsureVerifiedClaimsInRequestObject;
+import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectExp;
+import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectNBFClaim;
+import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectPurpose;
+
 public class ConnectIdAuCibaRPProfileBehavior extends FAPICIBARPProfileBehavior {
 
 	@Override
 	public void exposeProfileSpecificEndpoints() {
 		module.exposeEnvString("userinfo_endpoint", "server", "userinfo_endpoint");
+	}
+
+	@Override
+	public void applyProfileSpecificBackchannelRequestChecks() {
+		super.applyProfileSpecificBackchannelRequestChecks();
+		module.callCondition(AustraliaConnectIdEnsureRequestObjectSigningAlgIsPS256.class);
+		module.callCondition(AustraliaConnectIdValidateRequestObjectExp.class);
+		module.callCondition(AustraliaConnectIdValidateRequestObjectNBFClaim.class);
+		module.callCondition(AustraliaConnectIdValidateRequestObjectPurpose.class);
+		module.callCondition(AustraliaConnectIdEnsureRequestObjectContainsNoAcrClaims.class);
+		module.callCondition(AustraliaConnectIdCheckForFAPI2ClaimsInRequestObject.class);
+		module.callCondition(AustraliaConnectIdEnsureVerifiedClaimsInRequestObject.class);
+		module.callCondition(AustraliaConnectIdEnsureRequestObjectContainsTrustFramework.class);
 	}
 
 }
