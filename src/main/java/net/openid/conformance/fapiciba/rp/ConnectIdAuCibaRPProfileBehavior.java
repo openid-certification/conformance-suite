54018fdfd35c9b1395a77f04799e37e2cbbf6d5e
Marcus Almgren <marcus@almgren.pt>
2026-04-02 14:36:10 +0100

    Implement binding_message validation for Australia ConnectID CIBA RP tests
    - Create AustraliaConnectIdValidateRequestObjectBindingMessage to enforce length and character set requirements
    - Update ConnectIdAuCibaRPProfileBehavior to use the new binding_message validation condition

--- src/main/java/net/openid/conformance/fapiciba/rp/ConnectIdAuCibaRPProfileBehavior.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/ConnectIdAuCibaRPProfileBehavior.java
@@ -6,6 +6,7 @@ import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObject
 import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObjectContainsTrustFramework;
 import net.openid.conformance.condition.as.AustraliaConnectIdEnsureRequestObjectSigningAlgIsPS256;
 import net.openid.conformance.condition.as.AustraliaConnectIdEnsureVerifiedClaimsInRequestObject;
+import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectBindingMessage;
 import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectExp;
 import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectNBFClaim;
 import net.openid.conformance.condition.as.AustraliaConnectIdValidateRequestObjectPurpose;
@@ -24,7 +25,7 @@ public class ConnectIdAuCibaRPProfileBehavior extends FAPICIBARPProfileBehavior 
 		module.callCondition(AustraliaConnectIdEnsureRequestObjectSigningAlgIsPS256.class, "CID-SP-4.2-8");
 		module.callCondition(AustraliaConnectIdValidateRequestObjectExp.class, "CID-SP-4.2-10");
 		module.callCondition(AustraliaConnectIdValidateRequestObjectNBFClaim.class, "CID-SP-4.2-11");
-		module.callCondition(AustraliaConnectIdValidateRequestObjectPurpose.class, "CID-IDA-5.2-10");
+		module.callCondition(AustraliaConnectIdValidateRequestObjectBindingMessage.class, "CID-IDA-5.2-10");
 		module.callCondition(AustraliaConnectIdEnsureRequestObjectContainsNoAcrClaims.class, "CID-IDA-5.2-5");
 		module.callCondition(AustraliaConnectIdCheckForFAPI2ClaimsInRequestObject.class, "CID-IDA-5.2-7");
 		module.callCondition(AustraliaConnectIdEnsureVerifiedClaimsInRequestObject.class, "CID-IDA-5.2-11");
