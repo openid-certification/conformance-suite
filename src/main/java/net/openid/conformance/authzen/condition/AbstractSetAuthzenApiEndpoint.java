4fc447428c1ad4954d8e64061f992074f805a962
Edmund Jay <ejay@mgi1.com>
2026-04-01 18:21:07 -0700

    AuthZen: add new conditions for setting Search API endpoints and use default values if endpoint URL is not set in server config

--- src/main/java/net/openid/conformance/authzen/condition/AbstractSetAuthzenApiEndpoint.java
+++ src/main/java/net/openid/conformance/authzen/condition/AbstractSetAuthzenApiEndpoint.java
@@ -8,8 +8,29 @@ public abstract class AbstractSetAuthzenApiEndpoint extends AbstractCondition {
 
 	public Environment setAuthzenApiEndpoint(Environment env, String endpointName, String endpointConfigPath) {
 		String endpointUrl = env.getString("pdp", endpointConfigPath);
-		if(Strings.isNullOrEmpty(endpointUrl)) {
-			throw error(endpointName + " URL is not set");
+		if(Strings.isNullOrEmpty(endpointUrl)) { // use default if not set
+			endpointUrl = env.getString("pdp", "policy_decision_point");
+			if(!endpointUrl.endsWith("/")) {
+				endpointUrl += "/";
+			}
+			endpointUrl += "access/v1/";
+
+			switch (endpointConfigPath) {
+				case "access_evaluations_endpoint":
+					endpointUrl += "evaluations";
+					break;
+				case "search_subject_endpoint":
+					endpointUrl += "search/subject";
+					break;
+				case "search_resource_endpoint":
+					endpointUrl += "search/resource";
+					break;
+				case "search_action_endpoint":
+					endpointUrl += "search/action";
+					break;
+				default:
+					throw error ("Unknown API endpoint", args("Endpoint name", endpointName, "path", endpointConfigPath));
+			}
 		}
 		env.putString("authzen_api_endpoint", endpointUrl);
 		logSuccess("Successfully set API endpoint", args(endpointName, endpointUrl));
