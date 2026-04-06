320f244d988f48dc2ead1219b70f05e4d7215128
Joseph Heenan <joseph.heenan@oidf.org>
2026-04-06 22:31:35 +0100

    Validate request_uri_method only on outer request

---
+++ src/test/java/net/openid/conformance/condition/as/CheckRequestUriMethodParameter_UnitTest.java
@@ -0,0 +1,70 @@
+package net.openid.conformance.condition.as;
+
+import com.google.gson.JsonObject;
+import net.openid.conformance.condition.Condition;
+import net.openid.conformance.condition.ConditionError;
+import net.openid.conformance.logging.TestInstanceEventLog;
+import net.openid.conformance.testmodule.Environment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class CheckRequestUriMethodParameter_UnitTest {
+
+	@Spy
+	private Environment env = new Environment();
+
+	@Mock
+	private TestInstanceEventLog eventLog;
+
+	private CheckRequestUriMethodParameter cond;
+
+	@BeforeEach
+	public void setUp() {
+		cond = new CheckRequestUriMethodParameter();
+		cond.setProperties("UNIT-TEST", eventLog, Condition.ConditionResult.INFO);
+	}
+
+	@Test
+	public void testEvaluate_acceptsPostFromHttpRequest() {
+		JsonObject httpRequestParams = new JsonObject();
+		httpRequestParams.addProperty("request_uri_method", "post");
+		env.putObject("authorization_endpoint_http_request_params", httpRequestParams);
+
+		cond.execute(env);
+
+		assertEquals("post", env.getString("request_uri_method"));
+	}
+
+	@Test
+	public void testEvaluate_ignoresRequestObjectOnlyValue() {
+		env.putObject("authorization_endpoint_http_request_params", new JsonObject());
+
+		JsonObject requestObjectClaims = new JsonObject();
+		requestObjectClaims.addProperty("request_uri_method", "post");
+		JsonObject requestObject = new JsonObject();
+		requestObject.add("claims", requestObjectClaims);
+		env.putObject("authorization_request_object", requestObject);
+
+		cond.execute(env);
+
+		assertNull(env.getString("request_uri_method"));
+	}
+
+	@Test
+	public void testEvaluate_rejectsUnknownHttpRequestValue() {
+		JsonObject httpRequestParams = new JsonObject();
+		httpRequestParams.addProperty("request_uri_method", "patch");
+		env.putObject("authorization_endpoint_http_request_params", httpRequestParams);
+
+		assertThrows(ConditionError.class, () -> cond.execute(env));
+	}
+}
