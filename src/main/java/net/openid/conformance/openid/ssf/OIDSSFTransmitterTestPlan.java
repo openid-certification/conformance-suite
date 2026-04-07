78d24bada94082701c1ef702bd433ac0de8b3a14
Thomas Darimont <thomas.darimont@oidf.org>
2026-04-07 10:15:33 +0200

    SSF: Refine SSF CAEP/non-CAEP test plans
    - Remove certification name and update display name for openid-ssf-transmitter-test-plan
    
    Fixes #1737
    
    Signed-off-by: Thomas Darimont <thomas.darimont@oidf.org>

--- src/main/java/net/openid/conformance/openid/ssf/OIDSSFTransmitterTestPlan.java
+++ src/main/java/net/openid/conformance/openid/ssf/OIDSSFTransmitterTestPlan.java
@@ -3,13 +3,12 @@ package net.openid.conformance.openid.ssf;
 import net.openid.conformance.plan.PublishTestPlan;
 import net.openid.conformance.plan.TestPlan;
 import net.openid.conformance.testmodule.TestModule;
-import net.openid.conformance.variant.VariantSelection;
 
 import java.util.List;
 
 @PublishTestPlan(
 	testPlanName = "openid-ssf-transmitter-test-plan",
-	displayName = "OpenID Shared Signals Framework 1.0 Final: Transmitter",
+	displayName = "OpenID Shared Signals Framework 1.0 Final: Transmitter test (alpha version - may be incomplete or incorrect, please email certification@oidf.org)",
 	summary = "Collection of tests to verify the behavior of a OpenID Shared Signals Framework Transmitter.",
 	profile = TestPlan.ProfileNames.ssftest,
 	specFamily = TestPlan.SpecFamilyNames.ssf
@@ -41,10 +40,4 @@ public class OIDSSFTransmitterTestPlan implements TestPlan {
 	public List<ModuleListEntry> testModulesWithVariants() {
 		return List.of(new ModuleListEntry(testModules, List.of()));
 	}
-
-	@Override
-	public List<String> certificationProfileName(VariantSelection variant) {
-		return List.of("OpenID SSF Transmitter 1.0 Final");
-	}
-
 }
