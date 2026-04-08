fd29fa68afad5140c88e9f24e96fcf5560cbb229
Thomas Darimont <thomas.darimont@oidf.org>
2026-04-08 18:50:25 +0200

    VCI: Ensure credential configuration is injected to as client.scope for HAIP

--- src/main/java/net/openid/conformance/fapi2spfinal/VCIHaipProfileBehavior.java
+++ src/main/java/net/openid/conformance/fapi2spfinal/VCIHaipProfileBehavior.java
@@ -1,7 +1,13 @@
 package net.openid.conformance.fapi2spfinal;
 
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.Condition.ConditionResult;
 import net.openid.conformance.condition.client.CheckDiscEndpointTokenEndpointAuthMethodsSupportedContainsAttestation;
+import net.openid.conformance.sequence.ConditionSequence;
+import net.openid.conformance.testmodule.ConditionCallBuilder;
+import net.openid.conformance.variant.VCI1FinalCredentialFormat;
+import net.openid.conformance.vci10issuer.condition.VCIDetermineCredentialConfigurationTransferMethod;
+import net.openid.conformance.vci10issuer.condition.VCIEnsureScopePresentInCredentialConfigurationForHaip;
 
 /**
  * Profile behavior for VCI HAIP (High Assurance Interoperability Profile) tests.
@@ -18,5 +24,17 @@ public class VCIHaipProfileBehavior extends VCIProfileBehavior {
 	@Override
 	public Class<? extends AbstractCondition> getDiscoveryTokenEndpointAuthMethodsCheck() {
 		return CheckDiscEndpointTokenEndpointAuthMethodsSupportedContainsAttestation.class;
+	}
+
+	@Override
+	protected ConditionSequence configureCredentialConfigurationResolution(VCI1FinalCredentialFormat vciCredentialFormat) {
+		ConditionSequence seq = super.configureCredentialConfigurationResolution(vciCredentialFormat);
+		// HAIP requires scope to be present for every credential configuration
+		seq.insertBefore(VCIDetermineCredentialConfigurationTransferMethod.class,
+			new ConditionCallBuilder(VCIEnsureScopePresentInCredentialConfigurationForHaip.class)
+				.onFail(ConditionResult.FAILURE)
+				.requirements("HAIP-4.1", "HAIP-4.3")
+				.dontStopOnFailure());
+		return seq;
 	}
 }
