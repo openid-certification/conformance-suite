a1fb9bbef6ba5831000b329dc9f13d083c5484ef
Joseph Heenan <joseph.heenan@oidf.org>
2026-04-10 11:35:40 +0100

    VCIAddCredentialConfigurationIdToEnv: throw error instead of TestFailureException
    TestConditions should throw errors - I can't remember exactly what goes wrong it
    they don't but the failure wouldn't use the condition name at least.

--- src/main/java/net/openid/conformance/vci10issuer/condition/VCIAddCredentialConfigurationIdToEnv.java
+++ src/main/java/net/openid/conformance/vci10issuer/condition/VCIAddCredentialConfigurationIdToEnv.java
@@ -1,20 +1,26 @@
 package net.openid.conformance.vci10issuer.condition;
 
 import net.openid.conformance.condition.AbstractCondition;
+import net.openid.conformance.condition.PostEnvironment;
+import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
-import net.openid.conformance.testmodule.TestFailureException;
 
 public class VCIAddCredentialConfigurationIdToEnv extends AbstractCondition {
 
 	@Override
+	@PreEnvironment(required = "config")
+	@PostEnvironment(strings = "vci_credential_configuration_id")
 	public Environment evaluate(Environment env) {
 
 		String vciCredentialConfigurationId = env.getString("config", "vci.credential_configuration_id");
 		if (vciCredentialConfigurationId == null || vciCredentialConfigurationId.isBlank()) {
-			throw new TestFailureException(getTestId(), "credential_configuration_id cannot be null or empty!");
+			throw error("'Credential Configuration ID' is missing or empty in the test configuration");
 		}
 
 		env.putString("vci_credential_configuration_id", vciCredentialConfigurationId);
+
+		log("Extracted credential_configuration_id from config",
+			args("vci_credential_configuration_id", vciCredentialConfigurationId));
 		return env;
 	}
 }
