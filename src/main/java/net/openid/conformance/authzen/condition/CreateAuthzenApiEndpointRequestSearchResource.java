b66ab2d582f182b1d14137ca16b88eb5c1e598ba
Edmund Jay <ejay@mgi1.com>
2026-04-21 13:49:21 -0700

    AuthZen: fix incorrect resource parameter name in CreateAuthzenApiEndpointRequestSearchResource

--- src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestSearchResource.java
+++ src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestSearchResource.java
@@ -7,7 +7,7 @@ import net.openid.conformance.testmodule.Environment;
 public class CreateAuthzenApiEndpointRequestSearchResource extends CreateAuthzenApiEndpointRequestParameter {
 
 	public CreateAuthzenApiEndpointRequestSearchResource(JsonObject requestParameter) {
-		super("subject", requestParameter);
+		super("resource", requestParameter);
 		this.requiredProperties = new String[]{"type"};
 		this.optionalProperties = new String[] {"properties"};
 	}
@@ -15,10 +15,10 @@ public class CreateAuthzenApiEndpointRequestSearchResource extends CreateAuthzen
 	@Override
 	@PreEnvironment(required = "authzen_api_endpoint_request")
 	public Environment evaluate(Environment env) {
-		JsonObject subject = createAuthzenApiEndpointRequestParameter(env).getAsJsonObject();
+		JsonObject resource = createAuthzenApiEndpointRequestParameter(env).getAsJsonObject();
 		JsonObject request = env.getObject("authzen_api_endpoint_request");
-		request.add("resource", subject);
-		logSuccess("Created Search API resource parameter", args(requestParameterName, subject));
+		request.add("resource", resource);
+		logSuccess("Created Search API resource parameter", args(requestParameterName, resource));
 		return env;
 	}
 }
