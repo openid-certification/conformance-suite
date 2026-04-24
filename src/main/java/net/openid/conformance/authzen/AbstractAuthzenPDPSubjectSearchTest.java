ea375ea64171a06fca99450b8621f65d68b29157
Edmund Jay <edmund.jay@oidf.org>
2026-04-24 14:05:51 -0700

    Authzen: show endpoint configuration specific to the test plan

--- src/main/java/net/openid/conformance/authzen/AbstractAuthzenPDPSubjectSearchTest.java
+++ src/main/java/net/openid/conformance/authzen/AbstractAuthzenPDPSubjectSearchTest.java
@@ -4,6 +4,12 @@ import com.google.gson.JsonObject;
 import net.openid.conformance.authzen.condition.CreateAuthzenSubjectSearchApiRequestSteps;
 import net.openid.conformance.authzen.condition.SetAuthzenApiEndpointToSubjectSearchEndpoint;
 import net.openid.conformance.sequence.ConditionSequence;
+import net.openid.conformance.variant.PDPServerMetadata;
+import net.openid.conformance.variant.VariantConfigurationFields;
+
+@VariantConfigurationFields(parameter = PDPServerMetadata.class, value = "static", configurationFields = {
+	"pdp.search_subject_endpoint"
+})
 
 public abstract class AbstractAuthzenPDPSubjectSearchTest extends AbstractAuthzenPDPSearchTest {
 
