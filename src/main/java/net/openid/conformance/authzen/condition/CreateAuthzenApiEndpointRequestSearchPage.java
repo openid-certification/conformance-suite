b3376f490079dde7e05d60b105dc3a6ca5c85d1d
Edmund Jay <edmund.jay@oidf.org>
2026-04-26 15:31:24 -0700

    Authzen: use env page token in search request if not already set

--- src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestSearchPage.java
+++ src/main/java/net/openid/conformance/authzen/condition/CreateAuthzenApiEndpointRequestSearchPage.java
@@ -18,13 +18,12 @@ public class CreateAuthzenApiEndpointRequestSearchPage extends CreateAuthzenApiE
 		JsonObject page = createAuthzenApiEndpointRequestParameter(env).getAsJsonObject();
 		JsonObject request = env.getObject("authzen_api_endpoint_request");
 
-		// Set token in page if exists,
+		// Set page.token from env if it's not set
 		// token in the initial paginated request should not be set since no token exist yet
-		if(!Strings.isNullOrEmpty(env.getString("authzen_search_endpoint_request_page_token"))) {
+		if(!Strings.isNullOrEmpty(env.getString("authzen_search_endpoint_request_page_token")) && !page.has("token")) {
 			page.addProperty("token", env.getString("authzen_search_endpoint_request_page_token"));
-		} else {
-			page.remove("token");
 		}
+
 		request.add("page", page);
 		logSuccess("Created Search API page parameter", args(requestParameterName, page));
 		return env;
