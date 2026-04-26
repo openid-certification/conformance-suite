94c8673061fd474f50ab90b279b8eb198a350045
Edmund Jay <edmund.jay@oidf.org>
2026-04-26 15:32:14 -0700

    Authzen: use Set to compare search results to avoid duplicate values

--- src/main/java/net/openid/conformance/authzen/condition/EnsureAuthzenSearchResponseValsMatchExpectedVals.java
+++ src/main/java/net/openid/conformance/authzen/condition/EnsureAuthzenSearchResponseValsMatchExpectedVals.java
@@ -6,7 +6,8 @@ import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PreEnvironment;
 import net.openid.conformance.testmodule.Environment;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class EnsureAuthzenSearchResponseValsMatchExpectedVals extends AbstractCondition {
 
@@ -16,8 +17,9 @@ public class EnsureAuthzenSearchResponseValsMatchExpectedVals extends AbstractCo
 		JsonArray expected = env.getElementFromObject("authzen_search_endpoint_expected_response", "results").getAsJsonArray();
 		JsonArray results = env.getElementFromObject("authzen_search_endpoint_response", "results").getAsJsonArray();
 
-		List<JsonElement> expectedList = expected.deepCopy().asList();
-		List<JsonElement> resultsList = results.deepCopy().asList();
+		// Expected and actual responses should not contain duplicate values, but use Set operations just in case
+		Set<JsonElement> expectedList = new HashSet<>(expected.asList());
+		Set<JsonElement> resultsList = new HashSet<>(results.asList());
 
 		resultsList.removeAll(expectedList);
 		expectedList.removeAll(results.asList());
@@ -26,12 +28,12 @@ public class EnsureAuthzenSearchResponseValsMatchExpectedVals extends AbstractCo
 
 		if(!resultsList.isEmpty()) { // should not contain any elements after removing all expected elements
 			if(!expectedList.isEmpty()) {
-				throw error("Search results do not match", args("missing expected elements", expectedList, "unexpected elements", resultsList));
+				throw error("Search result does not match", args("missing expected elements", expectedList, "unexpected elements", resultsList));
 			} else {
-				throw error("Search results contains unexpected elements", args("unexpected elements", resultsList));
+				throw error("Search result contains unexpected elements", args("unexpected elements", resultsList));
 			}
 		} else if(!expectedList.isEmpty()){
-			throw error("Search results is missing expected elements", args("missing expected elements", expectedList));
+			throw error("Search result is missing expected elements", args("missing expected elements", expectedList));
 		}
 
 		logSuccess("The search response match expected values");
