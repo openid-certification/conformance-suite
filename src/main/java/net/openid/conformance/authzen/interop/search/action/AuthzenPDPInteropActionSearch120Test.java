9ffc43b0cd5a5ab7e12551e994172a5d21c4bb67
Edmund Jay <edmund.jay@oidf.org>
2026-05-04 13:19:56 -0700

    Authzen: add remaining action search tests

---
+++ src/main/java/net/openid/conformance/authzen/interop/search/action/AuthzenPDPInteropActionSearch120Test.java
@@ -0,0 +1,42 @@
+package net.openid.conformance.authzen.interop.search.action;
+
+import net.openid.conformance.authzen.AbstractAuthzenPDPActionSearchTest;
+import net.openid.conformance.testmodule.PublishTestModule;
+
+@PublishTestModule(
+	testName = "authzen-pdp-interop-action-search-120",
+	displayName = "Authzen Action Search API Test 120",
+	summary = "Authzen Action Search API test 120 with payload\n" + AuthzenPDPInteropActionSearch120Test.payload,
+	profile = "Authzen"
+)
+public class AuthzenPDPInteropActionSearch120Test extends AbstractAuthzenPDPActionSearchTest {
+
+	public static final String payload = """
+		{
+			"subject": {
+				"type": "user",
+				"id": "felix"
+			},
+			"resource": {
+				"type": "record",
+				"id": "120"
+			}
+		}""";
+
+	@Override
+	protected String getExpectedSearchResponseJson() { return """
+		{
+			"results": [
+				{
+					"name": "view"
+				}
+			]
+		}""";
+	}
+
+	@Override
+	protected String getPayload() {
+		return payload;
+	}
+
+}
