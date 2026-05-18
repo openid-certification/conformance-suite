a48cfb0bbb6797815365151163c2b3c086447b28
Edmund Jay <edmund.jay@oidf.org>
2026-05-18 15:55:28 -0700

    Authzen: check endpoints are HTTPS

--- src/main/java/net/openid/conformance/authzen/condition/CheckPDPServerConfiguration.java
+++ src/main/java/net/openid/conformance/authzen/condition/CheckPDPServerConfiguration.java
@@ -1,16 +1,40 @@
 package net.openid.conformance.authzen.condition;
 
 import com.google.common.collect.ImmutableList;
-import net.openid.conformance.condition.common.AbstractCheckServerConfiguration;
+import net.openid.conformance.condition.PreEnvironment;
+import net.openid.conformance.condition.client.CheckDiscEndpointAllEndpointsAreHttps;
+import net.openid.conformance.testmodule.Environment;
 
 import java.util.List;
 
-public class CheckPDPServerConfiguration extends AbstractCheckServerConfiguration {
+public class CheckPDPServerConfiguration extends CheckDiscEndpointAllEndpointsAreHttps {
 
-	@Override
-	protected List<String> getExpectedListEndpoint() {
+	protected List<String> getRequiredEndpoints() {
 		return ImmutableList.of("policy_decision_point",
 			"access_evaluation_endpoint");
 	}
 
+	protected List<String> getOptionalEndpoints() {
+		return ImmutableList.of("access_evaluations_endpoint",
+			"search_subject_endpoint",
+			"search_resource_endpoint",
+			"search_action_endpoint");
+	}
+
+	@Override
+	@PreEnvironment(required = "server")
+	public Environment evaluate(Environment env) {
+
+		for (String key : getRequiredEndpoints()) {
+			env = validate(env, key);
+		}
+
+		var discoveryDoc = env.getObject("server");
+		for (String endpoint : getOptionalEndpoints()) {
+			if (discoveryDoc.has(endpoint)) {
+				env = validate(env, endpoint);
+			}
+		}
+		return env;
+	}
 }
