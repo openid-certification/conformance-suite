51c42857d7af6ad1a4a54678c849fea416a41168
Edmund Jay <edmund.jay@oidf.org>
2026-05-20 01:33:33 -0700

    Authzen: check resource search results contain id and type and that type matches requested type

--- src/main/java/net/openid/conformance/authzen/AbstractAuthzenPDPResourceSearchTest.java
+++ src/main/java/net/openid/conformance/authzen/AbstractAuthzenPDPResourceSearchTest.java
@@ -2,6 +2,9 @@ package net.openid.conformance.authzen;
 
 import com.google.gson.JsonObject;
 import net.openid.conformance.authzen.condition.CreateAuthzenResourceSearchApiRequestSteps;
+import net.openid.conformance.authzen.condition.EnsureValidResourceSearchResponse;
+import net.openid.conformance.authzen.condition.EnsureValidSearchResponsePage;
+import net.openid.conformance.authzen.condition.ExtractAuthzenApiEndpointSearchResponse;
 import net.openid.conformance.authzen.condition.SetAuthzenApiEndpointToResourceSearchEndpoint;
 import net.openid.conformance.sequence.ConditionSequence;
 import net.openid.conformance.variant.PDPServerMetadata;
@@ -29,5 +32,10 @@ public abstract class AbstractAuthzenPDPResourceSearchTest extends AbstractAuthz
 		callAndStopOnFailure(SetAuthzenApiEndpointToResourceSearchEndpoint.class);
 	}
 
-
+	@Override
+	protected void processAuthApiEndpointResponse() {
+		callAndStopOnFailure(ExtractAuthzenApiEndpointSearchResponse.class, "AUTHZEN-8.3");
+		callAndStopOnFailure(EnsureValidResourceSearchResponse.class, "AUTHZEN-8.3", "AUTHZEN-8.5");
+		callAndStopOnFailure(EnsureValidSearchResponsePage.class, "AUTHZEN-8.2.2", "AUTHZEN-8.3");
+	}
 }
