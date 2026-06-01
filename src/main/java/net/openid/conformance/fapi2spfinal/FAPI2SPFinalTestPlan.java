6c642ccac466e207ebaf997522f0ff996fce4206
Domingos Creado <domingos.creado@gmail.com>
2026-06-01 12:43:12 -0300

    feat: redirect KSA profile to message signing plan from SP Final plans

--- src/main/java/net/openid/conformance/fapi2spfinal/FAPI2SPFinalTestPlan.java
+++ src/main/java/net/openid/conformance/fapi2spfinal/FAPI2SPFinalTestPlan.java
@@ -118,6 +118,8 @@ public class FAPI2SPFinalTestPlan implements TestPlan {
 					MethodHandles.lookup().lookupClass().getSimpleName()));
 			case "cbuae":
 				throw new RuntimeException("CBUAE profile requires the usage of JAR, please use the message signing test plan.");
+			case "ksa":
+				throw new RuntimeException("KSA profile requires the usage of JAR, please use the message signing test plan.");
 			default:
 				throw new RuntimeException("Unknown profile %s for %s".formatted(
 					profile, MethodHandles.lookup().lookupClass().getSimpleName()));
