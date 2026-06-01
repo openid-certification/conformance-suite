0f72c58981fbc616ed636251c902656d1c53e09b
Domingos Creado <domingos.creado@gmail.com>
2026-06-01 14:45:22 -0300

    fix: KSA client uses resource URL, not userinfo endpoint

--- src/main/java/net/openid/conformance/fapi2spfinal/KsaClientProfileBehavior.java
+++ src/main/java/net/openid/conformance/fapi2spfinal/KsaClientProfileBehavior.java
@@ -2,17 +2,12 @@ package net.openid.conformance.fapi2spfinal;
 
 /**
  * Profile behavior for KSA client tests.
- * Requires mTLS everywhere and treats userinfo as the resource endpoint.
+ * Requires mTLS everywhere.
  */
 public class KsaClientProfileBehavior extends FAPI2ClientProfileBehavior {
 
 	@Override
 	public boolean requiresMtlsEverywhere() {
-		return true;
-	}
-
-	@Override
-	public boolean userInfoIsResourceEndpoint() {
 		return true;
 	}
 }
