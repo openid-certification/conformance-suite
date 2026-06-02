1fdd6627c3cf227dec74993369ef040af1c4a7bf
Marcus Almgren <marcus@almgren.pt>
2026-06-02 17:39:37 +0100

    Explicitly add card_primary_account_number to the configuration which is used as login_hint in the ConnectID 3ds use case(s).

--- src/main/java/net/openid/conformance/condition/client/SetConnectIdCibaLoginHintToCardPrimaryAccountNumber.java
+++ src/main/java/net/openid/conformance/condition/client/SetConnectIdCibaLoginHintToCardPrimaryAccountNumber.java
@@ -1,5 +1,6 @@
 package net.openid.conformance.condition.client;
 
+import com.google.common.base.Strings;
 import net.openid.conformance.condition.AbstractCondition;
 import net.openid.conformance.condition.PostEnvironment;
 import net.openid.conformance.condition.PreEnvironment;
@@ -7,17 +8,20 @@ import net.openid.conformance.testmodule.Environment;
 
 public class SetConnectIdCibaLoginHintToCardPrimaryAccountNumber extends AbstractCondition {
 
-	public static final String CARD_PRIMARY_ACCOUNT_NUMBER = "6372069742108725";
-
 	@Override
 	@PreEnvironment(required = "config")
 	@PostEnvironment(required = "config")
 	public Environment evaluate(Environment env) {
+		String cardPrimaryAccountNumber = env.getString("config", "client.card_primary_account_number");
+		if (Strings.isNullOrEmpty(cardPrimaryAccountNumber)) {
+			throw error("'Card primary account number' field is missing from the 'Client' section in the test configuration");
+		}
+
 		env.putString("config", "client.hint_type", "login_hint");
-		env.putString("config", "client.hint_value", CARD_PRIMARY_ACCOUNT_NUMBER);
+		env.putString("config", "client.hint_value", cardPrimaryAccountNumber);
 
 		logSuccess("Set ConnectID CIBA login_hint to a card primary account number",
-			args("login_hint", CARD_PRIMARY_ACCOUNT_NUMBER));
+			args("login_hint", cardPrimaryAccountNumber));
 
 		return env;
 	}
