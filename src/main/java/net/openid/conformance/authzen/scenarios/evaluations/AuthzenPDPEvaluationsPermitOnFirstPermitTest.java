fd332963beb949cba670ecbb52dbacc13b0391c8
Edmund Jay <edmund.jay@oidf.org>
2026-06-04 12:24:59 -0700

    Authzen: fix expected responses for deny_on_first_deny and permit_on_first_permit tests

--- src/main/java/net/openid/conformance/authzen/scenarios/evaluations/AuthzenPDPEvaluationsPermitOnFirstPermitTest.java
+++ src/main/java/net/openid/conformance/authzen/scenarios/evaluations/AuthzenPDPEvaluationsPermitOnFirstPermitTest.java
@@ -35,7 +35,6 @@ public class AuthzenPDPEvaluationsPermitOnFirstPermitTest extends AbstractAuthze
 			{
 				"evaluations": [
 					{ "decision": false },
-					{ "decision": true },
 					{ "decision": true }
 				]
 			}
