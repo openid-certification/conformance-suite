8b4225477713495ea2fb407ccc1241acefe3e2ef
Edmund Jay <edmund.jay@oidf.org>
2026-06-04 17:36:07 -0700

    Authzen: remove todo in EnsureValidEvaluationsResponse and add comment regarding normalizing of top-level decision to evaluations array

--- src/main/java/net/openid/conformance/authzen/condition/EnsureValidEvaluationsResponse.java
+++ src/main/java/net/openid/conformance/authzen/condition/EnsureValidEvaluationsResponse.java
@@ -15,7 +15,8 @@ public class EnsureValidEvaluationsResponse extends AbstractCondition {
 		JsonObject evaluationsResponse = env.getObject("authzen_evaluations_endpoint_response");
 		JsonElement evaluationsElem = evaluationsResponse.get("evaluations");
 		if (null == evaluationsElem) {
-			// TODO Check decision element if evaluations is not available
+			// top-level decision element is normalized to evaluations array if it exists by
+			// NormalizeAuthzenEvaluationsResponseSingleDecisionToArray
 			throw error("No evaluations element in API response", args("authzen_evaluations_endpoint_response", evaluationsResponse));
 		}
 		if(!evaluationsElem.isJsonArray()) {
