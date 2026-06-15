0dba437798cacfd8a56dc3c049a63005ef7bfd1c
Marcus Almgren <marcus@almgren.pt>
2026-06-15 15:44:34 +0100

    Remove redundant configuration field

--- src/main/java/net/openid/conformance/fapiciba/FAPICIBAID1ConnectIdEnsureAuthorizationRequestWith3DSPaymentAuthorizationDetailsSucceeds.java
+++ src/main/java/net/openid/conformance/fapiciba/FAPICIBAID1ConnectIdEnsureAuthorizationRequestWith3DSPaymentAuthorizationDetailsSucceeds.java
@@ -3,7 +3,6 @@ package net.openid.conformance.fapiciba;
 import net.openid.conformance.condition.client.AddConnectIdCiba3DSPaymentAuthorizationDetailsToRequestObject;
 import net.openid.conformance.condition.client.SetConnectIdCibaLoginHintToCardPrimaryAccountNumber;
 import net.openid.conformance.testmodule.PublishTestModule;
-import net.openid.conformance.variant.ConfigurationFields;
 import net.openid.conformance.variant.FAPICIBAProfile;
 import net.openid.conformance.variant.VariantNotApplicable;
 
@@ -13,9 +12,6 @@ import net.openid.conformance.variant.VariantNotApplicable;
 	summary = "This test sends a ConnectID CIBA backchannel authentication request for the 3DS payment authentication use case. The signed request object includes binding_message, a card primary account number login_hint, and valid authorization_details of type 3ds:payment_authorisation. The server must authenticate successfully.",
 	profile = "FAPI-CIBA-ID1"
 )
-@ConfigurationFields({
-	"client.card_primary_account_number"
-})
 @VariantNotApplicable(parameter = FAPICIBAProfile.class, values = {"plain_fapi", "openbanking_uk", "openbanking_brazil"})
 public class FAPICIBAID1ConnectIdEnsureAuthorizationRequestWith3DSPaymentAuthorizationDetailsSucceeds extends AbstractFAPICIBAID1 {
 
