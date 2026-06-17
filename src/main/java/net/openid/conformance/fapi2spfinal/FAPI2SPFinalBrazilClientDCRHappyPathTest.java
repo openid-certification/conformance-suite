7b4dc56abad42fb4d36933f77d3840ba717fb57f
Alan Smillie <alans@emobix.co.uk>
2026-06-17 14:33:44 +0100

    Add TLS 1.3 SHOULD warning to FAPI2 Final incoming TLS checks (RFC9325 §3.1.1)
    Closes #1680

--- src/main/java/net/openid/conformance/fapi2spfinal/FAPI2SPFinalBrazilClientDCRHappyPathTest.java
+++ src/main/java/net/openid/conformance/fapi2spfinal/FAPI2SPFinalBrazilClientDCRHappyPathTest.java
@@ -41,6 +41,7 @@ import net.openid.conformance.condition.as.dynregistration.ValidateRequireAuthTi
 import net.openid.conformance.condition.as.dynregistration.ValidateUserinfoSignedResponseAlg;
 import net.openid.conformance.condition.common.CreateRandomRegistrationClientUri;
 import net.openid.conformance.condition.common.EnsureIncomingTls12WithBCP195SecureCipherOrTls13;
+import net.openid.conformance.condition.common.EnsureIncomingTls13;
 import net.openid.conformance.condition.rs.ExtractBearerAccessTokenFromHeader;
 import net.openid.conformance.condition.rs.RequireBearerRegistrationAccessToken;
 import net.openid.conformance.testmodule.OIDFJSON;
@@ -110,6 +111,7 @@ public class FAPI2SPFinalBrazilClientDCRHappyPathTest extends AbstractFAPI2SPFin
 			call(exec().mapKey("client_request", requestId));
 
 			callAndContinueOnFailure(EnsureIncomingTls12WithBCP195SecureCipherOrTls13.class, Condition.ConditionResult.WARNING, "FAPI2-SP-FINAL-5.2.1-1,FAPI2-SP-FINAL-5.2.1-3", "FAPI2-SP-FINAL-5.2.1-3");
+			callAndContinueOnFailure(EnsureIncomingTls13.class, Condition.ConditionResult.WARNING, "RFC9325-3.1.1");
 
 			call(exec().unmapKey("client_request"));
 
@@ -158,6 +160,7 @@ public class FAPI2SPFinalBrazilClientDCRHappyPathTest extends AbstractFAPI2SPFin
 		call(exec().mapKey("client_request", requestId));
 
 		callAndContinueOnFailure(EnsureIncomingTls12WithBCP195SecureCipherOrTls13.class, Condition.ConditionResult.WARNING, "FAPI2-SP-FINAL-5.2.1-1,FAPI2-SP-FINAL-5.2.1-3", "FAPI2-SP-FINAL-5.2.1-3");
+		callAndContinueOnFailure(EnsureIncomingTls13.class, Condition.ConditionResult.WARNING, "RFC9325-3.1.1");
 		callAndContinueOnFailure(FAPIBrazilValidateRegistrationClientUriQueryParams.class, Condition.ConditionResult.WARNING, "OIDCR-3.2", "OIDCR-4.1");
 
 
