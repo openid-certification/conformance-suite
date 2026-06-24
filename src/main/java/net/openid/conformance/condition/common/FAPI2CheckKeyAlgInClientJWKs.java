e6c91e6677367d7a5cead027de1e6eaeb0880b66
Alan Smillie <alans@emobix.co.uk>
2026-06-24 09:50:35 +0100

    FAPI2: accept Ed25519 alongside EdDSA in client JWKs key-alg checks
    Client-published OKP signing keys may now declare alg=Ed25519 as well
    as alg=EdDSA; both require an Ed25519 curve. Use the shared
    FAPI2_ALLOWED_ALGS constant for the permitted-algorithm set.

--- src/main/java/net/openid/conformance/condition/common/FAPI2CheckKeyAlgInClientJWKs.java
+++ src/main/java/net/openid/conformance/condition/common/FAPI2CheckKeyAlgInClientJWKs.java
@@ -1,12 +1,15 @@
 package net.openid.conformance.condition.common;
 
+import net.openid.conformance.condition.client.FAPI2CheckDiscEndpointIdTokenSigningAlgValuesSupported;
+
+import java.util.Arrays;
 import java.util.Set;
 
 public class FAPI2CheckKeyAlgInClientJWKs extends AbstractFAPI2CheckKeyAlgInClientJWKs {
 
 	@Override
 	protected Set<String> getPermitted() {
-		return Set.of( "PS256", "ES256", "EdDSA" );
+		return Set.copyOf(Arrays.asList(FAPI2CheckDiscEndpointIdTokenSigningAlgValuesSupported.FAPI2_ALLOWED_ALGS));
 	}
 
 }
