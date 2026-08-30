de8e89be354fbfa2ccf3b34d74c61cb5809a0e32
Marcus Almgren <marcus@almgren.pt>
2026-08-30 14:25:30 +0100

    fix(ciba): allow generic ping fallback polling

--- src/main/java/net/openid/conformance/fapiciba/rp/AbstractFAPICIBAClientPingWithInvalidNotificationTest.java
+++ src/main/java/net/openid/conformance/fapiciba/rp/AbstractFAPICIBAClientPingWithInvalidNotificationTest.java
@@ -1,14 +1,22 @@
 package net.openid.conformance.fapiciba.rp;
 
 import net.openid.conformance.condition.Condition;
+import net.openid.conformance.variant.FAPICIBAProfile;
 
 public abstract class AbstractFAPICIBAClientPingWithInvalidNotificationTest extends AbstractFAPICIBAClientTest {
 
 	@Override
 	protected void sendPingRequestAndVerifyResponse() {
-		rejectFurtherClientInteractions();
+		if (shouldRejectFurtherClientInteractionsWhileWaitingForTimeout()) {
+			rejectFurtherClientInteractions();
+		}
 		callAndStopOnFailure(getPingNotificationCondition(), Condition.ConditionResult.FAILURE, "CIBA-10.2");
 		verifyPingResponse();
+	}
+
+	@Override
+	protected boolean shouldRejectFurtherClientInteractionsWhileWaitingForTimeout() {
+		return FAPICIBAProfile.OPENBANKING_BRAZIL.equals(profile);
 	}
 
 	protected abstract Class<? extends Condition> getPingNotificationCondition();
